685887a4b9ee6d01682473b28b66526932cb5bb2
Steve Brush <steve.brush@blackbaud.com>
2017-04-11 12:35:10 -0400

    Added ability to reverse column order (#52)

--- libs/stache/stache/src/modules/grid/row.component.spec.ts
+++ libs/stache/stache/src/modules/grid/row.component.spec.ts
@@ -5,6 +5,7 @@ import { expect } from '@blackbaud/skyux-builder/runtime/testing/browser';
 import { StacheRowComponent } from './row.component';
 
 describe('StacheRowComponent', () => {
+  let component: StacheRowComponent;
   let fixture: ComponentFixture<StacheRowComponent>;
   let element: HTMLElement;
 
@@ -17,11 +18,18 @@ describe('StacheRowComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(StacheRowComponent);
+    component = fixture.componentInstance;
     element = fixture.nativeElement;
   });
 
-  it('should create an element with a class', () => {
+  it('should create an element with a specific classname', () => {
     fixture.detectChanges();
     expect(element.querySelector('.stache-row')).toExist();
+  });
+
+  it('should add a classname to reverse the column order', () => {
+    component.reverseColumnOrder = true;
+    fixture.detectChanges();
+    expect(element.querySelector('.stache-row-reverse')).toExist();
   });
 });
--- libs/stache/stache/src/modules/grid/row.component.ts
+++ libs/stache/stache/src/modules/grid/row.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { InputConverter } from '../shared';
 
 @Component({
   selector: 'stache-row',
   templateUrl: './row.component.html',
   styleUrls: ['./row.component.scss']
 })
-export class StacheRowComponent { }
+export class StacheRowComponent {
+  @Input()
+  @InputConverter()
+  public reverseColumnOrder: boolean = false;
+}
