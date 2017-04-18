adcbd37eabe9157a28858d2d48cc281f91e87f71
Brandon Hare <blackbaud-brandonhare@users.noreply.github.com>
2017-04-18 07:14:28 -0400

    Added tests for page summary. (#72)
    * Added tests for page summary.
    
    * Fixed app extras.
    
    * Removed unused SkyAppConfig
    
    * Used toHaveText, instead of innerText

---
+++ libs/stache/stache/src/modules/page-summary/fixtures/page-summary.component.fixture.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stache-test-component',
+  templateUrl: './page-summary.component.fixture.html'
+})
+export class StachePageSummaryTestComponent { }
