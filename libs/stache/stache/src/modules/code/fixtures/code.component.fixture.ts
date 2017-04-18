e4c412568641374bbffe34d827919ee78f21398a
Steve Brush <steve.brush@blackbaud.com>
2017-04-18 15:41:14 -0400

    Added tests for code component (#90)

---
+++ libs/stache/stache/src/modules/code/fixtures/code.component.fixture.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stache-test-component',
+  templateUrl: './code.component.fixture.html'
+})
+export class StacheCodeTestComponent { }
