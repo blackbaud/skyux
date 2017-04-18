cf92f9f58175e0cea2eb9ca3bdf817d0380810e7
Brandon Hare <blackbaud-brandonhare@users.noreply.github.com>
2017-04-18 07:07:33 -0400

    Added tests for page header. (#73)
    * Added tests for page header.
    
    * Removed unused config

---
+++ libs/stache/stache/src/modules/page-header/fixtures/page-header.component.fixture.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stache-test-component',
+  templateUrl: './page-header.component.fixture.html'
+})
+export class StachePageHeaderTestComponent {}
---
+++ libs/stache/stache/src/modules/page-header/page-header.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { expect } from '@blackbaud/skyux-builder/runtime/testing/browser';
+
+import { StachePageHeaderComponent } from './page-header.component';
+import { StachePageHeaderTestComponent } from './fixtures/page-header.component.fixture';
+
+describe('Stache Page Summary Component', () => {
+  let component: StachePageHeaderComponent;
+  let fixture: ComponentFixture<StachePageHeaderComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        StachePageHeaderComponent,
+        StachePageHeaderTestComponent
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(StachePageHeaderComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should render the component', () => {
+    expect(fixture).toExist();
+  });
+
+  it('should transclude the content', () => {
+    const text = 'Header';
+    let testFixture = TestBed.createComponent(StachePageHeaderTestComponent);
+    let testElement = testFixture.nativeElement;
+    testFixture.detectChanges();
+    expect(testElement.querySelector('.stache-page-header')).toHaveText(text);
+  });
+});
