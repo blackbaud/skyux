ff3238ecb1f27e6459dd587746de52417703e2d0
Brandon Jones <brandon.jones@blackbaud.com>
2017-05-04 09:42:37 -0400

    app nav re-introduced (#136)

---
+++ libs/stache/stache/src/app/shared/app-nav.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './app-nav.component.html',
+  styleUrls: ['./app-nav.component.scss']
+})
+export class AppNavComponent {
+  public nav = [
+    {
+      name: 'Home',
+      path: '/'
+    },
+    {
+      name: 'Learn',
+      path: '/learn'
+    },
+    {
+      name: 'Components',
+      path: '/components'
+    },
+    {
+      name: 'Contribute',
+      path: '/contribute'
+    }
+  ];
+}
