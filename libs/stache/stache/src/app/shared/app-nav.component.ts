04d0c80ab0367fbe3a55c4872da2ff00030a033c
Steve Brush <steve.brush@blackbaud.com>
2017-05-04 13:23:46 -0400

    Removed test file (#138)

--- libs/stache/stache/src/app/shared/app-nav.component.ts
+++ libs/stache/stache/src/app/shared/app-nav.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  templateUrl: './app-nav.component.html',
-  styleUrls: ['./app-nav.component.scss']
+  templateUrl: './app-nav.component.html'
 })
 export class AppNavComponent {
   public nav = [
