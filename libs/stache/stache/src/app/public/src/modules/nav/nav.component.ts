2120b6a616075a699ce5a7860bce388dda742545
Steve Brush <steve.brush@blackbaud.com>
2017-07-06 12:36:55 -0400

    Fixed bug with nav component (#252)
    * Fixed bug with nav component
    
    * Updated version of builder

--- libs/stache/stache/src/app/public/src/modules/nav/nav.component.ts
+++ libs/stache/stache/src/app/public/src/modules/nav/nav.component.ts
@@ -92,7 +92,7 @@ export class StacheNavComponent implements OnInit, StacheNav {
       path = `/${path}`;
     }
 
-    const isActiveParent = (navDepth > 2  && `${activeUrl}/`.indexOf(`${path}/`) === 0);
+    const isActiveParent = (navDepth > 1  && `${activeUrl}/`.indexOf(`${path}/`) === 0);
 
     return (isActiveParent || activeUrl === path);
   }
