93cd101c12da7a8f2b93a28385152e59beab892a
Steve Brush <steve.brush@blackbaud.com>
2017-07-14 14:24:35 -0400

    Fixed navbar styles (#267)

--- libs/stache/stache/src/app/shared/app-nav/app-nav.component.ts
+++ libs/stache/stache/src/app/shared/app-nav/app-nav.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  templateUrl: './app-nav.component.html'
+  templateUrl: './app-nav.component.html',
+  styleUrls: ['./app-nav.component.scss']
 })
 export class AppNavComponent {
   public nav = [
