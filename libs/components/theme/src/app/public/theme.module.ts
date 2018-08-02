11194755f2f6a4b16e4c0294f91a960e8af60eef
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-02 16:34:02 -0400

    Added comment

--- libs/components/theme/src/app/public/theme.module.ts
+++ libs/components/theme/src/app/public/theme.module.ts
@@ -6,7 +6,9 @@ import {
   SkyAppStyleLoader
 } from './modules/style-loader';
 
-// Switch these lines after initial release:
+// Automatically insert the global stylesheet for consumers of this module:
+
+// (Switch these lines after initial release.)
 // require('style-loader!@skyux/theme/css/sky.css');
 require('style-loader!@skyux/theme/src/app/public/styles/sky.scss');
 
