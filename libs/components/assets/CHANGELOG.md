caec198941b7f5d917e66c60911940a4e47bcc1f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-17 23:01:13 -0400

    Release 3.0.0-alpha.2

--- libs/components/assets/CHANGELOG.md
+++ libs/components/assets/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 3.0.0-alpha.2 (2018-09-17)
+
+- Removed `Injectable` decorator and reverted changes to directory structure.
+
 # 3.0.0-alpha.1 (2018-09-17)
 
 - Added `Injectable` decorator and moved assets service to modules directory.
--- libs/components/assets/src/app/public/assets.service.ts
+++ libs/components/assets/src/app/public/assets.service.ts
@@ -1,13 +1,8 @@
-import {
-  Injectable
-} from '@angular/core';
-
+/* istanbul ignore next */
 /**
  * An abstract class for injecting the ability to retrieve an asset file's URL at runtime.
  * The implementing class will be generated during build time.
  */
-/* istanbul ignore next */
-@Injectable()
 export abstract class SkyAppAssetsService {
   public abstract getUrl(path: string): string;
 }
