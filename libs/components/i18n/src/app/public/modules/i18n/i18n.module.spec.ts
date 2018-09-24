8bc9a00486d372f861e8303a0e510597894147f4
Steve Brush <steve.brush@blackbaud.com>
2018-09-24 14:02:41 -0400

    Fixed linting errors (#11)

---
+++ libs/components/i18n/src/app/public/modules/i18n/i18n.module.spec.ts
@@ -0,0 +1,14 @@
+import {
+  expect
+} from '@skyux-sdk/testing';
+
+import {
+  SkyI18nModule
+} from './i18n.module';
+
+describe('SkyI18nModule', () => {
+  it('should export', () => {
+    const exportedModule = new SkyI18nModule();
+    expect(exportedModule).toBeDefined();
+  });
+});
--- libs/components/i18n/src/app/public/modules/i18n/i18n.module.ts
+++ libs/components/i18n/src/app/public/modules/i18n/i18n.module.ts
@@ -7,6 +7,14 @@ import {
 } from '@angular/http';
 
 import {
+  SkyAppAssetsService
+} from '@skyux/assets';
+
+import {
+  SkyAppWindowRef
+} from '@skyux/core';
+
+import {
   SkyAppHostLocaleProvider
 } from './host-locale-provider';
 
@@ -25,10 +33,19 @@ import {
   exports: [
     SkyAppResourcesPipe
   ],
+  imports: [
+    HttpModule
+  ],
   providers: [
-    HttpModule,
+    // This service is ultimately provided by Builder,
+    // but we need to add it to a module to avoid TSLint failures.
+    {
+      provide: SkyAppAssetsService,
+      useValue: SkyAppAssetsService
+    },
     SkyAppHostLocaleProvider,
-    SkyAppResourcesService
+    SkyAppResourcesService,
+    SkyAppWindowRef
   ]
 })
 export class SkyI18nModule { }
