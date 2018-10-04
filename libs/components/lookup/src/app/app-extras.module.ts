dbca13bd8cf295cd68c5638eda9b2dbc8f822ea6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-10-04 13:53:26 -0400

    Initial commit

---
+++ libs/components/lookup/src/app/app-extras.module.ts
@@ -0,0 +1,19 @@
+import {
+  NgModule
+} from '@angular/core';
+
+import {
+  SkySampleModule
+} from './public';
+
+@NgModule({
+  imports: [
+    SkySampleModule
+  ],
+  exports: [
+    SkySampleModule
+  ],
+  providers: [],
+  entryComponents: []
+})
+export class AppExtrasModule { }
