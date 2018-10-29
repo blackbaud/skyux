6b57f279b3439e0d667a6a82574a2285350b658e
Steve Brush <steve.brush@blackbaud.com>
2018-10-29 11:39:51 -0400

    Adopt Node LTS (#36)

--- libs/components/core/src/app/app-extras.module.ts
+++ libs/components/core/src/app/app-extras.module.ts
@@ -7,16 +7,12 @@ import {
   SkyNumericModule
 } from './public';
 
-import { SkyAppRuntimeModule } from '@blackbaud/skyux-builder/runtime';
-
 @NgModule({
   imports: [
-    SkyAppRuntimeModule,
     SkyMediaQueryModule,
     SkyNumericModule
   ],
   exports: [
-    SkyAppRuntimeModule,
     SkyMediaQueryModule,
     SkyNumericModule
   ],
