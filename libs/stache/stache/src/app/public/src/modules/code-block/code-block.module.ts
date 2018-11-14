ae5c508b02100103369deae0b9d35b21e0fd1e7f
Tim Pepper <38473653+Blackbaud-TimPepper@users.noreply.github.com>
2018-11-14 09:55:12 -0500

    Accessibility code block (#551)
    * update code block styles to be more accessible
    
    * improve contract for code block highlighting
    
    * update text highlighting color scheme to be more readable
    
    * fix style issue
    
    * fix prism style issue

--- libs/stache/stache/src/app/public/src/modules/code-block/code-block.module.ts
+++ libs/stache/stache/src/app/public/src/modules/code-block/code-block.module.ts
@@ -5,8 +5,6 @@ import { StacheCodeBlockComponent } from './code-block.component';
 import { StacheClipboardModule } from '../clipboard';
 import { SkyAppRuntimeModule } from '@blackbaud/skyux-builder/runtime';
 
-require('style-loader!prismjs/themes/prism.css');
-
 @NgModule({
   declarations: [
     StacheCodeBlockComponent
