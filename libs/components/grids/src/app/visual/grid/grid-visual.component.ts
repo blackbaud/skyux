3150ea21a86fe33728a94a9ec9c264d6cdeb2ed0
Steve Brush <steve.brush@blackbaud.com>
2018-11-19 15:28:05 -0500

    Added actual sort method to demo (#18)

--- libs/components/grids/src/app/visual/grid/grid-visual.component.ts
+++ libs/components/grids/src/app/visual/grid/grid-visual.component.ts
@@ -3,7 +3,8 @@ import {
 } from '@angular/core';
 
 import {
-  ListItemModel
+  ListItemModel,
+  ListSortFieldSelectorModel
 } from '@skyux/list-builder/modules/list/state';
 
 @Component({
@@ -23,6 +24,36 @@ export class GridVisualComponent {
     new ListItemModel('6', { column1: '20', column2: 'Fig', column3: 'ff' }),
     new ListItemModel('7', { column1: '21', column2: 'Grape', column3: 'gg' })
   ];
+
+  public sortChanged(activeSort: ListSortFieldSelectorModel) {
+    const sortField = activeSort.fieldSelector;
+    const descending = activeSort.descending;
+
+    this.items = this.items.sort((a: any, b: any) => {
+      let value1 = a.data[sortField];
+      let value2 = b.data[sortField];
+
+      if (value1 && typeof value1 === 'string') {
+        value1 = value1.toLowerCase();
+      }
+
+      if (value2 && typeof value2 === 'string') {
+        value2 = value2.toLowerCase();
+      }
+
+      if (value1 === value2) {
+        return 0;
+      }
+
+      let result = value1 > value2 ? 1 : -1;
+
+      if (descending) {
+        result *= -1;
+      }
+
+      return result;
+    }).slice();
+  }
 
   public triggerHighlight() {
     this.highlightText = 'e';
