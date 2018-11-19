74dcf1375ee36b5827f1f1336ea6dc50060fa021
Alex <alex.kingman@blackbaud.com>
2018-11-19 11:32:48 -0500

    Misc cleanup & ListItemModel update for multiselect grids (#5)
    * renamed spec file, added helper methods
    
    * added checked to listItemModel, added more codecoverage

--- libs/components/list-builder-common/src/app/public/helpers.spec.ts
+++ libs/components/list-builder-common/src/app/public/helpers.spec.ts
@@ -1,80 +1,135 @@
 import {
-  getData
+  EventEmitter
+} from '@angular/core';
+
+import {
+  Observable
+} from 'rxjs/Observable';
+
+import {
+  Subject
+} from 'rxjs/Subject';
+
+import {
+  compare,
+  getData,
+  isObservable
 } from './helpers';
 
 describe('list helpers', () => {
-  it('gets data based on a standard selector', () => {
-    let data = {
-      myResult: 'something',
-      otherResult: 'nothing'
-    };
-    let result = getData(data, 'myResult');
-    expect(result).toBe('something');
+
+  describe('getData', () => {
+    it('gets data based on a standard selector', () => {
+      let data = {
+        myResult: 'something',
+        otherResult: 'nothing'
+      };
+      let result = getData(data, 'myResult');
+      expect(result).toBe('something');
+    });
+
+    it('shifts data appropriately when a selector is at the front', () => {
+      let data = {
+        myResult: 'something',
+        otherResult: 'nothing'
+      };
+      let result = getData(data, '.myResult');
+      expect(result).toBe('something');
+    });
+
+    it('returns properly when null data', () => {
+      /* tslint:disable */
+      let data: any = {
+        myResult: null,
+        otherResult: 'nothing'
+      };
+      /* tslint:enable */
+
+      let result = getData(data, 'myResult');
+      expect(result).toBeNull();
+    });
+
+    it('returns property when selector is a nested selector', () => {
+      let data: any = {
+        myResults: { nestedValue: 'expected'},
+        otherResult: 'nothing'
+      };
+      let result = getData(data, 'myResults.nestedValue');
+      expect(result).toBe('expected');
+    });
+
+    it('returns property when selector is a nested selector that does not exits', () => {
+      let data: any = {
+        myResults: {},
+        otherResult: 'nothing'
+      };
+      let result = getData(data, 'myResults.nestedValue');
+      expect(result).toBeNull();
+    });
+
+    it('returns property when selector is a nested selector that is undefined', () => {
+      let data: any = {
+        myResults: { nestedValue: undefined },
+        otherResult: 'nothing'
+      };
+      let result = getData(data, 'myResults.nestedValue');
+      expect(result).toBeNull();
+    });
+
+    it('returns undefined when empty string selector defined', () => {
+      let data = {
+        myResult: 'something',
+        otherResult: 'nothing'
+      };
+      let result = getData(data, '');
+      expect(result).toBe(undefined);
+    });
+
+    it('returns undefined when no selector is supplied', () => {
+      let data: any = {
+        myResult: 'something',
+        otherResult: 'somethingtoo'
+      };
+      let result = getData(data, undefined);
+      expect(result).toBeUndefined();
+    });
   });
 
-  it('shifts data appropriately when a selector is at the front', () => {
-    let data = {
-      myResult: 'something',
-      otherResult: 'nothing'
-    };
-    let result = getData(data, '.myResult');
-    expect(result).toBe('something');
+  describe('compare', () => {
+    it('should compare null values', () => {
+      /* tslint:disable:no-null-keyword */
+      expect(compare(null, 'foo')).toEqual(1);
+      expect(compare('foo', null)).toEqual(-1);
+      /* tslint:enable:no-null-keyword */
+    });
+
+    it('should compare strings', () => {
+      expect(compare('foo', 'bar')).toEqual(1);
+      expect(compare('foo bar baz', 'foo bar baz')).toEqual(0);
+    });
+
+    it('should handle different cases when comparing', () => {
+      expect(compare('FooBar', 'foobar')).toEqual(0);
+    });
+
+    it('should compare numbers', () => {
+      expect(compare(1999, 2000)).toEqual(-1);
+      expect(compare(2000, 2000)).toEqual(0);
+      expect(compare(2000, 1999)).toEqual(1);
+    });
   });
 
-  it('returns properly when null data', () => {
-    /* tslint:disable */
-    let data: any = {
-      myResult: null,
-      otherResult: 'nothing'
-    };
-    /* tslint:enable */
+  describe('isObservable', () => {
+    it('should check if an object is an observable', () => {
+      const eventEmitter = new EventEmitter<void>();
+      const observable = new Observable<void>();
+      const subject = new Subject<void>();
 
-    let result = getData(data, 'myResult');
-    expect(result).toBeNull();
-  });
-
-  it('returns property when selector is a nested selector', () => {
-    let data: any = {
-      myResults: { nestedValue: 'expected'},
-      otherResult: 'nothing'
-    };
-    let result = getData(data, 'myResults.nestedValue');
-    expect(result).toBe('expected');
-  });
-
-  it('returns property when selector is a nested selector that does not exits', () => {
-    let data: any = {
-      myResults: {},
-      otherResult: 'nothing'
-    };
-    let result = getData(data, 'myResults.nestedValue');
-    expect(result).toBeNull();
-  });
-
-  it('returns property when selector is a nested selector that is undefined', () => {
-    let data: any = {
-      myResults: { nestedValue: undefined },
-      otherResult: 'nothing'
-    };
-    let result = getData(data, 'myResults.nestedValue');
-    expect(result).toBeNull();
-  });
-
-  it('returns undefined when empty string selector defined', () => {
-    let data = {
-      myResult: 'something',
-      otherResult: 'nothing'
-    };
-    let result = getData(data, '');
-    expect(result).toBe(undefined);
-  });
-
-  it('returns undefined when no selector is supplied', () => {
-    let data: any = {
-      myResult: 'something',
-      otherResult: 'somethingtoo'
-    };
-     let result = getData(data, undefined);
-    expect(result).toBeUndefined();
+      expect(isObservable(eventEmitter)).toEqual(true);
+      expect(isObservable(observable)).toEqual(true);
+      expect(isObservable(subject)).toEqual(true);
+      expect(isObservable({})).toEqual(false);
+      expect(isObservable('foobar')).toEqual(false);
+    });
   });
 });
--- libs/components/list-builder-common/src/app/public/helpers.ts
+++ libs/components/list-builder-common/src/app/public/helpers.ts
@@ -1,3 +1,7 @@
+import {
+  Observable
+} from 'rxjs/Observable';
+
 export function getData(item: any, selector: string): any {
   if (!selector) {
     return undefined;
@@ -25,4 +29,35 @@ export function getData(item: any, selector: string): any {
   }
 
   return result;
+}
+
+export function compare(value1: any, value2: any) {
+  /* tslint:disable:no-null-keyword */
+  if (value1 === null) {
+    return 1;
+  } else if (value2 === null) {
+    return -1;
+  }
+  /* tslint:enable:no-null-keyword */
+
+  if (value1 && typeof value1 === 'string') {
+    value1 = value1.toLowerCase();
+  }
+
+  if (value2 && typeof value2 === 'string') {
+    value2 = value2.toLowerCase();
+  }
+  if (value1 === value2) {
+    return 0;
+  }
+
+  return value1 > value2 ? 1 : -1;
+}
+
+/*
+  Taken directly from rxjs's internal utility to determine whether an object is an Obserable.
+  See: https://github.com/ReactiveX/rxjs/blob/master/src/internal/util/isObservable.ts
+*/
+export function isObservable<T>(obj: any): obj is Observable<T> {
+  return !!obj && (obj instanceof Observable || (typeof obj.lift === 'function' && typeof obj.subscribe === 'function'));
 }
