32612b91ea94cd73b785996fb5a89b3ac4a47575
Steve Brush <steve.brush@blackbaud.com>
2018-11-27 11:37:52 -0500

    Added buttons visual tests. (#37)

---
+++ libs/components/theme/src/app/visual/buttons/buttons-demo.component.ts
@@ -0,0 +1,10 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-buttons-demo',
+  templateUrl: './buttons-demo.component.html',
+  styleUrls: ['./buttons-demo.component.scss']
+})
+export class SkyButtonsDemoComponent { }
