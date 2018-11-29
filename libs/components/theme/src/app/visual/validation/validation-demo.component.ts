3cc72198a0d250a0606fe151b0cf2140184d5d07
Trevor Burch <Trevor.Burch@blackbaud.com>
2018-11-29 14:16:13 -0500

    Changed select field error state to on touched for consistency (#38)
    * Changed select field error state to on touched for consistency

---
+++ libs/components/theme/src/app/visual/validation/validation-demo.component.ts
@@ -0,0 +1,15 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-validation-demo',
+  templateUrl: './validation-demo.component.html',
+  styleUrls: ['./validation-demo.component.scss']
+})
+export class SkyValidationDemoComponent {
+
+  public textInput: string;
+  public selectInput: string;
+
+}
