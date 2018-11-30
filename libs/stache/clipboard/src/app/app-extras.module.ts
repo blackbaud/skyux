8a9a279b9799b0b072f1a4b2c4198ecaebbd5a99
Brandon Jones <brandon.jones@blackbaud.com>
2018-11-30 13:47:42 -0500

    Revert "initial commit"

--- libs/stache/clipboard/src/app/app-extras.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-
-// Specify entry components, module-level providers, etc. here.
-@NgModule({
-  providers: [],
-  entryComponents: []
-})
-export class AppExtrasModule { }
--- libs/stache/clipboard/src/app/home.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-home',
-  templateUrl: './home.component.html'
-})
-export class HomeComponent {}
