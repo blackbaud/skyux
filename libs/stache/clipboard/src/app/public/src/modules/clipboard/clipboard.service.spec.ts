15bf9a93822c8f98a735a2075fea67d2c33c962e
bdjones2410 <brandon.jones@blackbaud.com>
2018-12-06 12:25:11 -0500

    cleaned up code some

--- libs/stache/clipboard/src/app/public/src/modules/clipboard/clipboard.service.spec.ts
+++ libs/stache/clipboard/src/app/public/src/modules/clipboard/clipboard.service.spec.ts
@@ -1,72 +1,18 @@
 import { SkyCopyToClipboardService } from './clipboard.service';
-import { Renderer2, RendererFactory2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { SkyClipboardWindowRef } from '../shared';
 const mock = require('mock-require');
 const clipboardPath = 'clipboard-polyfill/build/clipboard-polyfill.promise';
 
-class MockRenderer {
-  public selectRootElement(element: any) {
-    return {
-      select: () => { },
-      setSelectionRange: (start: any, end: any) => { }
-    };
-  }
-
-  public appendChild(parentElement: any, childElement: any) { }
-}
-
-class MockRendererFactory {
-  public createRenderer(a: any, b: any) {
-    return new MockRenderer() as Renderer2;
-  }
-}
-
-class MockWindowRef {
-  public nativeWindow = {
-    pageYOffset: 10,
-    getSelection() {
-      return {
-        removeAllRanges() { }
-      };
-    },
-    document: {
-      execCommand(command: string) {
-        return true;
-      },
-      queryCommandSupported(command: string) {
-        return true;
-      },
-      createElement(element: string) {
-        return document.createElement(element);
-      },
-      body: { },
-      documentElement: {
-        scrollTop: 100
-      }
-    }
-  };
-}
-
 describe('SkyCopyToClipboardService', () => {
   let clipboardService: SkyCopyToClipboardService;
-  let mockRendererFactory: any;
-  let mockWindowRef: any;
   let testContent: string = 'Test demo content';
   let testElement = document.createElement('div');
   let mockClipboard: any;
   testElement.innerText = testContent;
 
   beforeEach(() => {
-    mockRendererFactory = new MockRendererFactory();
-    mockWindowRef  = new MockWindowRef();
-
     TestBed.configureTestingModule({
-      providers: [
-        { provide: SkyClipboardWindowRef, useValue: mockWindowRef },
-        SkyCopyToClipboardService,
-        { provide: RendererFactory2, useValue: mockRendererFactory}
-      ]
+      providers: []
     })
     .compileComponents();
 
--- libs/stache/clipboard/src/app/public/src/modules/clipboard/clipboard.service.ts
+++ libs/stache/clipboard/src/app/public/src/modules/clipboard/clipboard.service.ts
@@ -1,27 +1,14 @@
 // adapted from ngx-clipboard
 
 import { Injectable } from '@angular/core';
-import { SkyClipboardWindowRef } from '../shared';
 
 const clipboard = require("clipboard-polyfill/build/clipboard-polyfill.promise");
 
 @Injectable()
 export class SkyCopyToClipboardService {
-  private document: any;
-
-  constructor(
-    private windowRef: SkyClipboardWindowRef
-  ) {
-    this.document = this.windowRef.nativeWindow.document;
-  }
-
   public copyContent(element: HTMLElement) {
     let copyText = this.getTextFromElement(element);
     clipboard.writeText(copyText);
-  }
-
-  public verifyCopyCommandBrowserSupport() {
-    return this.document.queryCommandSupported('copy');
   }
 
   private getTextFromElement(element: HTMLElement): string {
