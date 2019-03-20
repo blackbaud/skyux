eaa27c57024094bdeec02771e1817081f820b7b7
Alex <alex.kingman@blackbaud.com>
2019-03-20 15:15:21 -0400

    added resource module to list-toolbar module (#37)

--- libs/components/list-builder/src/app/public/modules/list-toolbar/list-toolbar.module.ts
+++ libs/components/list-builder/src/app/public/modules/list-toolbar/list-toolbar.module.ts
@@ -1,6 +1,7 @@
 import {
   NgModule
 } from '@angular/core';
+
 import {
   CommonModule
 } from '@angular/common';
@@ -8,16 +9,28 @@ import {
 import {
   SkyToolbarModule
 } from '@skyux/layout';
+
 import {
   SkySearchModule
 } from '@skyux/lookup';
+
 import {
   SkyFilterModule,
   SkySortModule
 } from '@skyux/lists';
+
+import {
+  SkyCheckboxModule
+} from '@skyux/forms';
+
+import {
+  SkyI18nModule
+} from '@skyux/i18n';
+
 import {
   SkyListFiltersModule
 } from '../list-filters';
+
 import {
   SkyIconModule
 } from '@skyux/indicators';
@@ -25,12 +38,15 @@ import {
 import {
   SkyListToolbarComponent
 } from './list-toolbar.component';
+
 import {
   SkyListToolbarItemComponent
 } from './list-toolbar-item.component';
+
 import {
   SkyListToolbarItemRendererComponent
 } from './list-toolbar-item-renderer.component';
+
 import {
   SkyListToolbarSortComponent
 } from './list-toolbar-sort.component';
@@ -44,12 +60,8 @@ import {
 } from './list-toolbar-view-actions.component';
 
 import {
-  SkyCheckboxModule
-} from '@skyux/forms';
-
-import {
-  SkyI18nModule
-} from '@skyux/i18n';
+  SkyListBuilderResourcesModule
+} from '../shared/list-builder-resources.module';
 
 @NgModule({
   declarations: [
@@ -69,7 +81,8 @@ import {
     SkySortModule,
     SkyFilterModule,
     SkyListFiltersModule,
-    SkyIconModule
+    SkyIconModule,
+    SkyListBuilderResourcesModule
   ],
   exports: [
     SkyListToolbarComponent,
