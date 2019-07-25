5e189e474b3ba1c2721a8042d2fe5bf7921a2327
Steve Brush <steve.brush@blackbaud.com>
2019-07-25 13:50:57 -0400

    Added support for `@skyux-sdk/builder`. (#38)

--- libs/components/i18n/src/app/public/utils/format.ts
+++ libs/components/i18n/src/app/public/utils/format.ts
@@ -1,5 +1,9 @@
 // Using this class instead of the one found in `@skyux/core`
 // to avoid a circular dependency.
+
+// Need to add the following to classes which contain static methods.
+// See: https://github.com/ng-packagr/ng-packagr/issues/641
+// @dynamic
 export class Format {
   public static formatText(format: string, ...args: any[]): string {
     return String(format).replace(
