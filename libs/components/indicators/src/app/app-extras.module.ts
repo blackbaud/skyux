cdee07a89a7f3ddb12703b62ca2e93cf468bf399
Steve Brush <steve.brush@blackbaud.com>
2019-12-11 12:02:27 -0500

    Alert > Add docs (#49)

--- libs/components/indicators/src/app/app-extras.module.ts
+++ libs/components/indicators/src/app/app-extras.module.ts
@@ -3,6 +3,11 @@ import {
 } from '@angular/core';
 
 import {
+  SkyDocsToolsModule,
+  SkyDocsToolsOptions
+} from '@skyux/docs-tools';
+
+import {
   SkyAppLinkModule
 } from '@skyux/router';
 
@@ -23,6 +28,7 @@ import {
     SkyAlertModule,
     SkyAppLinkModule,
     SkyChevronModule,
+    SkyDocsToolsModule,
     SkyHelpInlineModule,
     SkyIconModule,
     SkyKeyInfoModule,
@@ -30,6 +36,15 @@ import {
     SkyTextHighlightModule,
     SkyTokensModule,
     SkyWaitModule
+  ],
+  providers: [
+    {
+      provide: SkyDocsToolsOptions,
+      useValue: {
+        gitRepoUrl: 'https://github.com/blackbaud/skyux-indicators',
+        packageName: '@skyux/indicators'
+      }
+    }
   ]
 })
 export class AppExtrasModule { }
---
+++ libs/components/indicators/src/app/docs/alert/alert-docs.component.ts
@@ -0,0 +1,9 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-alert-docs',
+  templateUrl: './alert-docs.component.html'
+})
+export class AlertDocsComponent { }
