aed2284637646515fe5661ed61cbecbf9ffdb907
Steve Brush <steve.brush@blackbaud.com>
2020-01-16 13:51:22 -0500

    Updated a11y, support multiline captions for image component. (#13)

--- libs/stache/media/src/app/public/modules/image/image.component.spec.ts
+++ libs/stache/media/src/app/public/modules/image/image.component.spec.ts
@@ -1,5 +1,6 @@
 import {
   async,
+  ComponentFixture,
   TestBed
 } from '@angular/core/testing';
 
@@ -14,6 +15,10 @@ import {
 import {
   SkyImageTestComponent
 } from './fixtures/image.component.fixture';
+
+function getCaptionElement(fixture: ComponentFixture<SkyImageTestComponent>): HTMLElement {
+  return fixture.nativeElement.querySelector('.sky-image-caption');
+}
 
 describe('Image component', () => {
   beforeEach(() => {
@@ -31,67 +36,60 @@ describe('Image component', () => {
     const fixture = TestBed.createComponent(SkyImageTestComponent);
     const cmp = fixture.componentInstance as SkyImageTestComponent;
     cmp.imageSource = '~/assets/demo-image.jpg';
-    const el = fixture.nativeElement as HTMLElement;
 
     fixture.detectChanges();
 
-    const captionContainerRef: any = el.querySelector('.sky-image-caption-container');
+    const captionElement = getCaptionElement(fixture);
     expect(cmp.caption).toBe(undefined);
-    expect(captionContainerRef).toBeFalsy();
+    expect(captionElement).toBeFalsy();
   }));
 
   it('should display a caption and default divider bar when a caption is present', async(() => {
     const fixture = TestBed.createComponent(SkyImageTestComponent);
     const cmp = fixture.componentInstance as SkyImageTestComponent;
     cmp.imageSource = '~/assets/demo-image.jpg';
-    const el = fixture.nativeElement as HTMLElement;
     cmp.caption = 'test caption';
 
     fixture.detectChanges();
 
-    const captionContainerRef: any = el.querySelector('.sky-image-caption-container');
-    const caption: any = el.querySelector('.sky-image-caption').textContent.trim();
+    const captionElement = getCaptionElement(fixture);
+    const caption = captionElement.textContent.trim();
     expect(caption).toBe('test caption');
     expect(cmp.captionType).toBe('default');
-    expect(captionContainerRef.classList.contains('sky-caption-default')).toBe(true);
+    expect(captionElement.classList.contains('sky-image-caption-default')).toBe(true);
   }));
 
   it('should display \'Do\' before the caption for success captionTypes', async(() => {
     const fixture = TestBed.createComponent(SkyImageTestComponent);
     const cmp = fixture.componentInstance as SkyImageTestComponent;
     cmp.imageSource = '~/assets/demo-image.jpg';
-    const el = fixture.nativeElement as HTMLElement;
     cmp.caption = 'test caption';
     cmp.captionType = 'success';
 
     fixture.detectChanges();
 
-    const captionContainerRef: any = el.querySelector('.sky-image-caption-container');
-    const caption: HTMLElement = el.querySelector('.sky-image-caption') as HTMLElement;
-    expect(caption.innerText.trim()).toBe('Do test caption');
-    expect(captionContainerRef.classList.contains('sky-caption-success')).toBe(true);
+    const captionElement = getCaptionElement(fixture);
+    expect(captionElement.innerText.trim()).toBe('Do test caption');
+    expect(captionElement.classList.contains('sky-image-caption-success')).toBe(true);
   }));
 
   it('should display \'Dont\' before the caption for danger captionTypes', async(() => {
     const fixture = TestBed.createComponent(SkyImageTestComponent);
     const cmp = fixture.componentInstance as SkyImageTestComponent;
     cmp.imageSource = '~/assets/demo-image.jpg';
-    const el = fixture.nativeElement as HTMLElement;
     cmp.caption = 'test caption';
     cmp.captionType = 'danger';
 
     fixture.detectChanges();
 
-    const captionContainerRef: any = el.querySelector('.sky-image-caption-container');
-    const caption: HTMLElement = el.querySelector('.sky-image-caption') as HTMLElement;
-    expect(caption.innerText.trim()).toBe('Don\'t test caption');
-    expect(captionContainerRef.classList.contains('sky-caption-danger')).toBe(true);
+    const captionElement = getCaptionElement(fixture);
+    expect(captionElement.innerText.trim()).toBe('Don\'t test caption');
+    expect(captionElement.classList.contains('sky-image-caption-danger')).toBe(true);
   }));
 
   it('should allow removing caption prefixes', async(() => {
     const fixture = TestBed.createComponent(SkyImageTestComponent);
     const cmp = fixture.componentInstance as SkyImageTestComponent;
-    const el = fixture.nativeElement as HTMLElement;
 
     cmp.imageSource = '~/assets/demo-image.jpg';
     cmp.caption = 'test caption';
@@ -100,8 +98,8 @@ describe('Image component', () => {
 
     fixture.detectChanges();
 
-    const caption = el.querySelector('.sky-image-caption') as HTMLElement;
-    expect(caption.innerText.trim()).toBe('test caption');
+    const captionElement = getCaptionElement(fixture);
+    expect(captionElement.innerText.trim()).toBe('test caption');
   }));
 
   it('should add a border with showBorder option set to true', async(() => {
@@ -147,6 +145,7 @@ describe('Image component', () => {
     const cmp = fixture.componentInstance as SkyImageTestComponent;
     cmp.imageSource = '~/assets/demo-image.jpg';
     cmp.imageAlt = 'demo image';
+    cmp.caption = 'Sample caption.';
     fixture.detectChanges();
     expect(fixture.nativeElement).toBeAccessible();
   }));
