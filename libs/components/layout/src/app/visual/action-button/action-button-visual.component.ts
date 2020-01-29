7b90a549fda8587be2998a8aa58c5321d61b396c
Steve Brush <steve.brush@blackbaud.com>
2020-01-29 11:27:05 -0500

    Fix action button focus outline (#64)
    * Fix action button focus boundaries
    
    * Remove screenshots
    
    * Screenshot name fixes
    
    * Visual test improvements
    
    * Fix margins
    
    * Remove screenshots

--- libs/components/layout/src/app/visual/action-button/action-button-visual.component.ts
+++ libs/components/layout/src/app/visual/action-button/action-button-visual.component.ts
@@ -6,6 +6,10 @@ import { Component} from '@angular/core';
 })
 export class ActionButtonVisualComponent {
 
+  // Padding to be applied to the action button container so that the focus outline
+  // is fully visible in the screenshot.
+  public containerPadding: number = 0;
+
   public permalink = {
     url: 'https://developer.blackbaud.com/skyux/components'
   };
@@ -14,5 +18,13 @@ export class ActionButtonVisualComponent {
 
   public buttonClicked() {
     this.buttonIsClicked = true;
+  }
+
+  public applyFocus(): void {
+    this.containerPadding = 15;
+    const actionButton: HTMLElement = document
+      .getElementById('screenshot-action-button')
+      .querySelector('.sky-action-button');
+    actionButton.focus();
   }
 }
