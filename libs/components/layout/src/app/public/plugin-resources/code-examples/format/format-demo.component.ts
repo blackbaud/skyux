1a45878a7ea0a4e4ba68093d5dd07b7c32406bd5
Paul Crowder <paul.crowder@blackbaud.com>
2020-03-13 15:59:38 -0400

    Added code examples (#92)
    * Added code examples
    
    * Regenerated package-lock.json to pick up layout 3.9.0 for demo

---
+++ libs/components/layout/src/app/public/plugin-resources/code-examples/format/format-demo.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-format-demo',
+  templateUrl: './format-demo.component.html',
+  styleUrls: ['./format-demo.component.scss']
+})
+export class FormatDemoComponent {
+
+}
