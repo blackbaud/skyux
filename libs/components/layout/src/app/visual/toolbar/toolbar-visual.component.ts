f16bccc160b39361ad7bd7872bd71ceb87421fd2
Paul Crowder <paul.crowder@blackbaud.com>
2020-06-04 14:53:09 -0400

    Modern theme for toolbar (#109)

--- libs/components/layout/src/app/visual/toolbar/toolbar-visual.component.ts
+++ libs/components/layout/src/app/visual/toolbar/toolbar-visual.component.ts
@@ -2,9 +2,21 @@ import {
   Component
 } from '@angular/core';
 
+import {
+  SkyThemeService,
+  SkyThemeSettings
+} from '@skyux/theme';
+
 @Component({
   selector: 'toolbar-visual',
   templateUrl: './toolbar-visual.component.html'
 })
 export class ToolbarVisualComponent {
+
+  constructor(private themeSvc: SkyThemeService) { }
+
+  public themeSettingsChange(themeSettings: SkyThemeSettings): void {
+    this.themeSvc.setTheme(themeSettings);
+  }
+
 }
