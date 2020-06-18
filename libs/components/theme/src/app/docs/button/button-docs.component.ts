f28dbc75982e3179bc20f124794c9dd38196e337
Denise Pena <Denise.Pena@blackbaud.me>
2020-06-18 10:21:40 -0500

    Added content for phase 2 of button docs (#143)
    * Added content for phase 2 of button docs
    
    * Fixed list on landing page
    
    * Style fix for inline link row
    
    * Replaced images
    
    * Implement feedback and formatting fixes
    
    * Removed button html from class info
    
    * More feedback edits
    
    * Add property to create Installation section
    
    * Added inline link to disabled section
    
    Co-authored-by: John Lyons <John.Lyons@blackbaud.me>

---
+++ libs/components/theme/src/app/docs/button/button-docs.component.ts
@@ -0,0 +1,9 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-button-docs',
+  templateUrl: './button-docs.component.html'
+})
+export class ButtonDocsComponent { }
