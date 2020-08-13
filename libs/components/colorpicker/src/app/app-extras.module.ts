de79813f3fa00584a3f96afd3bb67b99be2f1eee
Steve Brush <steve.brush@blackbaud.com>
2020-08-13 17:02:43 -0400

    Installed `@skyux/docs-tools` (#66)

--- libs/components/colorpicker/src/app/app-extras.module.ts
+++ libs/components/colorpicker/src/app/app-extras.module.ts
@@ -3,6 +3,11 @@ import {
 } from '@angular/core';
 
 import {
+  SkyDocsToolsModule,
+  SkyDocsToolsOptions
+} from '@skyux/docs-tools';
+
+import {
   SkyAppLinkModule
 } from '@skyux/router';
 
@@ -13,7 +18,17 @@ import {
 @NgModule({
   exports: [
     SkyAppLinkModule,
-    SkyColorpickerModule
+    SkyColorpickerModule,
+    SkyDocsToolsModule
+  ],
+  providers: [
+    {
+      provide: SkyDocsToolsOptions,
+      useValue: {
+        gitRepoUrl: 'https://github.com/blackbaud/skyux-colorpicker',
+        packageName: '@skyux/colorpicker'
+      }
+    }
   ]
 })
 export class AppExtrasModule { }
