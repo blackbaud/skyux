5e78742080fc9f8ea9b0225396bffb0299c24112
Steve Brush <steve.brush@blackbaud.com>
2020-08-13 16:58:27 -0400

    Installed `@skyux/docs-tools` (#84)

--- libs/components/select-field/src/app/app-extras.module.ts
+++ libs/components/select-field/src/app/app-extras.module.ts
@@ -3,6 +3,11 @@ import {
 } from '@angular/core';
 
 import {
+  SkyDocsToolsModule,
+  SkyDocsToolsOptions
+} from '@skyux/docs-tools';
+
+import {
   SkyAppLinkModule
 } from '@skyux/router';
 
@@ -13,7 +18,17 @@ import {
 @NgModule({
   exports: [
     SkyAppLinkModule,
+    SkyDocsToolsModule,
     SkySelectFieldModule
+  ],
+  providers: [
+    {
+      provide: SkyDocsToolsOptions,
+      useValue: {
+        gitRepoUrl: 'https://github.com/blackbaud/skyux-select-field',
+        packageName: '@skyux/select-field'
+      }
+    }
   ]
 })
 export class AppExtrasModule { }
