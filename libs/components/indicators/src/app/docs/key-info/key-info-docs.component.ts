81c444011f2f621b73a5b5e1d021d2f9de84e57a
Alex <alex.kingman@blackbaud.com>
2020-09-03 12:01:56 -0400

    Added demo and code example for key info component (#128)
    * Added demo and code example for key info component
    
    * style fixes
    
    * upgrade deps
    
    * fixed demo
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

--- libs/components/indicators/src/app/docs/key-info/key-info-docs.component.ts
+++ libs/components/indicators/src/app/docs/key-info/key-info-docs.component.ts
@@ -2,8 +2,28 @@ import {
   Component
 } from '@angular/core';
 
+import {
+  SkyDocsDemoControlPanelChange,
+  SkyDocsDemoControlPanelRadioChoice
+} from '@skyux/docs-tools';
+
 @Component({
   selector: 'app-key-info-docs',
   templateUrl: './key-info-docs.component.html'
 })
-export class KeyInfoDocsComponent {}
+export class KeyInfoDocsComponent {
+
+  public layout: string = 'vertical';
+
+  public layoutChoices: SkyDocsDemoControlPanelRadioChoice[] = [
+    { value: 'vertical', label: 'Vertical' },
+    { value: 'horizontal', label: 'Horizontal' }
+  ];
+
+  public onDemoSelectionChange(event: SkyDocsDemoControlPanelChange): void {
+    if (event.layout) {
+      this.layout = event.layout;
+    }
+  }
+
+}
---
+++ libs/components/indicators/src/app/public/plugin-resources/code-examples/key-info/basic/key-info-demo.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-key-info-demo',
+  templateUrl: './key-info-demo.component.html'
+})
+export class KeyInfoDemoComponent {
+
+  public layout: string = 'vertical';
+
+}
