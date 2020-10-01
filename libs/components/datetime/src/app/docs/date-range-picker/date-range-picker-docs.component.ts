578858c27b1a0a08176ed22a63c8623a219769b1
Alex <alex.kingman@blackbaud.com>
2020-10-01 13:14:57 -0400

    Added demo for date range picker (#231)
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

--- libs/components/datetime/src/app/docs/date-range-picker/date-range-picker-docs.component.ts
+++ libs/components/datetime/src/app/docs/date-range-picker/date-range-picker-docs.component.ts
@@ -1,11 +1,35 @@
 import {
-  ChangeDetectionStrategy,
-  Component
+  Component,
+  OnInit
 } from '@angular/core';
+
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup
+} from '@angular/forms';
 
 @Component({
   selector: 'app-date-range-picker-docs',
-  templateUrl: './date-range-picker-docs.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  templateUrl: './date-range-picker-docs.component.html'
 })
-export class DateRangePickerDocsComponent { }
+export class DateRangePickerDocsComponent implements OnInit {
+
+  public reactiveForm: FormGroup;
+
+  public get reactiveRange(): AbstractControl {
+    return this.reactiveForm.get('lastDonation');
+  }
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.reactiveForm = this.formBuilder.group({
+      lastDonation: new FormControl()
+    });
+  }
+
+}
