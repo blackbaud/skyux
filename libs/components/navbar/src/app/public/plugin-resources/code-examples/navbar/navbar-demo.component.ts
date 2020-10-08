0d361fe9164c346a8e18da9bc201097263b03e6b
Alex <alex.kingman@blackbaud.com>
2020-10-08 11:08:14 -0400

    Added demo for navbar component (#17)
    * Added demo for navbar component
    
    * changed link
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

---
+++ libs/components/navbar/src/app/public/plugin-resources/code-examples/navbar/navbar-demo.component.ts
@@ -0,0 +1,9 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-demo',
+  templateUrl: './navbar-demo.component.html'
+})
+export class NavbarDemoComponent {}
---
+++ libs/components/navbar/src/app/public/plugin-resources/code-examples/navbar/navbar-demo.module.ts
@@ -0,0 +1,34 @@
+import {
+  NgModule
+} from '@angular/core';
+
+import {
+  CommonModule
+} from '@angular/common';
+
+import {
+  SkyDropdownModule
+} from '@skyux/popovers';
+
+import {
+  SkyNavbarModule
+} from '@skyux/navbar';
+
+import {
+  NavbarDemoComponent
+} from './navbar-demo.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SkyDropdownModule,
+    SkyNavbarModule
+  ],
+  declarations: [
+    NavbarDemoComponent
+  ],
+  exports: [
+    NavbarDemoComponent
+  ]
+})
+export class NavbarDemoModule { }
