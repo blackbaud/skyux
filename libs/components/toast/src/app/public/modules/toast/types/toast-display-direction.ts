81004e0952ff4be3d014c47bd2cef6806013d9c0
John Lyons <John.Lyons@blackbaud.com>
2020-10-27 08:35:31 -0400

    Add missing JSDocs comments for toasts (#63)
    * Add missing JSDocs comments for toasts
    
    * Fix spacing for JSDocs asterisks
    
    Co-authored-by: John Lyons <John.Lyons@blackbaud.me>

--- libs/components/toast/src/app/public/modules/toast/types/toast-display-direction.ts
+++ libs/components/toast/src/app/public/modules/toast/types/toast-display-direction.ts
@@ -1,4 +1,10 @@
 export enum SkyToastDisplayDirection {
+  /**
+   * Displays new toasts below previous toasts.
+   */
   OldestOnTop,
+  /**
+   * Displays new toasts on top of previous toasts.
+   */
   NewestOnTop
 }
