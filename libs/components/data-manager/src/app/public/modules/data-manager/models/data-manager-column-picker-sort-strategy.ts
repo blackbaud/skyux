c3b8053fcb002a847fa3db41c886a3d219887330
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2021-01-15 10:08:07 -0500

    remove const enum (#22)

--- libs/components/data-manager/src/app/public/modules/data-manager/models/data-manager-column-picker-sort-strategy.ts
+++ libs/components/data-manager/src/app/public/modules/data-manager/models/data-manager-column-picker-sort-strategy.ts
@@ -1,4 +1,4 @@
-export const enum SkyDataManagerColumnPickerSortStrategy {
+export enum SkyDataManagerColumnPickerSortStrategy {
   None = 'none',
   SelectedThenAlphabetical = 'selectedThenAlphabetical'
 }
