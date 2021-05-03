f69daecbc57f7491f297f3b18bbadeebe73c82f1
Steve Brush <steve.brush@blackbaud.com>
2021-05-03 11:48:55 -0400

    Deprecate confirm types (#147)
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>

--- libs/components/modals/src/app/public/modules/confirm/confirm-type.ts
+++ libs/components/modals/src/app/public/modules/confirm/confirm-type.ts
@@ -1,6 +1,7 @@
 export enum SkyConfirmType {
   /**
-   * Allows you to define your own `buttons` using the buttons property to specify an array of `SkyConfirmButtonConfig` objects.
+   * Allows you to define your own `buttons` using the buttons property to
+   * specify an array of `SkyConfirmButtonConfig` objects.
    */
   Custom = 0,
   /**
@@ -9,10 +10,14 @@ export enum SkyConfirmType {
   OK = 1,
   /**
    * Displays two buttons with **Yes** and **Cancel** labels.
+   * @deprecated Use the `Custom` type to follow the guidance that labels
+   * should clearly indicate the actions that occur when users select buttons.
    */
   YesCancel = 2,
   /**
    * Displays three buttons with **Yes**, **No**, and **Cancel** labels.
+   * @deprecated Use the `Custom` type to follow the guidance that labels
+   * should clearly indicate the actions that occur when users select buttons.
    */
   YesNoCancel = 3
 }
