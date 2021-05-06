e86764990bc5b1cc644cef843af81750d7d53160
Alex <alex.kingman@blackbaud.com>
2021-05-06 16:46:33 -0400

    Overflowing filter summary now wraps to next line (#233)
    * Overflowing filter summary now wraps to next line
    
    * fixed e2e
    
    * fix spacing
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

--- libs/components/lists/src/app/visual/filter/filter-visual.component.ts
+++ libs/components/lists/src/app/visual/filter/filter-visual.component.ts
@@ -25,6 +25,45 @@ export class FilterVisualComponent {
     }
   ];
 
+  public lotsOfFilters: Array<any> = [
+    {
+      label: 'Really long filter 1',
+      dismissible: false
+    },
+    {
+      label: 'Really long filter 2',
+      dismissible: true
+    },
+    {
+      label: 'Really long filter 3',
+      dismissible: true
+    },
+    {
+      label: 'Really long filter 4',
+      dismissible: true
+    },
+    {
+      label: 'Really long filter 5',
+      dismissible: true
+    },
+    {
+      label: 'Really long filter 6',
+      dismissible: true
+    },
+    {
+      label: 'Really long filter 7',
+      dismissible: true
+    },
+    {
+      label: 'Really long filter 8',
+      dismissible: true
+    },
+    {
+      label: 'Really long filter 9',
+      dismissible: true
+    }
+  ];
+
   constructor(
     public themeSvc: SkyThemeService
   ) {
