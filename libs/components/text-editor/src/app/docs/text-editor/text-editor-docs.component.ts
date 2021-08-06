d681fb13f5d6f6cbc007821e7203fae840ac4e85
Alex <alex.kingman@blackbaud.com>
2021-08-06 14:42:23 -0400

    Text editor demo, code examples (#20)
    * Text editor demo, code examples
    
    * minor
    
    * oninit
    
    * reset  baselines
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

--- libs/components/text-editor/src/app/docs/text-editor/text-editor-docs.component.ts
+++ libs/components/text-editor/src/app/docs/text-editor/text-editor-docs.component.ts
@@ -6,4 +6,8 @@ import {
   selector: 'app-text-editor-docs',
   templateUrl: './text-editor-docs.component.html'
 })
-export class TextEditorDocsComponent {}
+export class TextEditorDocsComponent {
+
+  public richText: string = `<font style="font-size: 18px" face="Arial" color="#a25353"><b>Exclusively committed to your impact</b></font><p>Since day one, Blackbaud has been 100% focused on driving impact for social good organizations.</p><p>We equip change agents with <b>cloud software</b>, <i>services</i>, <u>expertise</u>, and <font color="#a25353">data intelligence</font> designed with unmatched insight and supported with unparalleled commitment. Every day, our <b>customers</b> achieve unmatched impact as they advance their missions.</p><ul><li><a href="#">Build a better world</a></li><li><a href="#">Explore our solutions</a></li></ul>`;
+
+}
