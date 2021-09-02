22d745d0dd64252976417b8c0b09500661a5ee5d
Thomas Ortiz <thomasortiz95@gmail.com>
2021-09-02 16:11:01 -0400

    Contrib > Ported getStrings to lib-resources service (#125)

--- libs/components/i18n/src/app/public/modules/i18n/lib-resources.service.spec.ts
+++ libs/components/i18n/src/app/public/modules/i18n/lib-resources.service.spec.ts
@@ -1,9 +1,5 @@
 // #region imports
 import {
-  TestBed
-} from '@angular/core/testing';
-
-import {
   of as observableOf
 } from 'rxjs';
 
@@ -30,14 +26,26 @@ import {
 
 class MockSkyLibResourcesProvider implements SkyLibResourcesProvider {
   constructor(
+    // @ts-ignore - ignore in favor of using static test data
     private key: string
   ) { }
 
   public getString(localeInfo: SkyAppLocaleInfo, name: string): string {
     const resources: any = {
-      'en_US': { [this.key]: 'hello', [this.key + '_alternate']: 'hi' },
-      'fr_CA': { [this.key]: 'bonjour' },
-      'fr_FR': { [this.key]: 'hello {0} {1}' }
+      'en_US': {
+        'greeting': 'hello',
+        'greeting_alternate': 'hi',
+        'hi': 'hello',
+        'hi_alternate': 'howdy',
+        'template': 'format {0} me {1} {0}'
+      },
+      'fr_CA': {
+        'greeting': 'bonjour',
+        'hi!': 'bonjour!'
+      },
+      'fr_FR': {
+        'greeting': 'hello {0} {1}'
+      }
     };
 
     const values = resources[localeInfo.locale];
@@ -56,18 +64,9 @@ describe('Library resources service', () => {
   let mockProviders: SkyLibResourcesProvider[];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [],
-      providers: []
-    });
-
     mockLocaleProvider = {
       defaultLocale: 'en_US',
-      getLocaleInfo: () => {
-        return observableOf({
-          locale: 'en_US'
-        });
-      }
+      getLocaleInfo: () => observableOf({ locale: 'en_US' })
     };
 
     mockProviders = [
@@ -133,6 +132,81 @@ describe('Library resources service', () => {
 
     service.getString('greeting').pipe(take(1)).subscribe((value: string) => {
       expect(value).toEqual('hi');
+    });
+  });
+
+  describe('getStrings', () => {
+    it('returns a completed observable (this is default forkJoin behavior)', (done) => {
+      service = new SkyLibResourcesService(mockLocaleProvider, mockProviders, undefined);
+      const resources$ = service.getStrings({}).pipe(take(1));
+
+      resources$.subscribe({
+        next: () => fail(),
+        complete: () => done(),
+        error: () => fail()
+      });
+    });
+    it('returns a dictionary of resources (1 resource)', (done) => {
+      service = new SkyLibResourcesService(mockLocaleProvider, mockProviders, undefined);
+      const resources$ = service.getStrings({ hi: 'hello' }).pipe(take(1));
+
+      resources$.subscribe(values => {
+        expect(Object.keys(values).length).toBe(1);
+        expect(values.hi).toBe('hello');
+        expect((values as any)['hi_alternate']).toBeUndefined();
+        expect((values as any)['NOT DEFINED']).toBeUndefined();
+        done();
+      });
+    });
+    it('returns a dictionary of resources (1+ resources)', (done) => {
+      service = new SkyLibResourcesService(mockLocaleProvider, mockProviders, undefined);
+      const resources$ = service.getStrings({
+        hi: 'hello',
+        hi_alternate: 'hi_alternate'
+      }).pipe(take(1));
+
+      resources$.subscribe(values => {
+        expect(Object.keys(values).length).toBe(2);
+        expect((values as any)['NOT DEFINED']).toBeUndefined();
+        expect(values.hi).toBe('hello');
+        expect(values.hi_alternate).toBe('howdy');
+        done();
+      });
+    });
+    it('handles templated resources', (done) => {
+      service = new SkyLibResourcesService(mockLocaleProvider, mockProviders, undefined);
+      const resources$ = service.getStrings({
+        hi: 'hello',
+        hiAlternate: 'hi_alternate',
+        hiWithTemplateSyntax: ['hi'],
+        template: ['template', 'a', 'b'],
+        templateWithMissingTokens: ['template']
+      }).pipe(take(1));
+
+      resources$.subscribe(values => {
+        expect(Object.keys(values).length).toBe(5);
+        expect(values.hi).toBe('hello');
+        expect(values.hiAlternate).toBe('howdy');
+        expect(values.hiWithTemplateSyntax).toBe('hello');
+        expect(values.template).toBe('format a me b a');
+        expect(values.templateWithMissingTokens).toBe('format {0} me {1} {0}');
+        done();
+      });
+    });
+    it('handles locales', done => {
+      spyOn(mockLocaleProvider, 'getLocaleInfo').and.returnValue(observableOf({ locale: 'fr_FR' }));
+      service = new SkyLibResourcesService(mockLocaleProvider, mockProviders, undefined);
+      const resources$ = service.getStrings({
+        greeting: 'bonjour',
+        'hi!': 'bonjour!'
+      }).pipe(take(1));
+
+      resources$.subscribe(values => {
+        expect(Object.keys(values).length).toBe(2);
+        expect(values.greeting).toBe('bonjour');
+        expect(values['hi!']).toBe('bonjour!');
+        done();
+      });
     });
   });
 });
--- libs/components/i18n/src/app/public/modules/i18n/lib-resources.service.ts
+++ libs/components/i18n/src/app/public/modules/i18n/lib-resources.service.ts
@@ -40,6 +40,10 @@ import {
 } from './resource-name-provider';
 // #endregion
 
+type ResourceKey = string;
+type TemplatedResource = [ResourceKey, ...any[]];
+type ResourceDictionary = Record<string, ResourceKey | TemplatedResource>;
+
 @Injectable()
 export class SkyLibResourcesService {
   constructor(
@@ -48,6 +52,11 @@ export class SkyLibResourcesService {
     @Optional() private resourceNameProvider: SkyAppResourceNameProvider
   ) { }
 
+  /**
+   * Gets a resource string based on its name.
+   * @param name The name of the resource string.
+   * @param args Any templated args.
+   */
   public getString(name: string, ...args: any[]): Observable<string> {
     let mappedNameObs = this.resourceNameProvider ?
     this.resourceNameProvider.getResourceName(name) : observableOf(name);
@@ -57,6 +66,42 @@ export class SkyLibResourcesService {
     return forkJoin([mappedNameObs, localeInfoObs]).pipe(
       map(([mappedName, localeInfo]) => this.getStringForLocale(localeInfo, mappedName, ...args))
     );
+  }
+
+  /**
+   * Gets a Resource String Dictionary.
+   *
+   * This is similar to forkJoin's dictionary syntax.
+   *
+   * @param dictionary a Record of **SomeObjectKey** to a Value that is either
+   *   - (1) **ResourceKey**
+   *   - (2) or an **Array** where the first item is the **ResourceKey** and the other items are template args.
+   * @return an `Observable` of a resource string dictionary in the same shape as the passed dictionary.
+   *
+   * @example
+   * ```
+   * service.getStrings({
+   *    simpleKey: 'hello',
+   *    arraySyntax: ['hi'],
+   *    arraySyntaxWithTemplateArgs: ['template', 'a', 'b'],
+   * }
+   * ```
+   */
+  public getStrings<T extends ResourceDictionary>(dictionary: T): Observable<{ [K in keyof T]: string }> {
+    const resources$: Record<string, Observable<string>> = {};
+
+    for (const objKey of Object.keys(dictionary)) {
+      const resource: string | [string, ...any[]] = dictionary[objKey];
+
+      if (typeof resource === 'string') {
+        resources$[objKey] = this.getString(resource);
+      } else {
+        const [key, ...templateItems] = resource;
+        resources$[objKey] = this.getString(key, ...templateItems);
+      }
+    }
+
+    return forkJoin(resources$);
   }
 
   public getStringForLocale(
