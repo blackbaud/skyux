b74e7b73ccece931dbd067196235c4013dc82513
Alex <alex.kingman@blackbaud.com>
2021-09-21 09:28:49 -0400

    Source code will handle pre-decoded strings (#173)
    * Source code will handle pre-decoded strings
    
    * coverage
    
    * coverage
    
    * code style
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

---
+++ libs/stache/docs-tools/src/app/public/modules/source-code/source-code.service.spec.ts
@@ -0,0 +1,54 @@
+import {
+  SkyDocsSourceCodeProvider
+} from './source-code-provider';
+
+import {
+  SkyDocsSourceCodeService
+} from './source-code.service';
+
+describe('Source code service', () => {
+  let service: SkyDocsSourceCodeService;
+  const path = 'foo/bar';
+
+  it('getSourceCode should handle decoded rawContents', () => {
+    const mockSourceCodeProvider: SkyDocsSourceCodeProvider = {
+      sourceCode: [
+        {
+          fileName: 'foo',
+          filePath: 'foo/bar',
+          rawContents: '<baz></baz>'
+        }
+      ]
+    };
+    service = new SkyDocsSourceCodeService(mockSourceCodeProvider);
+    expect(service.getSourceCode(path)[0].rawContents).toEqual('<baz></baz>');
+  });
+
+  it('getSourceCode should handle encoded rawContents', () => {
+    const mockSourceCodeProvider: SkyDocsSourceCodeProvider = {
+      sourceCode: [
+        {
+          fileName: 'foo',
+          filePath: 'foo/bar',
+          rawContents: '%3Cbaz%3E%3C/baz%3E'
+        }
+      ]
+    };
+    service = new SkyDocsSourceCodeService(mockSourceCodeProvider);
+    expect(service.getSourceCode(path)[0].rawContents).toEqual('<baz></baz>');
+  });
+
+  it('getSourceCode should handle decoded rawContents with URI reserved characters', () => {
+    const mockSourceCodeProvider: SkyDocsSourceCodeProvider = {
+      sourceCode: [
+        {
+          fileName: 'foo',
+          filePath: 'foo/bar',
+          rawContents: '<baz style="width: 50%;"></baz>'
+        }
+      ]
+    };
+    service = new SkyDocsSourceCodeService(mockSourceCodeProvider);
+    expect(service.getSourceCode(path)[0].rawContents).toEqual('<baz style="width: 50%;"></baz>');
+  });
+});
--- libs/stache/docs-tools/src/app/public/modules/source-code/source-code.service.ts
+++ libs/stache/docs-tools/src/app/public/modules/source-code/source-code.service.ts
@@ -26,7 +26,19 @@ export class SkyDocsSourceCodeService {
     return sourceCode
       .filter(file => file.filePath.indexOf(path) === 0)
       .map(file => {
-        file.rawContents = decodeURIComponent(file.rawContents);
+        // TODO: Remove decoding after migrating to Angular CLI. Code examples will not be encoded for SKY UX 5 libraries.
+        // SKY UX 4 libraries will return rawContents encoded, while later libraries will
+        // return rawContents decoded. Check if the contents need decoded before returning them.
+        let decodedContents: string;
+        try {
+          decodedContents = decodeURIComponent(file.rawContents);
+          if (decodedContents !== file.rawContents) {
+            file.rawContents = decodedContents;
+          }
+        } catch (error) {
+          // Pre-decoded contents may contain characters like '%' that would throw errors.
+          // In these cases, pass along the original rawContents.
+        }
         return file;
       });
   }
