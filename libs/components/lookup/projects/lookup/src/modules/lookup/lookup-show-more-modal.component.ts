ba07f372801c647a540998da4f0ce6eacbc6e424
Steve Brush <steve.brush@blackbaud.com>
2022-01-26 09:09:20 -0500

    Fix SkyLookupShowMoreModalComponent selector (#248)

--- libs/components/lookup/projects/lookup/src/modules/lookup/lookup-show-more-modal.component.ts
+++ libs/components/lookup/projects/lookup/src/modules/lookup/lookup-show-more-modal.component.ts
@@ -17,7 +17,7 @@ import { SkyLookupShowMoreNativePickerContext } from './types/lookup-show-more-n
  * Internal component to implement the native picker.
  */
 @Component({
-  selector: 'skyux-lookup-show-more-modal',
+  selector: 'sky-lookup-show-more-modal',
   templateUrl: './lookup-show-more-modal.component.html',
   styleUrls: ['./lookup-show-more-modal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
