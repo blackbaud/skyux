95d9471843d6a7bcd17272488f06c5956125e152
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-02-22 14:45:34 -0600

    Add schematic for adding the angular CDK (#17)
    * Add schematic for adding the angular CDK
    
    * Updated version where migration will run
    
    * Update package list for migration

---
+++ libs/components/packages/src/schematics/migrations/add-cdk/add-cdk.schematic.ts
@@ -0,0 +1,36 @@
+import { Rule } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+  addPackageJsonDependency,
+  NodeDependencyType,
+} from '@schematics/angular/utility/dependencies';
+
+import { readRequiredFile } from '../../utility/tree';
+
+export default function (): Rule {
+  return async (tree, context) => {
+    const filePath = 'package.json';
+    const relevantPackages = ['@skyux/flyout'];
+    const packageJson = JSON.parse(readRequiredFile(tree, filePath));
+    let packageFound = false;
+
+    for (const relevantPackage of relevantPackages) {
+      if (packageJson.dependencies[relevantPackage]) {
+        packageFound = true;
+        break;
+      }
+    }
+
+    if (packageFound) {
+      // Run Thing
+      context.addTask(new NodePackageInstallTask());
+
+      addPackageJsonDependency(tree, {
+        type: NodeDependencyType.Default,
+        name: '@angular/cdk',
+        version: '^12.0.0',
+        overwrite: true,
+      });
+    }
+  };
+}
