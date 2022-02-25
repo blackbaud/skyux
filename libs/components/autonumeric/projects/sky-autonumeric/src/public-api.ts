f2123257543128db328418e6bb5e859960550ab6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 08:39:27 -0500

    chore: prepared autonumeric for monorepo

--- libs/components/autonumeric/projects/sky-autonumeric/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-export * from './modules/autonumeric/autonumeric-options-provider';
-export * from './modules/autonumeric/autonumeric-options';
-export * from './modules/autonumeric/autonumeric.module';
-
-// Components and directives must be exported to support Angular's "partial" Ivy compiler.
-// Obscure names are used to indicate types are not part of the public API.
-export { SkyAutonumericDirective as λ1 } from './modules/autonumeric/autonumeric.directive';
