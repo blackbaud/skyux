09dfca404a7f9a6d45e49efce7fd4735f46949c7
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 08:41:07 -0500

    chore: prepared list-builder-view-checklist for monorepo

--- libs/components/list-builder-view-checklist/projects/list-builder-view-checklist/README.md
+++
@@ -1,5 +0,0 @@
-# @skyux/list-builder-view-checklist
-
-[![npm](https://img.shields.io/npm/v/@skyux/list-builder-view-checklist.svg)](https://www.npmjs.com/package/@skyux/list-builder-view-checklist)
-[![status](https://travis-ci.org/blackbaud/skyux-list-builder-view-checklist.svg?branch=master)](https://travis-ci.org/blackbaud/skyux-list-builder-view-checklist)
-[![coverage](https://codecov.io/gh/blackbaud/skyux-list-builder-view-checklist/branch/master/graphs/badge.svg?branch=master)](https://codecov.io/gh/blackbaud/skyux-list-builder-view-checklist/branch/master)
---
+++ libs/components/list-builder-view-checklist/src/index.ts
@@ -0,0 +1,16 @@
+export * from './lib/modules/list-view-checklist/list-view-checklist.module';
+export * from './lib/modules/list-view-checklist/state/checklist-state-action.type';
+export * from './lib/modules/list-view-checklist/state/checklist-state.model';
+export * from './lib/modules/list-view-checklist/state/checklist-state.rxstate';
+export * from './lib/modules/list-view-checklist/state/checklist-state.state-node';
+export * from './lib/modules/list-view-checklist/state/items/item.model';
+export * from './lib/modules/list-view-checklist/state/items/items.orchestrator';
+export * from './lib/modules/list-view-checklist/state/items/load.action';
+
+// The following export is used internally by `@skyux/select-field`.
+// TODO: Find a way to remove this in the next major version release.
+export * from './lib/modules/list-view-checklist/list-view-checklist.component';
+
+// Components and directives must be exported to support Angular’s “partial” Ivy compiler.
+// Obscure names are used to indicate types are not part of the public API.
+export { SkyListViewChecklistItemComponent as λ1 } from './lib/modules/list-view-checklist/list-view-checklist-item.component';
