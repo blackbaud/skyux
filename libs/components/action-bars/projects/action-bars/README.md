92a397dfc86c3c0d3b6faf38cfec56582bcf694e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 08:38:44 -0500

    chore: prepared action-bars for monorepo

--- libs/components/action-bars/projects/action-bars/README.md
+++
@@ -1,5 +0,0 @@
-# @skyux/action-bars
-
-[![npm](https://img.shields.io/npm/v/@skyux/action-bars.svg)](https://www.npmjs.com/package/@skyux/action-bars)
-![SKY UX CI](https://github.com/blackbaud/skyux-action-bars/workflows/SKY%20UX%20CI/badge.svg)
-[![coverage](https://codecov.io/gh/blackbaud/skyux-action-bars/branch/master/graphs/badge.svg?branch=master)](https://codecov.io/gh/blackbaud/skyux-action-bars/branch/master)
---
+++ libs/components/action-bars/src/index.ts
@@ -0,0 +1,11 @@
+export * from './lib/modules/summary-action-bar/summary-action-bar.module';
+
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of the public API.
+export { SkySummaryActionBarSummaryComponent as λ1 } from './lib/modules/summary-action-bar/summary/summary-action-bar-summary.component';
+export { SkySummaryActionBarComponent as λ2 } from './lib/modules/summary-action-bar/summary-action-bar.component';
+export { SkySummaryActionBarActionsComponent as λ3 } from './lib/modules/summary-action-bar/actions/summary-action-bar-actions.component';
+export { SkySummaryActionBarCancelComponent as λ4 } from './lib/modules/summary-action-bar/actions/summary-action-bar-cancel.component';
+export { SkySummaryActionBarPrimaryActionComponent as λ5 } from './lib/modules/summary-action-bar/actions/summary-action-bar-primary-action.component';
+export { SkySummaryActionBarSecondaryActionComponent as λ6 } from './lib/modules/summary-action-bar/actions/summary-action-bar-secondary-action.component';
+export { SkySummaryActionBarSecondaryActionsComponent as λ7 } from './lib/modules/summary-action-bar/actions/summary-action-bar-secondary-actions.component';
