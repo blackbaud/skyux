2cc89c597501671dbb9ac01bb476a5c18d62d48d
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 08:39:40 -0500

    chore: prepared avatar for monorepo

---
+++ libs/components/avatar/src/index.ts
@@ -0,0 +1,7 @@
+export * from './lib/modules/avatar/avatar-size';
+export * from './lib/modules/avatar/avatar-src';
+export * from './lib/modules/avatar/avatar.module';
+
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of public API.
+export { SkyAvatarComponent as λ1 } from './lib/modules/avatar/avatar.component';
