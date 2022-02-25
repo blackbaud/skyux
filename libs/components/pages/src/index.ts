240ff96fee437364c299a02654105b6565232f0a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 08:41:47 -0500

    chore: prepared pages for monorepo

---
+++ libs/components/pages/src/index.ts
@@ -0,0 +1,12 @@
+export * from './lib/modules/action-hub/action-hub.module';
+export * from './lib/modules/action-hub/types/action-hub-needs-attention';
+export * from './lib/modules/action-hub/types/page-link';
+export * from './lib/modules/action-hub/types/recent-link';
+export * from './lib/modules/page-header/page-header.module';
+
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of the public API.
+export { SkyActionHubComponent as λ1 } from './lib/modules/action-hub/action-hub.component';
+export { SkyActionHubButtonsComponent as λ2 } from './lib/modules/action-hub/action-hub-buttons.component';
+export { SkyActionHubContentComponent as λ3 } from './lib/modules/action-hub/action-hub-content.component';
+export { SkyPageHeaderComponent as λ4 } from './lib/modules/page-header/page-header.component';
