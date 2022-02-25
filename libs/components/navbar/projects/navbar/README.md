e1b1d7567cf18e5a166d4bd4ab7ca8d2723b934f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 08:41:43 -0500

    chore: prepared navbar for monorepo

--- libs/components/navbar/projects/navbar/README.md
+++
@@ -1,5 +0,0 @@
-# @skyux/navbar
-
-[![npm](https://img.shields.io/npm/v/@skyux/navbar.svg)](https://www.npmjs.com/package/@skyux/navbar)
-![SKY UX CI](https://github.com/blackbaud/skyux-navbar/workflows/SKY%20UX%20CI/badge.svg)
-[![coverage](https://codecov.io/gh/blackbaud/skyux-navbar/branch/master/graphs/badge.svg?branch=master)](https://codecov.io/gh/blackbaud/skyux-navbar/branch/master)
---
+++ libs/components/navbar/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/modules/navbar/navbar.module';
+
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of the public API.
+export { SkyNavbarComponent as λ1 } from './lib/modules/navbar/navbar.component';
+export { SkyNavbarItemComponent as λ2 } from './lib/modules/navbar/navbar-item.component';
