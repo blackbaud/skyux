019f6160396acedd5167b791821ec8ec66d0e8c0
Alex Kingman <alex.kingman@blackbaud.me>
2022-02-28 14:06:11 -0500

    fixed build

--- apps/playground/src/app/app.module.ts
+++ apps/playground/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from '../../../code-examples/src/app/app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
