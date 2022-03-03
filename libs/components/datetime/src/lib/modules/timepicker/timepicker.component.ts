be2adf94905bdeeb19711e15bb1401c28c2c8996
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-03-03 09:14:31 -0500

    refactor: address 'prefer-rest-params' ESLint rule

--- libs/components/datetime/src/lib/modules/timepicker/timepicker.component.ts
+++ libs/components/datetime/src/lib/modules/timepicker/timepicker.component.ts
@@ -306,10 +306,8 @@ export class SkyTimepickerComponent implements OnInit, OnDestroy {
     };
 
     data = {
-      hours: Array.apply(undefined, Array(h)).map(function (
-        x: number,
-        i: number
-      ) {
+      // Create a sparse array with a length equal to the hour.
+      hours: Array(...Array(h)).map((_, i) => {
         if (format === 'hh') {
           return ++i;
         }
@@ -321,10 +319,8 @@ export class SkyTimepickerComponent implements OnInit, OnDestroy {
         /* sanity check */
         return 0;
       }),
-      minutes: Array.apply(undefined, Array(m)).map(function (
-        x: number,
-        i: number
-      ) {
+      // Create a sparse array with a length equal to the minute.
+      minutes: Array(...Array(m)).map(function (_, i) {
         return i * minuteMultiplier;
       }),
       localeFormat: localeFormat,
