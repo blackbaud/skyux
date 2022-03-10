9f24c399d0fbf4be5f802667f8f4af136127b61d
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-03-10 16:10:44 -0500

    Fix playground issue

--- apps/playground/src/app/components/tabs/vertical-tabset/vertical-tabset.module.ts
+++ apps/playground/src/app/components/tabs/vertical-tabset/vertical-tabset.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SkyBackToTopModule } from '@skyux/layout';
 import { SkyModalModule } from '@skyux/modals';
 import { SkyVerticalTabsetModule } from '@skyux/tabs';
 
@@ -9,6 +11,8 @@ import { VerticalTabsetComponent } from './vertical-tabset.component';
 @NgModule({
   declarations: [VerticalTabsetComponent, VerticalTabsetModalComponent],
   imports: [
+    CommonModule,
+    SkyBackToTopModule,
     SkyModalModule,
     SkyVerticalTabsetModule,
     VerticalTabsetRoutingModule,
