e296d9fa01beb2bc770ae69a4a0a6b95401e4389
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-03-10 15:46:46 -0500

    fix(components/lookup): open lookup show more modal correctly when no value is given while in single select mode (#53)

--- apps/playground/src/app/app.module.ts
+++ apps/playground/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
