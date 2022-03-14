faa7601024079d92e30525c3f263da9d0d6452bb
Steve Brush <steve.brush@blackbaud.com>
2022-03-14 17:42:00 -0400

    fix tests

--- scripts/test-affected.ts
+++ scripts/test-affected.ts
@@ -110,6 +110,11 @@ declare const require: {
   };
 };
 
+// Fix for crossvent "global is not defined" error. The crossvent library is used by Dragula,
+// which in turn is used by multiple SKY UX components.
+// https://github.com/bevacqua/dragula/issues/602
+(window as any).global = window;
+
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
