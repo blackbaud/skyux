e35a51ea4bf75b4d0299c93a229d9f29cb064881
Steve Brush <steve.brush@blackbaud.com>
2022-03-16 15:45:01 -0400

    revert change to karma

--- config/karma/karma.shared.conf.js
+++ config/karma/karma.shared.conf.js
@@ -7,9 +7,9 @@ module.exports = function (config) {
     ...baseConfig,
     client: {
       ...baseConfig.client,
-      // captureConsole: false,
+      captureConsole: false,
     },
-    reporters: ['progress'],
+    reporters: ['dots'],
     autoWatch: false,
     browsers: ['ChromeHeadless'], // Fallback browser in case env variables not set.
     singleRun: true,
