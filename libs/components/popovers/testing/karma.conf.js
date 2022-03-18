d9457636ccc148b30f2d7806a4b4b68ce0538f5e
Steve Brush <steve.brush@blackbaud.com>
2022-03-18 08:18:43 -0400

    chore: migration prep (#68)

--- libs/components/popovers/testing/karma.conf.js
+++ libs/components/popovers/testing/karma.conf.js
@@ -6,9 +6,4 @@ module.exports = function (config) {
     __dirname,
     '../../../../coverage/libs/components/popovers/testing'
   );
-
-  // TODO: remove these threshold overrides to meet 100% coverage!
-  config.coverageReporter.check = {
-    global: { statements: 87, branches: 69, functions: 99, lines: 87 },
-  };
 };
