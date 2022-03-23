533ae523f83dbedb19026781164675246cb66304
Braden Bisping <bradenneilbisping@tutanota.com>
2022-03-23 10:22:54 -0500

    fix tabset permalink issues

--- apps/integration/src/app/integrations/flyout-with-tabs/basic/flyout-with-tabs-content.component.ts
+++ apps/integration/src/app/integrations/flyout-with-tabs/basic/flyout-with-tabs-content.component.ts
@@ -5,10 +5,10 @@ import { Component } from '@angular/core';
   template: `
     <div class="sky-padding-even-large">
       <h2 id="flyout-title">Sample flyout</h2>
-      <sky-tabset ariaLabel="Tab demonstration">
-        <sky-tab [tabHeading]="'Tab 1'"> Content for Tab 1 </sky-tab>
-        <sky-tab [tabHeading]="'Tab 2'"> Content for Tab 2 </sky-tab>
-        <sky-tab [tabHeading]="'Tab 3'"> Content for Tab 3 </sky-tab>
+      <sky-tabset ariaLabel="Tab demonstration" permalinkId="blah">
+        <sky-tab [tabHeading]="'Tab 1'"> TAB 1111111 </sky-tab>
+        <sky-tab [tabHeading]="'Tab 2'"> TAB 22222222222 </sky-tab>
+        <sky-tab [tabHeading]="'Tab 3'"> TAB 3333333333333333 </sky-tab>
       </sky-tabset>
     </div>
   `,
