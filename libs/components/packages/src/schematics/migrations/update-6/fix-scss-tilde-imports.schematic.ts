d40bdcae62647ea79fd2e7e23477b901e6f5bcc0
Steve Brush <steve.brush@blackbaud.com>
2022-03-30 10:18:35 -0400

    feat(migrations): add migrate schematic to fix SCSS tilde imports

---
+++ libs/components/packages/src/schematics/migrations/update-6/fix-scss-tilde-imports.schematic.ts
@@ -0,0 +1,44 @@
+import { extname } from '@angular-devkit/core';
+import { Rule } from '@angular-devkit/schematics';
+
+import { readRequiredFile } from '../../utility/tree';
+
+/**
+ * Replaces the tilde character from SCSS imports with 'node_modules'.
+ * Angular 13 dropped support for tilde import statements in SCSS/CSS files.
+ * @see: https://github.com/angular/components/commit/f2ff9e31425f0e395e6926bcaf48f876688000d8
+ */
+export default function fixScssTildeImports(): Rule {
+  return async (tree) => {
+    tree.visit((filePath) => {
+      const extension = extname(filePath);
+      if (extension === '.scss' || extension === '.css') {
+        const content = readRequiredFile(tree, filePath);
+
+        const migratedContent = content
+          // Simply remove the tilde for known @skyux/theme imports, since we added exports for these
+          // files in libs/components/theme/package.json.
+          .replace(
+            /@(?:import|use) +['"]~@skyux\/theme\/scss\/(variables|mixins).*['"].*;?/g,
+            (match) => {
+              return match.replace('~', '');
+              // const index = match.indexOf('~');
+              // return match.slice(0, index) + match.slice(index + 1);
+            }
+          )
+          // For all other tilde imports, replace with 'node_modules'.
+          .replace(/@(?:import|use) +['"]~.*['"].*;?/g, (match) => {
+            // const index = match.indexOf('~');
+            // return (
+            //   match.slice(0, index) + 'node_modules/' + match.slice(index + 1)
+            // );
+            return match.replace('~', 'node_modules/');
+          });
+
+        if (content !== migratedContent) {
+          tree.overwrite(filePath, migratedContent);
+        }
+      }
+    });
+  };
+}
