50ba52ca5fdbefe1567e06216f771eb13deb32d8
Paula Hodgkins <paula.hodgkins@blackbaud.com>
2022-04-01 15:30:07 -0400

    doc: add modal classes content, fix misspellings

--- libs/components/modals/src/lib/modules/modal/modal-close-args.ts
+++ libs/components/modals/src/lib/modules/modal/modal-close-args.ts
@@ -1,4 +1,15 @@
+/**
+ * Contains an object with the data passed from users when
+ * a modal is closed, and the reason that the modal was closed.
+ */
 export class SkyModalCloseArgs {
+  /**
+   * Indicates the reason the modal was closed.
+   * Options include `"close"`, `"save"`, and `"cancel"`.
+   */
   public reason: string;
+  /**
+   * The data passed from users when the modal is closed.
+   */
   public data: any;
 }
