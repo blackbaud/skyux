cc92b3858813ed327090623de20c4f66fa1ffee2
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-04-06 14:28:54 -0400

    Work through phone-field

--- apps/code-examples/src/app/code-examples/navbar/navbar/navbar-demo.component.ts
+++ apps/code-examples/src/app/code-examples/navbar/navbar/navbar-demo.component.ts
@@ -4,4 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-navbar-demo',
   templateUrl: './navbar-demo.component.html',
 })
-export class NavbarDemoComponent {}
+export class NavbarDemoComponent {
+  public onDropdownItemClick(buttonText: string): void {
+    alert(buttonText + ' button clicked!');
+  }
+}
