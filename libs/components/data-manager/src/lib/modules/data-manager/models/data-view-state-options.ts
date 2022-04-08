a3666951e6e13e164135428e6dfdf831f5e82d8b
Paula Hodgkins <77297281+blackbaud-paulah@users.noreply.github.com>
2022-04-08 09:09:18 -0400

    docs: add descriptions for data manager classes (#118)
    * docs: add descriptions for data manager classes
    
    * John Lyon's edits

--- libs/components/data-manager/src/lib/modules/data-manager/models/data-view-state-options.ts
+++ libs/components/data-manager/src/lib/modules/data-manager/models/data-view-state-options.ts
@@ -12,8 +12,8 @@ export interface SkyDataViewStateOptions {
    */
   displayedColumnIds?: string[];
   /**
-   * An untyped property that can track any view-specific state information
-   * relevant to a data manager that existing properties do not cover.
+   * An untyped property that tracks any view-specific state information
+   * that is relevant to a data manager but that existing properties do not cover.
    */
   additionalData?: any;
 }
