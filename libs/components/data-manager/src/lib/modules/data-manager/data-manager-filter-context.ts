0e270bdf810dae6e8189c0a2d8a5abf3d8c3af0d
Steve Brush <steve.brush@blackbaud.com>
2022-04-11 11:34:05 -0400

    docs: add descriptions for data manager classes (#118) (#134)
    Co-authored-by: Paula Hodgkins <77297281+blackbaud-paulah@users.noreply.github.com>

--- libs/components/data-manager/src/lib/modules/data-manager/data-manager-filter-context.ts
+++ libs/components/data-manager/src/lib/modules/data-manager/data-manager-filter-context.ts
@@ -1,5 +1,11 @@
 import { SkyDataManagerFilterData } from './models/data-manager-filter-data';
 
+/**
+ * Sets the state of the filters.
+ */
 export class SkyDataManagerFilterModalContext {
+  /**
+   * Sets the state of the filters.
+   */
   public filterData: SkyDataManagerFilterData;
 }
--- libs/components/data-manager/src/lib/modules/data-manager/models/data-view-state.ts
+++ libs/components/data-manager/src/lib/modules/data-manager/models/data-view-state.ts
@@ -1,9 +1,25 @@
 import { SkyDataViewStateOptions } from './data-view-state-options';
 
+/**
+ * Provides options for defining how data is displayed, such as which columns appear.
+ */
 export class SkyDataViewState {
+  /**
+   * The IDs of the columns able to be displayed for column-based views. This property is required when utilizing a grid-based view, a column picker, or both.
+   */
   public columnIds: string[] = [];
+  /**
+   * The IDs of the columns displayed for column-based views.
+   */
   public displayedColumnIds: string[] = [];
+  /**
+   * The ID of this view.
+   */
   public viewId: string;
+  /**
+   * An untyped property that tracks any view-specific state information
+   * that is relevant to a data manager but that existing properties do not cover.
+   */
   public additionalData: any;
 
   constructor(data: SkyDataViewStateOptions) {
@@ -13,6 +29,10 @@ export class SkyDataViewState {
     this.additionalData = data.additionalData;
   }
 
+  /**
+   * Returns the `SkyDataViewStateOptions` for the current view.
+   * @returns The `SkyDataViewStateOptions`.
+   */
   public getViewStateOptions(): SkyDataViewStateOptions {
     return {
       viewId: this.viewId,
