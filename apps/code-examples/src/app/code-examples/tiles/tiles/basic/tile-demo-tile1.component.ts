c2e4064f2882386784c761bf0d272584b2038387
Steve Brush <steve.brush@blackbaud.com>
2022-04-12 16:32:18 -0400

    docs(components/tiles): fix selectors for drag-and-drop functionality (#143)

--- apps/code-examples/src/app/code-examples/tiles/tiles/basic/tile-demo-tile1.component.ts
+++ apps/code-examples/src/app/code-examples/tiles/tiles/basic/tile-demo-tile1.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tile-demo-tile1',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'div.tile1',
   templateUrl: './tile-demo-tile1.component.html',
 })
 export class TileDemoTile1Component {
