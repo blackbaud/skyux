85341bbbb4de2c25acf72d32a7fea0e65526b871
Steve Brush <steve.brush@blackbaud.com>
2022-04-19 12:36:22 -0400

    fix(components/indicators): allow string assignments to `SkyAlertType`

--- libs/components/indicators/src/lib/modules/alert/alert-type.ts
+++ libs/components/indicators/src/lib/modules/alert/alert-type.ts
@@ -1,4 +1,10 @@
 /**
  * Specifies a style for the alert to determine the icon and background color.
  */
-export type SkyAlertType = 'danger' | 'info' | 'success' | 'warning';
+export type SkyAlertType =
+  | 'danger'
+  | 'info'
+  | 'success'
+  | 'warning'
+  // Add 'string' for backward compatibility.
+  | string;
