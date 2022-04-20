eb83db015854cc0a035abd82cd59dddd0b72c53b
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-04-20 13:18:29 -0400

    File was not added

--- libs/components/grids/src/lib/modules/grid/grid.module.ts
+++ libs/components/grids/src/lib/modules/grid/grid.module.ts
@@ -20,7 +20,7 @@ import { SkyGridColumnComponent } from './grid-column.component';
 import { SkyGridComponent } from './grid.component';
 
 /**
- * @deprecated Use `SkyAgGridModule` instead.
+ * @deprecated `SkyGridComponent` and its features are deprecated. We recommend using the data grid instead. For more information, see https://developer.blackbaud.com/skyux/components/data-grid
  */
 @NgModule({
   declarations: [
