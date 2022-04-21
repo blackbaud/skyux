da1c47f72ad06cc748b153ea13df84fe9fdc7b50
Steve Brush <steve.brush@blackbaud.com>
2022-04-21 15:39:53 -0400

    Remove async popover example

--- apps/code-examples/src/app/features/popovers.module.ts
+++ apps/code-examples/src/app/features/popovers.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PopoverDemoComponent as PopoverAsynchronousDemoComponent } from '../code-examples/popovers/popover/asynchronous/popover-demo.component';
-import { PopoverDemoModule as PopoverAsynchronousDemoModule } from '../code-examples/popovers/popover/asynchronous/popover-demo.module';
 import { PopoverDemoComponent as PopoverBasicDemoComponent } from '../code-examples/popovers/popover/basic/popover-demo.component';
 import { PopoverDemoModule as PopoverBasicDemoModule } from '../code-examples/popovers/popover/basic/popover-demo.module';
 import { PopoverDemoComponent as PopoverProgrammaticDemoComponent } from '../code-examples/popovers/popover/programmatic/popover-demo.component';
 import { PopoverDemoModule as PopoverProgrammaticDemoModule } from '../code-examples/popovers/popover/programmatic/popover-demo.module';
 
 const routes: Routes = [
-  {
-    path: 'popover/asynchronous',
-    component: PopoverAsynchronousDemoComponent,
-  },
   {
     path: 'popover/basic',
     component: PopoverBasicDemoComponent,
@@ -32,7 +26,6 @@ export class PopoversRoutingModule {}
 @NgModule({
   imports: [
     PopoversRoutingModule,
-    PopoverAsynchronousDemoModule,
     PopoverBasicDemoModule,
     PopoverProgrammaticDemoModule,
   ],
