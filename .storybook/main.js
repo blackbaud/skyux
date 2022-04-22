2a8a2c0457e629045702eee377fe3da39b7cc151
John White <john.white@blackbaud.me>
2022-04-22 14:37:19 -0400

    Add a11y addon. Add eslint configuration.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials'],
-  // uncomment the property below if you want to apply some webpack config globally
+  addons: ['@storybook/addon-a11y', '@storybook/addon-essentials'] // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
-
   //   // Return the altered config
   //   return config;
   // },
+  ,
+  core: {
+    builder: 'webpack5'
+  }
 };
