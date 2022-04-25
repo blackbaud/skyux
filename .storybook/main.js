5e9aa4b69e04eb12a19e10970dd248f8ba1c4019
John White <john.white@blackbaud.me>
2022-04-25 19:01:43 -0400

    Create storybook preview

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,21 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-essentials'] // uncomment the property below if you want to apply some webpack config globally
+  addons: [
+    '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false,
+      },
+    },
+  ],
+  // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
   //   // Return the altered config
   //   return config;
   // },
-  ,
   core: {
-    builder: 'webpack5'
-  }
+    builder: 'webpack5',
+  },
 };
---
+++ libs/components/storybook/README.md
@@ -0,0 +1,7 @@
+# sdk-storybook
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running lint
+
+Run `nx lint sdk-storybook` to execute the lint via [ESLint](https://eslint.org/).
