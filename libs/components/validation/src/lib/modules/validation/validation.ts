ac166e3bf088260a6d436796ee5e6719e96b63a5
Braden Bisping <bradenneilbisping@tutanota.com>
2022-05-02 13:58:55 -0500

    remove redundant default options

--- libs/components/validation/src/lib/modules/validation/validation.ts
+++ libs/components/validation/src/lib/modules/validation/validation.ts
@@ -21,27 +21,8 @@ export abstract class SkyValidation {
       urlValidationOptions
     );
     if (urlValidationOptions && urlValidationOptions.rulesetVersion === 2) {
-      /**
-       * these are the default options for Validator.js
-       */
-      const ourOptions = {
-        protocols: ['http', 'https', 'ftp'],
-        require_tld: true,
-        require_protocol: false,
-        require_host: true,
-        require_port: false,
-        require_valid_protocol: true,
-        allow_underscores: false,
-        host_whitelist: false,
-        host_blacklist: false,
-        allow_trailing_dot: false,
-        allow_protocol_relative_urls: false,
-        allow_fragments: true,
-        allow_query_components: true,
-        disallow_auth: false,
-        validate_length: true,
-      };
-      return isURL(url, ourOptions);
+      // we are using their default options
+      return isURL(url);
     } else {
       const regex = /^((http|https):\/\/)?([\w-]+\.)+[\w-]+/i;
       return regex.test(url);
