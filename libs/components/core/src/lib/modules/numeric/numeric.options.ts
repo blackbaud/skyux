1ca682353c08d64df610d93e1b05d3ed600bac60
Steve Brush <steve.brush@blackbaud.com>
2022-05-05 08:32:38 -0400

    remove duplicate JSDocs

--- libs/components/core/src/lib/modules/numeric/numeric.options.ts
+++ libs/components/core/src/lib/modules/numeric/numeric.options.ts
@@ -51,51 +51,19 @@ export interface SkyNumericOptions {
  * @deprecated Use the `SkyNumericOptions` interface instead.
  */
 export class NumericOptions implements SkyNumericOptions {
-  /**
-   * Specifies the maximum number of digits after the decimal separator.
-   * @default 1
-   */
   public digits?: number = 1;
 
-  /**
-   * Specifies how to format the number. Options are `currency` or `number`.
-   * @default "number"
-   */
   public format?: string = 'number';
 
-  /**
-   * Specifies the format of the currency.
-   * @default "standard"
-   */
   public currencySign?: 'accounting' | 'standard' = 'standard';
 
-  /**
-   * Specifies the ISO4217 currency code to use for currency formatting.
-   * @default "USD"
-   */
   public iso?: string = 'USD';
 
-  /**
-   * Specifies the locale code to use when formatting.
-   */
   public locale?: string;
 
-  /**
-   * Specifies the minimum number of digits after the decimal separator. This property only applies
-   * when the `truncate` property is set to `false`. If `digits` specifies a maximum number of
-   * digits, then `minDigits` must be less than that value.
-   */
   public minDigits?: number;
 
-  /**
-   * Indicates whether to shorten numbers to rounded numbers and abbreviation characters
-   * such as K for thousands, M for millions, B for billions, and T for trillions.
-   */
   public truncate?: boolean = true;
 
-  /**
-   * Specifies the minimum value at which numbers are shortened to rounded numbers and abbreviation characters. Values under `1000` are not truncated.
-   * @default 1000
-   */
   public truncateAfter?: number = 1000;
 }
