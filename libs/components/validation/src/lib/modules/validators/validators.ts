6d3a0dc7af8801aea103cd746a81b9f69f6cb389
Braden Bisping <bradenneilbisping@tutanota.com>
2022-05-09 15:33:31 -0500

    overload url function so parameters are optional

--- libs/components/validation/src/lib/modules/validators/validators.ts
+++ libs/components/validation/src/lib/modules/validators/validators.ts
@@ -1,5 +1,6 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
+import { SkyUrlValidationOptions } from '../url-validation/url-validation-options';
 import { SkyValidation } from '../validation/validation';
 
 // Need to add the following to classes which contain static methods.
@@ -32,15 +33,53 @@ export class SkyValidators {
    * immediately when users enter values.
    * @param control
    */
-  public static url(control: AbstractControl): ValidationErrors | null {
-    const value = control.value;
+  public static url(
+    abstractControl: AbstractControl
+  ): ValidationErrors | undefined;
 
-    if (!value) {
-      return undefined;
+  /**
+   * Validates URLs in reactive forms. Add this validator with ruleset parameters directly to
+   * the form control model in the component class. If users enter values that are not valid
+   * URLs, the validator throws an error. Since this is a sync validator, it returns a set of
+   * validation errors or `undefined` immediately when users enter values.
+   * @param control
+   */
+  public static url(
+    skyUrlValidationOptions: SkyUrlValidationOptions
+  ): ValidatorFn | undefined;
+
+  public static url(
+    value: AbstractControl | SkyUrlValidationOptions
+  ): ValidatorFn | ValidationErrors | undefined {
+    const typeTester = value as SkyUrlValidationOptions;
+    if (typeTester.rulesetVersion === undefined) {
+      // there are no SkyUrlValidationOptions passed in, so return ValidationErrors | undefined
+      const abstractControl = value as AbstractControl;
+      const abstractControlValue = abstractControl.value;
+      if (!abstractControlValue) {
+        return;
+      }
+      return SkyValidation.isUrl(abstractControlValue)
+        ? undefined
+        : { skyUrl: { invalid: abstractControlValue } };
+    } else {
+      // there are SkyUrlValidationOptions passed in, so return ValidatorFn
+      const skyUrlValidationOptions = value as SkyUrlValidationOptions;
+      return (
+        abstractControl: AbstractControl
+      ): ValidationErrors | undefined => {
+        const abstractControlValue = abstractControl.value;
+        if (!abstractControlValue) {
+          return;
+        }
+
+        return SkyValidation.isUrl(
+          abstractControl.value,
+          skyUrlValidationOptions
+        )
+          ? undefined
+          : { skyUrl: { invalid: abstractControl.value } };
+      };
     }
-
-    return SkyValidation.isUrl(value)
-      ? undefined
-      : { skyUrl: { invalid: value } };
   }
 }
