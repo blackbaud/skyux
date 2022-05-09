881512a5bb83cc1eb717b9ab60b5fd7c8c3f9ed1
Braden Bisping <bradenneilbisping@tutanota.com>
2022-05-09 09:53:42 -0500

    add unit tests for non-onceability & general functionality

--- libs/components/validation/src/lib/modules/url-validation/url-validation.directive.spec.ts
+++ libs/components/validation/src/lib/modules/url-validation/url-validation.directive.spec.ts
@@ -283,3 +283,111 @@ describe('URL validation - ruleset v2', () => {
     });
   }));
 });
+describe('URL validation - non-onceability', () => {
+  function setInput(
+    element: HTMLElement,
+    text: string,
+    compFixture: ComponentFixture<any>
+  ) {
+    const inputEvent = document.createEvent('Event');
+    const params = {
+      bubbles: false,
+      cancelable: false,
+    };
+
+    inputEvent.initEvent('input', params.bubbles, params.cancelable);
+
+    const changeEvent = document.createEvent('Event');
+    changeEvent.initEvent('change', params.bubbles, params.cancelable);
+    const inputEl = element.querySelector('input');
+    inputEl.value = text;
+
+    inputEl.dispatchEvent(inputEvent);
+    compFixture.detectChanges();
+
+    inputEl.dispatchEvent(changeEvent);
+    compFixture.detectChanges();
+  }
+
+  let component: UrlValidationRulesetTestComponent;
+  let fixture: ComponentFixture<UrlValidationRulesetTestComponent>;
+  let ngModel: NgModel;
+  let nativeElement: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [SkyUrlValidationFixturesModule, FormsModule],
+    });
+    fixture = TestBed.createComponent(UrlValidationRulesetTestComponent);
+    nativeElement = fixture.nativeElement as HTMLElement;
+    const input = fixture.debugElement.query(By.css('input'));
+    ngModel = input.injector.get(NgModel);
+    component = fixture.componentInstance;
+  });
+
+  it('should change validation rules asynchronously/reactively', fakeAsync(() => {
+    component.skyUrlValidationOptions = {
+      rulesetVersion: 1,
+    };
+    fixture.detectChanges();
+    tick();
+    setInput(nativeElement, 'sub.domain,com/pagename', fixture);
+    fixture.detectChanges();
+
+    expect(nativeElement.querySelector('input').value).toBe(
+      'sub.domain,com/pagename'
+    );
+
+    expect(ngModel.control.valid).toBe(true);
+    expect(ngModel.control.pristine).toBe(false);
+    expect(ngModel.control.touched).toBe(false);
+
+    component.skyUrlValidationOptions = {
+      rulesetVersion: 2,
+    };
+    fixture.detectChanges();
+    tick();
+
+    expect(ngModel.control.valid).toBe(false);
+    expect(ngModel.control.pristine).toBe(false);
+    expect(ngModel.control.touched).toBe(false);
+  }));
+
+  it('should validate incorrect input using ruleset version 2', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
+    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    fixture.detectChanges();
+
+    expect(nativeElement.querySelector('input').value).toBe(
+      '[]awefhawenfc0293ejwf]'
+    );
+
+    expect(ngModel.control.valid).toBe(false);
+    expect(ngModel.control.pristine).toBe(false);
+    expect(ngModel.control.touched).toBe(false);
+  }));
+
+  it('should handle invalid and then valid input using ruleset version 2', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
+    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    setInput(nativeElement, 'blackbaud.com', fixture);
+
+    expect(nativeElement.querySelector('input').value).toBe('blackbaud.com');
+    expect(component.urlValidator).toEqual('blackbaud.com');
+
+    expect(ngModel.control.valid).toBe(true);
+    expect(ngModel.control.pristine).toBe(false);
+    expect(ngModel.control.touched).toBe(false);
+  }));
+
+  it('should pass accessibility using ruleset version 2', async(() => {
+    fixture.detectChanges();
+    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(fixture.nativeElement).toBeAccessible();
+    });
+  }));
+});
