c655425c7b37d9a9f0c2ef19f9734605937ee6da
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-05-10 13:39:47 -0400

    fix(sdk/prettier-schematics): include .angular/cache and ssets in Prettier's ignore file

--- libs/sdk/prettier-schematics/src/schematics/ng-add/rules/write-prettier-ignore.ts
+++ libs/sdk/prettier-schematics/src/schematics/ng-add/rules/write-prettier-ignore.ts
@@ -20,6 +20,12 @@ dist
 node_modules
 package-lock.json
 
+# Ignore: assets
+assets
+
+# Ignore Angular cache
+.angular/cache
+
 # Don't format the following since the order of its import statements is deliberate.
 test.ts`
     );
