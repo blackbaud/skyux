26c7deb768d1cdf9db3b47692e48e70765dc1876
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-05-11 16:48:38 -0400

    fix(components/tabs): use padding instead of margin when styling sectioned form content sections (#238)

--- apps/playground/src/app/components/tabs/sectioned-form/sectioned-form.module.ts
+++ apps/playground/src/app/components/tabs/sectioned-form/sectioned-form.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SkyDateRangePickerModule } from '@skyux/datetime';
 import { SkyCheckboxModule } from '@skyux/forms';
 import { SkyModalModule } from '@skyux/modals';
 import { SkySectionedFormModule } from '@skyux/tabs';
 
 import { SectionedFormAddressFormDemoComponent } from './sectioned-form-address-form-demo.component';
+import { SectionedFormDateFormDemoComponent } from './sectioned-form-date-form-demo.component';
 import { SectionedFormInformationFormDemoComponent } from './sectioned-form-information-form-demo.component';
 import { SectionedFormModalComponent } from './sectioned-form-modal.component';
 import { SectionedFormPhoneFormDemoComponent } from './sectioned-form-phone-form-demo.component';
@@ -16,6 +18,7 @@ import { SectionedFormComponent } from './sectioned-form.component';
   declarations: [
     SectionedFormComponent,
     SectionedFormAddressFormDemoComponent,
+    SectionedFormDateFormDemoComponent,
     SectionedFormInformationFormDemoComponent,
     SectionedFormModalComponent,
     SectionedFormPhoneFormDemoComponent,
@@ -24,6 +27,7 @@ import { SectionedFormComponent } from './sectioned-form.component';
     CommonModule,
     FormsModule,
     SkyCheckboxModule,
+    SkyDateRangePickerModule,
     SkyModalModule,
     SkySectionedFormModule,
     SectionedFormRoutingModule,
