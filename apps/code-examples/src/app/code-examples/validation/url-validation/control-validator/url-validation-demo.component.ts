17283517b23333381382779c4d62ba5ce78e3387
Braden Bisping <bradenneilbisping@tutanota.com>
2022-05-11 15:40:33 -0500

    make URL validation code examples toggeable

--- apps/code-examples/src/app/code-examples/validation/url-validation/control-validator/url-validation-demo.component.ts
+++ apps/code-examples/src/app/code-examples/validation/url-validation/control-validator/url-validation-demo.component.ts
@@ -6,7 +6,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { SkyValidators } from '@skyux/validation';
+import { SkyUrlValidationOptions, SkyValidators } from '@skyux/validation';
 
 @Component({
   selector: 'app-url-validation-demo',
@@ -21,9 +21,28 @@ export class UrlValidationDemoComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) {}
 
+  public skyUrlValidationOptions: SkyUrlValidationOptions = {
+    rulesetVersion: 1,
+  };
+
   public ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       url: new FormControl(undefined, [Validators.required, SkyValidators.url]),
     });
+  }
+
+  public toggleRuleset(): void {
+    if (this.skyUrlValidationOptions.rulesetVersion === 1) {
+      this.skyUrlValidationOptions.rulesetVersion = 2;
+    } else if (this.skyUrlValidationOptions.rulesetVersion === 2) {
+      this.skyUrlValidationOptions.rulesetVersion = 1;
+    }
+    const urlFormControl = this.formGroup.get('url');
+    urlFormControl.clearValidators();
+    urlFormControl.addValidators([
+      Validators.required,
+      SkyValidators.url(this.skyUrlValidationOptions),
+    ]);
+    this.formGroup.get('url').updateValueAndValidity();
   }
 }
--- apps/code-examples/src/app/code-examples/validation/url-validation/directive/url-validation-demo.component.ts
+++ apps/code-examples/src/app/code-examples/validation/url-validation/directive/url-validation-demo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SkyUrlValidationOptions } from '@skyux/validation';
 
 @Component({
   selector: 'app-url-validation-demo',
@@ -8,4 +9,20 @@ export class UrlValidationDemoComponent {
   public demoModel: {
     url?: string;
   } = {};
+
+  public skyUrlValidationOptions: SkyUrlValidationOptions = {
+    rulesetVersion: 1,
+  };
+
+  public toggleRuleset(): void {
+    if (this.skyUrlValidationOptions.rulesetVersion === 1) {
+      this.skyUrlValidationOptions = {
+        rulesetVersion: 2,
+      };
+    } else if (this.skyUrlValidationOptions.rulesetVersion === 2) {
+      this.skyUrlValidationOptions = {
+        rulesetVersion: 1,
+      };
+    }
+  }
 }
