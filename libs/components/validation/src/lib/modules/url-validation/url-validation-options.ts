be5746df7b6dd69968b0109872fae81f6997bdda
Braden Bisping <bradenneilbisping@tutanota.com>
2022-05-19 09:14:03 -0500

    describe each ruleset in JSDocs

--- libs/components/validation/src/lib/modules/url-validation/url-validation-options.ts
+++ libs/components/validation/src/lib/modules/url-validation/url-validation-options.ts
@@ -4,6 +4,10 @@
 export interface SkyUrlValidationOptions {
   /**
    * Indicates which ruleset version of URL validation to use.
+   *
+   * Ruleset v1 (the default) uses a simple, custom regex.
+   *
+   * Ruleset v2 uses the stricter, 3rd party [Validator.js library](https://github.com/validatorjs/validator.js/).
    */
   rulesetVersion: 1 | 2;
 }
