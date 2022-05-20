d2d86aafb39e42ec1aa81ab3fce19658c6c34c6f
John White <john.white@blackbaud.me>
2022-05-20 00:11:52 -0400

    Fix build

--- libs/components/indicators/.storybook/preview.ts
+++ libs/components/indicators/.storybook/preview.ts
@@ -1,1 +1,18 @@
-export * from '../../../../.storybook/preview';
+import { CommonModule } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { PreviewWrapperModule } from '@skyux/storybook';
+import { SkyThemeService } from '@skyux/theme';
+import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
+
+export const parameters = {};
+
+export const decorators = [
+  moduleMetadata({
+    imports: [CommonModule, NoopAnimationsModule, PreviewWrapperModule],
+    providers: [SkyThemeService],
+  }),
+  componentWrapperDecorator(
+    (story) => `<skyux-preview-wrapper>${story}</skyux-preview-wrapper>`,
+    ({ globals }) => ({ theme: globals.theme })
+  ),
+];
