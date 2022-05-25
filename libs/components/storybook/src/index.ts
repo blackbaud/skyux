64b0f7fa5ee3c0892c11e46b7e485547a807eac3
John White <john.white@blackbaud.me>
2022-05-25 14:27:34 -0400

    Switch to Storybook Composition

--- libs/components/storybook/src/index.ts
+++ libs/components/storybook/src/index.ts
@@ -4,3 +4,5 @@ export * from './lib/preview-wrapper/preview-wrapper-global-types';
 export * from './lib/preview-wrapper/preview-wrapper-parameters';
 export * from './lib/preview-wrapper/preview-wrapper.component';
 export * from './lib/preview-wrapper/preview-wrapper.module';
+
+export * from './lib/welcome/welcome.component';
---
+++ tools/generators/storybook-composition/index.ts
@@ -0,0 +1,48 @@
+import {
+  Tree,
+  formatFiles,
+  generateFiles,
+  getProjects,
+  joinPathFragments,
+  logger,
+} from '@nrwl/devkit';
+
+import { relative } from 'path';
+
+type Options = {
+  projects: string;
+  baseUrl: string;
+};
+
+export default async function (tree: Tree, schema: Options) {
+  const allProjects = getProjects(tree);
+  const storybookProject = allProjects.get('storybook');
+  const storybookProjectRoot = storybookProject.root;
+  const relativeToRoot = relative(`/${storybookProjectRoot}/.storybook`, `/`);
+
+  const projects = schema.projects
+    .split(',')
+    .map((project) => project.trim())
+    .filter((project) => project)
+    .filter((project) => {
+      const target = allProjects.get(project).targets['build-storybook'];
+      return target && target.executor === '@nrwl/storybook:build';
+    });
+
+  if (projects.length === 0) {
+    logger.fatal(`None of these projects have a Storybook target.`);
+    return;
+  }
+
+  generateFiles(
+    tree,
+    joinPathFragments(__dirname, './files'),
+    'libs/components/storybook/.storybook',
+    {
+      projects,
+      baseUrl: schema.baseUrl.replace(/\/$/, ''),
+      relativeToRoot,
+    }
+  );
+  return formatFiles(tree);
+}
