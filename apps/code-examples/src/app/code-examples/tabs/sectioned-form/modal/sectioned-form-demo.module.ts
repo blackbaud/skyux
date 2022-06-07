54d884a39b051d2ccf9d8544ce3d74696a2abe52
John White <john.white@blackbaud.me>
2022-06-06 22:05:48 -0400

    Remove unnecessary import

--- apps/code-examples/src/app/code-examples/tabs/sectioned-form/modal/sectioned-form-demo.module.ts
+++ apps/code-examples/src/app/code-examples/tabs/sectioned-form/modal/sectioned-form-demo.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { SkyIdModule } from '@skyux/core';
 import { SkyInputBoxModule } from '@skyux/forms';
 import { SkyModalModule } from '@skyux/modals';
