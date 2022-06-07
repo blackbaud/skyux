3bf13ad57a2f63e386428298cde7cb61c3de1e8c
Steve Brush <steve.brush@blackbaud.com>
2022-06-07 13:38:05 -0400

    fix(components/packages): fix the `ng add` schematic to install essential SKY UX packages (#279)

--- libs/components/packages/src/schematics/ng-add/ng-add.schematic.ts
+++ libs/components/packages/src/schematics/ng-add/ng-add.schematic.ts
@@ -1,70 +1,51 @@
-import { Rule, SchematicContext, chain } from '@angular-devkit/schematics';
+import { Rule, chain } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+  NodeDependencyType,
+  addPackageJsonDependency,
+} from '@schematics/angular/utility/dependencies';
 
 import fs from 'fs-extra';
-import getLatestVersion from 'latest-version';
 import path from 'path';
 
 import { addCrossventFix } from '../rules/add-crossvent-fix';
 import { applySkyuxStylesheetsToWorkspace } from '../rules/apply-skyux-stylesheets-to-workspace';
 import { installAngularCdk } from '../rules/install-angular-cdk';
-import { readRequiredFile } from '../utility/tree';
 import { getWorkspace } from '../utility/workspace';
 
-async function ensureLatestVersions(
-  context: SchematicContext,
-  dependencies: {
-    [_: string]: string;
-  },
-  packageGroup: {
-    [_: string]: string;
-  }
-): Promise<void> {
-  for (const packageName in dependencies) {
-    const version = packageGroup[packageName];
+function installEssentialSkyUxPackages(skyuxVersion: string): Rule {
+  return async (tree) => {
+    const packageNames = [
+      '@skyux/assets',
+      '@skyux/core',
+      '@skyux/i18n',
+      '@skyux/theme',
+    ];
 
-    if (version) {
-      // Get the latest version from NPM.
-      const latestVersion = await getLatestVersion(packageName, { version });
-
-      if (latestVersion !== dependencies[packageName]) {
-        context.logger.info(
-          `Updated "${packageName}" from version (${dependencies[packageName]}) to (${latestVersion}).`
-        );
-
-        dependencies[packageName] = latestVersion;
-      } else {
-        context.logger.info(
-          `Skipped "${packageName}" because it is already on the latest version.`
-        );
-      }
+    for (const packageName of packageNames) {
+      addPackageJsonDependency(tree, {
+        type: NodeDependencyType.Default,
+        name: packageName,
+        version: `^${skyuxVersion}`,
+        overwrite: true,
+      });
     }
-  }
+  };
 }
 
 export default function ngAdd(): Rule {
   return async (tree, context) => {
     const { workspace } = await getWorkspace(tree);
 
-    const packageJsonPath = 'package.json';
-    const packageJson = JSON.parse(readRequiredFile(tree, packageJsonPath));
-
-    const { packageGroup } = fs.readJsonSync(
-      path.resolve(__dirname, '../../../package.json')
-    )['ng-update'];
-
-    await ensureLatestVersions(context, packageJson.dependencies, packageGroup);
-    await ensureLatestVersions(
-      context,
-      packageJson.devDependencies,
-      packageGroup
+    // Get the currently installed version of SKY UX.
+    const { version: skyuxVersion } = fs.readJsonSync(
+      path.resolve(__dirname, '../../../../../../package.json')
     );
-
-    tree.overwrite(packageJsonPath, JSON.stringify(packageJson, undefined, 2));
 
     context.addTask(new NodePackageInstallTask());
 
     return chain([
+      installEssentialSkyUxPackages(skyuxVersion),
       installAngularCdk(),
       addCrossventFix(workspace),
       applySkyuxStylesheetsToWorkspace(),
