1d643eb12ded868afffa1f91dca95355f6858ee2
Braden Bisping <bradennbisping@gmail.com>
2022-06-29 14:47:40 -0500

    add key-info to code-examples SPA

--- apps/code-examples/src/app/features/indicators.module.ts
+++ apps/code-examples/src/app/features/indicators.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AlertDemoComponent as AlertBasicDemoComponent } from '../code-examples/indicators/alert/basic/alert-demo.component';
 import { AlertDemoModule as AlertBasicDemoModule } from '../code-examples/indicators/alert/basic/alert-demo.module';
+import { KeyInfoDemoComponent } from '../code-examples/indicators/key-info/basic/key-info-demo.component';
+import { KeyInfoDemoModule } from '../code-examples/indicators/key-info/basic/key-info-demo.module';
 import { StatusIndicatorDemoComponent } from '../code-examples/indicators/status-indicator/basic/status-indicator-demo.component';
 import { StatusIndicatorDemoModule } from '../code-examples/indicators/status-indicator/basic/status-indicator-demo.module';
 
@@ -14,6 +16,10 @@ const routes: Routes = [
   {
     path: 'status-indicator/basic',
     component: StatusIndicatorDemoComponent,
+  },
+  {
+    path: 'key-info/basic',
+    component: KeyInfoDemoComponent,
   },
 ];
 
@@ -28,6 +34,7 @@ export class IndicatorsFeatureRoutingModule {}
     IndicatorsFeatureRoutingModule,
     AlertBasicDemoModule,
     StatusIndicatorDemoModule,
+    KeyInfoDemoModule,
   ],
 })
 export class IndicatorsFeatureModule {}
