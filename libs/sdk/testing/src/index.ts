3791e2fc47f290223158cdc9c0b374025521bfd6
jason.bodnar@blackbaud.com <jason.bodnar@blackbaud.com>
2022-06-30 17:30:01 -0500

    Add SkyBy.skyId() to make it easier to match data-sky-id

--- libs/sdk/testing/src/index.ts
+++ libs/sdk/testing/src/index.ts
@@ -2,5 +2,7 @@ export * from './lib/a11y/a11y-analyzer';
 export * from './lib/a11y/a11y-analyzer-config';
 export * from './lib/matchers/matchers';
 export * from './lib/matchers/to-be-visible-options';
+export * from './lib/query-predicates/sky-by';
 export * from './lib/test-utility/test-utility';
 export * from './lib/test-utility/test-utility-dom-event-options';
+
