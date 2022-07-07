51be1722d625141a1441c547f38abedfb55a0601
Steve Brush <steve.brush@blackbaud.com>
2022-07-07 11:08:16 -0400

    chore(sdk/testing): revert "add `SkyBy.skyId()` to make it easier to match data-sky-id" (#330)

--- libs/sdk/testing/src/lib/query-predicates/sky-by.ts
+++
@@ -1,8 +0,0 @@
-import { DebugElement, Predicate } from '@angular/core';
-import { By } from '@angular/platform-browser';
-
-export class SkyBy {
-  static skyId(skyId: string): Predicate<DebugElement> {
-    return By.css(`[data-sky-id="${skyId}"`);
-  }
-}
