abb1617d00534d40c6d7579c223dcbb90d3bc52e
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-07-08 13:40:18 -0400

    deprecation(components/layout): `SkyErrorModalService` is deprecated; use a standard modal with an error component instead (#328)

--- libs/components/errors/src/lib/modules/error/error-modal.service.spec.ts
+++ libs/components/errors/src/lib/modules/error/error-modal.service.spec.ts
@@ -1,3 +1,5 @@
+import { TestBed } from '@angular/core/testing';
+import { SkyLogService } from '@skyux/core';
 import { SkyModalConfigurationInterface, SkyModalService } from '@skyux/modals';
 
 import { ErrorModalConfig } from './error-modal-config';
@@ -6,7 +8,7 @@ import { SkyErrorModalService } from './error-modal.service';
 import { MockModalService } from './fixtures/mocks';
 
 describe('Error modal service', () => {
-  it('should open with correct parameters', () => {
+  it('should open with correct parameters (log service undefined)', () => {
     const modalService = new MockModalService({
       removeComponent: function (): any {},
       addComponent: function (ref: any): any {},
@@ -20,12 +22,67 @@ describe('Error modal service', () => {
 
     const expectedProviders = [{ provide: ErrorModalConfig, useValue: config }];
 
+    const logServiceSpy = spyOn(
+      SkyLogService.prototype,
+      'deprecated'
+    ).and.stub();
+
     const service = new SkyErrorModalService(modalService as SkyModalService);
     service.open(config);
 
     expect(modalService.openCalls.length).toBe(1);
     expect(modalService.openCalls[0].component).toBe(
       SkyErrorModalFormComponent
+    );
+
+    // Since the log service was not provided the deprecation call should be skipped.
+    expect(logServiceSpy).not.toHaveBeenCalled();
+
+    // Uses the modalService Open overload that takes config
+    // instead of providers (despite the property name)
+    const modalConfig = modalService.openCalls[0]
+      .providers as SkyModalConfigurationInterface;
+    expect(modalConfig.ariaRole).toBe('alertdialog');
+    expect(modalConfig.providers).toEqual(expectedProviders);
+  });
+
+  it('should open with correct parameters (log service defined)', () => {
+    const modalService = new MockModalService({
+      removeComponent: function (): any {},
+      addComponent: function (ref: any): any {},
+    } as any);
+
+    const config: ErrorModalConfig = {
+      errorTitle: 'Error title',
+      errorDescription: 'Description of error',
+      errorCloseText: 'Close button text',
+    };
+
+    const expectedProviders = [{ provide: ErrorModalConfig, useValue: config }];
+
+    const logServiceSpy = spyOn(
+      SkyLogService.prototype,
+      'deprecated'
+    ).and.stub();
+
+    const service = new SkyErrorModalService(
+      modalService as SkyModalService,
+      TestBed.inject(SkyLogService)
+    );
+    service.open(config);
+
+    expect(modalService.openCalls.length).toBe(1);
+    expect(modalService.openCalls[0].component).toBe(
+      SkyErrorModalFormComponent
+    );
+
+    expect(logServiceSpy).toHaveBeenCalledWith(
+      "SkyErrorModalService's open method",
+      {
+        deprecationMajorVersion: 6,
+        replacementRecommendation:
+          'We recommend using a standard modal with an error component instead.',
+      }
     );
 
     // Uses the modalService Open overload that takes config
