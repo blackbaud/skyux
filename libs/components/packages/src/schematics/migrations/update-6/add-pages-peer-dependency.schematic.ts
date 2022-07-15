fa6501e02062119cb6e3d1883f939c58f84f1f3b
John White <john.white@blackbaud.me>
2022-07-15 12:25:26 -0400

    Change schematic to not require @nrwl/workspace

--- libs/components/packages/src/schematics/migrations/update-6/add-pages-peer-dependency.schematic.ts
+++ libs/components/packages/src/schematics/migrations/update-6/add-pages-peer-dependency.schematic.ts
@@ -1,13 +1,35 @@
 import { Rule } from '@angular-devkit/schematics';
-import { addDepsToPackageJson } from '@nrwl/workspace';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+  NodeDependencyType,
+  addPackageJsonDependency,
+} from '@schematics/angular/utility/dependencies';
 
 import fs from 'fs-extra';
 import path from 'path';
+import { JsonFile } from '../../utility/json-file';
 
 export default function addPagesPeerDependency(): Rule {
-  // Get the currently installed version of SKY UX.
-  const { version: skyuxVersion } = fs.readJsonSync(
-    path.resolve(__dirname, '../../../../package.json')
-  );
-  return addDepsToPackageJson({ '@skyux/modals': skyuxVersion }, {});
+  return async (tree, context) => {
+    const packageJson = new JsonFile(tree, 'package.json');
+    const dependencies = packageJson.get(['dependencies']) || {};
+    if ('@skyux/modals' in dependencies) {
+      return;
+    }
+    const devDependencies = packageJson.get(['devDependencies']) || {};
+    if ('@skyux/modals' in devDependencies) {
+      return;
+    }
+    // Get the currently installed version of SKY UX.
+    const { version: skyuxVersion } = fs.readJsonSync(
+      path.resolve(__dirname, '../../../../package.json')
+    );
+    addPackageJsonDependency(tree, {
+      type: NodeDependencyType.Default,
+      name: '@skyux/modals',
+      version: `^${skyuxVersion}`,
+      overwrite: true,
+    });
+    context.addTask(new NodePackageInstallTask());
+  }
 }
