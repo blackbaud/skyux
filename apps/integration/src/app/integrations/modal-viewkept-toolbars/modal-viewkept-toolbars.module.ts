826a78e72df3b110345c004f5c4cde074f38fd4a
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-07-22 14:16:28 -0400

    Integration tests and default fix

--- apps/integration/src/app/integrations/modal-viewkept-toolbars/modal-viewkept-toolbars.module.ts
+++ apps/integration/src/app/integrations/modal-viewkept-toolbars/modal-viewkept-toolbars.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SkyViewkeeperModule } from '@skyux/core';
 import { SkyCheckboxModule } from '@skyux/forms';
+import { SkyIconModule } from '@skyux/indicators';
 import { SkyToolbarModule } from '@skyux/layout';
 import { SkySearchModule } from '@skyux/lookup';
 import { SkyModalModule } from '@skyux/modals';
+import { SkyThemeModule } from '@skyux/theme';
 
 import { ModalViewkeptToolbarsModalComponent } from './modal-viewkept-toolbars-modal.component';
 import { ModalViewkeptToolbarsRoutingModule } from './modal-viewkept-toolbars-routing-module';
@@ -18,8 +20,10 @@ import { ModalViewkeptToolbarsComponent } from './modal-viewkept-toolbars.compon
   imports: [
     CommonModule,
     SkyCheckboxModule,
+    SkyIconModule,
     SkyModalModule,
     SkySearchModule,
+    SkyThemeModule,
     SkyToolbarModule,
     SkyViewkeeperModule,
     ModalViewkeptToolbarsRoutingModule,
