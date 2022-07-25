49d2d3e2c8482eaf5f07e65a54c823f415c5ea02
John White <750350+johnhwhite@users.noreply.github.com>
2022-07-25 10:17:13 -0400

    feat(components/forms): add inline help support for checkbox component (#354)
    * feat(components/forms): add inline help support for checkbox component
    
    * Update tests to match trimmed values
    
    * Add code example

---
+++ apps/playground/src/app/components/forms/checkbox/checkbox-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CheckboxComponent } from './checkbox.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CheckboxComponent,
+    data: {
+      name: 'Checkbox',
+      icon: 'check-square-o',
+      library: 'forms',
+    },
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CheckboxRoutingModule {
+  public static routes = routes;
+}
--- libs/components/forms/src/lib/modules/checkbox/checkbox-label.component.ts
+++ libs/components/forms/src/lib/modules/checkbox/checkbox-label.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sky-checkbox-label',
   templateUrl: './checkbox-label.component.html',
+  styleUrls: ['./checkbox-label.component.scss'],
 })
 export class SkyCheckboxLabelComponent {}
