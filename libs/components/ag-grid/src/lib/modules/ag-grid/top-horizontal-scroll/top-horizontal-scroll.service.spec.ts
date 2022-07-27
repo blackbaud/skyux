ef5ff96bcb8254c034e65109eccb5447b1703b02
Mukul Sharma <mukul.sharma@blackbaud.me>
2022-07-27 17:51:30 +0530

    tests refactor

--- libs/components/ag-grid/src/lib/modules/ag-grid/top-horizontal-scroll/top-horizontal-scroll.service.spec.ts
+++ libs/components/ag-grid/src/lib/modules/ag-grid/top-horizontal-scroll/top-horizontal-scroll.service.spec.ts
@@ -2,6 +2,8 @@ import { ElementRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SkyDataManagerService } from '@skyux/data-manager';
 
+import { MutationObserverService } from 'libs/components/core/src/lib/modules/mutation/mutation-observer-service';
+
 import { SkyAgGridDataManagerFixtureComponent } from '../fixtures/ag-grid-data-manager.component.fixture';
 import { SkyAgGridFixtureModule } from '../fixtures/ag-grid.module.fixture';
 
@@ -10,6 +12,7 @@ import { TopHorizontalScrollService } from './top-horizontal-scroll.service';
 describe('topHorizontalScrollService', () => {
   let topHorizontalScrollService: TopHorizontalScrollService;
   let agGridDataManagerFixtureComponent: ComponentFixture<SkyAgGridDataManagerFixtureComponent>;
+  let mutationObserverService: MutationObserverService;
 
   let elementRef: ElementRef;
 
@@ -19,18 +22,26 @@ describe('topHorizontalScrollService', () => {
       providers: [SkyDataManagerService, TopHorizontalScrollService],
     });
 
+    mutationObserverService = TestBed.inject(MutationObserverService);
+
+    spyOn(mutationObserverService, 'create').and.callFake(
+      (cb): MutationObserver => {
+        cb();
+        return new MutationObserver(cb);
+      }
+    );
+
     agGridDataManagerFixtureComponent = TestBed.createComponent(
       SkyAgGridDataManagerFixtureComponent
     );
     topHorizontalScrollService = TestBed.inject(TopHorizontalScrollService);
-
     agGridDataManagerFixtureComponent.detectChanges();
+    elementRef = agGridDataManagerFixtureComponent.elementRef;
+    topHorizontalScrollService.appendTopScrollbarToGrid(elementRef);
   });
 
   describe('appendTopScrollbarToGrid', () => {
     it('should add a scroll at top', () => {
-      elementRef = agGridDataManagerFixtureComponent.elementRef;
-      topHorizontalScrollService.appendTopScrollbarToGrid(elementRef);
       const scrollElement = elementRef.nativeElement.querySelectorAll(
         '.ag-body-horizontal-scroll'
       );
@@ -39,8 +50,6 @@ describe('topHorizontalScrollService', () => {
     });
 
     it('should listen to bottom scroll event', () => {
-      elementRef = agGridDataManagerFixtureComponent.elementRef;
-      topHorizontalScrollService.appendTopScrollbarToGrid(elementRef);
       const cloneViewportElement: HTMLElement =
         elementRef.nativeElement.querySelectorAll(
           '.ag-body-horizontal-scroll-viewport'
@@ -57,8 +66,6 @@ describe('topHorizontalScrollService', () => {
     });
 
     it('should listen to top scroll event', () => {
-      elementRef = agGridDataManagerFixtureComponent.elementRef;
-      topHorizontalScrollService.appendTopScrollbarToGrid(elementRef);
       const scrollViewportElement: HTMLElement =
         elementRef.nativeElement.querySelectorAll(
           '.ag-body-horizontal-scroll-viewport'
