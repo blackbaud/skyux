52dfc70ef71de3079a5f436973a63ba54860e83c
John White <750350+johnhwhite@users.noreply.github.com>
2022-08-01 16:38:02 -0400

    feat(components/forms): add inline help support for toggle switch component (#361)

--- apps/code-examples/src/app/features/forms.module.ts
+++ apps/code-examples/src/app/features/forms.module.ts
@@ -7,6 +7,8 @@ import { CheckboxDemoComponent as InlineHelpCheckboxDemoComponent } from '../cod
 import { CheckboxDemoModule as InlineHelpCheckboxDemoModule } from '../code-examples/forms/checkbox/inline-help/checkbox-demo.module';
 import { RadioDemoComponent as InlineHelpRadioDemoComponent } from '../code-examples/forms/radio/inline-help/radio-demo.component';
 import { RadioDemoModule as InlineHelpRadioDemoModule } from '../code-examples/forms/radio/inline-help/radio-demo.module';
+import { ToggleSwitchDemoComponent as InlineHelpToggleSwitchComponent } from '../code-examples/forms/toggle-switch/inline-help/toggle-switch-demo.component';
+import { SkyToggleSwitchDemoModule as InlineHelpToggleSwitchModule } from '../code-examples/forms/toggle-switch/inline-help/toggle-switch-demo.module';
 
 const routes: Routes = [
   {
@@ -20,6 +22,10 @@ const routes: Routes = [
   {
     path: 'checkbox/inline-help',
     component: InlineHelpCheckboxDemoComponent,
+  },
+  {
+    path: 'toggle-switch/inline-help',
+    component: InlineHelpToggleSwitchComponent,
   },
 ];
 
@@ -35,6 +41,7 @@ export class FormsRoutingModule {}
     FormsRoutingModule,
     InlineHelpCheckboxDemoModule,
     InlineHelpRadioDemoModule,
+    InlineHelpToggleSwitchModule,
   ],
 })
 export class FormsModule {}
