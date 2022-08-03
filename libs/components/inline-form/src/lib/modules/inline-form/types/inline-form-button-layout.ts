7e8402464b7dc7c6398b3b94af005d0ec1861d28
Braden Bisping <bradennbisping@gmail.com>
2022-08-03 15:08:21 -0500

    add descriptions to SkyInlineFormButtonLayout enums

--- libs/components/inline-form/src/lib/modules/inline-form/types/inline-form-button-layout.ts
+++ libs/components/inline-form/src/lib/modules/inline-form/types/inline-form-button-layout.ts
@@ -1,7 +1,22 @@
 export enum SkyInlineFormButtonLayout {
+  /**
+   * Displays custom buttons.
+   */
   Custom = 0,
+  /**
+   * Displays Done and Cancel buttons.
+   */
   DoneCancel = 1,
+  /**
+   * Displays Done, Delete, and Cancel buttons.
+   */
   DoneDeleteCancel = 2,
+  /**
+   * Displays Save and Cancel buttons.
+   */
   SaveCancel = 3,
+  /**
+   * Displays Save, Delete, and Cancel buttons.
+   */
   SaveDeleteCancel = 4,
 }
