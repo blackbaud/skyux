be79c1936cb4837073dafdb2987eaaf27adc33af
Erika McVey <erika.mcvey@blackbaud.com>
2022-08-11 10:26:08 -0400

    correctly show input error messages

--- apps/code-examples/src/app/code-examples/tabs/wizard/basic/wizard-demo-modal.component.ts
+++ apps/code-examples/src/app/code-examples/tabs/wizard/basic/wizard-demo-modal.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { SkyModalInstance } from '@skyux/modals';
 
 @Component({
@@ -28,11 +33,11 @@ export class WizardDemoModalComponent implements OnInit {
   public mailingList: FormControl;
 
   public ngOnInit(): void {
-    this.firstName = new FormControl();
+    this.firstName = new FormControl('', Validators.required);
     this.middleName = new FormControl();
-    this.lastName = new FormControl();
-    this.phoneNumber = new FormControl();
-    this.email = new FormControl();
+    this.lastName = new FormControl('', Validators.required);
+    this.phoneNumber = new FormControl('', Validators.required);
+    this.email = new FormControl('', Validators.required);
     this.termsAccepted = new FormControl(false);
     this.mailingList = new FormControl(false);
 
