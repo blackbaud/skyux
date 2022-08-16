5c12fe9b19a9664ea6f2d75466c3cb934fbc0efb
Erika McVey <erika.mcvey@blackbaud.com>
2022-08-16 14:50:21 -0400

    label visual tests

---
+++ apps/indicators-storybook/src/app/label/label.component.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, Story, moduleMetadata } from '@storybook/angular';
+
+import { LabelComponent } from './label.component';
+import { LabelModule } from './label.module';
+
+export default {
+  id: 'labelcomponent-label',
+  title: 'Components/Label',
+  component: LabelComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LabelModule],
+    }),
+  ],
+} as Meta<LabelComponent>;
+const Template: Story<LabelComponent> = (args: LabelComponent) => ({
+  props: args,
+});
+export const Label = Template.bind({});
+Label.args = {};
