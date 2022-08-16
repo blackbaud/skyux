08e0426882892016792be32480820f9639e51171
Steve Brush <steve.brush@blackbaud.com>
2022-08-16 10:35:13 -0400

    feat(components/core): add overlay harness for interacting with overlay components in tests

--- libs/components/core/testing/src/public-api.ts
+++ libs/components/core/testing/src/public-api.ts
@@ -1,3 +1,5 @@
+export * from './overlay/overlay-harness-filters';
+export * from './overlay/overlay-harness';
 export * from './shared/component-harness';
 export * from './shared/harness-filters';
 export * from './core-testing.module';
