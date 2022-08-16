87bba9d24ac489b485c9f7b99627c2f5a3ff50e9
Steve Brush <steve.brush@blackbaud.com>
2022-08-16 13:10:09 -0400

    add comments

--- libs/components/forms/testing/src/input-box/input-box-harness-filters.ts
+++ libs/components/forms/testing/src/input-box/input-box-harness-filters.ts
@@ -1,4 +1,7 @@
 import { SkyHarnessFilters } from '@skyux/core/testing';
 
+/**
+ * A set of criteria that can be used to filter a list of SkyInputBoxHarness instances.
+ */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface SkyInputBoxHarnessFilters extends SkyHarnessFilters {}
--- libs/components/forms/testing/src/input-box/input-box-harness.ts
+++ libs/components/forms/testing/src/input-box/input-box-harness.ts
@@ -8,9 +8,16 @@ import { SkyComponentHarness } from '@skyux/core/testing';
 
 import { SkyInputBoxHarnessFilters } from './input-box-harness-filters';
 
+/**
+ * Harness for interacting with an input box component in tests.
+ */
 export class SkyInputBoxHarness extends SkyComponentHarness {
   public static hostSelector = 'sky-input-box';
 
+  /**
+   * Gets a `HarnessPredicate` that can be used to search for a
+   * `SkyInputBoxHarness` that meets certain criteria.
+   */
   public static with(
     filters: SkyInputBoxHarnessFilters
   ): HarnessPredicate<SkyInputBoxHarness> {
