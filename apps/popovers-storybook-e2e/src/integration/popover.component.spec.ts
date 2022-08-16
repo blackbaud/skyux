5db97ef764497585203343ec9e0a872889490870
Paul Crowder <paul.crowder@blackbaud.com>
2022-08-16 15:19:25 -0400

    Screenshot fixes

--- apps/popovers-storybook-e2e/src/integration/popover.component.spec.ts
+++ apps/popovers-storybook-e2e/src/integration/popover.component.spec.ts
@@ -1,16 +1,20 @@
 ['default', 'modern-light', 'modern-dark'].forEach((theme) => {
   describe(`popovers-storybook in ${theme} theme`, () => {
     beforeEach(() =>
-      cy.visit(
-        `/iframe.html?globals=theme:${theme}&id=popovercomponent-popover--popover`
-      )
+      cy
+        .viewport(1280, 700)
+        .visit(
+          `/iframe.html?globals=theme:${theme}&id=popovercomponent-popover--popover`
+        )
     );
     it('should render the component', () => {
-      cy.get('app-popover')
-        .should('exist')
-        .should('be.visible')
-        .screenshot(`popovercomponent-popover--popover-${theme}`)
-        .percySnapshot(`popovercomponent-popover--popover-${theme}`);
+      cy.get('.popover-message').should('exist').should('be.visible');
+
+      // Capture the window viewport instead of the element; otherwise the popover
+      // elements shift when taking a screenshot.
+      cy.screenshot(`popovercomponent-popover--popover-${theme}`, {
+        capture: 'viewport',
+      }).percySnapshot(`popovercomponent-popover--popover-${theme}`);
     });
   });
 });
