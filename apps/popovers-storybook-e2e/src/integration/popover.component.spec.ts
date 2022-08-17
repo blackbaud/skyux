2e7f0b488640c8297df98d108b962bfeccb68fb1
Paul Crowder <paul.crowder@blackbaud.com>
2022-08-17 13:53:11 -0400

    Try waiting as test

--- apps/popovers-storybook-e2e/src/integration/popover.component.spec.ts
+++ apps/popovers-storybook-e2e/src/integration/popover.component.spec.ts
@@ -13,6 +13,9 @@
         .should('be.visible')
         .should('have.length', 16);
 
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000);
+
       // Capture the window viewport instead of the element; otherwise the popover
       // elements shift when taking a screenshot.
       cy.screenshot(`popovercomponent-popover--popover-${theme}`, {
