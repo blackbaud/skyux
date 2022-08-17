cefafdc070f2c14bc8baa21b29dce135ff2cf1dd
Paul Crowder <paul.crowder@blackbaud.com>
2022-08-17 12:51:26 -0400

    Update popover.component.spec.ts

--- apps/popovers-storybook-e2e/src/integration/popover.component.spec.ts
+++ apps/popovers-storybook-e2e/src/integration/popover.component.spec.ts
@@ -8,7 +8,10 @@
         )
     );
     it('should render the component', () => {
-      cy.get('.popover-message').should('exist').should('be.visible');
+      cy.get('.popover-message')
+        .should('exist')
+        .should('be.visible')
+        .should('have.length', 16);
 
       // Capture the window viewport instead of the element; otherwise the popover
       // elements shift when taking a screenshot.
