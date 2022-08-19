5e81b1d0a545978155bd0a9d420d02627cce064c
Steve Brush <steve.brush@blackbaud.com>
2022-08-19 10:14:19 -0400

    code cleanup

--- libs/components/indicators/testing/src/public-api.ts
+++ libs/components/indicators/testing/src/public-api.ts
@@ -2,6 +2,7 @@ export * from './tokens/token-harness';
 export * from './tokens/token-harness-filters';
 export * from './tokens/tokens-harness';
 export * from './tokens/tokens-harness-filters';
+
 export * from './alert-fixture';
 export * from './label-fixture';
 export * from './wait-fixture';
