52542a28db5b6a65436af2beb6f941ec23b1404d
Braden Bisping <bradennbisping@gmail.com>
2022-08-19 11:49:50 -0500

    remove cards view from data manager's code example

--- apps/code-examples/src/app/code-examples/data-manager/data-manager/basic/data-manager-demo.module.ts
+++ apps/code-examples/src/app/code-examples/data-manager/data-manager/basic/data-manager-demo.module.ts
@@ -3,26 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SkyDataManagerModule } from '@skyux/data-manager';
 import { SkyCheckboxModule } from '@skyux/forms';
-import { SkyCardModule, SkyToolbarModule } from '@skyux/layout';
+import { SkyToolbarModule } from '@skyux/layout';
 import { SkyRepeaterModule } from '@skyux/lists';
 import { SkyModalModule } from '@skyux/modals';
 
 import { DataManagerFiltersModalDemoComponent } from './data-filter-modal.component';
 import { DataManagerDemoComponent } from './data-manager-demo.component';
-import { DataViewCardsDemoComponent } from './data-view-cards.component';
 import { DataViewRepeaterDemoComponent } from './data-view-repeater.component';
 
 @NgModule({
   declarations: [
     DataManagerDemoComponent,
     DataManagerFiltersModalDemoComponent,
-    DataViewCardsDemoComponent,
     DataViewRepeaterDemoComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
-    SkyCardModule,
     SkyCheckboxModule,
     SkyDataManagerModule,
     SkyModalModule,
