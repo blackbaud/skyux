be6f23bbf918a67b93e84090fc8545d536a2ad36
Steve Brush <steve.brush@blackbaud.com>
2022-08-22 16:37:54 -0400

    comments

--- libs/components/lists/testing/src/repeater/repeater-harness.ts
+++ libs/components/lists/testing/src/repeater/repeater-harness.ts
@@ -3,9 +3,16 @@ import { SkyComponentHarness } from '@skyux/core/testing';
 
 import { SkyRepeaterHarnessFilters } from './repeater-harness-filters';
 
+/**
+ * Harness for interacting with a repeater component in tests.
+ */
 export class SkyRepeaterHarness extends SkyComponentHarness {
   public static hostSelector = 'sky-repeater';
 
+  /**
+   * Gets a `HarnessPredicate` that can be used to search for a
+   * `SkyRepeaterHarness` that meets certain criteria.
+   */
   public static with(
     filters: SkyRepeaterHarnessFilters
   ): HarnessPredicate<SkyRepeaterHarness> {
--- libs/components/lists/testing/src/repeater/repeater-item-harness-filters.ts
+++ libs/components/lists/testing/src/repeater/repeater-item-harness-filters.ts
@@ -1,5 +1,8 @@
 import { SkyHarnessFilters } from '@skyux/core/testing';
 
+/**
+ * A set of criteria that can be used to filter a list of `SkyRepeaterItemHarness` instances.
+ */
 export interface SkyRepeaterItemHarnessFilters extends SkyHarnessFilters {
   bodyTextContent?: string | RegExp;
   title?: string | RegExp;
