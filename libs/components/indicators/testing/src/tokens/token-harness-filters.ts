54857b7553308a9fab1febd4ecf7d2882df90bb8
Steve Brush <steve.brush@blackbaud.com>
2022-08-22 09:26:17 -0400

    cleanup tests

--- libs/components/indicators/testing/src/tokens/token-harness-filters.ts
+++ libs/components/indicators/testing/src/tokens/token-harness-filters.ts
@@ -1,7 +1,7 @@
 import { BaseHarnessFilters } from '@angular/cdk/testing';
 
 /**
- * A set of criteria that can be used to filter a list of SkyTokenHarness instances.
+ * A set of criteria that can be used to filter a list of `SkyTokenHarness` instances.
  */
 export interface SkyTokenHarnessFilters extends BaseHarnessFilters {
   textContent?: string | RegExp;
