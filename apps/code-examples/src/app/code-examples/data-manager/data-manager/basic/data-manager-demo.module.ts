66e1e90f590687fdd126e6efb5cbab5e1c539bb4
Braden Bisping <bradennbisping@gmail.com>
2022-08-22 16:46:28 -0500

    add grid to data manager code-example

--- apps/code-examples/src/app/code-examples/data-manager/data-manager/basic/data-manager-demo.module.ts
+++ apps/code-examples/src/app/code-examples/data-manager/data-manager/basic/data-manager-demo.module.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SkyAgGridModule } from '@skyux/ag-grid';
 import { SkyDataManagerModule } from '@skyux/data-manager';
 import { SkyCheckboxModule } from '@skyux/forms';
 import { SkyToolbarModule } from '@skyux/layout';
 import { SkyRepeaterModule } from '@skyux/lists';
 import { SkyModalModule } from '@skyux/modals';
 
+import { AgGridModule } from 'ag-grid-angular';
+
 import { DataManagerFiltersModalDemoComponent } from './data-filter-modal.component';
 import { DataManagerDemoComponent } from './data-manager-demo.component';
+import { DataViewGridDemoComponent } from './data-view-grid.component';
 import { DataViewRepeaterDemoComponent } from './data-view-repeater.component';
 
 @NgModule({
@@ -16,12 +20,15 @@ import { DataViewRepeaterDemoComponent } from './data-view-repeater.component';
     DataManagerDemoComponent,
     DataManagerFiltersModalDemoComponent,
     DataViewRepeaterDemoComponent,
+    DataViewGridDemoComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     SkyCheckboxModule,
     SkyDataManagerModule,
+    SkyAgGridModule,
+    AgGridModule,
     SkyModalModule,
     SkyRepeaterModule,
     SkyToolbarModule,
