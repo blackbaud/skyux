3ff766654d80e6c55363f274a90286f02ae24154
Steve Brush <steve.brush@blackbaud.com>
2022-08-22 15:48:52 -0400

    jsdocs

--- libs/components/forms/testing/src/checkbox/checkbox-label-harness.ts
+++ libs/components/forms/testing/src/checkbox/checkbox-label-harness.ts
@@ -1,10 +1,16 @@
 import { ComponentHarness } from '@angular/cdk/testing';
 
+/**
+ * Harness for interacting with a checkbox label component in tests.
+ */
 export class SkyCheckboxLabelHarness extends ComponentHarness {
   public static hostSelector = 'sky-checkbox-label';
 
   #getLabelContent = this.locatorFor('.sky-switch-label');
 
+  /**
+   * Gets the text content of the checkbox label.
+   */
   public async getText(): Promise<string> {
     return (await this.#getLabelContent()).text();
   }
