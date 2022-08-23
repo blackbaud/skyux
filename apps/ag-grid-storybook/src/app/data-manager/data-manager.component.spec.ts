ce72a69e05f403f193484d947b4b05dd6405b15f
John White <john.white@blackbaud.me>
2022-08-23 10:48:38 -0400

    Fix test

--- apps/ag-grid-storybook/src/app/data-manager/data-manager.component.spec.ts
+++ apps/ag-grid-storybook/src/app/data-manager/data-manager.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DataManagerComponent } from './data-manager.component';
 import { DataManagerModule } from './data-manager.module';
@@ -8,8 +9,22 @@ describe('DataManagerComponent', () => {
   let fixture: ComponentFixture<DataManagerComponent>;
 
   beforeEach(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+
     TestBed.configureTestingModule({
-      imports: [DataManagerModule],
+      imports: [DataManagerModule, NoopAnimationsModule],
     });
     fixture = TestBed.createComponent(DataManagerComponent);
     component = fixture.componentInstance;
