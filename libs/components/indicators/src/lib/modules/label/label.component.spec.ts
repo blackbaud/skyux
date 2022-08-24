b3233b7d1a2d65807d0e2ac3687f64e9dbc182fa
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-08-24 13:47:20 -0400

    fix(components/indicators): label and status indicator show icons when default states are being used (#410)

--- libs/components/indicators/src/lib/modules/label/label.component.spec.ts
+++ libs/components/indicators/src/lib/modules/label/label.component.spec.ts
@@ -1,10 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { expect, expectAsync } from '@skyux-sdk/testing';
 
 import { SkyLabelFixturesModule } from './fixtures/label-fixtures.module';
 import { LabelTestComponent } from './fixtures/label.component.fixture';
 
 describe('Label component', () => {
+  function getLabel(
+    fixture: ComponentFixture<LabelTestComponent>
+  ): HTMLElement {
+    return fixture.nativeElement.querySelector(
+      '#label-with-label-type .sky-label'
+    );
+  }
+
+  function getLabelWithoutLabelType(
+    fixture: ComponentFixture<LabelTestComponent>
+  ): HTMLElement {
+    return fixture.nativeElement.querySelector(
+      '#label-without-label-type .sky-label'
+    );
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [SkyLabelFixturesModule],
@@ -13,23 +29,39 @@ describe('Label component', () => {
 
   it('should add the appropriate CSS class based on the label type', () => {
     const fixture = TestBed.createComponent(LabelTestComponent);
-    const el = fixture.nativeElement as HTMLElement;
 
     fixture.componentInstance.labelType = 'danger';
 
     fixture.detectChanges();
-    expect(el.querySelector('.sky-label')).toHaveCssClass('sky-label-danger');
+    expect(getLabel(fixture)).toHaveCssClass('sky-label-danger');
   });
 
   it("should render the label's contents in the expected location", async () => {
     const fixture = TestBed.createComponent(LabelTestComponent);
-    const el = fixture.nativeElement as HTMLElement;
+    fixture.componentInstance.labelType = 'info';
 
     fixture.detectChanges();
-    expect(el.querySelector('.sky-label')).toHaveText('Test label');
+    expect(getLabel(fixture)).toHaveText('Test label');
 
     // Accessibility checks
     await fixture.whenStable();
     await expectAsync(fixture.nativeElement).toBeAccessible();
+  });
+
+  it('should render the correct icon when a `labelType` is given', () => {
+    const fixture = TestBed.createComponent(LabelTestComponent);
+
+    fixture.componentInstance.labelType = 'danger';
+
+    fixture.detectChanges();
+    expect(getLabel(fixture).querySelector('i')).toHaveCssClass('fa-warning');
+  });
+  it('should render the correct icon when no `labelType` is given', () => {
+    const fixture = TestBed.createComponent(LabelTestComponent);
+
+    fixture.detectChanges();
+    expect(getLabelWithoutLabelType(fixture).querySelector('i')).toHaveCssClass(
+      'fa-exclamation-circle'
+    );
   });
 });
