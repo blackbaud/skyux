daa46fcab0762a15ed8672ea0aace5ea76e6425d
Braden Bisping <63625452+bradenbiz@users.noreply.github.com>
2022-08-25 10:29:39 -0500

    chore(apps/code-examples): remove deprecated cards component code references & fix code-example for Data Manager (#419)
    * remove unused code-example
    
    * remove cards view from data manager's code example
    
    * add data manager routing to code-examples spa
    
    * add grid to data manager code-example
    
    * fix broken data manager stackblitz & code example
    
    Co-authored-by: Braden Bisping <bradennbisping@gmail.com>

--- apps/code-examples/src/app/code-examples/data-manager/data-manager/basic/data-manager-demo.module.ts
+++ apps/code-examples/src/app/code-examples/data-manager/data-manager/basic/data-manager-demo.module.ts
@@ -1,30 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SkyAgGridModule } from '@skyux/ag-grid';
 import { SkyDataManagerModule } from '@skyux/data-manager';
 import { SkyCheckboxModule } from '@skyux/forms';
-import { SkyCardModule, SkyToolbarModule } from '@skyux/layout';
+import { SkyToolbarModule } from '@skyux/layout';
 import { SkyRepeaterModule } from '@skyux/lists';
 import { SkyModalModule } from '@skyux/modals';
 
+import { AgGridModule } from 'ag-grid-angular';
+
 import { DataManagerFiltersModalDemoComponent } from './data-filter-modal.component';
 import { DataManagerDemoComponent } from './data-manager-demo.component';
-import { DataViewCardsDemoComponent } from './data-view-cards.component';
+import { DataViewGridDemoComponent } from './data-view-grid.component';
 import { DataViewRepeaterDemoComponent } from './data-view-repeater.component';
 
 @NgModule({
   declarations: [
     DataManagerDemoComponent,
     DataManagerFiltersModalDemoComponent,
-    DataViewCardsDemoComponent,
     DataViewRepeaterDemoComponent,
+    DataViewGridDemoComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
-    SkyCardModule,
     SkyCheckboxModule,
     SkyDataManagerModule,
+    SkyAgGridModule,
+    AgGridModule,
     SkyModalModule,
     SkyRepeaterModule,
     SkyToolbarModule,
