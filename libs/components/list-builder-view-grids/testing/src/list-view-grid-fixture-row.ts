c9a29ac089727cfef0d6fc0387a27904e96e7fb2
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-09-01 10:17:44 -0400

    docs: mark testing interfaces as internal (#471)

--- libs/components/list-builder-view-grids/testing/src/list-view-grid-fixture-row.ts
+++ libs/components/list-builder-view-grids/testing/src/list-view-grid-fixture-row.ts
@@ -2,6 +2,7 @@ import { SkyListViewGridFixtureCell } from './list-view-grid-fixture-cell';
 
 /**
  * Properties of a list view grid row.
+ * @internal
  */
 export interface SkyListViewGridFixtureRow {
   cells: SkyListViewGridFixtureCell[];
--- libs/components/lookup/testing/src/lookup/lookup-selection-harness-filters.ts
+++ libs/components/lookup/testing/src/lookup/lookup-selection-harness-filters.ts
@@ -2,6 +2,7 @@ import { SkyTokenHarnessFilters } from '@skyux/indicators/testing';
 
 /**
  * A set of criteria that can be used to filter a list of `SkyLookupSelectionHarness` instances.
+ * @internal
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface SkyLookupSelectionHarnessFilters
