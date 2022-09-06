69db993f24e2e947e6b1e112223237ea0f668c5d
John White <750350+johnhwhite@users.noreply.github.com>
2022-09-06 11:56:09 -0400

    Run migration.

--- apps/indicators-storybook-e2e/src/integration/label.component.spec.ts
+++ apps/indicators-storybook-e2e/src/integration/label.component.spec.ts
@@ -1,16 +1,20 @@
-['default', 'modern-light', 'modern-dark'].forEach((theme) => {
-  describe(`indicators-storybook in ${theme} theme`, () => {
-    beforeEach(() =>
-      cy.visit(
-        `/iframe.html?globals=theme:${theme}&id=labelcomponent-label--label`
-      )
-    );
-    it('should render the component', () => {
-      cy.get('app-label')
-        .should('exist')
-        .should('be.visible')
-        .screenshot(`labelcomponent-label--label-${theme}`)
-        .percySnapshot(`labelcomponent-label--label-${theme}`);
+import { E2eVariations } from '@skyux-sdk/e2e-schematics';
+
+describe(`indicators-storybook`, () => {
+  E2eVariations.forEachTheme((theme) => {
+    describe(`in ${theme} theme`, () => {
+      beforeEach(() =>
+        cy.visit(
+          `/iframe.html?globals=theme:${theme}&id=labelcomponent-label--label`
+        )
+      );
+      it('should render the component', () => {
+        cy.get('app-label')
+          .should('exist')
+          .should('be.visible')
+          .screenshot(`labelcomponent-label--label-${theme}`)
+          .percySnapshot(`labelcomponent-label--label-${theme}`);
+      });
     });
   });
 });
--- apps/tabs-storybook-e2e/src/integration/wizard.component.spec.ts
+++ apps/tabs-storybook-e2e/src/integration/wizard.component.spec.ts
@@ -1,46 +1,50 @@
-['default', 'modern-light', 'modern-dark'].forEach((theme) => {
-  describe(`tabs-storybook in ${theme} theme`, () => {
-    beforeEach(() =>
-      cy.visit(
-        `/iframe.html?globals=theme:${theme}&id=wizardcomponent-wizard--wizard`
-      )
-    );
-    it('should render the component on a large screen', () => {
-      cy.get('app-wizard')
-        .should('exist')
-        .should('be.visible')
-        .get('.open-wizard-btn')
-        .should('exist')
-        .should('be.visible')
-        .click()
-        .get('.sky-modal')
-        .should('exist')
-        .should('be.visible')
-        .screenshot(`wizardcomponent-wizard--wizard-lg-${theme}`)
-        .percySnapshot(`wizardcomponent-wizard--wizard-lg-${theme}`, {
-          widths: [1280],
-        });
-    });
+import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 
-    it('should render the component on a small screen', () => {
-      cy.viewport(375, 812)
-        .get('app-wizard')
-        .should('exist')
-        .should('be.visible')
-        .get('.open-wizard-btn')
-        .should('exist')
-        .should('be.visible')
-        .click()
-        .get('.sky-modal')
-        .should('exist')
-        .should('be.visible')
-        .get('sky-dropdown')
-        .should('exist')
-        .should('be.visible')
-        .screenshot(`wizardcomponent-wizard--wizard-sm-${theme}`)
-        .percySnapshot(`wizardcomponent-wizard--wizard-sm-${theme}`, {
-          widths: [375],
-        });
+describe(`tabs-storybook`, () => {
+  E2eVariations.forEachTheme((theme) => {
+    describe(`in ${theme} theme`, () => {
+      beforeEach(() =>
+        cy.visit(
+          `/iframe.html?globals=theme:${theme}&id=wizardcomponent-wizard--wizard`
+        )
+      );
+      it('should render the component on a large screen', () => {
+        cy.get('app-wizard')
+          .should('exist')
+          .should('be.visible')
+          .get('.open-wizard-btn')
+          .should('exist')
+          .should('be.visible')
+          .click()
+          .get('.sky-modal')
+          .should('exist')
+          .should('be.visible')
+          .screenshot(`wizardcomponent-wizard--wizard-lg-${theme}`)
+          .percySnapshot(`wizardcomponent-wizard--wizard-lg-${theme}`, {
+            widths: [1280],
+          });
+      });
+  
+      it('should render the component on a small screen', () => {
+        cy.viewport(375, 812)
+          .get('app-wizard')
+          .should('exist')
+          .should('be.visible')
+          .get('.open-wizard-btn')
+          .should('exist')
+          .should('be.visible')
+          .click()
+          .get('.sky-modal')
+          .should('exist')
+          .should('be.visible')
+          .get('sky-dropdown')
+          .should('exist')
+          .should('be.visible')
+          .screenshot(`wizardcomponent-wizard--wizard-sm-${theme}`)
+          .percySnapshot(`wizardcomponent-wizard--wizard-sm-${theme}`, {
+            widths: [375],
+          });
+      });
     });
   });
 });
