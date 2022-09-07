c30b0057babc2bdd4400255482de4aceb28542fe
Steve Brush <steve.brush@blackbaud.com>
2022-09-07 15:03:34 -0400

    update fix

--- libs/components/assets/src/lib/assets.service.spec.ts
+++ libs/components/assets/src/lib/assets.service.spec.ts
@@ -1,21 +1,8 @@
 import { SkyAppAssetsService } from './assets.service';
 
-class MockAssetsService extends SkyAppAssetsService {
-  #urls = { foo: 'https://foo.com' };
-
-  public getUrl(path: string) {
-    return (this.#urls as any)[path];
-  }
-
-  public getAllUrls() {
-    return this.#urls;
-  }
-}
-
 describe('Assets service', () => {
   it('should have expected methods', () => {
-    const svc = new MockAssetsService();
-    expect(svc.getAllUrls()).toEqual({ foo: 'https://foo.com' });
-    expect(svc.getUrl('foo')).toEqual('https://foo.com');
+    expect(SkyAppAssetsService.prototype).not.toBeNull();
+    expect(SkyAppAssetsService.prototype.getUrl).not.toBeNull();
   });
 });
