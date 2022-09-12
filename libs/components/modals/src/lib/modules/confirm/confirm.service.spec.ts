0a23849089257e1baaac84d5d5aaad283fdf7288
Corey Archer <corey.archer@blackbaud.com>
2022-09-12 16:16:42 -0400

    deprecation(modals/confirm): deprecate autofocus

--- libs/components/modals/src/lib/modules/confirm/confirm.service.spec.ts
+++ libs/components/modals/src/lib/modules/confirm/confirm.service.spec.ts
@@ -1,5 +1,9 @@
+import { TestBed } from '@angular/core/testing';
+import { SkyLogService } from '@skyux/core';
+
 import { SkyConfirmConfig } from './confirm-config';
 import { SKY_CONFIRM_CONFIG } from './confirm-config-token';
+import { SkyConfirmType } from './confirm-type';
 import { SkyConfirmComponent } from './confirm.component';
 import { SkyConfirmService } from './confirm.service';
 import { MockSkyModalService } from './fixtures/mocks';
@@ -10,7 +14,10 @@ describe('Confirm service', () => {
 
   beforeEach(() => {
     modalService = new MockSkyModalService();
-    confirmService = new SkyConfirmService(modalService as any);
+    confirmService = new SkyConfirmService(
+      modalService as any,
+      TestBed.inject(SkyLogService)
+    );
   });
 
   it('should open confirmation dialog with correct parameters', () => {
@@ -27,11 +34,49 @@ describe('Confirm service', () => {
       ],
     };
 
+    const logServiceSpy = spyOn(
+      SkyLogService.prototype,
+      'deprecated'
+    ).and.stub();
+
     confirmService.open(config);
 
     expect(modalService.openCalls.length).toBe(1);
     expect(modalService.openCalls[0].component).toBe(SkyConfirmComponent);
     expect(modalService.openCalls[0].config).toEqual(expectedConfig);
+    expect(logServiceSpy).not.toHaveBeenCalled();
+  });
+
+  it('should open confirmation dialog with correct parameters and warn of deprecated property', () => {
+    const config: SkyConfirmConfig = {
+      message: 'dialog description',
+      type: SkyConfirmType.Custom,
+      buttons: [
+        { text: 'OK', styleType: 'primary', action: 'save' },
+        { text: 'Cancel', autofocus: true, action: 'cancel' },
+      ],
+    };
+
+    const expectedConfig = {
+      providers: [
+        {
+          provide: SKY_CONFIRM_CONFIG,
+          useValue: config,
+        },
+      ],
+    };
+
+    const logServiceSpy = spyOn(
+      SkyLogService.prototype,
+      'deprecated'
+    ).and.stub();
+
+    confirmService.open(config);
+
+    expect(modalService.openCalls.length).toBe(1);
+    expect(modalService.openCalls[0].component).toBe(SkyConfirmComponent);
+    expect(modalService.openCalls[0].config).toEqual(expectedConfig);
+    expect(logServiceSpy).toHaveBeenCalledWith('autofocus');
   });
 
   it('should subscribe to the modal closed event and emit args', () => {
