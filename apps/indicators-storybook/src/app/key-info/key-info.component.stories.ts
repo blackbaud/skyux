605fbcbbbff55bfddfecf747a36d32e6c2a808d3
Corey Archer <corey.archer@blackbaud.me>
2022-09-15 17:08:27 -0400

    test(components/indicators): key info visual tests

---
+++ apps/indicators-storybook/src/app/key-info/key-info.component.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, Story, moduleMetadata } from '@storybook/angular';
+
+import { KeyInfoComponent } from './key-info.component';
+import { KeyInfoModule } from './key-info.module';
+
+export default {
+  id: 'keyinfocomponent-keyinfo',
+  title: 'Components/Key Info',
+  component: KeyInfoComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [KeyInfoModule],
+    }),
+  ],
+} as Meta<KeyInfoComponent>;
+const Template: Story<KeyInfoComponent> = (args: KeyInfoComponent) => ({
+  props: args,
+});
+export const KeyInfo = Template.bind({});
+KeyInfo.args = {};
---
+++ apps/indicators-storybook/src/app/key-info/key-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key-info',
+  templateUrl: './key-info.component.html',
+  styleUrls: ['./key-info.component.scss'],
+})
+export class KeyInfoComponent {
+  public layouts = ['vertical', 'horizontal'];
+}
