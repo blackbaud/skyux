170236b9669d437be49a7b4f654e5ebd000c7102
Sandhya <sandhya.rajasabeson@blackbaud.me>
2022-09-19 13:28:44 -0400

    deprecation sentence

--- libs/components/modals/src/lib/modules/modal/modal.interface.ts
+++ libs/components/modals/src/lib/modules/modal/modal.interface.ts
@@ -62,7 +62,7 @@ export interface SkyModalConfigurationInterface {
   tiledBody?: boolean;
 
   /**
-   * @deprecated Use `sky-inline-help` in the modal header instead.
+   * @deprecated Use SkyHelpInlineComponent in the modal header instead.
    * Specifies a `helpKey` string. This property displays
    * the <i class="fa fa-question-circle" aria-hidden="true"></i> button in the modal header.
    * When users click this button, the `helpOpened` event broadcasts the `helpKey` parameter.
