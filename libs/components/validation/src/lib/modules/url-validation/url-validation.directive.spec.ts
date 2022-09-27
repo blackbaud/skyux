4834fd83ec9048c9559e73ee115d17d97250c350
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-09-27 13:38:04 -0400

    Feedback changes

--- libs/components/validation/src/lib/modules/url-validation/url-validation.directive.spec.ts
+++ libs/components/validation/src/lib/modules/url-validation/url-validation.directive.spec.ts
@@ -13,11 +13,18 @@ import { SkyUrlValidationFixturesModule } from './fixtures/url-validation-fixtur
 import { UrlValidationRulesetTestComponent } from './fixtures/url-validation-ruleset.component.fixture';
 import { UrlValidationTestComponent } from './fixtures/url-validation.component.fixture';
 
+function getInputElement(
+  fixture: ComponentFixture<
+    UrlValidationTestComponent | UrlValidationRulesetTestComponent
+  >
+): HTMLInputElement {
+  return fixture.nativeElement.querySelector('input') as HTMLInputElement;
+}
+
 describe('URL validation via directive - ruleset v1 (implicit)', () => {
   function setInput(
-    element: HTMLElement,
     text: string,
-    compFixture: ComponentFixture<any>
+    compFixture: ComponentFixture<UrlValidationTestComponent>
   ) {
     const inputEvent = document.createEvent('Event');
     const params = {
@@ -29,29 +36,25 @@ describe('URL validation via directive - ruleset v1 (implicit)', () => {
 
     const changeEvent = document.createEvent('Event');
     changeEvent.initEvent('change', params.bubbles, params.cancelable);
-    const inputEl = element.querySelector('input');
-    if (inputEl) {
-      inputEl.value = text;
-    }
+    const inputEl = getInputElement(fixture);
+    inputEl.value = text;
 
-    inputEl?.dispatchEvent(inputEvent);
+    inputEl.dispatchEvent(inputEvent);
     compFixture.detectChanges();
 
-    inputEl?.dispatchEvent(changeEvent);
+    inputEl.dispatchEvent(changeEvent);
     compFixture.detectChanges();
   }
 
   let component: UrlValidationTestComponent;
   let fixture: ComponentFixture<UrlValidationTestComponent>;
   let ngModel: NgModel;
-  let nativeElement: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [SkyUrlValidationFixturesModule, FormsModule],
     });
     fixture = TestBed.createComponent(UrlValidationTestComponent);
-    nativeElement = fixture.nativeElement as HTMLElement;
     const input = fixture.debugElement.query(By.css('input'));
     ngModel = input.injector.get(NgModel);
     component = fixture.componentInstance;
@@ -60,12 +63,10 @@ describe('URL validation via directive - ruleset v1 (implicit)', () => {
   it('should validate correct input', fakeAsync(() => {
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, 'https://blackbaud.com', fixture);
+    setInput('https://blackbaud.com', fixture);
     fixture.detectChanges();
 
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      'https://blackbaud.com'
-    );
+    expect(getInputElement(fixture).value).toBe('https://blackbaud.com');
 
     expect(ngModel.control.valid).toBe(true);
     expect(ngModel.control.pristine).toBe(false);
@@ -75,12 +76,10 @@ describe('URL validation via directive - ruleset v1 (implicit)', () => {
   it('should validate incorrect input', fakeAsync(() => {
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    setInput('[]awefhawenfc0293ejwf]', fixture);
     fixture.detectChanges();
 
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      '[]awefhawenfc0293ejwf]'
-    );
+    expect(getInputElement(fixture).value).toBe('[]awefhawenfc0293ejwf]');
 
     expect(ngModel.control.valid).toBe(false);
     expect(ngModel.control.pristine).toBe(false);
@@ -90,19 +89,17 @@ describe('URL validation via directive - ruleset v1 (implicit)', () => {
   it('should validate invalid and then valid input', fakeAsync(() => {
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    setInput('[]awefhawenfc0293ejwf]', fixture);
     fixture.detectChanges();
     tick();
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      '[]awefhawenfc0293ejwf]'
-    );
+    expect(getInputElement(fixture).value).toBe('[]awefhawenfc0293ejwf]');
     expect(component.urlValidator).toEqual('[]awefhawenfc0293ejwf]');
     expect(ngModel.control.valid).toBe(false);
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, 'blackbaud.com', fixture);
+    setInput('blackbaud.com', fixture);
 
-    expect(nativeElement.querySelector('input')?.value).toBe('blackbaud.com');
+    expect(getInputElement(fixture).value).toBe('blackbaud.com');
     expect(component.urlValidator).toEqual('blackbaud.com');
 
     expect(ngModel.control.valid).toBe(true);
@@ -112,7 +109,7 @@ describe('URL validation via directive - ruleset v1 (implicit)', () => {
 
   it('should pass accessibility', async(() => {
     fixture.detectChanges();
-    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    setInput('[]awefhawenfc0293ejwf]', fixture);
     fixture.detectChanges();
     fixture.whenStable().then(() => {
       expect(fixture.nativeElement).toBeAccessible();
@@ -121,20 +118,14 @@ describe('URL validation via directive - ruleset v1 (implicit)', () => {
 });
 
 describe('URL validation via directive - ruleset v1 (explicit)', () => {
-  function setInput(
-    element: HTMLElement,
-    text: string,
-    compFixture: ComponentFixture<any>
-  ) {
+  function setInput(text: string, compFixture: ComponentFixture<any>) {
     const params = {
       bubbles: false,
       cancelable: false,
     };
 
-    const inputEl = element.querySelector('input');
-    if (inputEl) {
-      inputEl.value = text;
-    }
+    const inputEl = getInputElement(fixture);
+    inputEl.value = text;
 
     SkyAppTestUtility.fireDomEvent(inputEl, 'input', params);
     compFixture.detectChanges();
@@ -146,14 +137,12 @@ describe('URL validation via directive - ruleset v1 (explicit)', () => {
   let component: UrlValidationRulesetTestComponent;
   let fixture: ComponentFixture<UrlValidationRulesetTestComponent>;
   let ngModel: NgModel;
-  let nativeElement: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [SkyUrlValidationFixturesModule, FormsModule],
     });
     fixture = TestBed.createComponent(UrlValidationRulesetTestComponent);
-    nativeElement = fixture.nativeElement as HTMLElement;
     const input = fixture.debugElement.query(By.css('input'));
     ngModel = input.injector.get(NgModel);
     component = fixture.componentInstance;
@@ -165,12 +154,10 @@ describe('URL validation via directive - ruleset v1 (explicit)', () => {
     };
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, 'https://blackbaud.com', fixture);
+    setInput('https://blackbaud.com', fixture);
     fixture.detectChanges();
 
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      'https://blackbaud.com'
-    );
+    expect(getInputElement(fixture).value).toBe('https://blackbaud.com');
 
     expect(ngModel.control.valid).toBe(true);
     expect(ngModel.control.pristine).toBe(false);
@@ -183,12 +170,10 @@ describe('URL validation via directive - ruleset v1 (explicit)', () => {
     };
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    setInput('[]awefhawenfc0293ejwf]', fixture);
     fixture.detectChanges();
 
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      '[]awefhawenfc0293ejwf]'
-    );
+    expect(getInputElement(fixture).value).toBe('[]awefhawenfc0293ejwf]');
 
     expect(ngModel.control.valid).toBe(false);
     expect(ngModel.control.pristine).toBe(false);
@@ -198,19 +183,16 @@ describe('URL validation via directive - ruleset v1 (explicit)', () => {
 
 describe('URL validation via directive - ruleset v2', () => {
   function setInput(
-    element: HTMLElement,
     text: string,
-    compFixture: ComponentFixture<any>
+    compFixture: ComponentFixture<UrlValidationRulesetTestComponent>
   ) {
     const params = {
       bubbles: false,
       cancelable: false,
     };
 
-    const inputEl = element.querySelector('input');
-    if (inputEl) {
-      inputEl.value = text;
-    }
+    const inputEl = getInputElement(fixture);
+    inputEl.value = text;
 
     SkyAppTestUtility.fireDomEvent(inputEl, 'input', params);
     compFixture.detectChanges();
@@ -222,14 +204,12 @@ describe('URL validation via directive - ruleset v2', () => {
   let component: UrlValidationRulesetTestComponent;
   let fixture: ComponentFixture<UrlValidationRulesetTestComponent>;
   let ngModel: NgModel;
-  let nativeElement: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [SkyUrlValidationFixturesModule, FormsModule],
     });
     fixture = TestBed.createComponent(UrlValidationRulesetTestComponent);
-    nativeElement = fixture.nativeElement as HTMLElement;
     const input = fixture.debugElement.query(By.css('input'));
     ngModel = input.injector.get(NgModel);
     component = fixture.componentInstance;
@@ -238,12 +218,10 @@ describe('URL validation via directive - ruleset v2', () => {
   it('should validate correct input using ruleset version 2', fakeAsync(() => {
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, 'https://blackbaud.com', fixture);
+    setInput('https://blackbaud.com', fixture);
     fixture.detectChanges();
 
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      'https://blackbaud.com'
-    );
+    expect(getInputElement(fixture).value).toBe('https://blackbaud.com');
 
     expect(ngModel.control.valid).toBe(true);
     expect(ngModel.control.pristine).toBe(false);
@@ -253,12 +231,10 @@ describe('URL validation via directive - ruleset v2', () => {
   it('should validate incorrect input using ruleset version 2', fakeAsync(() => {
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    setInput('[]awefhawenfc0293ejwf]', fixture);
     fixture.detectChanges();
 
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      '[]awefhawenfc0293ejwf]'
-    );
+    expect(getInputElement(fixture).value).toBe('[]awefhawenfc0293ejwf]');
 
     expect(ngModel.control.valid).toBe(false);
     expect(ngModel.control.pristine).toBe(false);
@@ -268,19 +244,17 @@ describe('URL validation via directive - ruleset v2', () => {
   it('should validate invalid and then valid input using ruleset version 2', fakeAsync(() => {
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    setInput('[]awefhawenfc0293ejwf]', fixture);
     fixture.detectChanges();
     tick();
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      '[]awefhawenfc0293ejwf]'
-    );
+    expect(getInputElement(fixture).value).toBe('[]awefhawenfc0293ejwf]');
     expect(component.urlValidator).toEqual('[]awefhawenfc0293ejwf]');
     expect(ngModel.control.valid).toBe(false);
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, 'blackbaud.com', fixture);
+    setInput('blackbaud.com', fixture);
 
-    expect(nativeElement.querySelector('input')?.value).toBe('blackbaud.com');
+    expect(getInputElement(fixture).value).toBe('blackbaud.com');
     expect(component.urlValidator).toEqual('blackbaud.com');
 
     expect(ngModel.control.valid).toBe(true);
@@ -290,7 +264,7 @@ describe('URL validation via directive - ruleset v2', () => {
 
   it('should pass accessibility using ruleset version 2', async(() => {
     fixture.detectChanges();
-    setInput(nativeElement, '[]awefhawenfc0293ejwf]', fixture);
+    setInput('[]awefhawenfc0293ejwf]', fixture);
     fixture.detectChanges();
     fixture.whenStable().then(() => {
       expect(fixture.nativeElement).toBeAccessible();
@@ -298,20 +272,14 @@ describe('URL validation via directive - ruleset v2', () => {
   }));
 });
 describe('URL validation via directive - non-onceability', () => {
-  function setInput(
-    element: HTMLElement,
-    text: string,
-    compFixture: ComponentFixture<any>
-  ) {
+  function setInput(text: string, compFixture: ComponentFixture<any>) {
     const params = {
       bubbles: false,
       cancelable: false,
     };
 
-    const inputEl = element.querySelector('input');
-    if (inputEl) {
-      inputEl.value = text;
-    }
+    const inputEl = getInputElement(fixture);
+    inputEl.value = text;
 
     SkyAppTestUtility.fireDomEvent(inputEl, 'input', params);
     compFixture.detectChanges();
@@ -323,14 +291,12 @@ describe('URL validation via directive - non-onceability', () => {
   let component: UrlValidationRulesetTestComponent;
   let fixture: ComponentFixture<UrlValidationRulesetTestComponent>;
   let ngModel: NgModel;
-  let nativeElement: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [SkyUrlValidationFixturesModule, FormsModule],
     });
     fixture = TestBed.createComponent(UrlValidationRulesetTestComponent);
-    nativeElement = fixture.nativeElement as HTMLElement;
     const input = fixture.debugElement.query(By.css('input'));
     ngModel = input.injector.get(NgModel);
     component = fixture.componentInstance;
@@ -342,12 +308,10 @@ describe('URL validation via directive - non-onceability', () => {
     };
     fixture.detectChanges();
     tick();
-    setInput(nativeElement, 'sub.domain,com/pagename', fixture);
+    setInput('sub.domain,com/pagename', fixture);
     fixture.detectChanges();
 
-    expect(nativeElement.querySelector('input')?.value).toBe(
-      'sub.domain,com/pagename'
-    );
+    expect(getInputElement(fixture).value).toBe('sub.domain,com/pagename');
 
     expect(ngModel.control.valid).toBe(true);
     expect(ngModel.control.pristine).toBe(false);
