3e59f77a88c98c34384abc361b348edc11b15a42
Corey Archer <Corey.Archer@blackbaud.me>
2022-09-27 14:45:39 -0400

    chore(components/layout): back to top strict mode support (#569)

--- libs/components/layout/karma.conf.js
+++ libs/components/layout/karma.conf.js
@@ -11,15 +11,6 @@ module.exports = function (config) {
     coverageReporter: {
       ...baseConfig.coverageReporter,
       dir: join(__dirname, '../../../coverage/libs/components/layout'),
-      // TODO: remove these threshold overrides to meet 100% coverage!
-      check: {
-        global: {
-          statements: 99,
-          branches: 99,
-          functions: 100,
-          lines: 99,
-        },
-      },
     },
   });
 };
