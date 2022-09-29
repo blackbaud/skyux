977672a09d5bc7e83ff0bc802ed753211de834cf
Corey Archer <corey.archer@blackbaud.me>
2022-09-29 14:06:00 -0400

    chore(components/layout): card strict mode support

--- libs/components/layout/testing/src/action-button-fixture.ts
+++ libs/components/layout/testing/src/action-button-fixture.ts
@@ -11,26 +11,26 @@ export class SkyActionButtonFixture {
   /**
    * The action button's current header text.
    */
-  public get headerText(): string {
+  public get headerText(): string | undefined {
     return SkyAppTestUtility.getText(
-      this.debugEl.query(By.css('.sky-action-button-header'))
+      this.#debugEl.query(By.css('.sky-action-button-header'))
     );
   }
 
   /**
    * The action button's current details text.
    */
-  public get detailsText(): string {
+  public get detailsText(): string | undefined {
     return SkyAppTestUtility.getText(
-      this.debugEl.query(By.css('sky-action-button-details'))
+      this.#debugEl.query(By.css('sky-action-button-details'))
     );
   }
 
   /**
    * The action button's current icon type.
    */
-  public get iconType(): string {
-    const classList = this.debugEl.query(By.css('.fa.sky-icon')).nativeElement
+  public get iconType(): string | undefined {
+    const classList = this.#debugEl.query(By.css('.fa.sky-icon')).nativeElement
       .classList;
 
     for (let i = 0, n = classList.length; i < n; i++) {
@@ -40,12 +40,14 @@ export class SkyActionButtonFixture {
         return cls.substr(3);
       }
     }
+
+    return undefined;
   }
 
-  private debugEl: DebugElement;
+  #debugEl: DebugElement;
 
   constructor(fixture: ComponentFixture<any>, skyTestId: string) {
-    this.debugEl = SkyAppTestUtility.getDebugElementByTestId(
+    this.#debugEl = SkyAppTestUtility.getDebugElementByTestId(
       fixture,
       skyTestId,
       'sky-action-button'
@@ -56,7 +58,7 @@ export class SkyActionButtonFixture {
    * Clicks the action button.
    */
   public actionClick(): void {
-    this.debugEl
+    this.#debugEl
       .query(By.css('.sky-action-button'))
       .triggerEventHandler('click', {});
   }
