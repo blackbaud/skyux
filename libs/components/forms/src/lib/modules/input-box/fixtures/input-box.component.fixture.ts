53a9f3dac1f7b1c18887ec655e9001ae198606ef
Erika McVey <erika.mcvey@blackbaud.com>
2022-10-06 11:09:18 -0400

    feat(components/forms): support status indicator errors for input box

--- libs/components/forms/src/lib/modules/input-box/fixtures/input-box.component.fixture.ts
+++ libs/components/forms/src/lib/modules/input-box/fixtures/input-box.component.fixture.ts
@@ -17,6 +17,8 @@ export class InputBoxFixtureComponent {
 
   public errorField: FormControl;
 
+  public errorStatusIndicatorField: FormControl;
+
   public errorForm: FormGroup;
 
   public errorNgModelValue: string | undefined;
@@ -26,6 +28,8 @@ export class InputBoxFixtureComponent {
 
   constructor() {
     this.errorField = new FormControl('', [Validators.required]);
+
+    this.errorStatusIndicatorField = new FormControl('', [Validators.required]);
 
     this.errorForm = new FormGroup({
       errorFormField: new FormControl('', [Validators.required]),
