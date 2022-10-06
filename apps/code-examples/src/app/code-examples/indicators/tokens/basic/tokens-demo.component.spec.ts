22fb7b9137449d6c4c04a84a6f9ef77e525a89cb
Paul Crowder <paul.crowder@blackbaud.com>
2022-10-06 10:11:11 -0400

    docs(components/indicators): add spec files to tokens code examples (#620)

---
+++ apps/code-examples/src/app/code-examples/indicators/tokens/basic/tokens-demo.component.spec.ts
@@ -0,0 +1,62 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SkyToken } from '@skyux/indicators';
+import { SkyTokensHarness } from '@skyux/indicators/testing';
+
+import { TokensDemoColor } from './tokens-demo-color';
+import { TokensDemoComponent } from './tokens-demo.component';
+import { TokensDemoModule } from './tokens-demo.module';
+
+describe('Tokens basic demo', () => {
+  async function setupTest() {
+    const fixture = TestBed.createComponent(TokensDemoComponent);
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+
+    const tokensHarness = await loader.getHarness(
+      SkyTokensHarness.with({ dataSkyId: 'color-tokens' })
+    );
+
+    return { tokensHarness, fixture };
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, TokensDemoModule],
+    });
+  });
+
+  it('should display the expected initial tokens', async () => {
+    const { tokensHarness } = await setupTest();
+
+    await expectAsync(tokensHarness.getTokensText()).toBeResolvedTo([
+      'Red',
+      'Black',
+      'Blue',
+      'Brown',
+      'Green',
+      'Orange',
+      'Pink',
+      'Purple',
+      'Turquoise',
+      'White',
+      'Yellow',
+    ]);
+  });
+
+  it('should update the tokens array when one is dismissed', async () => {
+    const { tokensHarness, fixture } = await setupTest();
+
+    const blueToken: SkyToken<TokensDemoColor> = {
+      value: { name: 'Blue' },
+    };
+
+    expect(fixture.componentInstance.colors).toContain(blueToken);
+
+    await tokensHarness.dismissTokens({
+      text: 'Blue',
+    });
+
+    expect(fixture.componentInstance.colors).not.toContain(blueToken);
+  });
+});
