3fbabf28cb406a220aa4d7dbfe282b8a81e6365a
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.com>
2022-10-11 13:50:33 -0400

    feat(components/angular-tree-component): add inline help support for angular tree component (#659)
    Co-authored-by: Sandhya <sandhya.rajasabeson@blackbaud.me>

---
+++ apps/code-examples/src/app/code-examples/angular-tree-component/angular-tree/inline-help/angular-tree-demo.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TreeModule } from '@circlon/angular-tree-component';
+import { SkyAngularTreeModule } from '@skyux/angular-tree-component';
+import { SkyCheckboxModule, SkyRadioModule } from '@skyux/forms';
+import { SkyHelpInlineModule } from '@skyux/indicators';
+import { SkyFluidGridModule } from '@skyux/layout';
+import { SkyDropdownModule } from '@skyux/popovers';
+
+import { AngularTreeDemoComponent } from './angular-tree-demo.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SkyAngularTreeModule,
+    SkyCheckboxModule,
+    SkyDropdownModule,
+    SkyFluidGridModule,
+    SkyRadioModule,
+    TreeModule,
+    SkyHelpInlineModule,
+  ],
+  declarations: [AngularTreeDemoComponent],
+  exports: [AngularTreeDemoComponent],
+})
+export class AngularTreeDemoModule {}
