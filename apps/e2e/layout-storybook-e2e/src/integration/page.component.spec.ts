3292d49f7ae31c2a1a1bca3514c600dbd29be5c7
Paul Crowder <paul.crowder@blackbaud.com>
2022-10-12 14:20:16 -0400

    Page layout fit visual tests

---
+++ apps/e2e/layout-storybook-e2e/src/integration/page.component.spec.ts
@@ -0,0 +1,23 @@
+import { E2eVariations } from '@skyux-sdk/e2e-schematics';
+
+describe('layout-storybook', () => {
+  E2eVariations.forEachTheme((theme) => {
+    describe(`in ${theme} theme`, () => {
+      beforeEach(() =>
+        cy.visit(
+          `/iframe.html?globals=theme:${theme}&id=pagecomponent-page--page`
+        )
+      );
+
+      it('should constrain the contents of the page with layout fit to the available viewport', () => {
+        cy.get('app-page')
+          .should('exist')
+          .should('be.visible')
+          .screenshot(`pagecomponent-page--page-${theme}`)
+          .percySnapshot(`pagecomponent-page--page-${theme}`, {
+            widths: E2eVariations.DISPLAY_WIDTHS,
+          });
+      });
+    });
+  });
+});
