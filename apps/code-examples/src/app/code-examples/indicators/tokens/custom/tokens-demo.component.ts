16ee384008e309d2d9ce64f9caa7ef55fa03e2eb
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-10-13 14:36:58 -0400

    remove

--- apps/code-examples/src/app/code-examples/indicators/tokens/custom/tokens-demo.component.ts
+++ apps/code-examples/src/app/code-examples/indicators/tokens/custom/tokens-demo.component.ts
@@ -1,186 +1,84 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { SkyIdService } from '@skyux/core';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  SkyToken,
+  SkyTokenSelectedEventArgs,
+  SkyTokensMessage,
+  SkyTokensMessageType,
+} from '@skyux/indicators';
 
-import axe from 'axe-core';
+import { Subject } from 'rxjs';
 
-interface Selection {
-  id: string;
-  name: string;
-}
-
-interface Item {
-  id: string;
-  name: string;
-}
-
-function parseMessage(violations: axe.Result[]): string {
-  let message = 'Expected element to pass accessibility checks.\n\n';
-
-  violations.forEach((violation) => {
-    const wcagTags = violation.tags
-      .filter((tag) => tag.match(/wcag\d{3}|^best*/gi))
-      .join(', ');
-
-    const html = violation.nodes.reduce(
-      (accumulator: string, node: axe.NodeResult) => {
-        return `${accumulator}\n${node.html}\n`;
-      },
-      '       Elements:\n'
-    );
-
-    const error = [
-      `aXe - [Rule: '${violation.id}'] ${violation.help} - WCAG: ${wcagTags}`,
-      `       Get help at: ${violation.helpUrl}\n`,
-      `${html}\n\n`,
-    ].join('\n');
-
-    message += `${error}\n`;
-  });
-
-  return message;
-}
+import { TokensDemoItem } from './tokens-demo-item';
 
 @Component({
   selector: 'app-tokens-demo',
   templateUrl: './tokens-demo.component.html',
-  styleUrls: ['./tokens-demo.component.scss'],
 })
-export class TokensDemoComponent implements OnInit {
-  @ViewChild('inputEl', { read: ElementRef })
-  private inputEl: ElementRef;
+export class TokensDemoComponent implements OnDestroy {
+  public myTokens: SkyToken<TokensDemoItem>[];
+  public tokensController = new Subject<SkyTokensMessage>();
+  public selectedToken = '';
 
-  protected activeDescendant: string | undefined;
-  protected activeIndex = 0;
-  protected activeSelectionIndex = 0;
-  protected data: Item[];
-  protected panelId: string;
-  protected panelOpen = false;
-  protected selections: Selection[] = [];
-  protected selectionsHasFocus = false;
+  #defaultItems: TokensDemoItem[] = [
+    { label: 'Canada' },
+    { label: 'Older than 55' },
+    { label: 'Employed' },
+    { label: 'Added before 2018' },
+  ];
 
-  protected get selectionsSummary(): string {
-    return this.selections.length
-      ? 'The following items are selected: ' +
-          this.selections.map((s) => s.name).join(', ')
-      : 'No selections found.';
+  constructor() {
+    this.myTokens = this.#getTokens(this.#defaultItems);
   }
 
-  #elementRef: ElementRef;
-  #idSvc: SkyIdService;
-
-  constructor(elementRef: ElementRef, idSvc: SkyIdService) {
-    this.#elementRef = elementRef;
-    this.#idSvc = idSvc;
-
-    this.panelId = this.#idSvc.generateId();
-    this.data = [
-      { id: this.#idSvc.generateId(), name: 'Puns' },
-      { id: this.#idSvc.generateId(), name: 'Riddles' },
-      { id: this.#idSvc.generateId(), name: 'Observations' },
-      { id: this.#idSvc.generateId(), name: 'Knock-knock' },
-      { id: this.#idSvc.generateId(), name: 'One-liners' },
-    ];
+  public ngOnDestroy(): void {
+    this.tokensController.complete();
   }
 
-  public async ngOnInit(): Promise<void> {
-    await this.#checkAccessibility();
+  public resetTokens(): void {
+    this.createTokens();
   }
 
-  public onFocus(): void {
-    this.panelOpen = false;
-    this.activeIndex = 0;
-    this.activeSelectionIndex = 0;
-    this.selectionsHasFocus = false;
+  public changeTokens(): void {
+    this.myTokens = this.#getTokens([
+      { label: 'Paid' },
+      { label: 'Pending' },
+      { label: 'Past due' },
+    ]);
   }
 
-  public async onKeyUp(evt: KeyboardEvent): Promise<void> {
-    const value = (evt.target as HTMLInputElement).value;
-    this.panelOpen = !!value;
-
-    if (!this.panelOpen) {
-      this.activeDescendant = undefined;
-    }
-
-    switch (evt.key) {
-      case 'ArrowLeft':
-        this.selectionsHasFocus = true;
-        this.activeSelectionIndex--;
-        if (this.activeSelectionIndex < 0) {
-          this.activeSelectionIndex = this.selections.length - 1;
-        }
-        this.activeDescendant =
-          this.selections[this.activeSelectionIndex].id + '_selection';
-        this.panelOpen = false;
-        break;
-      case 'ArrowRight':
-        this.selectionsHasFocus = true;
-        this.activeSelectionIndex++;
-        if (this.activeSelectionIndex > this.selections.length - 1) {
-          this.activeSelectionIndex = 0;
-        }
-        this.activeDescendant =
-          this.selections[this.activeSelectionIndex].id + '_selection';
-        this.panelOpen = false;
-        break;
-      default:
-        break;
-    }
-
-    if (this.panelOpen) {
-      switch (evt.key) {
-        case 'ArrowDown':
-          this.activeIndex++;
-          if (this.activeIndex > this.data.length - 1) {
-            this.activeIndex = 0;
-          }
-          this.activeDescendant = this.data[this.activeIndex].id;
-          break;
-        case 'ArrowUp':
-          this.activeIndex--;
-          if (this.activeIndex < 0) {
-            this.activeIndex = this.data.length - 1;
-          }
-          this.activeDescendant = this.data[this.activeIndex].id;
-          break;
-        case 'Escape':
-          this.panelOpen = false;
-          break;
-        case 'Enter':
-          this.selections.push({
-            id: this.data[this.activeIndex].id,
-            name: this.data[this.activeIndex].name,
-          });
-          this.activeDescendant = undefined;
-          this.activeIndex = 0;
-          this.inputEl.nativeElement.value = '';
-          this.panelOpen = false;
-          this.selectionsHasFocus = false;
-          break;
-      }
-    }
-
-    await this.#checkAccessibility();
+  public destroyTokens(): void {
+    this.myTokens = undefined;
   }
 
-  protected removeSelection(selection: Selection): void {
-    this.selections.splice(this.selections.indexOf(selection));
+  public createTokens(): void {
+    this.myTokens = this.#getTokens(this.#defaultItems);
   }
 
-  async #checkAccessibility(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          const results = await axe.run(this.#elementRef.nativeElement);
-          if (results.violations.length) {
-            console.log(parseMessage(results.violations));
-          } else {
-            console.log('Axe passed. Okay.');
-          }
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      });
+  public onTokenSelected(
+    args: SkyTokenSelectedEventArgs<TokensDemoItem>
+  ): void {
+    this.selectedToken = args.token.value.label;
+  }
+
+  public onFocusIndexUnderRange(): void {
+    console.log('Focus index was less than zero.');
+  }
+
+  public onFocusIndexOverRange(): void {
+    console.log('Focus index was greater than the number of tokens.');
+  }
+
+  public focusLastToken(): void {
+    this.tokensController.next({
+      type: SkyTokensMessageType.FocusLastToken,
+    });
+  }
+
+  #getTokens(data: TokensDemoItem[]): SkyToken<TokensDemoItem>[] {
+    return data.map((item) => {
+      return {
+        value: item,
+      };
     });
   }
 }
--- apps/code-examples/src/app/code-examples/indicators/tokens/custom/tokens-demo.module.ts
+++ apps/code-examples/src/app/code-examples/indicators/tokens/custom/tokens-demo.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { SkyIdModule } from '@skyux/core';
-import { SkyIconModule, SkyTokensModule } from '@skyux/indicators';
+import { SkyTokensModule } from '@skyux/indicators';
 
 import { TokensDemoComponent } from './tokens-demo.component';
 
 @NgModule({
-  imports: [CommonModule, SkyIconModule, SkyIdModule, SkyTokensModule],
+  imports: [CommonModule, SkyTokensModule],
   exports: [TokensDemoComponent],
   declarations: [TokensDemoComponent],
 })
