dd24b1d0943f6ea1e8647ce8464f466f6b3b9054
John White <750350+johnhwhite@users.noreply.github.com>
2022-10-18 16:56:13 -0400

    build: migrate to cypress 10 (#708)
    * build: migrate to cypress 10
    
    * Exact dependency

---
+++ apps/e2e/layout-storybook-e2e/cypress.config.ts
@@ -0,0 +1,21 @@
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+const cypressJsonConfig = {
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  video: false,
+  videosFolder: '../../../dist/cypress/apps/e2e/layout-storybook-e2e/videos',
+  screenshotsFolder:
+    '../../../dist/cypress/apps/e2e/layout-storybook-e2e/screenshots',
+  chromeWebSecurity: false,
+  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  supportFile: 'src/support/e2e.ts',
+};
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...cypressJsonConfig,
+  },
+});
