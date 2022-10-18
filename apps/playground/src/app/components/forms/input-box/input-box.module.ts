7d15414f4d5bdae50b5352d6d73354642376bdc2
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2022-10-18 10:41:50 -0400

    feat(components/forms): support status indicator errors for input box (#633) (#695)

--- apps/playground/src/app/components/forms/input-box/input-box.module.ts
+++ apps/playground/src/app/components/forms/input-box/input-box.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SkyIdModule } from '@skyux/core';
 import { SkyCharacterCounterModule, SkyInputBoxModule } from '@skyux/forms';
-import { SkyHelpInlineModule, SkyIconModule } from '@skyux/indicators';
+import {
+  SkyHelpInlineModule,
+  SkyIconModule,
+  SkyStatusIndicatorModule,
+} from '@skyux/indicators';
 
 import { InputBoxRoutingModule } from './input-box-routing.module';
 import { InputBoxVisualHostComponent } from './input-box-visual-host.component';
@@ -21,6 +25,7 @@ import { InputBoxComponent } from './input-box.component';
     SkyIconModule,
     SkyIdModule,
     SkyInputBoxModule,
+    SkyStatusIndicatorModule,
   ],
 })
 export class InputBoxModule {
