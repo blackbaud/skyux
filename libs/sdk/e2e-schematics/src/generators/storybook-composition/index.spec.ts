1fe590351ea2cdb7ee2fb2f73c976381d218c484
John White <750350+johnhwhite@users.noreply.github.com>
2022-10-21 06:26:13 -0400

    Fix storybook composition being called from powershell

--- libs/sdk/e2e-schematics/src/generators/storybook-composition/index.spec.ts
+++ libs/sdk/e2e-schematics/src/generators/storybook-composition/index.spec.ts
@@ -2,7 +2,7 @@ import {
   applicationGenerator,
   storybookConfigurationGenerator,
 } from '@nrwl/angular/generators';
-import { createTreeWithEmptyV1Workspace } from '@nrwl/devkit/testing';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { Linter } from '@nrwl/linter';
 
 import configureStorybook from '../configure-storybook';
@@ -11,7 +11,7 @@ import generateStorybookComposition from './index';
 
 describe('storybook-composition', () => {
   it('should create composition', async () => {
-    const tree = createTreeWithEmptyV1Workspace();
+    const tree = createTreeWithEmptyWorkspace();
     tree.write('.gitignore', '#');
     for (const name of ['storybook', 'test-app']) {
       await applicationGenerator(tree, { name });
@@ -35,7 +35,7 @@ describe('storybook-composition', () => {
   });
 
   it('should skip non-storybook project', async () => {
-    const tree = createTreeWithEmptyV1Workspace();
+    const tree = createTreeWithEmptyWorkspace();
     tree.write('.gitignore', '#');
     for (const name of ['storybook', 'test-app']) {
       await applicationGenerator(tree, { name });
@@ -62,7 +62,7 @@ describe('storybook-composition', () => {
   });
 
   it('should error without storybook project', async () => {
-    const tree = createTreeWithEmptyV1Workspace();
+    const tree = createTreeWithEmptyWorkspace();
     const spy = jest.spyOn(console, 'error');
     await generateStorybookComposition(tree, {
       projectsJson: JSON.stringify(['test-app']),
@@ -76,5 +76,60 @@ describe('storybook-composition', () => {
     expect(spy).toHaveBeenCalledWith(
       `Unable to load a project named "storybook"`
     );
+    await applicationGenerator(tree, { name: 'storybook' });
+    await storybookConfigurationGenerator(tree, {
+      configureCypress: false,
+      generateCypressSpecs: false,
+      generateStories: false,
+      linter: Linter.None,
+      name: 'storybook',
+    });
+    await configureStorybook(tree, { name: 'storybook' });
+    await generateStorybookComposition(tree, {
+      projectsJson: '["test-app]',
+      baseUrl: '../storybooks',
+    });
+    await generateStorybookComposition(tree, {
+      projectsJson: '["test-app]',
+      baseUrl: '../storybooks',
+      ansiColor: false,
+    });
+    expect(spy).toHaveBeenCalledWith(
+      `Unable to parse projectsJson: ["test-app]`
+    );
+  });
+
+  it('should handle parameters without quotes', async () => {
+    const tree = createTreeWithEmptyWorkspace();
+    tree.write('.gitignore', '#');
+    for (const name of [
+      'storybook',
+      'test-app-one',
+      'test-app-two',
+      'test-app-three',
+    ]) {
+      await applicationGenerator(tree, { name });
+      await storybookConfigurationGenerator(tree, {
+        configureCypress: false,
+        generateCypressSpecs: false,
+        generateStories: false,
+        linter: Linter.None,
+        name,
+      });
+      await configureStorybook(tree, { name });
+    }
+    const storybookMain = 'apps/storybook/.storybook/main.ts';
+    expect(tree.isFile(storybookMain)).toBeTruthy();
+    expect(tree.read(storybookMain, 'utf-8')).not.toContain('test-app');
+    await generateStorybookComposition(tree, {
+      projectsJson: '[test-app-one, test-app-two]',
+      baseUrl: '../storybooks',
+    });
+    expect(tree.read(storybookMain, 'utf-8')).toContain('test-app-one');
+    await generateStorybookComposition(tree, {
+      projectsJson: 'test-app-one, test-app-two, test-app-three',
+      baseUrl: '../storybooks',
+    });
+    expect(tree.read(storybookMain, 'utf-8')).toContain('test-app-three');
   });
 });
