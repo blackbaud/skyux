33ca940025b439eba335ee5f980ca0049ec29bf7
John White <750350+johnhwhite@users.noreply.github.com>
2022-11-03 12:38:28 -0400

    Update key-info example to work with strict mode.

--- apps/code-examples/src/app/code-examples/indicators/key-info/inline-help/key-info-demo.component.ts
+++ apps/code-examples/src/app/code-examples/indicators/key-info/inline-help/key-info-demo.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SkyKeyInfoLayoutType } from '@skyux/indicators';
 
 @Component({
   selector: 'app-key-info-demo',
   templateUrl: './key-info-demo.component.html',
 })
 export class KeyInfoDemoComponent {
-  public layout = 'vertical';
+  @Input()
+  public set value(value: number | undefined) {
+    this.#_value = value;
+    this.layout = this.#_value >= 100 ? 'vertical' : 'horizontal';
+  }
+
+  public get value(): number {
+    return this.#_value;
+  }
+
+  public layout: SkyKeyInfoLayoutType = 'vertical';
+
+  #_value: number | undefined = 575;
 
   public onActionClick(): void {
     alert('Help inline button clicked!');
