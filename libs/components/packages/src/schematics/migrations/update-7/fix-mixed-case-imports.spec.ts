6af6826f14b85df71aab7d032bd2efbe56fbf562
John White <750350+johnhwhite@users.noreply.github.com>
2022-11-10 12:35:48 -0500

    switch to using test scaffold

--- libs/components/packages/src/schematics/migrations/update-7/fix-mixed-case-imports.spec.ts
+++ libs/components/packages/src/schematics/migrations/update-7/fix-mixed-case-imports.spec.ts
@@ -1,62 +1,54 @@
-import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+
+import { createTestApp } from '../../testing/scaffold';
 
 describe('fix mixed case imports', () => {
   const runner = new SchematicTestRunner(
     'schematics',
     require.resolve('../migration-collection.json')
   );
-  const angularJson = {
-    version: 1,
-    projects: {
-      test: {
-        projectType: 'application',
-        root: '',
-        architect: {},
-      },
-    },
-  };
 
-  function setupTest() {
-    const tree = Tree.empty();
-    tree.create('/angular.json', JSON.stringify(angularJson));
+  async function setupTest() {
+    const tree = await createTestApp(runner, {
+      projectName: 'my-app',
+    });
     const runSchematicAsync = async () =>
       runner.runSchematicAsync('fix-mixed-case-imports', {}, tree).toPromise();
     return { tree, runSchematicAsync };
   }
 
   it('should not change import paths that are correct', async () => {
-    const { tree, runSchematicAsync } = setupTest();
-    tree.create('/index.ts', `import { A } from './a';`);
-    tree.create('/a.ts', `export class A {}`);
-    tree.create('/empty.ts', ``);
+    const { tree, runSchematicAsync } = await setupTest();
+    tree.create('/src/index.ts', `import { A } from './a';`);
+    tree.create('/src/a.ts', `export class A {}`);
+    tree.create('/src/empty.ts', ``);
     await runSchematicAsync();
-    expect(tree.readText('/index.ts')).toBe(`import { A } from './a';`);
+    expect(tree.readText('/src/index.ts')).toBe(`import { A } from './a';`);
   });
 
   it('should update import paths with backslashes', async () => {
-    const { tree, runSchematicAsync } = setupTest();
-    tree.create('/windows.ts', "import { A } from '.\\a';");
-    tree.create('/a.ts', `export class A {}`);
+    const { tree, runSchematicAsync } = await setupTest();
+    tree.create('/src/windows.ts', "import { A } from '.\\a';");
+    tree.create('/src/a.ts', `export class A {}`);
     await runSchematicAsync();
-    expect(tree.readText('/windows.ts')).toBe(`import { A } from './a';`);
+    expect(tree.readText('/src/windows.ts')).toBe(`import { A } from './a';`);
   });
 
   it('should change import paths that are incorrect', async () => {
-    const { tree, runSchematicAsync } = setupTest();
+    const { tree, runSchematicAsync } = await setupTest();
     tree.create(
-      '/index.ts',
+      '/src/index.ts',
       `import { A } from './A';
       import { C } from './path/c';
       import { D } from './Path/D';
       import { NonExistent } from './non';
       export class B extends A {}`
     );
-    tree.create('/a.ts', `export class A {}`);
-    tree.create('/Path/C.ts', `export class C {}`);
-    tree.create('/Path/d.ts', `export class D {}`);
+    tree.create('/src/a.ts', `export class A {}`);
+    tree.create('/src/Path/C.ts', `export class C {}`);
+    tree.create('/src/Path/d.ts', `export class D {}`);
     await runSchematicAsync();
-    expect(tree.readText('/index.ts')).toEqual(
+    expect(tree.readText('/src/index.ts')).toEqual(
       `import { A } from './a';
       import { C } from './Path/C';
       import { D } from './Path/d';
