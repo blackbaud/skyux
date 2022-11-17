89a990fb3d48edb0a70e5758d25dbd510b621fc1
Steve Brush <steve.brush@blackbaud.com>
2022-11-17 10:32:24 -0500

    fix(components/packages): remove package before installing it to prevent duplicates (#839)

--- libs/components/packages/src/schematics/migrations/update-7/add-axe-core-dependency.ts
+++ libs/components/packages/src/schematics/migrations/update-7/add-axe-core-dependency.ts
@@ -1,33 +1,10 @@
 import { Rule } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import {
-  NodeDependencyType,
-  addPackageJsonDependency,
-} from '@schematics/angular/utility/dependencies';
+import { NodeDependencyType } from '@schematics/angular/utility/dependencies';
 
-import { readRequiredFile } from '../../utility/tree';
+import { ensurePeersInstalled } from '../../rules/ensure-peers-installed';
 
 export default function (): Rule {
-  return async (tree, context) => {
-    const packageJson: {
-      dependencies?: Record<string, string>;
-      devDependencies?: Record<string, string>;
-    } = JSON.parse(readRequiredFile(tree, '/package.json'));
-
-    const dependencies: Record<string, string> = {
-      ...(packageJson.dependencies || {}),
-      ...(packageJson.devDependencies || {}),
-    };
-
-    if (dependencies['@skyux-sdk/testing']) {
-      context.addTask(new NodePackageInstallTask());
-
-      addPackageJsonDependency(tree, {
-        type: NodeDependencyType.Dev,
-        name: 'axe-core',
-        version: '3.5.6',
-        overwrite: false,
-      });
-    }
-  };
+  return ensurePeersInstalled('@skyux-sdk/testing', [
+    { name: 'axe-core', version: '3.5.6', type: NodeDependencyType.Dev },
+  ]);
 }
--- libs/components/packages/src/schematics/migrations/update-7/add-moment-dependency.ts
+++ libs/components/packages/src/schematics/migrations/update-7/add-moment-dependency.ts
@@ -1,33 +1,10 @@
 import { Rule } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import {
-  NodeDependencyType,
-  addPackageJsonDependency,
-} from '@schematics/angular/utility/dependencies';
+import { NodeDependencyType } from '@schematics/angular/utility/dependencies';
 
-import { readRequiredFile } from '../../utility/tree';
+import { ensurePeersInstalled } from '../../rules/ensure-peers-installed';
 
 export default function (): Rule {
-  return async (tree, context) => {
-    const packageJson: {
-      dependencies?: Record<string, string>;
-      devDependencies?: Record<string, string>;
-    } = JSON.parse(readRequiredFile(tree, '/package.json'));
-
-    const dependencies: Record<string, string> = {
-      ...(packageJson.dependencies || {}),
-      ...(packageJson.devDependencies || {}),
-    };
-
-    if (dependencies['@skyux/datetime']) {
-      context.addTask(new NodePackageInstallTask());
-
-      addPackageJsonDependency(tree, {
-        type: NodeDependencyType.Default,
-        name: 'moment',
-        version: '2.29.4',
-        overwrite: false,
-      });
-    }
-  };
+  return ensurePeersInstalled('@skyux/datetime', [
+    { name: 'moment', version: '2.29.4', type: NodeDependencyType.Default },
+  ]);
 }
