0b1a9305ebc6fe93800933949bf84aeba4f6d59a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-11-17 12:43:19 -0500

    fix(components/modals): use `Provider[]` instead of `StaticProvider[]` for modal configuration

--- libs/components/modals/src/lib/modules/modal/modal.interface.ts
+++ libs/components/modals/src/lib/modules/modal/modal.interface.ts
@@ -1,4 +1,4 @@
-import { StaticProvider } from '@angular/core';
+import { Provider } from '@angular/core';
 
 // TODO: defaults won't show in the generated docs until this work is done:
 // https://github.com/blackbaud/skyux-docs-tools/issues/38
@@ -24,7 +24,7 @@ export interface SkyModalConfigurationInterface {
    * In Angular, a provider is something that can create or deliver a service.
    * This property can be used to pass context values from the component that launches the modal to the modal component.
    */
-  providers?: StaticProvider[];
+  providers?: Provider[];
 
   /**
    * Specifies the HTML element ID (without the leading `#`) of the element that describes
