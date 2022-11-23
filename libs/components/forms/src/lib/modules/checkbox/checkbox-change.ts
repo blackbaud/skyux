08bbcb81e6139ea9751bc0ba8aa041e3f0f77b3f
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-11-23 11:14:08 -0500

    fix(components/forms): revert accidental breaking change of the checkbox component's id property (#852)

--- libs/components/forms/src/lib/modules/checkbox/checkbox-change.ts
+++ libs/components/forms/src/lib/modules/checkbox/checkbox-change.ts
@@ -5,6 +5,7 @@ import { SkyCheckboxComponent } from './checkbox.component';
  * Fires when users select or deselect the checkbox.
  */
 export class SkyCheckboxChange {
+  // TODO: Consider if we should still be exporting this component as it makes it a part of our public API
   public source: SkyCheckboxComponent | undefined;
   public checked: boolean | undefined;
 }
