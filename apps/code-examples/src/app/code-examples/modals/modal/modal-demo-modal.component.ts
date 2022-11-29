8ddec3d0d7bc5778133df66a54544df013ed90bb
Paul Crowder <paul.crowder@blackbaud.com>
2022-11-29 16:42:26 -0500

    docs(code-examples/modals): add provider and data service to basic demo

--- apps/code-examples/src/app/code-examples/modals/modal/modal-demo-modal.component.ts
+++ apps/code-examples/src/app/code-examples/modals/modal/modal-demo-modal.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { SkyWaitService } from '@skyux/indicators';
 import { SkyModalInstance } from '@skyux/modals';
+
+import { ModalDemoContext } from './modal-demo-context';
+import { ModalDemoDataService } from './modal-demo-data.service';
 
 @Component({
   selector: 'app-modal-demo-modal',
   templateUrl: './modal-demo-modal.component.html',
 })
 export class ModalDemoModalComponent {
-  constructor(public instance: SkyModalInstance) {}
+  public demoForm: FormGroup<{
+    value1?: FormControl<string>;
+  }>;
+
+  #dataSvc: ModalDemoDataService;
+  #instance: SkyModalInstance;
+  #waitSvc: SkyWaitService;
+
+  constructor(
+    instance: SkyModalInstance,
+    waitSvc: SkyWaitService,
+    dataSvc: ModalDemoDataService,
+    context: ModalDemoContext,
+    fb: FormBuilder
+  ) {
+    this.#instance = instance;
+    this.#waitSvc = waitSvc;
+    this.#dataSvc = dataSvc;
+
+    this.demoForm = fb.group(context.data);
+  }
+
+  public saveForm(): void {
+    // Use the data service to save the data.
+    this.#waitSvc
+      .blockingWrap(this.#dataSvc.save(this.demoForm.value))
+      .subscribe((data) => {
+        // Notify the modal instance that data was saved and return the saved data.
+        this.#instance.save(data);
+      });
+  }
+
+  public cancelForm(): void {
+    this.#instance.cancel();
+  }
 }
--- apps/code-examples/src/app/code-examples/modals/modal/modal-demo.module.ts
+++ apps/code-examples/src/app/code-examples/modals/modal/modal-demo.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SkyIdModule } from '@skyux/core';
 import { SkyInputBoxModule } from '@skyux/forms';
 import { SkyModalModule } from '@skyux/modals';
@@ -12,6 +12,7 @@ import { ModalDemoComponent } from './modal-demo.component';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     SkyIdModule,
     SkyInputBoxModule,
     SkyModalModule,
