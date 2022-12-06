379826e47f29a998ce2e69ca3386018b33ca7f65
John White <750350+johnhwhite@users.noreply.github.com>
2022-12-06 16:37:29 -0500

    fix(components/flyout): viewkeeper z-index adjustment

---
+++ apps/playground/src/app/components/flyout/flyout/data-manager/data-manager-edit-modal-context.ts
@@ -0,0 +1,5 @@
+import { SkyAgGridDemoRow } from './data-manager-data';
+
+export class DataManagerEditModalContext {
+  public gridData: SkyAgGridDemoRow[] = [];
+}
---
+++ apps/playground/src/app/components/flyout/flyout/data-manager/data-manager.component.ts
@@ -0,0 +1,127 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import {
+  SkyDataManagerService,
+  SkyDataManagerState,
+} from '@skyux/data-manager';
+import { SkyModalCloseArgs, SkyModalService } from '@skyux/modals';
+
+import { SKY_AG_GRID_DEMO_DATA } from './data-manager-data';
+import { DataManagerEditModalContext } from './data-manager-edit-modal-context';
+import { DataManagerEditModalComponent } from './data-manager-edit-modal.component';
+import { DataManagerFiltersModalComponent } from './data-manager-filter-modal.component';
+
+@Component({
+  selector: 'app-data-manager',
+  templateUrl: './data-manager.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [SkyDataManagerService],
+})
+export class DataManagerComponent implements OnInit {
+  public items = SKY_AG_GRID_DEMO_DATA;
+
+  public activeViewId = 'dataEntryGridWithDataManagerView';
+
+  public dataState = new SkyDataManagerState({});
+
+  public dataManagerConfig = {
+    filterModalComponent: DataManagerFiltersModalComponent,
+    sortOptions: [
+      {
+        id: 'az',
+        label: 'Name (A - Z)',
+        descending: false,
+        propertyName: 'name',
+      },
+      {
+        id: 'za',
+        label: 'Name (Z - A)',
+        descending: true,
+        propertyName: 'name',
+      },
+    ],
+  };
+
+  public defaultDataState = new SkyDataManagerState({
+    filterData: {
+      filtersApplied: false,
+      filters: {
+        hideSales: false,
+      },
+    },
+    views: [
+      {
+        viewId: 'dataEntryGridWithDataManagerView',
+        displayedColumnIds: [
+          'selected',
+          'name',
+          'age',
+          'startDate',
+          'endDate',
+          'department',
+          'jobTitle',
+          'validationCurrency',
+          'validationDate',
+        ],
+      },
+    ],
+  });
+
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+    private dataManagerService: SkyDataManagerService,
+    private modalService: SkyModalService
+  ) {
+    this.dataManagerService
+      .getDataStateUpdates('dataEntryGridDataManager')
+      .subscribe((state) => {
+        this.dataState = state;
+        this.changeDetector.detectChanges();
+      });
+    this.dataManagerService
+      .getActiveViewIdUpdates()
+      .subscribe((activeViewId) => {
+        this.activeViewId = activeViewId;
+        this.changeDetector.detectChanges();
+      });
+  }
+
+  public ngOnInit(): void {
+    this.dataManagerService.initDataManager({
+      activeViewId: this.activeViewId,
+      dataManagerConfig: this.dataManagerConfig,
+      defaultDataState: this.defaultDataState,
+    });
+  }
+
+  public openModal(): void {
+    const context = new DataManagerEditModalContext();
+    context.gridData = this.items.slice();
+    this.changeDetector.markForCheck();
+
+    const options = {
+      providers: [{ provide: DataManagerEditModalContext, useValue: context }],
+      ariaDescribedBy: 'docs-edit-grid-modal-content',
+      size: 'large',
+    };
+
+    const modalInstance = this.modalService.open(
+      DataManagerEditModalComponent,
+      options
+    );
+
+    modalInstance.closed.subscribe((result: SkyModalCloseArgs) => {
+      if (result.reason === 'cancel' || result.reason === 'close') {
+        alert('Edits canceled!');
+      } else {
+        this.items = result.data;
+        alert('Saving data!');
+      }
+      this.changeDetector.markForCheck();
+    });
+  }
+}
