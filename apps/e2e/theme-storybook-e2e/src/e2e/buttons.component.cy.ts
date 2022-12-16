a51235b7751e5beb35be88c9174b5d392d45e828
John White <750350+johnhwhite@users.noreply.github.com>
2022-12-16 18:24:47 -0500

    Update bundle budget. Tweak e2e tests.

--- apps/e2e/theme-storybook-e2e/src/e2e/buttons.component.cy.ts
+++ apps/e2e/theme-storybook-e2e/src/e2e/buttons.component.cy.ts
@@ -3,18 +3,21 @@ import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 describe('theme-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
-      beforeEach(() =>
+      beforeEach(() => {
+        cy.viewport(E2eVariations.DISPLAY_WIDTHS[0], 800);
         cy.visit(
           `/iframe.html?globals=theme:${theme}&id=buttonscomponent-buttons--buttons`
-        )
-      );
+        );
+      });
       it('should render the component', () => {
         cy.get('app-buttons')
           .should('exist')
           .should('be.visible')
+          .end()
+          .document()
           .screenshot(`buttonscomponent-buttons--buttons-${theme}`)
           .percySnapshot(`buttonscomponent-buttons--buttons-${theme}`, {
-            widths: E2eVariations.MOBILE_WIDTHS,
+            widths: E2eVariations.DISPLAY_WIDTHS,
           });
       });
     });
--- apps/e2e/theme-storybook-e2e/src/e2e/responsive.component.cy.ts
+++ apps/e2e/theme-storybook-e2e/src/e2e/responsive.component.cy.ts
@@ -3,11 +3,12 @@ import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 describe('theme-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
-      beforeEach(() =>
+      beforeEach(() => {
+        cy.viewport(E2eVariations.DISPLAY_WIDTHS[0], 800);
         cy.visit(
           `/iframe.html?globals=theme:${theme}&id=responsivecomponent-responsive--responsive`
-        )
-      );
+        );
+      });
       it('should render the component', () => {
         cy.get('#containers')
           .should('exist')
