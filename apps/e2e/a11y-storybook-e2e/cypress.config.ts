cb37788c54bdf7fefa81498deefaf108b52b47c4
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-12-23 11:39:29 -0500

    test(components/a11y): add visual testing infastructure for a11y (#903)

---
+++ apps/e2e/a11y-storybook-e2e/cypress.config.ts
@@ -0,0 +1,10 @@
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    video: false,
+  },
+});
