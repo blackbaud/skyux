674e7d10fb9c48bccd568333e49143917750a833
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-12-23 11:31:22 -0500

    Add back base skip links story

--- apps/e2e/a11y-storybook/src/app/app.module.ts
+++ apps/e2e/a11y-storybook/src/app/app.module.ts
@@ -4,7 +4,13 @@ import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-const routes: Route[] = [];
+const routes: Route[] = [
+  {
+    path: 'skip-link',
+    loadChildren: () =>
+      import('./skip-link/skip-link.module').then((m) => m.SkipLinkModule),
+  },
+];
 if (routes.length > 0 && routes.findIndex((r) => r.path === '') === -1) {
   routes.push({ path: '', redirectTo: `${routes[0].path}`, pathMatch: 'full' });
 }
