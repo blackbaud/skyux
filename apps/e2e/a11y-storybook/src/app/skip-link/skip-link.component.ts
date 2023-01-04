b70da2f455e3432acacec21dbda0248d797dd8f0
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-01-04 15:23:04 -0500

    Focus in TypeScript

--- apps/e2e/a11y-storybook/src/app/skip-link/skip-link.component.ts
+++ apps/e2e/a11y-storybook/src/app/skip-link/skip-link.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { SkySkipLinkService } from '@skyux/a11y';
 
 @Component({
@@ -10,10 +16,15 @@ export class SkipLinkComponent implements AfterViewInit {
   @ViewChild('skipLink', { read: ElementRef })
   public skipLink: ElementRef | undefined;
 
+  #changeDetector: ChangeDetectorRef;
   #skipLinkService: SkySkipLinkService;
 
-  constructor(skipLinkService: SkySkipLinkService) {
+  constructor(
+    skipLinkService: SkySkipLinkService,
+    changeDetector: ChangeDetectorRef
+  ) {
     this.#skipLinkService = skipLinkService;
+    this.#changeDetector = changeDetector;
   }
 
   public ngAfterViewInit(): void {
@@ -26,6 +37,9 @@ export class SkipLinkComponent implements AfterViewInit {
           },
         ],
       });
+
+      this.#changeDetector.detectChanges();
+      (document.querySelector('.sky-skip-link') as HTMLElement).focus();
     }
   }
 }
