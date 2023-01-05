4e739f67106cba62b3d176493b96a818a47f06fa
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-01-05 16:16:19 -0500

    test(components/a11y): skip links visual tests (#907)

--- apps/e2e/a11y-storybook-e2e/src/e2e/skip-link.component.cy.ts
+++ apps/e2e/a11y-storybook-e2e/src/e2e/skip-link.component.cy.ts
@@ -4,18 +4,19 @@ describe('a11y-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
       beforeEach(() =>
-        cy.visit(
-          `/iframe.html?globals=theme:${theme}&id=skiplinkcomponent-skiplink--skip-link`
-        )
+        cy
+          .viewport(500, 200)
+          .visit(
+            `/iframe.html?globals=theme:${theme}&id=skiplinkcomponent-skiplink--skip-link`
+          )
       );
       it('should render the component', () => {
-        cy.get('app-skip-link')
-          .should('exist')
-          .should('be.visible')
-          .screenshot(`skiplinkcomponent-skiplink--skip-link-${theme}`)
-          .percySnapshot(`skiplinkcomponent-skiplink--skip-link-${theme}`, {
-            widths: E2eVariations.DISPLAY_WIDTHS,
-          });
+        cy.get('.sky-skip-link').should('exist').should('be.visible').focus();
+        cy.skyVisualTest(`skiplinkcomponent-skiplink--skip-link-${theme}`, {
+          capture: 'fullPage',
+          overwrite: true,
+          disableTimersAndAnimations: true,
+        });
       });
     });
   });
--- apps/e2e/a11y-storybook/src/app/skip-link/skip-link.component.ts
+++ apps/e2e/a11y-storybook/src/app/skip-link/skip-link.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { SkySkipLinkService } from '@skyux/a11y';
 
 @Component({
   selector: 'app-skip-link',
   templateUrl: './skip-link.component.html',
   styleUrls: ['./skip-link.component.scss'],
 })
-export class SkipLinkComponent {}
+export class SkipLinkComponent implements AfterViewInit {
+  @ViewChild('skipLink', { read: ElementRef })
+  public skipLink: ElementRef | undefined;
+
+  #skipLinkService: SkySkipLinkService;
+
+  constructor(skipLinkService: SkySkipLinkService) {
+    this.#skipLinkService = skipLinkService;
+  }
+
+  public ngAfterViewInit(): void {
+    if (this.skipLink) {
+      this.#skipLinkService.setSkipLinks({
+        links: [
+          {
+            title: 'Area 1',
+            elementRef: this.skipLink,
+          },
+        ],
+      });
+    }
+  }
+}
