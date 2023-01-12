30fbd6d0d54bfc792bcc327aaabf78aa03a9a7d2
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-01-12 15:14:26 -0500

    refactor(components/indicators): implement dynamic component service for service constructed waits

--- apps/playground/src/app/components/indicators/wait/wait.component.ts
+++ apps/playground/src/app/components/indicators/wait/wait.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { SkyWaitService } from '@skyux/indicators';
 
 @Component({
   selector: 'app-wait',
   templateUrl: './wait.component.html',
-  styleUrls: ['./wait.component.scss'],
 })
 export class WaitComponent {
   public isWaiting: boolean;
   public isFullPageWaiting: boolean;
+  public isServiceFullPageWaiting: boolean;
   public isNonBlocking: boolean;
+
+  #changeDetector: ChangeDetectorRef;
+  #waitSvc: SkyWaitService;
+
+  constructor(changeDetector: ChangeDetectorRef, waitSvc: SkyWaitService) {
+    this.#changeDetector = changeDetector;
+    this.#waitSvc = waitSvc;
+  }
+
+  public toggleFullPageWait(): void {
+    this.isFullPageWaiting = !this.isFullPageWaiting;
+
+    if (this.isFullPageWaiting) {
+      setTimeout(() => {
+        this.isFullPageWaiting = false;
+        this.#changeDetector.markForCheck();
+      }, 5000);
+    }
+  }
+
+  public toggleFullPageWaitViaService(): void {
+    this.isServiceFullPageWaiting = !this.isServiceFullPageWaiting;
+    if (this.isServiceFullPageWaiting) {
+      if (this.isNonBlocking) {
+        this.#waitSvc.beginNonBlockingPageWait();
+      } else {
+        this.#waitSvc.beginBlockingPageWait();
+      }
+      setTimeout(() => {
+        this.isServiceFullPageWaiting = false;
+        this.#waitSvc.clearAllPageWaits();
+        this.#waitSvc.dispose();
+        this.#changeDetector.markForCheck();
+      }, 5000);
+    } else {
+      this.#waitSvc.clearAllPageWaits();
+      this.#waitSvc.dispose();
+    }
+  }
 }
