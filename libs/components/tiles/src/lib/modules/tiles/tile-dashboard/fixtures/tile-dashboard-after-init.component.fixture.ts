3511d61b23239825d7b267b2183c80b17ba4852a
Paul Crowder <paul.crowder@blackbaud.com>
2023-01-20 12:20:00 -0500

    fix(components/tiles): render tile dashboard if config is set after init

---
+++ libs/components/tiles/src/lib/modules/tiles/tile-dashboard/fixtures/tile-dashboard-after-init.component.fixture.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { SkyTileDashboardConfig } from '../../tile-dashboard-config/tile-dashboard-config';
+
+@Component({
+  selector: 'sky-demo-app',
+  templateUrl: './tile-dashboard-after-init.component.fixture.html',
+})
+export class TileDashboardAfterInitTestComponent {
+  public dashboardConfig: SkyTileDashboardConfig | undefined;
+}
--- libs/components/tiles/src/lib/modules/tiles/tile-dashboard/fixtures/tile-dashboard-fixtures.module.ts
+++ libs/components/tiles/src/lib/modules/tiles/tile-dashboard/fixtures/tile-dashboard-fixtures.module.ts
@@ -5,6 +5,7 @@ import { SkyTilesModule } from '../../tiles.module';
 
 import { Tile1TestComponent } from './tile1.component.fixture';
 import { Tile2TestComponent } from './tile2.component.fixture';
+import { TileDashboardAfterInitTestComponent } from './tile-dashboard-after-init.component.fixture';
 import { TileDashboardOnPushTestComponent } from './tile-dashboard-on-push.component.fixture';
 import { TileDashboardTestComponent } from './tile-dashboard.component.fixture';
 
@@ -12,15 +13,10 @@ import { TileDashboardTestComponent } from './tile-dashboard.component.fixture';
   declarations: [
     Tile1TestComponent,
     Tile2TestComponent,
+    TileDashboardAfterInitTestComponent,
     TileDashboardTestComponent,
     TileDashboardOnPushTestComponent,
   ],
   imports: [CommonModule, SkyTilesModule],
-  exports: [
-    Tile1TestComponent,
-    Tile2TestComponent,
-    TileDashboardTestComponent,
-    TileDashboardOnPushTestComponent,
-  ],
 })
 export class SkyTileDashboardFixturesModule {}
