1a2d7ef28e100a30d2a5d755b7a4532830d2e318
John White <750350+johnhwhite@users.noreply.github.com>
2023-01-23 15:37:47 -0500

    feat(components/router): test harness for skyhref (#934)
    * feat(components/router): test harness for skyhref
    
    * update harness
    
    * update dependencies
    
    * Test coverage and lint fix
    
    * updates based on feedback
    
    * fix test
    
    * use expectAsync in test, add return type to setupTest

--- apps/code-examples/src/app/code-examples/router/href/basic/sky-href-demo.component.spec.ts
+++ apps/code-examples/src/app/code-examples/router/href/basic/sky-href-demo.component.spec.ts
@@ -1,70 +1,80 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { SkyHrefModule, SkyHrefResolverService } from '@skyux/router';
+import { SkyHrefHarness, SkyHrefTestingModule } from '@skyux/router/testing';
 
-import { MockResolverService } from './fixtures/mock-resolver-service';
 import { SkyHrefDemoComponent } from './sky-href-demo.component';
 
 describe('SkyHrefDemoComponent', () => {
   let component: SkyHrefDemoComponent;
   let fixture: ComponentFixture<SkyHrefDemoComponent>;
-  let resolverService: MockResolverService;
 
-  async function setup(userHasAccess: boolean): Promise<void> {
-    resolverService = new MockResolverService(userHasAccess);
-
+  async function setup(options: {
+    userHasAccess: boolean;
+    dataSkyId: string;
+  }): Promise<{
+    fixture: ComponentFixture<SkyHrefDemoComponent>;
+    loader: HarnessLoader;
+    hrefHarness: SkyHrefHarness;
+  }> {
     TestBed.configureTestingModule({
       declarations: [SkyHrefDemoComponent],
-      imports: [SkyHrefModule],
-      providers: [
-        {
-          provide: SkyHrefResolverService,
-          useValue: resolverService,
-        },
+      imports: [
+        SkyHrefTestingModule.with({ userHasAccess: options.userHasAccess }),
       ],
     });
 
     fixture = TestBed.createComponent(SkyHrefDemoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    return fixture.whenStable().then();
+    await fixture.whenStable();
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+    const hrefHarness = await loader.getHarness(
+      SkyHrefHarness.with({ dataSkyId: options.dataSkyId })
+    );
+    return { fixture, loader, hrefHarness };
   }
 
   it('should create', async () => {
-    await setup(true);
+    await setup({ userHasAccess: true, dataSkyId: 'my-href-allow' });
     expect(component).toBeTruthy();
   });
 
   it('should show skyhref with access', async () => {
-    await setup(true);
-    expect(fixture.debugElement.query(By.css('#allow'))).toBeTruthy();
-    expect(
-      fixture.debugElement.query(By.css('#allow')).attributes['href']
-    ).toBeTruthy();
-    expect(
-      fixture.debugElement.query(By.css('#allow')).attributes['hidden']
-    ).toBeFalsy();
+    const { hrefHarness } = await setup({
+      userHasAccess: true,
+      dataSkyId: 'my-href-allow',
+    });
+    expect(hrefHarness).toBeTruthy();
+    await expectAsync(hrefHarness.getHref()).toBeResolvedTo(
+      'allow://example.com'
+    );
+    await expectAsync(hrefHarness.getText()).toBeResolvedTo(
+      'Example.com with “allow” protocol'
+    );
+    await expectAsync(hrefHarness.isVisible()).toBeResolvedTo(true);
   });
 
   it('should hide skyhref without access', async () => {
-    await setup(false);
-    expect(fixture.debugElement.query(By.css('#hidden'))).toBeTruthy();
-    expect(
-      fixture.debugElement.query(By.css('#hidden')).attributes['href']
-    ).toBeFalsy();
-    expect(
-      fixture.debugElement.query(By.css('#hidden')).attributes['hidden']
-    ).toBeTruthy();
+    const { hrefHarness } = await setup({
+      userHasAccess: false,
+      dataSkyId: 'my-href-hidden',
+    });
+    expect(hrefHarness).toBeTruthy();
+    await expectAsync(hrefHarness.getHref()).toBeResolvedTo(null);
+    await expectAsync(hrefHarness.getText()).toBeResolvedTo('');
+    await expectAsync(hrefHarness.isVisible()).toBeResolvedTo(false);
   });
 
   it('should unlink skyhref without access', async () => {
-    await setup(false);
-    expect(fixture.debugElement.query(By.css('#unlinked'))).toBeTruthy();
-    expect(
-      fixture.debugElement.query(By.css('#unlinked')).attributes['href']
-    ).toBeFalsy();
-    expect(
-      fixture.debugElement.query(By.css('#unlinked')).attributes['hidden']
-    ).toBeFalsy();
+    const { hrefHarness } = await setup({
+      userHasAccess: false,
+      dataSkyId: 'my-href-unlinked',
+    });
+    await expectAsync(hrefHarness.getHref()).toBeResolvedTo(null);
+    await expectAsync(hrefHarness.getText()).toBeResolvedTo(
+      'Example.com with “deny” protocol'
+    );
+    await expectAsync(hrefHarness.isVisible()).toBeResolvedTo(true);
   });
 });
---
+++ libs/components/router/testing/src/href/href-harness-filters.ts
@@ -0,0 +1,8 @@
+import { SkyHarnessFilters } from '@skyux/core/testing';
+
+/**
+ * A set of criteria that can be used to filter a list of `SkyHrefHarness` instances.
+ * @internal
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SkyHrefHarnessFilters extends SkyHarnessFilters {}
