b3cc7f8fc302e7a0c4b5870c1af500bdf2228fc0
Corey Archer <corey.archer@blackbaud.me>
2023-01-23 15:27:24 -0500

    add test to dynamiccomponentservice

--- libs/components/core/src/lib/modules/dynamic-component/fixtures/dynamic-component-test.component.fixture.ts
+++ libs/components/core/src/lib/modules/dynamic-component/fixtures/dynamic-component-test.component.fixture.ts
@@ -1,15 +1,30 @@
-import { Component, Inject, Input, OnInit, Optional } from '@angular/core';
+import {
+  Component,
+  Inject,
+  Input,
+  OnInit,
+  Optional,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'sky-dynamic-component-test',
   template: `<div class="component-test">
-    {{ message }}
-    <ng-container *ngIf="greeting">{{ greeting }}</ng-container>
-  </div>`,
+      {{ message }}
+      <ng-container *ngIf="greeting">{{ greeting }}</ng-container>
+    </div>
+    <div #content></div>`,
 })
 export class DynamicComponentTestComponent implements OnInit {
   @Input()
   public message: string | undefined;
+
+  @ViewChild('content', {
+    read: ViewContainerRef,
+    static: false,
+  })
+  public content: ViewContainerRef | undefined;
 
   constructor(@Inject('greeting') @Optional() public greeting?: string) {}
 
