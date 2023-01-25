edf34f3085b263ee9ad491b19cd809d2248d977c
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-01-25 14:17:01 -0500

    feat(components/modals): use Angular CDK focus trap within modals

--- libs/components/modals/src/lib/modules/modal/modal.module.ts
+++ libs/components/modals/src/lib/modules/modal/modal.module.ts
@@ -1,3 +1,4 @@
+import { A11yModule } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -24,6 +25,7 @@ import { SkyModalComponent } from './modal.component';
     SkyModalScrollShadowDirective,
   ],
   imports: [
+    A11yModule,
     CommonModule,
     RouterModule,
     SkyIconModule,
