d771d2d097199b8d46332c6827b18c74a3112eb6
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-02-21 10:17:14 -0500

    imports

--- apps/code-examples/src/app/code-examples/indicators/icon/icon-button/icon-button-demo.module.ts
+++ apps/code-examples/src/app/code-examples/indicators/icon/icon-button/icon-button-demo.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SkyIconModule } from '@skyux/indicators';
 
-import { IconDemoComponent } from './icon-demo.component';
+import { IconDemoComponent } from './icon-button-demo.component';
 
 @NgModule({
   imports: [CommonModule, SkyIconModule],
