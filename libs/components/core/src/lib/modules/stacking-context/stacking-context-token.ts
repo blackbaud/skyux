37f7c56f9dc35b43fcacd5b6df531815ee0e24d8
John White <750350+johnhwhite@users.noreply.github.com>
2023-02-27 14:34:13 -0500

    add `@internal` jsdocs comments

--- libs/components/core/src/lib/modules/stacking-context/stacking-context-token.ts
+++ libs/components/core/src/lib/modules/stacking-context/stacking-context-token.ts
@@ -2,6 +2,9 @@ import { InjectionToken } from '@angular/core';
 
 import { SkyStackingContext } from './stacking-context';
 
+/**
+ * @internal
+ */
 export const SKY_STACKING_CONTEXT = new InjectionToken<SkyStackingContext>(
   'SkyStackingContext'
 );
--- libs/components/core/src/lib/modules/stacking-context/stacking-context.ts
+++ libs/components/core/src/lib/modules/stacking-context/stacking-context.ts
@@ -1,5 +1,8 @@
 import { Observable } from 'rxjs';
 
+/**
+ * @internal
+ */
 export interface SkyStackingContext {
   // The z-index to use for the stacking context.
   zIndex: Observable<number>;
