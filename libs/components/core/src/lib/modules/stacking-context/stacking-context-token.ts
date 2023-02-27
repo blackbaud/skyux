93b931fcf2626197a24c486267ba63f5fa760d86
John White <750350+johnhwhite@users.noreply.github.com>
2023-02-27 15:54:11 -0500

    feat(components/core): create stacking context service (#1004)
    * feat(components/core): create stacking context service
    
    * prettier
    
    * add to exports, fix service name
    
    * switch to providing the service and supporting observables
    
    * add injection token to fix build issues
    
    * refactor to use injection token, only accept observable value
    
    * export type
    
    * close observables, rename token for clarity
    
    * add `@internal` jsdocs comments
    
    * expose z-index subjects as observables

---
+++ libs/components/core/src/lib/modules/stacking-context/stacking-context-token.ts
@@ -0,0 +1,10 @@
+import { InjectionToken } from '@angular/core';
+
+import { SkyStackingContext } from './stacking-context';
+
+/**
+ * @internal
+ */
+export const SKY_STACKING_CONTEXT = new InjectionToken<SkyStackingContext>(
+  'SkyStackingContext'
+);
