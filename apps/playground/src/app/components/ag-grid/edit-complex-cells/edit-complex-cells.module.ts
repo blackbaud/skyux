0a61ba81a695b4159ab1d3e4324025495943f16d
John White <750350+johnhwhite@users.noreply.github.com>
2023-03-01 15:28:45 -0500

    chore(apps/playground): add playground for editing ag-grid in recursive modal (#1038)

--- apps/playground/src/app/components/ag-grid/edit-complex-cells/edit-complex-cells.module.ts
+++ apps/playground/src/app/components/ag-grid/edit-complex-cells/edit-complex-cells.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 import { SkyAgGridModule } from '@skyux/ag-grid';
 import { SkyHelpInlineModule } from '@skyux/indicators';
 import { SkyToolbarModule } from '@skyux/layout';
+import { SkyModalModule } from '@skyux/modals';
 
 import { AgGridModule } from 'ag-grid-angular';
 
 import { CustomMultilineModule } from './custom-multiline/custom-multiline.module';
+import {
+  EditComplexCellsInModalComponent,
+  EditComplexCellsInModalModalComponent,
+  EditComplexCellsInModalModalGridComponent,
+  EditComplexCellsInModalModalNotGridComponent,
+} from './edit-complex-cells-in-modal.component';
 import { EditComplexCellsRoutingModule } from './edit-complex-cells-routing.module';
 import { EditComplexCellsComponent } from './edit-complex-cells.component';
 import { InlineHelpComponent } from './inline-help/inline-help.component';
 
 @NgModule({
-  declarations: [EditComplexCellsComponent, InlineHelpComponent],
+  declarations: [
+    EditComplexCellsComponent,
+    EditComplexCellsInModalModalComponent,
+    EditComplexCellsInModalModalGridComponent,
+    EditComplexCellsInModalModalNotGridComponent,
+    EditComplexCellsInModalComponent,
+    InlineHelpComponent,
+  ],
   imports: [
     AgGridModule,
     CommonModule,
@@ -21,6 +35,7 @@ import { InlineHelpComponent } from './inline-help/inline-help.component';
     SkyAgGridModule,
     SkyToolbarModule,
     SkyHelpInlineModule,
+    SkyModalModule,
   ],
 })
 export class EditComplexCellsModule {
