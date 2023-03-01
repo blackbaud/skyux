c06711d189c0d8580b07b6d7626795640bc4ee4e
John White <750350+johnhwhite@users.noreply.github.com>
2023-03-01 15:28:45 -0500

    chore(apps/playground): add playground for editing ag-grid in recursive modal (#1038)

--- apps/playground/src/app/components/ag-grid/edit-complex-cells/edit-complex-cells-routing.module.ts
+++ apps/playground/src/app/components/ag-grid/edit-complex-cells/edit-complex-cells-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 
 import { ComponentRouteInfo } from '../../../shared/component-info/component-route-info';
 
+import { EditComplexCellsInModalComponent } from './edit-complex-cells-in-modal.component';
 import { EditComplexCellsComponent } from './edit-complex-cells.component';
 
 const routes: ComponentRouteInfo[] = [
@@ -11,6 +12,15 @@ const routes: ComponentRouteInfo[] = [
     component: EditComplexCellsComponent,
     data: {
       name: 'AG Grid (complex cell editing)',
+      library: 'ag-grid',
+      icon: 'table',
+    },
+  },
+  {
+    path: 'modal',
+    component: EditComplexCellsInModalComponent,
+    data: {
+      name: 'AG Grid (complex cell editing in modal)',
       library: 'ag-grid',
       icon: 'table',
     },
