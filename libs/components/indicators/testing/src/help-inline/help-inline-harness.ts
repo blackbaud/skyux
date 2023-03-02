72d233238a649b3d57ffdef634288476fa7f8c94
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-03-02 16:59:55 -0500

    initial harness and export

---
+++ libs/components/indicators/testing/src/help-inline/help-inline-harness.ts
@@ -0,0 +1,6 @@
+/**
+ * Harness for interacting with a help inline component in tests.
+ */
+import { SkyComponentHarness } from '@skyux/core/testing';
+
+export class SkyHelpInlineHarness extends SkyComponentHarness {}
