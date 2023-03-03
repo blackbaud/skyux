ed9c38a54ec61b78f5d4ed2d32535514f2e1b10a
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.com>
2023-03-03 10:22:12 -0500

    feat(components/indicators): icon test harness (#994)

---
+++ apps/code-examples/src/app/code-examples/indicators/icon/icon-button/icon-button-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-demo',
+  templateUrl: './icon-button-demo.component.html',
+})
+export class IconDemoComponent {
+  public textButtonClick(): void {
+    alert('Text with icon button clicked');
+  }
+
+  public iconOnlyButtonClick(): void {
+    alert('Icon only button clicked');
+  }
+}
