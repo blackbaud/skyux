1cedd7203bcfbd8a2b6f6272d9aa4a6ce51bdeea
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-03-04 09:37:00 -0500

    update

--- libs/components/packages/src/schematics/testing/tree.ts
+++ libs/components/packages/src/schematics/testing/tree.ts
@@ -1,5 +1,6 @@
 import { UnitTestTree } from '@angular-devkit/schematics/testing';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function readJson(tree: UnitTestTree, filePath: string): any {
   return JSON.parse(tree.readContent(filePath));
 }
