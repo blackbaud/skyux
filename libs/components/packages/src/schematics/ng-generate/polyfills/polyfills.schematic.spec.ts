cf82af33d62ca2578f43b8fc08f985de10ffae96
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-03-06 16:39:17 -0500

    pr feedback

--- libs/components/packages/src/schematics/ng-generate/polyfills/polyfills.schematic.spec.ts
+++ libs/components/packages/src/schematics/ng-generate/polyfills/polyfills.schematic.spec.ts
@@ -12,13 +12,34 @@ describe('ng-generate/polyfills.schematic', () => {
   const collectionPath = path.join(__dirname, '../../../../collection.json');
   const runner = new SchematicTestRunner('generate', collectionPath);
 
-  async function setupTest(options: { project?: string }) {
+  async function setupTest(options: {
+    project?: string;
+    builder?: string;
+    targetOptions?: unknown;
+  }) {
+    const projectName = options.project || 'test-app';
+
     const tree = await createTestApp(runner, {
-      projectName: 'test-app',
+      projectName,
     });
 
+    const angularJson = readJson(tree, 'angular.json');
+    const architect = angularJson.projects[projectName].architect;
+
+    if (options.targetOptions !== undefined) {
+      architect.build.options = options.targetOptions;
+      architect.test.options = options.targetOptions;
+    }
+
+    if (options.builder) {
+      architect.build.builder = options.builder;
+      architect.test.builder = options.builder;
+    }
+
+    tree.overwrite('angular.json', JSON.stringify(angularJson, undefined, 2));
+
     const runSchematic = (
-      config: { project?: string } = { project: options.project }
+      config: { project?: string } = { project: projectName }
     ): Promise<UnitTestTree> => {
       return runner.runSchematic('polyfills', config, tree);
     };
@@ -26,23 +47,57 @@ describe('ng-generate/polyfills.schematic', () => {
     return { runSchematic, tree };
   }
 
+  function verifyPolyfill(tree: UnitTestTree): void {
+    const angularJson = readJson(tree, 'angular.json');
+    const architect = angularJson.projects['test-app'].architect;
+    const polyfill = '@skyux/packages/polyfills';
+
+    expect(architect.build.options.polyfills.includes(polyfill)).toEqual(true);
+    expect(architect.test.options.polyfills.includes(polyfill)).toEqual(true);
+  }
+
   it("should add '@skyux/packages/polyfills' to workspace config", async () => {
-    const { runSchematic, tree } = await setupTest({ project: 'test-app' });
+    const { runSchematic } = await setupTest({ project: 'test-app' });
+
+    const updatedTree = await runSchematic();
+
+    verifyPolyfill(updatedTree);
+  });
+
+  it("should not add '@skyux/packages/polyfills' if builder unapproved", async () => {
+    const { runSchematic, tree } = await setupTest({
+      project: 'test-app',
+      builder: 'foobar',
+      targetOptions: {},
+    });
 
     await runSchematic();
 
     const angularJson = readJson(tree, 'angular.json');
     const architect = angularJson.projects['test-app'].architect;
 
-    expect(architect.build.options.polyfills).toEqual([
-      'zone.js',
-      '@skyux/packages/polyfills',
-    ]);
+    expect(architect.build.options.polyfills).toBeUndefined();
+  });
 
-    expect(architect.test.options.polyfills).toEqual([
-      'zone.js',
-      'zone.js/testing',
-      '@skyux/packages/polyfills',
-    ]);
+  it('should handle empty options', async () => {
+    const { runSchematic } = await setupTest({
+      project: 'test-app',
+      targetOptions: {},
+    });
+
+    const updatedTree = await runSchematic();
+
+    verifyPolyfill(updatedTree);
+  });
+
+  it('should handle undefined options', async () => {
+    const { runSchematic } = await setupTest({
+      project: 'test-app',
+      targetOptions: false,
+    });
+
+    const updatedTree = await runSchematic();
+
+    verifyPolyfill(updatedTree);
   });
 });
