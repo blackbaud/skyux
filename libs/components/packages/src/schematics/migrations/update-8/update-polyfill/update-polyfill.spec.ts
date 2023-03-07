c24d41fca28ab6322b33c9e9c3b41c56e72bfefa
Steve Brush <steve.brush@blackbaud.com>
2023-03-07 06:34:47 -0500

    feat(components/packages)!: make `--project` a required parameter for `ng add` schematics (#1073)
    BREAKING CHANGE: A project name must be provided when a workspace has more than one project.

--- libs/components/packages/src/schematics/migrations/update-8/update-polyfill/update-polyfill.spec.ts
+++ libs/components/packages/src/schematics/migrations/update-8/update-polyfill/update-polyfill.spec.ts
@@ -1,58 +1,52 @@
 import { stripIndents } from '@angular-devkit/core/src/utils/literals';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 
-import { take } from 'rxjs/operators';
-
 import { createTestApp } from '../../../testing/scaffold';
-import { getWorkspace } from '../../../utility/workspace';
+import { readJson } from '../../../testing/tree';
 
-import { updatePolyfillSchematic } from './update-polyfill.schematic';
-
-describe('updatePolyfill', () => {
+describe('update-polyfill.schematic', () => {
   const runner = new SchematicTestRunner(
     '@skyux/packages',
-    require.resolve('../../../../collection.json')
+    require.resolve('../../migration-collection.json')
   );
 
-  it('should modify polyfills.ts', async () => {
+  async function setupTest(options: {
+    buildPolyfillsContents: string;
+    testPolyfillsContents: string;
+  }) {
     const tree = await createTestApp(runner, {
       projectName: 'test-app',
     });
-    const { workspace } = await getWorkspace(tree);
-    const projectDefinition = workspace.projects.get('test-app');
-    const projectRoot = projectDefinition?.root;
-    expect(projectRoot).toBeDefined();
-    tree.create(
-      `${projectRoot}/src/polyfills.ts`,
-      stripIndents`
-      import 'zone.js';
 
-      // This is a comment.
-      (window as any).global = window;
-    `
-    );
-    await runner
-      .callRule(updatePolyfillSchematic(), tree)
-      .pipe(take(1))
-      .toPromise();
-    const polyfills = tree.readText(`${projectRoot}/src/polyfills.ts`);
-    expect(polyfills).not.toContain('(window as any).global = window;');
-    expect(polyfills).not.toContain('This is a comment.');
-    expect(polyfills).toContain("import 'zone.js';");
-  });
+    // Simulate the file structure of an Angular 14 project.
+    tree.create(`src/polyfills.ts`, options.buildPolyfillsContents);
+    tree.create(`src/test.ts`, options.testPolyfillsContents);
 
-  it('should capture with start/end comments', async () => {
-    const tree = await createTestApp(runner, {
-      projectName: 'test-app',
-    });
-    const { workspace } = await getWorkspace(tree);
-    const projectDefinition = workspace.projects.get('test-app');
-    const projectRoot = projectDefinition?.root;
-    expect(projectRoot).toBeDefined();
-    tree.create(
-      `${projectRoot}/src/polyfills.ts`,
-      stripIndents`
-      import 'zone.js';
+    // Simulate the workspace configuration of an Angular 14 project.
+    const angularJson = readJson(tree, 'angular.json');
+    const architect = angularJson.projects['test-app'].architect;
+    architect.build.options.polyfills = 'src/polyfills.ts';
+    architect.test.options.polyfills = 'src/test.ts';
+    tree.overwrite('angular.json', JSON.stringify(angularJson, undefined, 2));
+
+    return {
+      runSchematic: () => runner.runSchematic('update-polyfill', {}, tree),
+      tree,
+    };
+  }
+
+  it('should modify polyfills.ts and test.ts', async () => {
+    const { runSchematic, tree } = await setupTest({
+      buildPolyfillsContents: stripIndents`
+      /***************************************************************************************************
+       * Zone JS is required by default for Angular itself.
+       */
+      import 'zone.js';  // Included with Angular CLI.
+
+
+      /***************************************************************************************************
+       * APPLICATION IMPORTS
+       */
 
       /***************************************************************************************************
        * SKY UX POLYFILLS - DO NOT MODIFY THIS SECTION
@@ -66,13 +60,92 @@ describe('updatePolyfill', () => {
       /*
        * END SKY UX POLYFILLS
        **************************************************************************************************/
-     `
+      `,
+      testPolyfillsContents: stripIndents`// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+    import 'zone.js/testing';
+    import { getTestBed } from '@angular/core/testing';
+    import {
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting
+    } from '@angular/platform-browser-dynamic/testing';
+
+    // Fix for crossvent "global is not defined" error. The crossvent library
+    // is used by Dragula, which in turn is used by multiple SKY UX components.
+    // See: https://github.com/bevacqua/dragula/issues/602
+    (window as any).global = window;
+
+    declare const require: {
+      context(path: string, deep?: boolean, filter?: RegExp): {
+        keys(): string[];
+        <T>(id: string): T;
+      };
+    };
+
+    // First, initialize the Angular testing environment.
+    getTestBed().initTestEnvironment(
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting(),
+      { teardown: { destroyAfterEach: true }},
     );
-    await runner
-      .callRule(updatePolyfillSchematic(), tree)
-      .pipe(take(1))
-      .toPromise();
-    const polyfills = tree.readText(`${projectRoot}/src/polyfills.ts`);
-    expect(polyfills).toMatchSnapshot();
+
+    // Then we find all the tests.
+    const context = require.context('./', true, /\.spec\.ts$/);
+    // And load the modules.
+    context.keys().map(context);
+    `,
+    });
+
+    await runSchematic();
+
+    expect(tree.readText(`src/polyfills.ts`)).toMatchSnapshot('polyfills.ts');
+    expect(tree.readText(`src/test.ts`)).toMatchSnapshot('test.ts');
+
+    // Check the workspace config.
+    const angularJson = readJson(tree, 'angular.json');
+    const architect = angularJson.projects['test-app'].architect;
+
+    expect(architect.build.options.polyfills).toEqual([
+      'src/polyfills.ts',
+      '@skyux/packages/polyfills',
+    ]);
+
+    expect(architect.test.options.polyfills).toEqual([
+      'src/test.ts',
+      '@skyux/packages/polyfills',
+    ]);
+  });
+
+  it('should only add polyfills configuration if our polyfill is found in polyfills.ts/test.ts', async () => {
+    const { runSchematic, tree } = await setupTest({
+      buildPolyfillsContents: `import 'zone.js';`,
+      testPolyfillsContents: stripIndents`
+    import 'zone.js/testing';
+    import { getTestBed } from '@angular/core/testing';
+    import {
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting
+    } from '@angular/platform-browser-dynamic/testing';
+
+    // First, initialize the Angular testing environment.
+    getTestBed().initTestEnvironment(
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting(),
+      { teardown: { destroyAfterEach: true }},
+    );
+    `,
+    });
+
+    await runSchematic();
+
+    expect(tree.readText(`src/polyfills.ts`)).toMatchSnapshot('polyfills.ts');
+    expect(tree.readText(`src/test.ts`)).toMatchSnapshot('test.ts');
+
+    // Check the workspace config.
+    const angularJson = readJson(tree, 'angular.json');
+    const architect = angularJson.projects['test-app'].architect;
+
+    expect(architect.build.options.polyfills).toEqual('src/polyfills.ts');
+    expect(architect.test.options.polyfills).toEqual('src/test.ts');
   });
 });
