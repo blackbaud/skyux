77467b135fa5d546b609c3af4cef8e3c10c284a9
John White <750350+johnhwhite@users.noreply.github.com>
2023-03-08 09:43:58 -0500

    add comment

--- libs/sdk/e2e-schematics/src/test-setup.ts
+++ libs/sdk/e2e-schematics/src/test-setup.ts
@@ -1,1 +1,3 @@
+// Adding this environment variable works the same as passing `--dry-run` to the generator.
+// It prevents the generator from running e.g. the `npm install` command.
 process.env['NX_DRY_RUN'] = 'true';
