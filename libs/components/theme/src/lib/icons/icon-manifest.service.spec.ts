36158a198f9e7b66ad0792776ac7bebe71736877
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-03-09 15:30:30 -0500

    fix tests

--- libs/components/theme/src/lib/icons/icon-manifest.service.spec.ts
+++ libs/components/theme/src/lib/icons/icon-manifest.service.spec.ts
@@ -1,9 +1,9 @@
-import * as _manifest from '@skyux/icons/assets/manifest.json';
+import * as iconManifest from '@skyux/icons/assets/manifest.json';
 
 import { SkyThemeIconManifest } from './icon-manifest';
 import { SkyThemeIconManifestService } from './icon-manifest.service';
 
-const manifest: SkyThemeIconManifest = _manifest;
+const manifest: SkyThemeIconManifest = iconManifest;
 
 describe('SKY theme icon manifest service', () => {
   let manifestSvc: SkyThemeIconManifestService;
@@ -13,6 +13,11 @@ describe('SKY theme icon manifest service', () => {
   });
 
   it('should return the manifest file from the @skyux/icons package', () => {
+    expect(manifestSvc.getManifest()).toEqual(
+      jasmine.objectContaining(manifest)
+    );
+    console.log('1:', JSON.stringify(manifestSvc.getManifest()));
+    console.log('\n\n', '2:', JSON.stringify(manifest));
     expect(manifestSvc.getManifest()).toEqual(manifest);
   });
 });
