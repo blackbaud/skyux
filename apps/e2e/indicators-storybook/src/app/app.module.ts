7eca191b499405acd20af7f2adf538a8e51e2996
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-03-17 12:54:58 -0400

    remove extra help inline in module

--- apps/e2e/indicators-storybook/src/app/app.module.ts
+++ apps/e2e/indicators-storybook/src/app/app.module.ts
@@ -36,13 +36,6 @@ const routes: Route[] = [
     path: 'wait',
     loadChildren: () => import('./wait/wait.module').then((m) => m.WaitModule),
   },
-  {
-    path: 'help-inline',
-    loadChildren: () =>
-      import('./help-inline/help-inline.module').then(
-        (m) => m.HelpInlineModule
-      ),
-  },
 ];
 if (routes.length > 0 && routes.findIndex((r) => r.path === '') === -1) {
   routes.push({ path: '', redirectTo: `${routes[0].path}`, pathMatch: 'full' });
