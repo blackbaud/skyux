ed3f688fe5e6c90cd99495b0df13f3b367aa47b3
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-03-21 16:52:49 -0400

    Undo extra files

--- apps/e2e/indicators-storybook/src/app/app.module.ts
+++ apps/e2e/indicators-storybook/src/app/app.module.ts
@@ -36,10 +36,6 @@ const routes: Route[] = [
     path: 'wait',
     loadChildren: () => import('./wait/wait.module').then((m) => m.WaitModule),
   },
-  {
-    path: 'icon',
-    loadChildren: () => import('./icon/icon.module').then((m) => m.IconModule),
-  },
 ];
 if (routes.length > 0 && routes.findIndex((r) => r.path === '') === -1) {
   routes.push({ path: '', redirectTo: `${routes[0].path}`, pathMatch: 'full' });
