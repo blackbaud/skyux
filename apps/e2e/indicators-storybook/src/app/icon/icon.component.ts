e4a70c3422c5bcb4f9ad43999b758acfa5a4806d
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2023-03-21 11:29:26 -0400

    feat(components/indicators): icon visual test (#1168) (#1170)
    Co-authored-by: Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.com>

---
+++ apps/e2e/indicators-storybook/src/app/icon/icon.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+})
+export class IconComponent {}
