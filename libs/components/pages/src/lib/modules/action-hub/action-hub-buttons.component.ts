ba0fcff960349dd2ead517eb62b783dadde70de9
John White <750350+johnhwhite@users.noreply.github.com>
2023-03-21 14:33:45 -0400

    additional styles, loading state

--- libs/components/pages/src/lib/modules/action-hub/action-hub-buttons.component.ts
+++ libs/components/pages/src/lib/modules/action-hub/action-hub-buttons.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 /**
  * Displays buttons after the page title.
@@ -11,8 +11,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
  */
 @Component({
   selector: 'sky-action-hub-buttons',
-  templateUrl: './action-hub-buttons.component.html',
+  templateUrl: 'action-hub-buttons.component.html',
   styleUrls: ['./action-hub-buttons.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SkyActionHubButtonsComponent {}
--- libs/components/pages/src/lib/modules/needs-attention/needs-attention.module.ts
+++ libs/components/pages/src/lib/modules/needs-attention/needs-attention.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { SkyTrimModule } from '@skyux/core';
 import { SkyI18nModule } from '@skyux/i18n';
 import { SkyIconModule, SkyWaitModule } from '@skyux/indicators';
-import { SkyBoxModule, SkyFluidGridModule } from '@skyux/layout';
+import { SkyBoxModule } from '@skyux/layout';
 import { SkyAppLinkModule, SkyHrefModule } from '@skyux/router';
 import { SkyThemeModule } from '@skyux/theme';
 
@@ -19,7 +19,6 @@ import { SkyNeedsAttentionComponent } from './needs-attention.component';
     CommonModule,
     SkyAppLinkModule,
     SkyBoxModule,
-    SkyFluidGridModule,
     SkyHrefModule,
     SkyI18nModule,
     SkyIconModule,
