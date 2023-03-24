f6c23b48e7a39c26bdd9d33d46f8763d452e976c
Corey Archer <corey.archer@blackbaud.me>
2023-03-24 14:58:58 -0400

    add content to snapshot

--- apps/e2e/lists-storybook/src/app/repeater/repeater.module.ts
+++ apps/e2e/lists-storybook/src/app/repeater/repeater.module.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkyRepeaterModule } from '@skyux/lists';
+import { SkyDropdownModule } from '@skyux/popovers';
 
 import { RepeaterComponent } from './repeater.component';
 
 const routes: Routes = [{ path: '', component: RepeaterComponent }];
 @NgModule({
   declarations: [RepeaterComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SkyRepeaterModule,
+    SkyDropdownModule,
+  ],
   exports: [RepeaterComponent],
 })
 export class RepeaterModule {}
