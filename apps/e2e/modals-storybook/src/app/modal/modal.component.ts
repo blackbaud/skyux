9f981c35484812ceaf0bab922f81c3eb4c4cb6fc
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-03-24 14:34:10 -0400

    hide buttons

--- apps/e2e/modals-storybook/src/app/modal/modal.component.ts
+++ apps/e2e/modals-storybook/src/app/modal/modal.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { SkyModalService } from '@skyux/modals';
+import {
+  SkyModalConfigurationInterface,
+  SkyModalInstance,
+  SkyModalService,
+} from '@skyux/modals';
 
 import { ModalBasicComponent } from './modals/modal-basic.component';
 
@@ -9,44 +13,68 @@ import { ModalBasicComponent } from './modals/modal-basic.component';
   styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent {
+  public buttonsHidden = false;
+
   constructor(private modal: SkyModalService) {}
 
+  public hideButtons(): void {
+    this.buttonsHidden = true;
+  }
+  public showButtons(): void {
+    this.buttonsHidden = false;
+  }
+
+  private openModal(
+    modalInstance: any,
+    options?: SkyModalConfigurationInterface
+  ): SkyModalInstance {
+    this.hideButtons();
+
+    const instance = this.modal.open(modalInstance, options);
+
+    instance.closed.subscribe(() => {
+      this.showButtons();
+    });
+
+    return instance;
+  }
+
   public onOpenModalClick(): void {
-    this.modal.open(ModalBasicComponent, {
+    this.openModal(ModalBasicComponent, {
       providers: [],
     });
   }
 
   public onOpenSmallModalClick(): void {
-    this.modal.open(ModalBasicComponent, {
+    this.openModal(ModalBasicComponent, {
       providers: [],
       size: 'small',
     });
   }
 
   public onOpenMediumModalClick(): void {
-    this.modal.open(ModalBasicComponent, {
+    this.openModal(ModalBasicComponent, {
       providers: [],
       size: 'medium',
     });
   }
 
   public onOpenLargeModalClick(): void {
-    this.modal.open(ModalBasicComponent, {
+    this.openModal(ModalBasicComponent, {
       providers: [],
       size: 'large',
     });
   }
 
   public onOpenFullPageModalClick(): void {
-    this.modal.open(ModalBasicComponent, {
+    this.openModal(ModalBasicComponent, {
       providers: [],
       fullPage: true,
     });
   }
 
   public onOpenHelpInlineModalClick(): void {
-    const modalInstance = this.modal.open(ModalBasicComponent, {
+    const modalInstance = this.openModal(ModalBasicComponent, {
       providers: [],
     });
     modalInstance.componentInstance.showHelp = true;
