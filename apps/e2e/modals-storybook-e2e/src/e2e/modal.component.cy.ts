6fe8714e039faf2e6d60118f06dfe2503ae94366
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-03-28 11:23:39 -0400

    animations off

--- apps/e2e/modals-storybook-e2e/src/e2e/modal.component.cy.ts
+++ apps/e2e/modals-storybook-e2e/src/e2e/modal.component.cy.ts
@@ -8,60 +8,65 @@ describe('modals-storybook', () => {
           `/iframe.html?globals=theme:${theme}&id=modalcomponent-modal--modal`
         )
       );
-      it('should render the component', () => {
-        cy.get('app-modal').should('exist').should('be.visible');
-        for (const x of [
-          'small',
-          'medium',
-          'large',
-          'full-page',
-          'help-inline',
-        ]) {
+      for (const x of [
+        'small',
+        'medium',
+        'large',
+        'full-page',
+        'help-inline',
+      ]) {
+        it(`should render the ${x} modal on desktop`, () => {
+          cy.get('app-modal').should('exist').should('be.visible');
           cy.get(`.open-${x}-modal-btn`)
             .should('exist')
             .should('be.visible')
             .click()
+            .end()
             .get('.sky-modal')
             .should('exist')
             .should('be.visible')
-            .screenshot(`modalcomponent-modal--${x}-modal-${theme}`)
+            .end()
+            .window()
+            .screenshot(`modalcomponent-modal--${x}-modal-${theme}`, {
+              disableTimersAndAnimations: true,
+              scale: false,
+            })
             .percySnapshot(`modalcomponent-modal--${x}-modal-${theme}`, {
               widths: E2eVariations.DISPLAY_WIDTHS,
             })
             .get('.sky-btn-close')
             .should('exist')
             .should('be.visible')
-            .click();
-        }
-      });
-
-      it('should render the component in mobile', () => {
-        cy.viewport('iphone-x', 'portrait');
-        cy.get('app-modal').should('exist').should('be.visible');
-        for (const x of [
-          'small',
-          'medium',
-          'large',
-          'full-page',
-          'help-inline',
-        ]) {
+            .click()
+            .end();
+        });
+        it(`should render the ${x} modal on mobile`, () => {
+          cy.viewport('iphone-x', 'portrait');
+          cy.get('app-modal').should('exist').should('be.visible');
           cy.get(`.open-${x}-modal-btn`)
             .should('exist')
             .should('be.visible')
             .click()
+            .end()
             .get('.sky-modal')
             .should('exist')
             .should('be.visible')
-            .screenshot(`modalcomponent-modal--${x}-modal-${theme}-mobile`)
+            .end()
+            .window()
+            .screenshot(`modalcomponent-modal--${x}-modal-${theme}-mobile`, {
+              disableTimersAndAnimations: true,
+              scale: false,
+            })
             .percySnapshot(`modalcomponent-modal--${x}-modal-${theme}-mobile`, {
               widths: E2eVariations.MOBILE_WIDTHS,
             })
             .get('.sky-btn-close')
             .should('exist')
             .should('be.visible')
-            .click();
-        }
-      });
+            .click()
+            .end();
+        });
+      }
     });
   });
 });
