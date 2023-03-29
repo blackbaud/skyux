e3892e14fefd9af051e2ae32f22690ed5eb67da6
John White <750350+johnhwhite@users.noreply.github.com>
2023-03-29 14:23:01 -0400

    Log deprecation warning

--- libs/components/pages/src/lib/modules/needs-attention/needs-attention.component.ts
+++ libs/components/pages/src/lib/modules/needs-attention/needs-attention.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  inject,
+} from '@angular/core';
+import { SkyLogService } from '@skyux/core';
 import { SkyFluidGridGutterSizeType } from '@skyux/layout';
 
 import { SkyActionHubNeedsAttention } from '../action-hub/types/action-hub-needs-attention';
@@ -8,9 +15,29 @@ import { SkyActionHubNeedsAttention } from '../action-hub/types/action-hub-needs
   templateUrl: './needs-attention.component.html',
   styleUrls: ['./needs-attention.component.scss'],
 })
-export class SkyNeedsAttentionComponent {
+export class SkyNeedsAttentionComponent implements OnChanges {
   @Input()
   public items: SkyActionHubNeedsAttention[] | undefined;
 
   public readonly gutterSize: SkyFluidGridGutterSizeType = 'large';
+
+  #logService = inject(SkyLogService);
+  #haveLoggedDeprecationWarning = false;
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (
+      !this.#haveLoggedDeprecationWarning &&
+      changes.items?.currentValue?.some(
+        (c: SkyActionHubNeedsAttention) => c.message
+      )
+    ) {
+      this.#logService.deprecated(`SkyActionHubNeedsAttention.message`, {
+        deprecationMajorVersion: 7,
+        replacementRecommendation: 'Use `title` instead.',
+        moreInfoUrl:
+          'https://developer.blackbaud.com/skyux/components/action-hub?docs-active-tab=development#interface-skyactionhubneedsattention',
+      });
+      this.#haveLoggedDeprecationWarning = true;
+    }
+  }
 }
