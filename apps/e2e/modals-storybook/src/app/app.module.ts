c54fd30f76b2263cc48118214ba0650253ba7653
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.com>
2023-04-04 16:24:26 -0400

    feat(components/modals): modal visual test (#1181)

--- apps/e2e/modals-storybook/src/app/app.module.ts
+++ apps/e2e/modals-storybook/src/app/app.module.ts
@@ -10,6 +10,11 @@ const routes: Route[] = [
     loadChildren: () =>
       import('./confirm/confirm.module').then((m) => m.ConfirmModule),
   },
+  {
+    path: 'modal',
+    loadChildren: () =>
+      import('./modal/modal.module').then((m) => m.ModalModule),
+  },
 ];
 if (routes.length > 0 && routes.findIndex((r) => r.path === '') === -1) {
   routes.push({ path: '', redirectTo: `${routes[0].path}`, pathMatch: 'full' });
