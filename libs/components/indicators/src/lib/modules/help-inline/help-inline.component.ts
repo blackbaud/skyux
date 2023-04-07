440c5bc63b01d83d89b42f08e34d707a93a4a6e1
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2023-04-07 16:26:38 -0400

    feat(components/indicators): add ability to set aria controls and expanded states on the help inline component (#1225) (#1230)

--- libs/components/indicators/src/lib/modules/help-inline/help-inline.component.ts
+++ libs/components/indicators/src/lib/modules/help-inline/help-inline.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'sky-help-inline',
@@ -6,6 +6,22 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./help-inline.component.scss'],
 })
 export class SkyHelpInlineComponent {
+  /**
+   * The ID of the element that the help inline button controls.
+   * This property [supports accessibility rules for disclosures](https://www.w3.org/TR/wai-aria-practices-1.1/#disclosure).
+   * For more information about the `aria-controls` attribute, see the [WAI-ARIA definition](https://www.w3.org/TR/wai-aria/#aria-controls).
+   */
+  @Input()
+  public ariaControls: string | undefined;
+
+  /**
+   * Whether an element controlled by the help inline button is expanded. When set to `true`, the property will only be applied to the HTML element when the `ariaControls` input is also set.
+   * This property [supports accessibility rules for disclosures](https://www.w3.org/TR/wai-aria-practices-1.1/#disclosure).
+   * For more information about the `aria-expanded` attribute, see the [WAI-ARIA definition](https://www.w3.org/TR/wai-aria/#aria-expanded).
+   */
+  @Input()
+  public ariaExpanded: boolean | undefined;
+
   /**
    * Fires when the user clicks the help inline button.
    */
