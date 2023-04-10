d81257812c0e3b5d929ae574e7980fed81d0a786
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-04-10 10:15:17 -0400

    chore(apps/playground): add tokens playground page (#1237)

---
+++ apps/playground/src/app/components/indicators/tokens/tokens-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ComponentRouteInfo } from '../../../shared/component-info/component-route-info';
+
+import { TokensComponent } from './tokens.component';
+
+const routes: ComponentRouteInfo[] = [
+  {
+    path: '',
+    component: TokensComponent,
+    data: {
+      name: 'Tokens',
+      icon: 'th-large',
+      library: 'indicators',
+    },
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TokensRoutingModule {
+  public static routes = routes;
+}
