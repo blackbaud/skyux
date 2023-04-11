a37bde92eaaa926f5123e5d251601374efe7a269
Paul Crowder <paul.crowder@blackbaud.com>
2023-04-11 10:16:13 -0400

    Update icon-manifest.ts

--- libs/components/theme/src/lib/icons/icon-manifest.ts
+++ libs/components/theme/src/lib/icons/icon-manifest.ts
@@ -1,5 +1,7 @@
+import { SkyIconManifest } from '@skyux/icons';
+
 import { SkyThemeIconManifestGlyph } from './icon-manifest-glyph';
 
-export interface SkyThemeIconManifest {
+export interface SkyThemeIconManifest extends SkyIconManifest {
   glyphs: SkyThemeIconManifestGlyph[];
 }
