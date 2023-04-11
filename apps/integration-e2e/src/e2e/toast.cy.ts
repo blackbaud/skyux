83745d4df17f00e064d1341421cad1a4ccd3de21
John White <750350+johnhwhite@users.noreply.github.com>
2023-04-11 15:47:15 -0400

    fix(components/toast): close button hover text is incorrect (#1247)
    * fix(components/toast): close button hover text is incorrect
    
    * update test

---
+++ apps/integration-e2e/src/e2e/toast.cy.ts
@@ -0,0 +1,15 @@
+describe('ToastComponent', () => {
+  it('should have a valid title on the close button', () => {
+    cy.visit('/#/integrations/toast');
+    cy.get('app-toast button.sky-btn.sky-btn-primary')
+      .should('be.visible')
+      .should('contain.text', 'Open Toasts')
+      .click();
+    cy.get('sky-toaster').should('exist');
+    cy.get('sky-toaster sky-toast').should('exist');
+    cy.get('sky-toaster sky-toast:first-child button.sky-toast-btn-close')
+      .should('be.visible')
+      .should('have.attr', 'title')
+      .should('not.equal', 'skyux_toast_close_button');
+  });
+});
