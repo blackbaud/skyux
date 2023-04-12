1a3d88d1d2030926c768091cb7e96817b50a33dc
Paul Crowder <paul.crowder@blackbaud.com>
2023-04-12 12:50:38 -0400

    use `ResizeObserver` polyfill in Jest tests

--- apps/e2e/lists-storybook/src/test-setup.ts
+++ apps/e2e/lists-storybook/src/test-setup.ts
@@ -1,1 +1,4 @@
 import 'jest-preset-angular/setup-jest';
+
+// jsdom (used by Jest) doesn't natively support ResizeObserver, so use a polyfill.
+global.ResizeObserver = require('resize-observer-polyfill');
