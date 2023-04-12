29e6079111e62adaff17d9a814539676c9afa86d
Paul Crowder <paul.crowder@blackbaud.com>
2023-04-12 15:18:58 -0400

    feat(components/lists): add inline form repeater code example and improved other examples

---
+++ apps/code-examples/src/app/code-examples/lists/repeater/inline-form/repeater-demo.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SkyIdModule } from '@skyux/core';
+import { SkyInputBoxModule } from '@skyux/forms';
+import { SkyIconModule } from '@skyux/indicators';
+import { SkyInlineFormModule } from '@skyux/inline-form';
+import { SkyRepeaterModule } from '@skyux/lists';
+
+import { RepeaterDemoComponent } from './repeater-demo.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SkyIconModule,
+    SkyIdModule,
+    SkyInlineFormModule,
+    SkyInputBoxModule,
+    SkyRepeaterModule,
+  ],
+  exports: [RepeaterDemoComponent],
+  declarations: [RepeaterDemoComponent],
+})
+export class RepeaterDemoModule {}
