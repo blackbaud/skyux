c9bb5e686210e2cbad8bb9ff3937c25f90f843b8
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.com>
2023-04-13 14:31:11 -0400

    feat(components/modals): modal test harness (#1265)

--- libs/components/modals/testing/src/public-api.ts
+++ libs/components/modals/testing/src/public-api.ts
@@ -1,4 +1,8 @@
 export * from './modal-fixture';
+
 export * from './confirm/confirm-harness';
 export * from './confirm/confirm-button-harness';
 export * from './confirm/confirm-button-harness-filters';
+
+export * from './modal/modal-harness';
+export * from './modal/modal-harness-filters';
