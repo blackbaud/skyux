16a57071984317ae8b40828238a6c93b29cbc68f
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2023-04-13 14:46:36 -0400

    feat(components/lists): add inline form repeater code example and improved other examples (#1261) (#1268)

--- apps/code-examples/src/app/code-examples/lists/repeater/add-remove/repeater-demo.component.ts
+++ apps/code-examples/src/app/code-examples/lists/repeater/add-remove/repeater-demo.component.ts
@@ -13,14 +13,26 @@ export class RepeaterDemoComponent {
   public items: RepeaterDemoItem[] = [
     {
       title: 'Call Robert Hernandez',
-      note: 'Robert recently gave a very generous gift.  We should call him to thank him.',
+      note: 'Robert recently gave a very generous gift. We should call him to thank him.',
       status: 'Completed',
       isSelected: false,
     },
     {
       title: 'Send invitation to Spring Ball',
-      note: "The Spring Ball is coming up soon.  Let's get those invitations out!",
+      note: "The Spring Ball is coming up soon. Let's get those invitations out!",
       status: 'Past due',
+      isSelected: false,
+    },
+    {
+      title: 'Assign prospects',
+      note: 'There are 14 new prospects who are not assigned to fundraisers.',
+      status: 'Due tomorrow',
+      isSelected: false,
+    },
+    {
+      title: 'Process gift receipts',
+      note: 'There are 28 recent gifts that are not receipted.',
+      status: 'Due next week',
       isSelected: false,
     },
   ];
