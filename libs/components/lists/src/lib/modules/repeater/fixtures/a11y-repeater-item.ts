77052d8a227a4520125ce0fba6133d7f17d6388f
Corey Archer <Corey.Archer@blackbaud.me>
2023-04-17 17:25:16 -0400

    test(components/lists): add missing accessibility test to repeater (#1252)

---
+++ libs/components/lists/src/lib/modules/repeater/fixtures/a11y-repeater-item.ts
@@ -0,0 +1,11 @@
+export interface A11yRepeaterItem {
+  selectable?: boolean;
+  selected?: boolean;
+  disabled?: boolean;
+  expanded?: boolean;
+  reorderable?: boolean;
+  title?: string;
+  message?: string;
+  context?: boolean;
+  tag?: string;
+}
