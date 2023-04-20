988778f0ef9143d75642410997559274455e0e0a
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-04-20 10:57:55 -0400

    missing code spec file

---
+++ apps/code-examples/src/app/code-examples/layout/box/basic/box-demo.component.spec.ts
@@ -0,0 +1,37 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SkyBoxHarness } from '@skyux/layout/testing';
+
+import { BoxDemoComponent } from './box-demo.component';
+import { BoxDemoModule } from './box-demo.module';
+
+fdescribe('Basic box', async () => {
+  async function setupTest(): Promise<{
+    boxHarness: SkyBoxHarness;
+    fixture: ComponentFixture<BoxDemoComponent>;
+  }> {
+    const fixture = TestBed.createComponent(BoxDemoComponent);
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+    const boxHarness = await loader.getHarness(
+      SkyBoxHarness.with({
+        dataSkyId: 'box-demo',
+      })
+    );
+
+    return { boxHarness, fixture };
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [BoxDemoModule],
+    });
+  });
+
+  it('should display the correct box', async () => {
+    const { boxHarness, fixture } = await setupTest();
+
+    fixture.detectChanges();
+
+    await expectAsync(boxHarness.getAriaLabel()).toBeResolvedTo('boxDemo');
+  });
+});
