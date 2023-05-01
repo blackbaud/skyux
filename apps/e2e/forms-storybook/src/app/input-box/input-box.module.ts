ec2e845466a72ef2535d5ffe62c25bbc8b02a1d0
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-05-01 16:48:53 -0400

    Fixes

--- apps/e2e/forms-storybook/src/app/input-box/input-box.module.ts
+++ apps/e2e/forms-storybook/src/app/input-box/input-box.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { SkyIdModule } from '@skyux/core';
 import { SkyCharacterCounterModule, SkyInputBoxModule } from '@skyux/forms';
@@ -14,7 +14,6 @@ const routes: Routes = [{ path: '', component: InputBoxComponent }];
   declarations: [InputBoxComponent],
   imports: [
     CommonModule,
-    FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild(routes),
     SkyCharacterCounterModule,
