61da566e0d09ae620dcfb66b8c1c132b4c350511
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-05-05 11:33:44 -0400

    feat(components/autonumeric): update `autonumeric` peer dependency to `4.8.1` (#1348)
    BREAKING CHANGE: The `@skyux/autonumeric` peer dependency has been updated to `4.8.1`. Version `4.8.0` of `autonumeric` introduced the `negativePositiveSignBehavior` option and this option defaults to `false`. However, the behavior this option enables was previously on by default. To maintain this behavior,  enable this option on the `skyAutonumeric` instance. For more information, see the [`autonumeric` library's CHANGELOG](https://github.com/autoNumeric/autoNumeric/blob/next/CHANGELOG.md]).

---
+++ libs/components/packages/src/schematics/migrations/update-8/update-autonumeric/update-autonumeric.ts
@@ -0,0 +1,48 @@
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+} from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+  NodeDependencyType,
+  addPackageJsonDependency,
+  getPackageJsonDependency,
+} from '@schematics/angular/utility/dependencies';
+
+export const UPDATE_TO_VERSION = '4.8.1';
+
+/**
+ * Update the version of the `autonumeric` dependency.
+ */
+function updateVersion(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const autonumericDependency = getPackageJsonDependency(tree, 'autonumeric');
+
+    // Autonumeric is not installed, so we don't need to do anything.
+    if (!autonumericDependency) {
+      return;
+    }
+
+    const type = NodeDependencyType.Default;
+    const overwrite = true;
+    const version = UPDATE_TO_VERSION;
+
+    addPackageJsonDependency(tree, {
+      name: `autonumeric`,
+      overwrite,
+      type,
+      version,
+    });
+
+    context.addTask(new NodePackageInstallTask());
+  };
+}
+
+/**
+ * Upgrade to `autonumeric` version `4.8.1`.
+ */
+export default function (): Rule {
+  return chain([updateVersion()]);
+}
