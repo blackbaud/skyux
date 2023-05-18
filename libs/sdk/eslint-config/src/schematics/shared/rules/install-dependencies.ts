72f6986cecfbe2479fe61082a208c8edb423baab
Steve Brush <steve.brush@blackbaud.com>
2023-05-18 15:59:05 -0400

    fix lint

--- libs/sdk/eslint-config/src/schematics/shared/rules/install-dependencies.ts
+++ libs/sdk/eslint-config/src/schematics/shared/rules/install-dependencies.ts
@@ -13,7 +13,7 @@ export function installDependencies(): Rule {
       '@angular-eslint/eslint-plugin': '^15.2.1',
       '@angular-eslint/eslint-plugin-template': '^15.2.1',
       '@angular-eslint/template-parser': '^15.2.1',
-      '@typescript-eslint/eslint-plugin': '^5.48.2',
+      '@typescript-eslint/eslint-plugin': '^5.48.5',
       '@typescript-eslint/parser': '^5.48.2',
       'eslint-plugin-deprecation': '^1.4.1',
       eslint: '^8.36.0',
