f7ad59f5ea7ea928ecc05db7aceb859f35b0655c
Steve Brush <steve.brush@blackbaud.com>
2023-05-18 15:07:12 -0400

    cleanup

--- libs/sdk/eslint-config/src/schematics/shared/rules/modify-eslint-config.ts
+++ libs/sdk/eslint-config/src/schematics/shared/rules/modify-eslint-config.ts
@@ -1,9 +1,34 @@
-import { Rule } from "@angular-devkit/schematics";
-import { readRequiredFile } from "../utility/tree";
+import { Rule } from '@angular-devkit/schematics';
+
+import { EsLintConfig } from '../types/eslint-config';
+import { readRequiredFile } from '../utility/tree';
+
+const ESLINT_CONFIG_NAME = '@skyux-sdk/eslint-config/recommended';
 
 export function modifyEsLintConfig(): Rule {
   return async (tree) => {
-    const esLintConfig = readRequiredFile(tree, '.eslintrc.json');
-    const esLintConfig = JSON.parse(esLintConfig);
+    const esLintConfig = JSON.parse(
+      readRequiredFile(tree, '.eslintrc.json')
+    ) as EsLintConfig;
+
+    esLintConfig.parser = '@typescript-eslint/parser';
+
+    esLintConfig.parserOptions = {
+      project: ['tsconfig.json'],
+      tsconfigRootDir: '.',
+    };
+
+    if (Array.isArray(esLintConfig.overrides)) {
+      for (const override of esLintConfig.overrides) {
+        if (override.files.find((f) => f.endsWith('.ts'))) {
+          const hasPrettier = override.extends?.includes('prettier');
+          override.extends = [ESLINT_CONFIG_NAME];
+
+          if (hasPrettier) {
+            override.extends.push('prettier');
+          }
+        }
+      }
+    }
   };
 }
