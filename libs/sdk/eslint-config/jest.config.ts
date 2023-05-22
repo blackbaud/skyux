c7f0af0f5369207e03f017eedec979de54609757
Steve Brush <steve.brush@blackbaud.com>
2023-05-22 12:33:17 -0400

    feat: add `@skyux-sdk/eslint-config` library (#1387)

---
+++ libs/sdk/eslint-config/jest.config.ts
@@ -0,0 +1,30 @@
+/* eslint-disable */
+export default {
+  displayName: 'sdk-eslint-config',
+
+  globals: {},
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/sdk/eslint-config',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/schematics/shared/testing',
+    '<rootDir>/src/schematics/shared/utility',
+  ],
+  preset: '../../../jest.preset.js',
+};
