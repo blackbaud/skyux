1801b54179975d6563a9a282c0cf6824c485f164
John White <750350+johnhwhite@users.noreply.github.com>
2023-05-23 17:08:23 -0400

    More logging, longer timeouts

--- apps/e2e/ag-grid-storybook-e2e/cypress.config.ts
+++ apps/e2e/ag-grid-storybook-e2e/cypress.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   e2e: {
     ...nxE2EPreset(__dirname),
     video: false,
+    defaultCommandTimeout: 60000,
+    execTimeout: 60000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 60000,
+    responseTimeout: 60000,
+    taskTimeout: 60000,
     setupNodeEvents: (on, config) => {
       sendCypressScreenshotsToPercy(on, config);
     },
