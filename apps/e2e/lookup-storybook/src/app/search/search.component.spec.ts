5dcc61d8a85fa668fb476c197aebe42e6381321c
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-05-30 16:01:28 -0400

    Fix test

--- apps/e2e/lookup-storybook/src/app/search/search.component.spec.ts
+++ apps/e2e/lookup-storybook/src/app/search/search.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SearchComponent } from './search.component';
 import { SearchModule } from './search.module';
@@ -8,8 +9,31 @@ describe('SearchComponent', () => {
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+
+    Object.defineProperty(window, 'ResizeObserver', {
+      writable: true,
+      value: jest.fn().mockImplementation(() => ({
+        observe: jest.fn(),
+        unobserve: jest.fn(),
+        disconnect: jest.fn(),
+      })),
+    });
+
     TestBed.configureTestingModule({
-      imports: [SearchModule],
+      imports: [SearchModule, NoopAnimationsModule],
     });
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
