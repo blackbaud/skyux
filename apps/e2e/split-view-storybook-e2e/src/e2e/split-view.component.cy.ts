8ea69052cc5d52ebc21a77ee040fe557948ce0cb
Corey Archer <Corey.Archer@blackbaud.me>
2023-05-31 14:14:34 -0400

    test(components/split-view): update split view tests (#1415)

--- apps/e2e/split-view-storybook-e2e/src/e2e/split-view.component.cy.ts
+++ apps/e2e/split-view-storybook-e2e/src/e2e/split-view.component.cy.ts
@@ -3,25 +3,36 @@ import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 describe('split-view-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
-      beforeEach(() =>
-        cy.visit(
-          `/iframe.html?globals=theme:${theme}&id=splitviewcomponent-splitview--split-view`
-        )
+      // TODO: make new array with Lg and Xs widths
+      E2eVariations.DISPLAY_WIDTHS.concat(E2eVariations.MOBILE_WIDTHS).forEach(
+        (width) => {
+          describe(`at ${width}px`, () => {
+            beforeEach(() => {
+              cy.viewport(width, 960);
+              cy.visit(
+                `/iframe.html?globals=theme:${theme}&id=splitviewcomponent-splitview--split-view`
+              );
+            });
+            it(`should fill the page when dock is set to fill at width ${width}`, () => {
+              cy.get('#ready')
+                .should('exist')
+                .end()
+                .get('.screenshot-area')
+                .should('exist')
+                .should('be.visible')
+                .screenshot(
+                  `splitviewcomponent-splitview--split-view-dock-fill-${theme}-${width}`
+                )
+                .percySnapshot(
+                  `splitviewcomponent-splitview--split-view-dock-fill-${theme}-${width}`,
+                  {
+                    widths: [width],
+                  }
+                );
+            });
+          });
+        }
       );
-      it('should fill the page when dock is set to fill', () => {
-        cy.get('.screenshot-area')
-          .should('exist')
-          .should('be.visible')
-          .screenshot(
-            `splitviewcomponent-splitview--split-view-dock-fill-${theme}`
-          )
-          .percySnapshot(
-            `splitviewcomponent-splitview--split-view-dock-fill-${theme}`,
-            {
-              widths: E2eVariations.DISPLAY_WIDTHS,
-            }
-          );
-      });
     });
   });
 });
