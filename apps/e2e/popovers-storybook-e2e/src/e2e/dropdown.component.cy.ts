6aee4727ef7db682ddabebb7bf41b59708b53499
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-05-31 15:52:23 -0400

    different button screenshots

--- apps/e2e/popovers-storybook-e2e/src/e2e/dropdown.component.cy.ts
+++ apps/e2e/popovers-storybook-e2e/src/e2e/dropdown.component.cy.ts
@@ -3,19 +3,28 @@ import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 describe('popovers-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
-      beforeEach(() =>
-        cy.visit(
-          `/iframe.html?globals=theme:${theme}&id=dropdowncomponent-dropdown--dropdown`
-        )
-      );
-      it('should render the component', () => {
-        cy.get('app-dropdown')
-          .should('exist')
-          .should('be.visible')
-          .screenshot(`dropdowncomponent-dropdown--dropdown-${theme}`)
-          .percySnapshot(`dropdowncomponent-dropdown--dropdown-${theme}`, {
-            widths: E2eVariations.DISPLAY_WIDTHS,
+      ['default', 'primary', 'link', 'disabled'].forEach((buttonStyle) => {
+        describe(`${buttonStyle} button`, () => {
+          beforeEach(() => {
+            cy.visit(
+              `/iframe.html?globals=theme:${theme}&id=dropdowncomponent-dropdown--dropdown-${buttonStyle}-button`
+            );
           });
+          it('should render the dropdown button', () => {
+            cy.get('app-dropdown')
+              .should('exist')
+              .should('be.visible')
+              .screenshot(
+                `dropdowncomponent-dropdown--dropdown-${buttonStyle}-button-${theme}`
+              )
+              .percySnapshot(
+                `dropdowncomponent-dropdown--dropdown-${buttonStyle}-button-${theme}`,
+                {
+                  widths: E2eVariations.DISPLAY_WIDTHS,
+                }
+              );
+          });
+        });
       });
     });
   });
