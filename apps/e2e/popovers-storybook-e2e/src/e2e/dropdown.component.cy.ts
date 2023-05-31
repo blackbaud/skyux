0342bde298a831324563923d03cb288d525f9e76
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-05-31 16:15:37 -0400

    alignment screenshots

--- apps/e2e/popovers-storybook-e2e/src/e2e/dropdown.component.cy.ts
+++ apps/e2e/popovers-storybook-e2e/src/e2e/dropdown.component.cy.ts
@@ -4,25 +4,57 @@ describe('popovers-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
       ['default', 'primary', 'link', 'disabled'].forEach((buttonStyle) => {
-        describe(`${buttonStyle} button`, () => {
+        it(`should render ${buttonStyle} style dropdown buttons`, () => {
+          cy.visit(
+            `/iframe.html?globals=theme:${theme}&id=dropdowncomponent-dropdown--dropdown-${buttonStyle}-button`
+          );
+          cy.get('app-dropdown')
+            .should('exist')
+            .should('be.visible')
+            .screenshot(
+              `dropdowncomponent-dropdown--dropdown-${buttonStyle}-button-${theme}`
+            )
+            .percySnapshot(
+              `dropdowncomponent-dropdown--dropdown-${buttonStyle}-button-${theme}`,
+              {
+                widths: E2eVariations.DISPLAY_WIDTHS,
+              }
+            );
+        });
+      });
+      ['left', 'center', 'right'].forEach((horizontalAlignment) => {
+        describe(`${horizontalAlignment} aligned dropdown menu`, () => {
           beforeEach(() => {
             cy.visit(
-              `/iframe.html?globals=theme:${theme}&id=dropdowncomponent-dropdown--dropdown-${buttonStyle}-button`
+              `/iframe.html?globals=theme:${theme}&id=dropdowncomponent-dropdown--dropdown-${horizontalAlignment}-aligned`
             );
           });
-          it('should render the dropdown button', () => {
-            cy.get('app-dropdown')
-              .should('exist')
-              .should('be.visible')
-              .screenshot(
-                `dropdowncomponent-dropdown--dropdown-${buttonStyle}-button-${theme}`
-              )
-              .percySnapshot(
-                `dropdowncomponent-dropdown--dropdown-${buttonStyle}-button-${theme}`,
-                {
-                  widths: E2eVariations.DISPLAY_WIDTHS,
-                }
-              );
+          ['select', 'context-menu', 'tab'].forEach((buttonType) => {
+            it(`should open the ${buttonType} style dropdown's menu`, () => {
+              cy.get('app-dropdown').should('exist').should('be.visible');
+              cy.get(`.sky-dropdown-button-type-${buttonType}`)
+                .last()
+                .should('exist')
+                .should('be.visible')
+                .click()
+                .end()
+                .get('.sky-dropdown-menu')
+                .should('exist')
+                .should('be.visible')
+                .screenshot(
+                  `dropdowncomponent-dropdown--dropdown-${buttonType}-button-${horizontalAlignment}-algnment-${theme}`
+                )
+                .percySnapshot(
+                  `dropdowncomponent-dropdown--dropdown-${buttonType}-button-${horizontalAlignment}-algnment-${theme}`,
+                  {
+                    widths: E2eVariations.DISPLAY_WIDTHS,
+                  }
+                )
+                .get('body')
+                .should('exist')
+                .click()
+                .end();
+            });
           });
         });
       });
