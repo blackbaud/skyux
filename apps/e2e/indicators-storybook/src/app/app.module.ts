7db8337d88382c05e82b061bb1074f5e89ee7536
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2023-06-01 16:14:50 -0400

    test(components/indicators): status indicator visual tests (#1423)

--- apps/e2e/indicators-storybook/src/app/app.module.ts
+++ apps/e2e/indicators-storybook/src/app/app.module.ts
@@ -36,6 +36,13 @@ const routes: Route[] = [
     path: 'wait',
     loadChildren: () => import('./wait/wait.module').then((m) => m.WaitModule),
   },
+  {
+    path: 'status-indicator',
+    loadChildren: () =>
+      import('./status-indicator/status-indicator.module').then(
+        (m) => m.StatusIndicatorModule
+      ),
+  },
 ];
 if (routes.length > 0 && routes.findIndex((r) => r.path === '') === -1) {
   routes.push({ path: '', redirectTo: `${routes[0].path}`, pathMatch: 'full' });
---
+++ apps/e2e/indicators-storybook/src/app/status-indicator/status-indicator.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-indicator',
+  templateUrl: './status-indicator.component.html',
+  styleUrls: ['./status-indicator.component.scss'],
+})
+export class StatusIndicatorComponent {}
