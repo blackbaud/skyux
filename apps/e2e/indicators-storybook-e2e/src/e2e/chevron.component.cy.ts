c2098b3fda6f75d760a0f5a6692570a05e4e79bd
Erika McVey <erika.mcvey@blackbaud.com>
2023-06-01 16:20:48 -0400

    test(components/indicators): chevron visual tests

---
+++ apps/e2e/indicators-storybook-e2e/src/e2e/chevron.component.cy.ts
@@ -0,0 +1,22 @@
+import { E2eVariations } from '@skyux-sdk/e2e-schematics';
+
+describe('indicators-storybook', () => {
+  E2eVariations.forEachTheme((theme) => {
+    describe(`in ${theme} theme`, () => {
+      beforeEach(() =>
+        cy.visit(
+          `/iframe.html?globals=theme:${theme}&id=chevroncomponent-chevron--chevron`
+        )
+      );
+      it('should render the component', () => {
+        cy.get('app-chevron')
+          .should('exist')
+          .should('be.visible')
+          .screenshot(`chevroncomponent-chevron--chevron-${theme}`)
+          .percySnapshot(`chevroncomponent-chevron--chevron-${theme}`, {
+            widths: E2eVariations.DISPLAY_WIDTHS,
+          });
+      });
+    });
+  });
+});
--- apps/e2e/indicators-storybook/src/app/app.module.ts
+++ apps/e2e/indicators-storybook/src/app/app.module.ts
@@ -43,6 +43,11 @@ const routes: Route[] = [
         (m) => m.StatusIndicatorModule
       ),
   },
+  {
+    path: 'chevron',
+    loadChildren: () =>
+      import('./chevron/chevron.module').then((m) => m.ChevronModule),
+  },
 ];
 if (routes.length > 0 && routes.findIndex((r) => r.path === '') === -1) {
   routes.push({ path: '', redirectTo: `${routes[0].path}`, pathMatch: 'full' });
