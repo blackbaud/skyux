6d4d07cc1733fdc2f71b657f83a807cbc217df06
Erika McVey <erika.mcvey@blackbaud.com>
2023-06-01 16:23:21 -0400

    expansion indicator visual tests

---
+++ apps/e2e/indicators-storybook-e2e/src/e2e/expansion-indicator.component.cy.ts
@@ -0,0 +1,27 @@
+import { E2eVariations } from '@skyux-sdk/e2e-schematics';
+
+describe('indicators-storybook', () => {
+  E2eVariations.forEachTheme((theme) => {
+    describe(`in ${theme} theme`, () => {
+      beforeEach(() =>
+        cy.visit(
+          `/iframe.html?globals=theme:${theme}&id=expansionindicatorcomponent-expansionindicator--expansion-indicator`
+        )
+      );
+      it('should render the component', () => {
+        cy.get('app-expansion-indicator')
+          .should('exist')
+          .should('be.visible')
+          .screenshot(
+            `expansionindicatorcomponent-expansionindicator--expansion-indicator-${theme}`
+          )
+          .percySnapshot(
+            `expansionindicatorcomponent-expansionindicator--expansion-indicator-${theme}`,
+            {
+              widths: E2eVariations.DISPLAY_WIDTHS,
+            }
+          );
+      });
+    });
+  });
+});
--- apps/e2e/indicators-storybook/src/app/app.module.ts
+++ apps/e2e/indicators-storybook/src/app/app.module.ts
@@ -48,6 +48,13 @@ const routes: Route[] = [
     loadChildren: () =>
       import('./chevron/chevron.module').then((m) => m.ChevronModule),
   },
+  {
+    path: 'expansion-indicator',
+    loadChildren: () =>
+      import('./expansion-indicator/expansion-indicator.module').then(
+        (m) => m.ExpansionIndicatorModule
+      ),
+  },
 ];
 if (routes.length > 0 && routes.findIndex((r) => r.path === '') === -1) {
   routes.push({ path: '', redirectTo: `${routes[0].path}`, pathMatch: 'full' });
