3ed5556630df6f31e2620755662d6c861e0b8305
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-06-01 15:39:11 -0400

    test: add e2e infrastructure for datetime, navbar, and flyout

---
+++ apps/e2e/datetime-storybook/src/app/datepicker/datepicker.component.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, moduleMetadata } from '@storybook/angular';
+
+import { DatepickerComponent } from './datepicker.component';
+import { DatepickerModule } from './datepicker.module';
+
+export default {
+  id: 'datepickercomponent-datepicker',
+  title: 'Components/Datepicker',
+  component: DatepickerComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [DatepickerModule],
+    }),
+  ],
+} as Meta<DatepickerComponent>;
+export const Datepicker = {
+  render: (args: DatepickerComponent) => ({
+    props: args,
+  }),
+  args: {},
+};
