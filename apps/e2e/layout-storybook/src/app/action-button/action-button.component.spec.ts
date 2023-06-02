ef0f3b3210f779cd8a1e75b9739eb666b19273ba
Erika McVey <erika.mcvey@blackbaud.com>
2023-06-02 13:58:20 -0400

    mock matchMedia

--- apps/e2e/layout-storybook/src/app/action-button/action-button.component.spec.ts
+++ apps/e2e/layout-storybook/src/app/action-button/action-button.component.spec.ts
@@ -8,6 +8,20 @@ describe('ActionButtonComponent', () => {
   let fixture: ComponentFixture<ActionButtonComponent>;
 
   beforeEach(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+
     TestBed.configureTestingModule({
       imports: [ActionButtonModule],
     });
