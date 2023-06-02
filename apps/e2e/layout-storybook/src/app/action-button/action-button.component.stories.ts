12822e4ed70c50eec637a72339ec0ba24fbc45e6
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2023-06-02 14:29:11 -0400

    test(components/layout): action button visual tests (#1429)

---
+++ apps/e2e/layout-storybook/src/app/action-button/action-button.component.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, moduleMetadata } from '@storybook/angular';
+
+import { ActionButtonComponent } from './action-button.component';
+import { ActionButtonModule } from './action-button.module';
+
+export default {
+  id: 'actionbuttoncomponent-actionbutton',
+  title: 'Components/Action Button',
+  component: ActionButtonComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [ActionButtonModule],
+    }),
+  ],
+} as Meta<ActionButtonComponent>;
+export const ActionButton = {
+  render: (args: ActionButtonComponent) => ({
+    props: args,
+  }),
+  args: {},
+};
