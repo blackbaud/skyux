194e70e1ecbf45c92fac1d2bb598cc8d289727c4
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-06-06 15:17:00 -0400

    test(components/flyout): add flyout visual tests

--- apps/e2e/flyout-storybook/src/app/flyout/flyout.component.stories.ts
+++ apps/e2e/flyout-storybook/src/app/flyout/flyout.component.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, moduleMetadata } from '@storybook/angular';
+import { Meta, StoryFn, moduleMetadata } from '@storybook/angular';
 
 import { FlyoutComponent } from './flyout.component';
 import { FlyoutModule } from './flyout.module';
@@ -13,9 +13,23 @@ export default {
     }),
   ],
 } as Meta<FlyoutComponent>;
-export const Flyout = {
-  render: (args: FlyoutComponent) => ({
-    props: args,
-  }),
-  args: {},
-};
+const Flyout: StoryFn<FlyoutComponent> = (args: FlyoutComponent) => ({
+  props: args,
+});
+
+export const FlyoutStandard = Flyout.bind({});
+
+export const FlyoutHeaderButtons = Flyout.bind({});
+FlyoutHeaderButtons.args = { showHeaderButtons: true };
+
+export const FlyoutResponsiveXs = Flyout.bind({});
+FlyoutResponsiveXs.args = { responsive: 'xs' };
+
+export const FlyoutResponsiveSm = Flyout.bind({});
+FlyoutResponsiveSm.args = { responsive: 'sm' };
+
+export const FlyoutResponsiveMd = Flyout.bind({});
+FlyoutResponsiveMd.args = { responsive: 'md' };
+
+export const FlyoutResponsiveLg = Flyout.bind({});
+FlyoutResponsiveLg.args = { responsive: 'lg' };
