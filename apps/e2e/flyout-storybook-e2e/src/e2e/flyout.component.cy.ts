576ef36e880040c1d4491f6d11965a354f62d1f7
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-06-06 16:56:57 -0400

    test(components/flyout): add flyout visual tests (#1442)

--- apps/e2e/flyout-storybook-e2e/src/e2e/flyout.component.cy.ts
+++ apps/e2e/flyout-storybook-e2e/src/e2e/flyout.component.cy.ts
@@ -3,19 +3,37 @@ import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 describe('flyout-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
-      beforeEach(() =>
-        cy.visit(
-          `/iframe.html?globals=theme:${theme}&id=flyoutcomponent-flyout--flyout`
-        )
-      );
-      it('should render the component', () => {
-        cy.get('app-flyout')
-          .should('exist')
-          .should('be.visible')
-          .screenshot(`flyoutcomponent-flyout--flyout-${theme}`)
-          .percySnapshot(`flyoutcomponent-flyout--flyout-${theme}`, {
-            widths: E2eVariations.DISPLAY_WIDTHS,
-          });
+      [
+        'standard',
+        'header-buttons',
+        'responsive-xs',
+        'responsive-sm',
+        'responsive-md',
+        'responsive-lg',
+      ].forEach((style) => {
+        it(`should render the component (${style})`, () => {
+          cy.visit(
+            `/iframe.html?globals=theme:${theme}&id=flyoutcomponent-flyout--flyout-${style}`
+          );
+          cy.get('#ready').should('exist').end();
+
+          cy.get('.sky-flyout .sky-flyout-content')
+            .should('exist')
+            .should('be.visible')
+            .click()
+            .end();
+
+          cy.get('app-flyout')
+            .should('exist')
+            .should('be.visible')
+            .screenshot(`flyoutcomponent-flyout--flyout-${style}-${theme}`, {
+              capture: 'fullPage',
+            })
+            .percySnapshot(`flyoutcomponent-flyout--flyout-${style}-${theme}`, {
+              enableJavaScript: true,
+              widths: E2eVariations.DISPLAY_WIDTHS,
+            });
+        });
       });
     });
   });
--- apps/e2e/flyout-storybook/src/app/flyout/flyout.component.ts
+++ apps/e2e/flyout-storybook/src/app/flyout/flyout.component.ts
@@ -1,8 +1,100 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnDestroy,
+  inject,
+} from '@angular/core';
+import {
+  SkyFlyoutAction,
+  SkyFlyoutInstance,
+  SkyFlyoutPermalink,
+  SkyFlyoutService,
+} from '@skyux/flyout';
+import { FontLoadingService } from '@skyux/storybook';
+
+import { BehaviorSubject, Subscription } from 'rxjs';
+
+import { FlyoutResponsiveComponent } from './flyout-responsive.component';
+import { FlyoutStandardComponent } from './flyout-standard.component';
 
 @Component({
   selector: 'app-flyout',
   templateUrl: './flyout.component.html',
   styleUrls: ['./flyout.component.scss'],
 })
-export class FlyoutComponent {}
+export class FlyoutComponent implements AfterViewInit, OnDestroy {
+  public flyout: SkyFlyoutInstance<any> | undefined;
+
+  @Input()
+  public responsive: 'xs' | 'sm' | 'md' | 'lg' | undefined;
+
+  @Input()
+  public showHeaderButtons = false;
+
+  public readonly ready = new BehaviorSubject(false);
+
+  #fontLoadingService = inject(FontLoadingService);
+  #flyoutService = inject(SkyFlyoutService);
+  #subscriptions = new Subscription();
+
+  public ngOnDestroy(): void {
+    this.#subscriptions.unsubscribe();
+  }
+
+  public ngAfterViewInit() {
+    this.#subscriptions.add(
+      this.#fontLoadingService.ready().subscribe(() => {
+        this.ready.next(true);
+      })
+    );
+
+    this.openFlyout();
+  }
+
+  public openFlyout(): void {
+    let primaryAction: SkyFlyoutAction | undefined;
+    let permalink: SkyFlyoutPermalink | undefined;
+
+    if (this.showHeaderButtons) {
+      primaryAction = {
+        callback: () => {
+          return;
+        },
+        label: 'Primary action',
+      };
+      permalink = {
+        label: 'Permalink',
+        url: 'javascript:void(0);',
+      };
+    }
+
+    let width = 500;
+
+    switch (this.responsive) {
+      case 'lg':
+        width = 1280;
+        break;
+      case 'md':
+        width = 1000;
+        break;
+      case 'sm':
+        width = 800;
+        break;
+      case 'xs':
+      default:
+        break;
+    }
+
+    this.flyout = this.#flyoutService.open(
+      this.responsive ? FlyoutResponsiveComponent : FlyoutStandardComponent,
+      {
+        defaultWidth: width,
+        primaryAction: primaryAction,
+        permalink: permalink,
+        showIterator: this.showHeaderButtons,
+        maxWidth: 5000,
+      }
+    );
+  }
+}
