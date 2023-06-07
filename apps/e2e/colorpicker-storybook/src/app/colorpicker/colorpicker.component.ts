7b38da61a34844d8d78816ccb22b51be232ec885
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2023-06-07 12:22:11 -0400

    test(components/colorpicker): colorpicker visual tests (#1444)

--- apps/e2e/colorpicker-storybook/src/app/colorpicker/colorpicker.component.ts
+++ apps/e2e/colorpicker-storybook/src/app/colorpicker/colorpicker.component.ts
@@ -1,8 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-colorpicker',
   templateUrl: './colorpicker.component.html',
   styleUrls: ['./colorpicker.component.scss'],
 })
-export class ColorpickerComponent {}
+export class ColorpickerComponent {
+  public colorForm: FormGroup;
+
+  public swatches12: string[] = [
+    '#333333',
+    '#888888',
+    '#EFEFEF',
+    '#FFF',
+    '#BD4040',
+    '#617FC2',
+    '#60AC68',
+    '#3486BA',
+    '#E87134',
+    '#DA9C9C',
+    '#A1B1A7',
+    '#68AFEF',
+  ];
+
+  public swatches6: string[] = [
+    '#BD4040',
+    '#617FC2',
+    '#60AC68',
+    '#3486BA',
+    '#E87134',
+    '#DA9C9C',
+  ];
+
+  constructor(formBuilder: FormBuilder) {
+    this.colorForm = formBuilder.group({
+      colorOne: new FormControl('#f00'),
+      colorTwo: new FormControl('#ff0'),
+      colorThree: new FormControl({ value: '#000', disabled: true }),
+      colorFour: new FormControl('#00f'),
+    });
+  }
+}
