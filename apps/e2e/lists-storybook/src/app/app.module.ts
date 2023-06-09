d8cadcf8c15a03862873c77ba63c697dbbbea700
Erika McVey <erika.mcvey@blackbaud.com>
2023-06-09 10:08:54 -0400

    test(components/lists): sort visual tests

--- apps/e2e/lists-storybook/src/app/app.module.ts
+++ apps/e2e/lists-storybook/src/app/app.module.ts
@@ -15,6 +15,10 @@ const routes: Route[] = [
     loadChildren: () =>
       import('./paging/paging.module').then((m) => m.PagingModule),
   },
+  {
+    path: 'sort',
+    loadChildren: () => import('./sort/sort.module').then((m) => m.SortModule),
+  },
 ];
 if (routes.length > 0 && routes.findIndex((r) => r.path === '') === -1) {
   routes.push({ path: '', redirectTo: `${routes[0].path}`, pathMatch: 'full' });
