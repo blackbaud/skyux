9bf8d02a3c7d35828a1822fed7cbf69686cfb7ca
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2023-06-13 15:34:26 -0400

    test(components/tabs): vertical tabset visual tests (#1458)

---
+++ apps/e2e/tabs-storybook/src/app/vertical-tabs/vertical-tabs.component.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, moduleMetadata } from '@storybook/angular';
+
+import { VerticalTabsComponent } from './vertical-tabs.component';
+import { VerticalTabsModule } from './vertical-tabs.module';
+
+export default {
+  id: 'verticaltabscomponent-verticaltabs',
+  title: 'Components/Vertical Tabs',
+  component: VerticalTabsComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [VerticalTabsModule],
+    }),
+  ],
+} as Meta<VerticalTabsComponent>;
+export const VerticalTabs = {
+  render: (args: VerticalTabsComponent) => ({
+    props: args,
+  }),
+  args: {},
+};
