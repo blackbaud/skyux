628ee987c6ba738219cb83f0686d581dafbce508
Corey Archer <Corey.Archer@blackbaud.me>
2023-06-20 15:51:18 -0400

    feat(components/popovers): popover test harness and sample tests (#1390)

---
+++ libs/components/popovers/testing/src/popover/harness/popover-content-harness-filters.ts
@@ -0,0 +1,8 @@
+import { BaseHarnessFilters } from '@angular/cdk/testing';
+
+/**
+ * A set of criteria that can be used to filter a list of `SkyComponentHarness` instances.
+ * @internal
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SkyPopoverContentHarnessFilters extends BaseHarnessFilters {}
