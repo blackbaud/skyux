bb88ac64c4dcef374eae57bba7bfffaa6964a7eb
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-06-21 16:25:31 -0400

    initial setup

---
+++ apps/e2e/indicators-storybook/src/app/text-highlight/text-highlight.component.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, moduleMetadata } from '@storybook/angular';
+
+import { TextHighlightComponent } from './text-highlight.component';
+import { TextHighlightModule } from './text-highlight.module';
+
+export default {
+  id: 'texthighlightcomponent-texthighlight',
+  title: 'Components/Text Highlight',
+  component: TextHighlightComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [TextHighlightModule],
+    }),
+  ],
+} as Meta<TextHighlightComponent>;
+export const TextHighlight = {
+  render: (args: TextHighlightComponent) => ({
+    props: args,
+  }),
+  args: {},
+};
---
+++ apps/e2e/indicators-storybook/src/app/text-highlight/text-highlight.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-highlight',
+  templateUrl: './text-highlight.component.html',
+  styleUrls: ['./text-highlight.component.scss'],
+})
+export class TextHighlightComponent {}
