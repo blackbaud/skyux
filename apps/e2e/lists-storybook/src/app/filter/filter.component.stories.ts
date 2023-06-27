598734bac355d78c7a4c144c751d5fa11a0e3e92
John White <750350+johnhwhite@users.noreply.github.com>
2023-06-27 16:38:53 -0400

    test(components/lists): add filter e2e tests

---
+++ apps/e2e/lists-storybook/src/app/filter/filter.component.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, moduleMetadata } from '@storybook/angular';
+
+import { FilterComponent } from './filter.component';
+import { FilterModule } from './filter.module';
+
+export default {
+  id: 'filtercomponent-filter',
+  title: 'Components/Filter',
+  component: FilterComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [FilterModule],
+    }),
+  ],
+} as Meta<FilterComponent>;
+export const Filter = {
+  render: (args: FilterComponent) => ({
+    props: args,
+  }),
+  args: {},
+};
