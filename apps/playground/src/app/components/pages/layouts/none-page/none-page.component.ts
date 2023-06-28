d78b6bebd69875ffd767ad3898cfc2f9bc39f694
Erika McVey <erika.mcvey@blackbaud.com>
2023-06-28 16:39:25 -0400

    fix none layout imports

--- apps/playground/src/app/components/pages/layouts/none-page/none-page.component.ts
+++ apps/playground/src/app/components/pages/layouts/none-page/none-page.component.ts
@@ -2,11 +2,9 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SkyPageModule } from '@skyux/pages';
 
-import { SplitViewContentComponent } from '../shared/split-view-content/split-view-content.component';
-
 @Component({
   standalone: true,
-  imports: [CommonModule, SkyPageModule, SplitViewContentComponent],
+  imports: [CommonModule, SkyPageModule],
   templateUrl: './none-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
