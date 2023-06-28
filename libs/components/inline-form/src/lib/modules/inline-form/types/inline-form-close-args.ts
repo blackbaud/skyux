75ad1545ee24e540a7e97161a82cdfe36b0ca095
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-06-28 15:43:39 -0400

    updated reason

--- libs/components/inline-form/src/lib/modules/inline-form/types/inline-form-close-args.ts
+++ libs/components/inline-form/src/lib/modules/inline-form/types/inline-form-close-args.ts
@@ -1,9 +1,8 @@
 export interface SkyInlineFormCloseArgs {
   /**
    * Returns a `string` value to explain why users clicked a custom button and initiated a `close` event.
-   * This correlates to the `action` in `SkyInlineFormButtonConfig`. The possible values are `cancel`, `delete`, `done`, `save`,
-   * and any custom values provided through `action`. The close event is automatic for the standard action values, but
-   * consumers must hook into the event for other custom values.
+   * This correlates to either the `action` in `SkyInlineFormButtonConfig` for custom buttons or the standard
+   * values of `cancel`, `delete`, `done`, or `save` for predefined buttons.
    */
   reason: string;
 }
