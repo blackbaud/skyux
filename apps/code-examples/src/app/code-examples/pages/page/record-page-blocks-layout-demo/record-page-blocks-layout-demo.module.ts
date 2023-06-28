06c1fc8deee3599914df160435dbc9ee2e3d0868
Erika McVey <erika.mcvey@blackbaud.com>
2023-06-28 17:17:44 -0400

    record page blocks layout demo

--- apps/code-examples/src/app/code-examples/pages/page/record-page-blocks-layout-demo/record-page-blocks-layout-demo.module.ts
+++ apps/code-examples/src/app/code-examples/pages/page/record-page-blocks-layout-demo/record-page-blocks-layout-demo.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
+import { SkyIconModule, SkyKeyInfoModule } from '@skyux/indicators';
+import {
+  SkyBoxModule,
+  SkyDescriptionListModule,
+  SkyFluidGridModule,
+} from '@skyux/layout';
+import { SkyRepeaterModule } from '@skyux/lists';
 import { SkyPageModule } from '@skyux/pages';
 
 import { RecordPageBlocksLayoutDemoComponent } from './record-page-blocks-layout-demo.component';
+import { RecordPageContentComponent } from './record-page-content.component';
 
 @NgModule({
-  declarations: [RecordPageBlocksLayoutDemoComponent],
+  declarations: [
+    RecordPageBlocksLayoutDemoComponent,
+    RecordPageContentComponent,
+  ],
   exports: [RecordPageBlocksLayoutDemoComponent],
-  imports: [SkyPageModule],
+  imports: [
+    SkyPageModule,
+    SkyFluidGridModule,
+    SkyBoxModule,
+    SkyDescriptionListModule,
+    SkyIconModule,
+    SkyKeyInfoModule,
+    SkyRepeaterModule,
+  ],
 })
 export class RecordPageBlocksLayoutDemoModule {}
---
+++ apps/code-examples/src/app/code-examples/pages/page/record-page-blocks-layout-demo/record-page-content.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-record-page-content',
+  templateUrl: './record-page-content.component.html',
+  styleUrls: ['./record-page-content.component.scss'],
+})
+export class RecordPageContentComponent {}
