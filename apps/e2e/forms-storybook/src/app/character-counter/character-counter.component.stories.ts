5f86f5435180ada27315e71647c0563981af82e4
John White <750350+johnhwhite@users.noreply.github.com>
2023-06-28 10:39:30 -0400

    test(components/forms): add character counter e2e tests (#1478)
    * test(components/forms): add character counter e2e tests
    
    * Prettier

---
+++ apps/e2e/forms-storybook/src/app/character-counter/character-counter.component.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, moduleMetadata } from '@storybook/angular';
+
+import { CharacterCounterComponent } from './character-counter.component';
+import { CharacterCounterModule } from './character-counter.module';
+
+export default {
+  id: 'charactercountercomponent-charactercounter',
+  title: 'Components/Character Counter',
+  component: CharacterCounterComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [CharacterCounterModule],
+    }),
+  ],
+} as Meta<CharacterCounterComponent>;
+export const CharacterCounter = {
+  render: (args: CharacterCounterComponent) => ({
+    props: args,
+  }),
+  args: {},
+};
