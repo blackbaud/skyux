8595ae23b40e631cb16ed10160ce2830e051f589
Corey Archer <corey.archer@blackbaud.me>
2023-06-29 12:48:24 -0400

    deprecation(components/forms): deprecate source property for checkbox and radio button change outputs

--- libs/components/forms/src/lib/modules/radio/types/radio-change.ts
+++ libs/components/forms/src/lib/modules/radio/types/radio-change.ts
@@ -1,9 +1,13 @@
 import { SkyRadioComponent } from '../radio.component';
 
+/**
+ * Fires when users select a radio button.
+ */
 export interface SkyRadioChange {
   /**
    * The radio component that triggered the change event.
    * @internal
+   * @deprecated
    */
   source: SkyRadioComponent;
   /**
