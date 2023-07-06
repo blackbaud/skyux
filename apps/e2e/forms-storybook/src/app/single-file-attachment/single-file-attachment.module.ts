9cce8aed2d7ab08cc7c2c8c56af85b2622de5273
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-07-06 14:33:45 -0400

    remove stuff

--- apps/e2e/forms-storybook/src/app/single-file-attachment/single-file-attachment.module.ts
+++ apps/e2e/forms-storybook/src/app/single-file-attachment/single-file-attachment.module.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { SkyFileAttachmentsModule } from '@skyux/forms';
 
 import { SingleFileAttachmentComponent } from './single-file-attachment.component';
 
 const routes: Routes = [{ path: '', component: SingleFileAttachmentComponent }];
 @NgModule({
   declarations: [SingleFileAttachmentComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    SkyFileAttachmentsModule,
+  ],
   exports: [SingleFileAttachmentComponent],
 })
 export class SingleFileAttachmentModule {}
