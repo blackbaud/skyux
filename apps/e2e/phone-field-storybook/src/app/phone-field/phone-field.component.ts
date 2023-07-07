8ad50f3c099888ab80ca00eba53608a13b13cf30
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-07-07 12:53:53 -0400

    add code example code

--- apps/e2e/phone-field-storybook/src/app/phone-field/phone-field.component.ts
+++ apps/e2e/phone-field-storybook/src/app/phone-field/phone-field.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-phone-field',
   templateUrl: './phone-field.component.html',
   styleUrls: ['./phone-field.component.scss'],
 })
-export class PhoneFieldComponent {}
+export class PhoneFieldComponent {
+  public phoneControl: UntypedFormControl;
+
+  public phoneForm: UntypedFormGroup;
+
+  constructor() {
+    this.phoneControl = new UntypedFormControl();
+    this.phoneForm = new UntypedFormGroup({
+      phoneControl: this.phoneControl,
+    });
+  }
+}
--- apps/e2e/phone-field-storybook/src/app/phone-field/phone-field.module.ts
+++ apps/e2e/phone-field-storybook/src/app/phone-field/phone-field.module.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { SkyInputBoxModule } from '@skyux/forms';
+import { SkyPhoneFieldModule } from '@skyux/phone-field';
 
 import { PhoneFieldComponent } from './phone-field.component';
 
 const routes: Routes = [{ path: '', component: PhoneFieldComponent }];
 @NgModule({
   declarations: [PhoneFieldComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    SkyInputBoxModule,
+    SkyPhoneFieldModule,
+  ],
   exports: [PhoneFieldComponent],
 })
 export class PhoneFieldModule {}
