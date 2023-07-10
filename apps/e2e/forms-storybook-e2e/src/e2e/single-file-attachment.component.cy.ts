3817749fc242f5185629e3af7504af8d6e96b1c3
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-07-10 11:11:21 -0400

    single image story

--- apps/e2e/forms-storybook-e2e/src/e2e/single-file-attachment.component.cy.ts
+++ apps/e2e/forms-storybook-e2e/src/e2e/single-file-attachment.component.cy.ts
@@ -3,28 +3,24 @@ import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 describe('forms-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
-      ['states', 'image', 'file'].forEach((mode) => {
-        describe(`for single file attachment ${mode}`, () => {
-          beforeEach(() =>
-            cy.visit(
-              `/iframe.html?globals=theme:${theme}&id=singlefileattachmentcomponent-singlefileattachment--single-file-attachment-${mode}`
-            )
+      beforeEach(() =>
+        cy.visit(
+          `/iframe.html?globals=theme:${theme}&id=singlefileattachmentcomponent-singlefileattachment--single-file-attachment`
+        )
+      );
+      it('should render the single file attachment component', () => {
+        cy.get('app-single-file-attachment')
+          .should('exist')
+          .should('be.visible')
+          .screenshot(
+            `singlefileattachmentcomponent-singlefileattachment--single-file-attachment-${theme}`
+          )
+          .percySnapshot(
+            `singlefileattachmentcomponent-singlefileattachment--single-file-attachment-${theme}`,
+            {
+              widths: E2eVariations.DISPLAY_WIDTHS,
+            }
           );
-          it('should render the single file attachment component', () => {
-            cy.get('app-single-file-attachment')
-              .should('exist')
-              .should('be.visible')
-              .screenshot(
-                `singlefileattachmentcomponent-singlefileattachment--single-file-attachment-${mode}-${theme}`
-              )
-              .percySnapshot(
-                `singlefileattachmentcomponent-singlefileattachment--single-file-attachment-${mode}-${theme}`,
-                {
-                  widths: E2eVariations.DISPLAY_WIDTHS,
-                }
-              );
-          });
-        });
       });
     });
   });
