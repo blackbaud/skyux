ae8e71ef691c9c9218bd49100f795339a65305ac
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.com>
2023-07-10 15:59:25 -0400

    test(components/forms): single file attachment visual test (#1497)

---
+++ apps/e2e/forms-storybook/src/app/single-file-attachment/single-file-attachment.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { SkyFileAttachmentsModule } from '@skyux/forms';
+import { SkyHelpInlineModule } from '@skyux/indicators';
+
+import { SingleFileAttachmentComponent } from './single-file-attachment.component';
+
+const routes: Routes = [{ path: '', component: SingleFileAttachmentComponent }];
+@NgModule({
+  declarations: [SingleFileAttachmentComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    SkyFileAttachmentsModule,
+    SkyHelpInlineModule,
+  ],
+  exports: [SingleFileAttachmentComponent],
+})
+export class SingleFileAttachmentModule {}
