c4b2d9cd49fd0ae1bcfe7395a29b1dbdc761992e
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-07-11 12:13:44 -0400

    screenshots

--- apps/e2e/phone-field-storybook/src/app/phone-field/phone-field.component.ts
+++ apps/e2e/phone-field-storybook/src/app/phone-field/phone-field.component.ts
@@ -8,13 +8,29 @@ import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 })
 export class PhoneFieldComponent {
   public phoneControl: UntypedFormControl;
+  public phoneControlError: UntypedFormControl;
+  public phoneControlInput: UntypedFormControl;
 
   public phoneForm: UntypedFormGroup;
+  public phoneFormError: UntypedFormGroup;
+  public phoneFormInput: UntypedFormGroup;
 
   constructor() {
     this.phoneControl = new UntypedFormControl();
     this.phoneForm = new UntypedFormGroup({
       phoneControl: this.phoneControl,
     });
+
+    this.phoneControlError = new UntypedFormControl();
+    this.phoneFormError = new UntypedFormGroup({
+      phoneControlError: this.phoneControlError,
+    });
+    this.phoneControlError.setValue('bbb');
+
+    this.phoneControlInput = new UntypedFormControl();
+    this.phoneFormInput = new UntypedFormGroup({
+      phoneControlInput: this.phoneControlInput,
+    });
+    this.phoneControlInput.setValue('2015550123');
   }
 }
