bee3f943bbea216c90cd92f5648ac5370f962d52
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-07-12 11:21:48 -0400

    add code example code

--- apps/e2e/text-editor-storybook/src/app/text-editor/text-editor.module.ts
+++ apps/e2e/text-editor-storybook/src/app/text-editor/text-editor.module.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { SkyTextEditorModule } from '@skyux/text-editor';
 
 import { TextEditorComponent } from './text-editor.component';
 
 const routes: Routes = [{ path: '', component: TextEditorComponent }];
 @NgModule({
   declarations: [TextEditorComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    SkyTextEditorModule,
+  ],
   exports: [TextEditorComponent],
 })
 export class TextEditorModule {}
