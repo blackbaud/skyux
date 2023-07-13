d045ed52f13696153c34a7583987d3bd5ce102ba
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-07-13 17:45:35 -0400

    all stories

--- apps/e2e/text-editor-storybook/src/app/text-editor/text-editor.component.ts
+++ apps/e2e/text-editor-storybook/src/app/text-editor/text-editor.component.ts
@@ -35,7 +35,7 @@ export class TextEditorComponent {
 
   public myForm: UntypedFormGroup;
 
-  public placeholder = 'Enter text here';
+  public placeholder = 'This is what placeholder text looks like';
 
   public menus = ['edit', 'format', 'merge-field'];
 
@@ -67,8 +67,6 @@ export class TextEditorComponent {
   public initialState = {
     boldState: true,
   } as SkyTextEditorStyleState;
-
-  //private richText = `<font style="font-size: 18px" face="Arial" color="#a25353"><b>Exclusively committed to your impact</b></font><p>Since day one, Blackbaud has been 100% focused on driving impact for social good organizations.</p><p>We equip change agents with <b>cloud software</b>, <i>services</i>, <u>expertise</u>, and <font color="#a25353">data intelligence</font> designed with unmatched insight and supported with unparalleled commitment. Every day, our <b>customers</b> achieve unmatched impact as they advance their missions.</p><ul><li><a href="#">Build a better world</a></li><li><a href="#">Explore our solutions</a></li></ul>`;
 
   #_disabledFlag = false;
 
