3499aa2c4eee76c4709b353eee3c18b3d8928487
Erika McVey <erika.mcvey@blackbaud.com>
2023-07-20 14:55:29 -0400

    modal form

--- apps/playground/src/app/dark-mode/modal/modal.component.ts
+++ apps/playground/src/app/dark-mode/modal/modal.component.ts
@@ -1,15 +1,22 @@
+/* eslint-disable @cspell/spellchecker */
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
-  AbstractControl,
+  FormControl,
   UntypedFormBuilder,
   UntypedFormGroup,
 } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SkyColorpickerModule } from '@skyux/colorpicker';
-import { SkyIdModule } from '@skyux/core';
-import { SkyInputBoxModule } from '@skyux/forms';
+import { SkyDatepickerModule } from '@skyux/datetime';
+import {
+  SkyCheckboxModule,
+  SkyInputBoxModule,
+  SkyRadioModule,
+} from '@skyux/forms';
 import { SkyIconModule } from '@skyux/indicators';
+import { SkyFluidGridModule } from '@skyux/layout';
+import { SkyLookupModule } from '@skyux/lookup';
 import { SkyModalInstance } from '@skyux/modals';
 import { SkyModalModule } from '@skyux/modals';
 
@@ -19,34 +26,53 @@ import { SkyModalModule } from '@skyux/modals';
   standalone: true,
   imports: [
     CommonModule,
+    SkyCheckboxModule,
     SkyColorpickerModule,
+    SkyDatepickerModule,
+    SkyFluidGridModule,
     SkyIconModule,
-    SkyIdModule,
     SkyInputBoxModule,
+    SkyLookupModule,
     SkyModalModule,
+    SkyRadioModule,
     ReactiveFormsModule,
   ],
 })
 export class ModalComponent {
   public form: UntypedFormGroup;
-  public fields: string[] = [];
+  public modal = inject(SkyModalInstance);
 
-  constructor(
-    fb: UntypedFormBuilder,
-    public modal: SkyModalInstance,
-    @Inject('modalTitle') public title: string
-  ) {
-    const controls: Record<string, AbstractControl> = {};
-    for (let i = 1; i <= 5; i++) {
-      const field = `${this.title} ${i}`;
-      this.fields.push(field);
-      controls[field] = fb.control('');
-    }
-    this.form = fb.group(controls);
-    this.modal.closed.subscribe((args) => {
-      if (args.reason === 'save') {
-        console.log(this.form.value);
-      }
+  #formBuilder = inject(UntypedFormBuilder);
+
+  public constituent = new FormControl('');
+  public fundraiser = new FormControl('');
+  public contactDate = new FormControl('');
+  public contactMethod = new FormControl('');
+  public notes = new FormControl('');
+  public sentiment = new FormControl('positive');
+  public additionalContact = new FormControl(false);
+
+  public constituents = [
+    { name: 'Barbara Durr' },
+    { name: 'Robert Hernandez' },
+    { name: 'Cristeen Lunsford' },
+  ];
+
+  public fundraisers = [
+    { name: 'Latrice Ashmore' },
+    { name: 'Wonda Lumpkin' },
+    { name: 'Ed Sipes' },
+  ];
+
+  constructor() {
+    this.form = this.#formBuilder.group({
+      constituent: this.constituent,
+      fundraiser: this.fundraiser,
+      contactDate: this.contactDate,
+      contactMethod: this.contactMethod,
+      notes: this.notes,
+      sentiment: this.sentiment,
+      additionalContact: this.additionalContact,
     });
   }
 }
