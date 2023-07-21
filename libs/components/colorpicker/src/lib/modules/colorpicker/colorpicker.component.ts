a4c594fc5435ea1d505663dc6bb782f248b5f1d8
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-07-21 10:08:54 -0400

    fix and comment

--- libs/components/colorpicker/src/lib/modules/colorpicker/colorpicker.component.ts
+++ libs/components/colorpicker/src/lib/modules/colorpicker/colorpicker.component.ts
@@ -619,6 +619,7 @@ export class SkyColorpickerComponent implements OnInit, OnDestroy {
     return hsva;
   }
 
+  // this background color does not take into account opacity, the text is no longer accesible when a dark color is picked with low opacity
   // http://www.w3.org/TR/AERT#color-contrast
   #getAccessibleIconColor(
     backgroundColor: SkyColorpickerOutput | undefined
