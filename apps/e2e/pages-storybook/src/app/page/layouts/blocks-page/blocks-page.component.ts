c6f3428ecaebb83635f1398d0fe04c6f368c22d6
Erika McVey <erika.mcvey@blackbaud.com>
2023-07-24 11:10:12 -0400

    add page header details to visual tests

--- apps/e2e/pages-storybook/src/app/page/layouts/blocks-page/blocks-page.component.ts
+++ apps/e2e/pages-storybook/src/app/page/layouts/blocks-page/blocks-page.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SkyLabelModule } from '@skyux/indicators';
 import { SkyBoxModule, SkyFluidGridModule } from '@skyux/layout';
 import { SkyPageModule } from '@skyux/pages';
 
 @Component({
   selector: 'app-blocks-page',
   standalone: true,
-  imports: [CommonModule, SkyBoxModule, SkyFluidGridModule, SkyPageModule],
+  imports: [
+    CommonModule,
+    SkyBoxModule,
+    SkyFluidGridModule,
+    SkyLabelModule,
+    SkyPageModule,
+  ],
   templateUrl: './blocks-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
