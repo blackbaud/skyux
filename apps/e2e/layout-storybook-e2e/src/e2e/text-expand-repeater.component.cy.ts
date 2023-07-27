c2968619ccfa24a48736ffbab35f0fecb6a1ae6a
Corey Archer <corey.archer@blackbaud.me>
2023-07-27 10:05:10 -0400

    update test name

--- apps/e2e/layout-storybook-e2e/src/e2e/text-expand-repeater.component.cy.ts
+++ apps/e2e/layout-storybook-e2e/src/e2e/text-expand-repeater.component.cy.ts
@@ -1,6 +1,6 @@
 import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 
-describe('layout-storybook', () => {
+describe('layout-storybook - text expand repeater', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
       beforeEach(() =>
