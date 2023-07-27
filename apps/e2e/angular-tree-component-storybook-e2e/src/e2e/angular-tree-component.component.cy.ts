bcd09a786404a2d4c2ada20d01d168c16f1ec677
Sandhya <sandhya.rajasabeson@blackbaud.me>
2023-07-27 17:32:59 -0400

    test(components/angular-tree-component): tree view visual test

--- apps/e2e/angular-tree-component-storybook-e2e/src/e2e/angular-tree-component.component.cy.ts
+++ apps/e2e/angular-tree-component-storybook-e2e/src/e2e/angular-tree-component.component.cy.ts
@@ -3,24 +3,51 @@ import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 describe('angular-tree-component-storybook', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
-      beforeEach(() =>
-        cy.visit(
-          `/iframe.html?globals=theme:${theme}&id=angulartreecomponentcomponent-angulartreecomponent--angular-tree-component`
-        )
-      );
-      it('should render the component', () => {
-        cy.get('app-angular-tree-component')
-          .should('exist')
-          .should('be.visible')
-          .screenshot(
-            `angulartreecomponentcomponent-angulartreecomponent--angular-tree-component-${theme}`
-          )
-          .percySnapshot(
-            `angulartreecomponentcomponent-angulartreecomponent--angular-tree-component-${theme}`,
-            {
-              widths: E2eVariations.DISPLAY_WIDTHS,
-            }
+      [
+        'basic',
+        'selection-multi-select',
+        'selection-multi-select-cascading',
+        'selection-single-select',
+        'modes',
+      ].forEach((mode) => {
+        describe(`in ${mode} Tree View Component`, () => {
+          beforeEach(() =>
+            cy.visit(
+              `/iframe.html?globals=theme:${theme}&id=angulartreecomponentcomponent-angulartreecomponent--${mode}-angular-tree-component`
+            )
           );
+          it('should render the component', () => {
+            cy.get('app-angular-tree-component')
+              .should('exist')
+              .should('be.visible');
+
+            cy.get('tree-node-content')
+              .should('exist')
+              .first()
+              .should('exist')
+              .should('be.visible')
+              .click()
+              .end();
+            cy.get('tree-node-content')
+              .should('exist')
+              .eq(1)
+              .should('exist')
+              .should('be.visible')
+              .click()
+              .end();
+
+            cy.get('app-angular-tree-component')
+              .screenshot(
+                `angulartreecomponentcomponent-angulartreecomponent--${mode}-angular-tree-component-${theme}`
+              )
+              .percySnapshot(
+                `angulartreecomponentcomponent-angulartreecomponent--${mode}-angular-tree-component-${theme}`,
+                {
+                  widths: E2eVariations.DISPLAY_WIDTHS,
+                }
+              );
+          });
+        });
       });
     });
   });
--- apps/e2e/angular-tree-component-storybook/src/app/angular-tree-component/angular-tree-component.component.ts
+++ apps/e2e/angular-tree-component-storybook/src/app/angular-tree-component/angular-tree-component.component.ts
@@ -1,8 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITreeOptions } from '@circlon/angular-tree-component';
 
 @Component({
   selector: 'app-angular-tree-component',
   templateUrl: './angular-tree-component.component.html',
   styleUrls: ['./angular-tree-component.component.scss'],
 })
-export class AngularTreeComponentComponent {}
+export class AngularTreeComponentComponent {
+  @Input()
+  public showModes: boolean = false;
+
+  @Input()
+  public set useCheckbox(flag: boolean) {
+    this.basicOptions.useCheckbox = flag;
+  }
+
+  @Input()
+  public set allowCascading(flag: boolean) {
+    this.basicOptions.useTriState = flag;
+  }
+
+  @Input()
+  public singleSelectFlag: boolean = false;
+
+  public basicOptions: ITreeOptions = {
+    animateExpand: true,
+  };
+
+  public selectionOptions: ITreeOptions = {
+    animateExpand: true,
+    useCheckbox: true,
+  };
+
+  public modeNodes: any[] = [
+    {
+      if: 1,
+      name: 'United States',
+      isExpanded: true,
+      showHelp: true,
+      children: [{ id: 2, name: 'Alabama' }],
+    },
+  ];
+
+  public nodes: any[] = [
+    {
+      id: 1,
+      name: 'United States',
+      isExpanded: true,
+      showHelp: true,
+      children: [
+        { id: 2, name: 'Alabama' },
+        { id: 3, name: 'California' },
+        {
+          id: 4,
+          name: 'Indiana',
+          isExpanded: false,
+          children: [
+            { id: 5, name: 'Adams' },
+            { id: 6, name: 'Allen' },
+            { id: 7, name: 'Bartholomew' },
+          ],
+        },
+        { id: 13, name: 'Florida' },
+      ],
+    },
+    {
+      id: 12,
+      name: 'Mexico',
+      showHelp: true,
+      isExpanded: true,
+      children: [],
+    },
+    {
+      id: 8,
+      name: 'Canada',
+      isExpanded: false,
+      children: [
+        { id: 9, name: 'Alberta' },
+        { id: 10, name: 'British Columbia' },
+        { id: 11, name: 'Manitoba' },
+      ],
+    },
+    {
+      id: 14,
+      name: 'Argentina',
+      isExpanded: true,
+      children: [],
+    },
+  ];
+}
