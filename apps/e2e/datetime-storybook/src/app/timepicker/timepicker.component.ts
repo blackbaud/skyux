c80ebdb73c744c4ee61d0d564133d73bc130a55e
Corey Archer <corey.archer@blackbaud.me>
2023-07-31 11:06:57 -0400

    static input value

--- apps/e2e/datetime-storybook/src/app/timepicker/timepicker.component.ts
+++ apps/e2e/datetime-storybook/src/app/timepicker/timepicker.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SkyTimepickerTimeOutput } from '@skyux/datetime';
 
 @Component({
   selector: 'app-timepicker',
@@ -8,4 +9,16 @@ import { Component, Input } from '@angular/core';
 export class TimepickerComponent {
   @Input()
   public timeFormat: 'hh' | 'HH' = 'hh';
+
+  public time: SkyTimepickerTimeOutput = {
+    hour: 0,
+    minute: 30,
+    timezone: -4,
+    meridie: 'AM',
+    customFormat: 'h:mm A',
+    local: '12:30 AM',
+    iso8601: new Date(
+      'Mon Jul 31 2023 00:30:00 GMT-0400 (Eastern Daylight Time)'
+    ),
+  };
 }
