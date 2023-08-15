c3a18a4593daf15204fe091cb1e8dd653623b18a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-08-15 15:26:53 -0400

    fix(components/flyout): move `SkyFlyoutService` to `SkyFlyoutModule` providers

--- apps/playground/src/app/components/flyout/flyout/flyout-responsive-demo.component.ts
+++ apps/playground/src/app/components/flyout/flyout/flyout-responsive-demo.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { SkyFlyoutService } from '@skyux/flyout';
 
 @Component({
   selector: 'app-flyout-responsive-demo',
   templateUrl: './flyout-responsive-demo.component.html',
-  providers: [SkyFlyoutService],
 })
 export class FlyoutResponsiveDemoComponent {}
