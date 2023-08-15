a57fba7c01650b9f6a6ee62e3f70a4ecb8c4842b
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2023-08-15 16:44:48 -0400

    feat(components/pages): make page component a responsive container (#1552)

--- libs/components/core/testing/src/public-api.ts
+++ libs/components/core/testing/src/public-api.ts
@@ -5,3 +5,8 @@ export { SkyHarnessFilters } from './shared/harness-filters';
 export { SkyCoreTestingModule } from './core-testing.module';
 export { MockSkyMediaQueryService } from './mock-media-query.service';
 export { MockSkyUIConfigService } from './mock-ui-config.service';
+export {
+  mockResizeObserver,
+  mockResizeObserverEntry,
+  mockResizeObserverHandle,
+} from './resize-observer-mock';
--- libs/components/modals/src/lib/modules/modal/modal-host.component.ts
+++ libs/components/modals/src/lib/modules/modal/modal-host.component.ts
@@ -110,6 +110,7 @@ export class SkyModalHostComponent implements OnDestroy {
 
     params.providers ||= [];
     params.providers.push(
+      { provide: SkyResizeObserverMediaQueryService },
       {
         provide: SkyModalHostService,
         useValue: hostService,
