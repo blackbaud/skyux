22b390278f9d89ce65265e92c2800d40e8dae56e
Steve Brush <steve.brush@blackbaud.com>
2023-08-17 08:50:43 -0400

    fix(components/core)!: move `SkyNumericService` to `SkyNumericModule` providers (#1584)
    BREAKING CHANGE: The `SkyNumericService` cannot be used in isolation; any component that injects `SkyNumericService` must also import `SkyNumericModule` into its module's providers.

--- libs/components/core/src/lib/modules/numeric/numeric.module.ts
+++ libs/components/core/src/lib/modules/numeric/numeric.module.ts
@@ -4,10 +4,11 @@ import { SkyI18nModule } from '@skyux/i18n';
 import { SkyCoreResourcesModule } from '../shared/sky-core-resources.module';
 
 import { SkyNumericPipe } from './numeric.pipe';
+import { SkyNumericService } from './numeric.service';
 
 @NgModule({
   declarations: [SkyNumericPipe],
-  providers: [SkyNumericPipe],
+  providers: [SkyNumericPipe, SkyNumericService],
   imports: [SkyI18nModule, SkyCoreResourcesModule],
   exports: [SkyNumericPipe],
 })
