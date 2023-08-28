c9a9cf8a04d5ae3f3630c6c9c47d70e83ffe4e4e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-08-28 09:54:48 -0400

    fix sectioned form e2e tests

--- apps/e2e/tabs-storybook/src/app/sectioned-form/sectioned-form-date-form-demo.component.ts
+++ apps/e2e/tabs-storybook/src/app/sectioned-form/sectioned-form-date-form-demo.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { SkyDateRangePickerModule } from '@skyux/datetime';
 
 @Component({
+  standalone: true,
   selector: 'app-sectioned-form-date-form-demo',
   templateUrl: './sectioned-form-date-form-demo.component.html',
+  imports: [SkyDateRangePickerModule],
 })
 export class SectionedFormDateFormDemoComponent {}
