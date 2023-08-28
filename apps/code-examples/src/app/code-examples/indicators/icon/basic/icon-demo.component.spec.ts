32ca21fffe06b93fad387afaf00c09883ed57cdc
Corey Archer <corey.archer@blackbaud.me>
2023-08-28 18:20:12 -0400

    update icon code examples and integrations

--- apps/code-examples/src/app/code-examples/indicators/icon/basic/icon-demo.component.spec.ts
+++ apps/code-examples/src/app/code-examples/indicators/icon/basic/icon-demo.component.spec.ts
@@ -1,15 +1,50 @@
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SkyIconHarness } from '@skyux/indicators/testing';
+import {
+  SkyTheme,
+  SkyThemeMode,
+  SkyThemeService,
+  SkyThemeSettings,
+  SkyThemeSettingsChange,
+} from '@skyux/theme';
+
+import { BehaviorSubject } from 'rxjs';
 
 import { IconDemoComponent } from './icon-demo.component';
 import { IconDemoModule } from './icon-demo.module';
 
 describe('Basic icon', () => {
-  async function setupTest(): Promise<{
+  let mockThemeSvc: {
+    settingsChange: BehaviorSubject<SkyThemeSettingsChange>;
+  };
+
+  async function setupTest(
+    options: { theme?: 'default' | 'modern' } = {}
+  ): Promise<{
     iconHarness: SkyIconHarness;
     fixture: ComponentFixture<IconDemoComponent>;
   }> {
+    mockThemeSvc = {
+      settingsChange: new BehaviorSubject<SkyThemeSettingsChange>({
+        currentSettings: new SkyThemeSettings(
+          SkyTheme.presets[options?.theme || 'default'],
+          SkyThemeMode.presets.light
+        ),
+        previousSettings: undefined,
+      }),
+    };
+
+    TestBed.configureTestingModule({
+      imports: [IconDemoModule],
+      providers: [
+        {
+          provide: SkyThemeService,
+          useValue: mockThemeSvc,
+        },
+      ],
+    }).compileComponents();
+
     const fixture = TestBed.createComponent(IconDemoComponent);
     const loader = TestbedHarnessEnvironment.loader(fixture);
     const iconHarness = await loader.getHarness(
@@ -21,19 +56,25 @@ describe('Basic icon', () => {
     return { iconHarness, fixture };
   }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [IconDemoModule],
-    });
-  });
-
-  it('should display the correct icon', async () => {
+  it('should display the correct icon in default theme', async () => {
     const { iconHarness, fixture } = await setupTest();
 
     fixture.detectChanges();
 
     await expectAsync(iconHarness.getIconName()).toBeResolvedTo('calendar');
-    await expectAsync(iconHarness.getIconType()).toBeResolvedTo('skyux');
+    await expectAsync(iconHarness.getVariant()).toBeRejectedWithError(
+      'Variant cannot be determined because variants are only assigned to icons with type `skyux`.'
+    );
+    await expectAsync(iconHarness.getIconSize()).toBeResolvedTo('4x');
+    await expectAsync(iconHarness.isFixedWidth()).toBeResolvedTo(true);
+  });
+
+  it('should display the correct icon in modern theme', async () => {
+    const { iconHarness, fixture } = await setupTest({ theme: 'modern' });
+
+    fixture.detectChanges();
+
+    await expectAsync(iconHarness.getIconName()).toBeResolvedTo('calendar');
     await expectAsync(iconHarness.getVariant()).toBeResolvedTo('solid');
     await expectAsync(iconHarness.getIconSize()).toBeResolvedTo('4x');
     await expectAsync(iconHarness.isFixedWidth()).toBeResolvedTo(true);
