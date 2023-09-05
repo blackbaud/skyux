2bb60e4b798bb363bf82aec7b204a76ea98ac161
Trevor Burch <Trevor.Burch@blackbaud.com>
2023-09-05 16:45:37 -0400

    fix(components/pages): `lastAccessed` property on the `SkyRecentLink` interface is more descriptive

--- libs/components/pages/src/lib/modules/action-hub/types/recent-link.ts
+++ libs/components/pages/src/lib/modules/action-hub/types/recent-link.ts
@@ -3,7 +3,7 @@ import { SkyPageLink } from './page-link';
 /**
  * Link that also has a date field to sort by.
  *
- * @property lastAccessed Date Object or ISO-8601 string.
+ * @property lastAccessed The date when the link was last accessed. Valid types are a Date object or ISO-8601 string.
  */
 export interface SkyRecentLink extends SkyPageLink {
   lastAccessed: Date | string;
