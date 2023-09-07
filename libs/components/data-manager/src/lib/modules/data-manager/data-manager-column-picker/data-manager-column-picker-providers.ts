acd109b7e36f087c0efbd5afb6414785b86a0465
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-09-07 12:04:32 -0400

    useClass

--- libs/components/data-manager/src/lib/modules/data-manager/data-manager-column-picker/data-manager-column-picker-providers.ts
+++ libs/components/data-manager/src/lib/modules/data-manager/data-manager-column-picker/data-manager-column-picker-providers.ts
@@ -1,11 +1,11 @@
-import { StaticProvider } from '@angular/core';
+import { Provider } from '@angular/core';
 
 import { SkyDataManagerColumnPickerImplService } from './data-manager-column-picker-impl.service';
 import { SkyDataManagerColumnPickerService } from './data-manager-column-picker.service';
 
-export const SKY_DATA_MANAGER_COLUMN_PICKER_PROVIDERS: StaticProvider[] = [
+export const SKY_DATA_MANAGER_COLUMN_PICKER_PROVIDERS: Provider[] = [
   {
     provide: SkyDataManagerColumnPickerService,
-    useValue: new SkyDataManagerColumnPickerImplService(),
+    useClass: SkyDataManagerColumnPickerImplService,
   },
 ];
