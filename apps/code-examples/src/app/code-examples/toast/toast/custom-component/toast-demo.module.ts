f0b340c308a35c648fc7bceeee7292017bc42b95
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-09-08 11:53:04 -0400

    toast standalone

--- apps/code-examples/src/app/code-examples/toast/toast/custom-component/toast-demo.module.ts
+++ apps/code-examples/src/app/code-examples/toast/toast/custom-component/toast-demo.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { ToastContentDemoComponent } from './toast-content-demo.component';
 import { ToastDemoComponent } from './toast-demo.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ToastContentDemoComponent, ToastDemoComponent],
+  declarations: [ToastDemoComponent],
   exports: [ToastDemoComponent],
 })
 export class ToastDemoModule {}
