bb7fd8541cfa56dd30720313688e8aec70bded20
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-09-15 19:40:02 -0400

    update action bars module

--- apps/code-examples/src/app/features/action-bars.module.ts
+++ apps/code-examples/src/app/features/action-bars.module.ts
@@ -1,37 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DemoComponent as BasicComponent } from '../code-examples/action-bars/summary-action-bar/basic/demo.component';
-import { DemoComponent as ModalComponent } from '../code-examples/action-bars/summary-action-bar/modal/demo.component';
-import { DemoComponent as TabComponent } from '../code-examples/action-bars/summary-action-bar/tab/demo.component';
-
 const routes: Routes = [
   {
     path: 'basic',
-    component: BasicComponent,
+    loadComponent: () =>
+      import(
+        '../code-examples/action-bars/summary-action-bar/basic/demo.component'
+      ).then((c) => c.DemoComponent),
   },
   {
     path: 'modal',
-    component: ModalComponent,
+    loadComponent: () =>
+      import(
+        '../code-examples/action-bars/summary-action-bar/modal/demo.component'
+      ).then((c) => c.DemoComponent),
   },
   {
     path: 'tab',
-    component: TabComponent,
+    loadComponent: () =>
+      import(
+        '../code-examples/action-bars/summary-action-bar/tab/demo.component'
+      ).then((c) => c.DemoComponent),
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class ActionBarsRoutingModule {}
-
-@NgModule({
-  imports: [
-    BasicComponent,
-    ModalComponent,
-    TabComponent,
-    ActionBarsRoutingModule,
-  ],
 })
 export class ActionBarsModule {}
