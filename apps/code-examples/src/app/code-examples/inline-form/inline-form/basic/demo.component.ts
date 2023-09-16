eb3b6f0fbe23fe1c29f06c90b583926d567578d4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-09-16 11:00:34 -0400

    inline form

---
+++ apps/code-examples/src/app/code-examples/inline-form/inline-form/basic/demo.component.ts
@@ -0,0 +1,65 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { SkyInputBoxModule } from '@skyux/forms';
+import { SkyIconModule } from '@skyux/indicators';
+import {
+  SkyInlineFormButtonLayout,
+  SkyInlineFormCloseArgs,
+  SkyInlineFormConfig,
+  SkyInlineFormModule,
+} from '@skyux/inline-form';
+
+@Component({
+  standalone: true,
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    SkyIconModule,
+    SkyInlineFormModule,
+    SkyInputBoxModule,
+  ],
+})
+export class DemoComponent {
+  protected firstName = 'Jane';
+  protected formGroup: FormGroup;
+
+  protected inlineFormConfig: SkyInlineFormConfig = {
+    buttonLayout: SkyInlineFormButtonLayout.SaveCancel,
+  };
+
+  protected showForm = false;
+
+  readonly #formBuilder = inject(FormBuilder);
+
+  constructor() {
+    this.formGroup = this.#formBuilder.group({
+      myFirstName: new FormControl(),
+    });
+  }
+
+  protected onInlineFormClose(args: SkyInlineFormCloseArgs): void {
+    if (args.reason === 'save') {
+      this.firstName = this.formGroup.get('myFirstName')?.value;
+    }
+
+    this.showForm = false;
+    this.formGroup.patchValue({
+      myFirstName: undefined,
+    });
+  }
+
+  protected onInlineFormOpen(): void {
+    this.showForm = true;
+    this.formGroup.patchValue({
+      myFirstName: this.firstName,
+    });
+  }
+}
