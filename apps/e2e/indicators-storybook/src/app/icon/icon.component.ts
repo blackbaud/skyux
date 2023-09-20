c2569d1586e71dc5d23bd2f39a2afe1c29e73016
John White <750350+johnhwhite@users.noreply.github.com>
2023-09-20 12:44:13 -0400

    Test all of the icons

--- apps/e2e/indicators-storybook/src/app/icon/icon.component.ts
+++ apps/e2e/indicators-storybook/src/app/icon/icon.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { getIconManifest } from '@skyux/icons';
 
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.scss'],
 })
-export class IconComponent {}
+export class IconComponent {
+  protected readonly icons = getIconManifest()
+    .glyphs.filter((glyph) => !glyph.deprecated)
+    .sort((a, b) => a.name.localeCompare(b.name));
+}
