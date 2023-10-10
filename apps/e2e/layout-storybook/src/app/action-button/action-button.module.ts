b4b4df0b0ed954ea3db724b01e913209be7de98b
Paul Crowder <paul.crowder@blackbaud.com>
2023-10-10 11:25:45 -0400

    fix(components/layout): fix action button responsive behavior in modals (#1798)

--- apps/e2e/layout-storybook/src/app/action-button/action-button.module.ts
+++ apps/e2e/layout-storybook/src/app/action-button/action-button.module.ts
@@ -2,13 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SkyActionButtonModule } from '@skyux/layout';
+import { SkyModalModule } from '@skyux/modals';
 
 import { ActionButtonComponent } from './action-button.component';
 
 const routes: Routes = [{ path: '', component: ActionButtonComponent }];
 @NgModule({
   declarations: [ActionButtonComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), SkyActionButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SkyActionButtonModule,
+    SkyModalModule,
+  ],
   exports: [ActionButtonComponent],
 })
 export class ActionButtonModule {}
