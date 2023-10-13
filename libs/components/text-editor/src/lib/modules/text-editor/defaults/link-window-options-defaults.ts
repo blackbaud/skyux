b71c2cf37292f768cb44c06d127eb5835d5da756
Jossahandy Perez <jossahandy.perez@blackbaud.me>
2023-10-13 17:04:00 -0500

    Add window options types and defaults files

---
+++ libs/components/text-editor/src/lib/modules/text-editor/defaults/link-window-options-defaults.ts
@@ -0,0 +1,6 @@
+import { SkyTextEditorLinkWindowOptionsType } from '@skyux/text-editor';
+
+export const LINK_WINDOW_OPTIONS_DEFAULT: SkyTextEditorLinkWindowOptionsType[] = [
+  'new',
+  'existing',
+]
