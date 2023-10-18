8e0b9da8df42a16b42467b09a5c56bcef66e7702
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-10-18 14:49:26 -0400

    feat(components/config): add `public` and `shared` properties to remote modules configuration

--- libs/components/config/src/lib/config-remote-modules.ts
+++ libs/components/config/src/lib/config-remote-modules.ts
@@ -1,11 +1,61 @@
-import { SkyuxConfigRemoteContainerReference } from './config-remote-container-reference';
+/**
+ * Configuration for projects that expose remote modules.
+ * @experimental
+ */
+export type SkyuxConfigRemoteModulesPublic = {
+  /**
+   * Information about remote modules exposed by this application.
+   */
+  public?: {
+    /**
+     * The version of this application's remote entry.
+     */
+    containerVersion: number;
+
+    /**
+     * A collection of public modules to expose to consuming SPAs.
+     * The module names must start with \"./\" and a capital letter.
+     */
+    modules: {
+      [moduleName: string]: string;
+    };
+  };
+};
 
 /**
  * Information about remote modules referenced by this application.
+ * @experimental
  */
-export interface SkyuxConfigRemoteModules {
+export type SkyuxConfigRemoteModulesReferenced = {
   /**
    * A collection of remote modules keyed by the remote container name.
    */
-  referenced?: Record<string, SkyuxConfigRemoteContainerReference>;
-}
+  referenced?: {
+    [appName: string]: {
+      /**
+       * The version of the referenced SPA's remote container.
+       */
+      containerVersion: number;
+
+      /**
+       * A collection of paths to the referenced remote modules.
+       */
+      modules: string[];
+    };
+  };
+};
+
+/**
+ * Configuration for projects that consume or expose remote modules.
+ * @experimental
+ */
+export type SkyuxConfigRemoteModules = SkyuxConfigRemoteModulesPublic &
+  SkyuxConfigRemoteModulesReferenced & {
+    /**
+     * Information about dependencies that should be shared between containers.
+     * The value for each entry should be an object with properties expected by
+     * Webpack's 'ModuleFederationPlugin' constructor.
+     * @see https://webpack.js.org/plugins/module-federation-plugin#sharing-libraries
+     */
+    shared?: { [packageName: string]: unknown };
+  };
