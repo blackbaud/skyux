50b2782ea9ee9b5674fc65eee5fce98c5b787c3f
John White <750350+johnhwhite@users.noreply.github.com>
2023-11-07 13:34:48 -0500

    Assert fonts loaded for e2e test

--- apps/integration/src/app/integrations/modal-viewkept-toolbars/modal-viewkept-toolbars.component.ts
+++ apps/integration/src/app/integrations/modal-viewkept-toolbars/modal-viewkept-toolbars.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { SkyModalService } from '@skyux/modals';
+import { FontLoadingService } from '@skyux/storybook';
+
+import { delay } from 'rxjs';
 
 import { ModalViewkeptToolbarsModalComponent } from './modal-viewkept-toolbars-modal.component';
 
@@ -9,6 +12,10 @@ import { ModalViewkeptToolbarsModalComponent } from './modal-viewkept-toolbars-m
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModalViewkeptToolbarsComponent {
+  protected readonly ready$ = inject(FontLoadingService)
+    .ready()
+    .pipe(delay(100));
+
   #modalService: SkyModalService;
 
   constructor(modalService: SkyModalService) {
