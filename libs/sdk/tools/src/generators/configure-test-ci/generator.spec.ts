c9c9153f76fb5ec335795dcd4d3c6c2237317564
John White <750350+johnhwhite@users.noreply.github.com>
2023-11-07 14:43:11 -0500

    ci: update test configuration (#1870)
    * ci: update test configuration
    
    * Simplify changes, add jest config.
    
    * Use long parameter name for `--configuration=ci`

---
+++ libs/sdk/tools/src/generators/configure-test-ci/generator.spec.ts
@@ -0,0 +1,105 @@
+import { applicationGenerator } from '@nx/angular/generators';
+import {
+  Tree,
+  readProjectConfiguration,
+  updateProjectConfiguration,
+} from '@nx/devkit';
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+
+import { configureTestCiGenerator } from './generator';
+
+describe('configure-test-ci generator', () => {
+  let tree: Tree;
+
+  beforeEach(async () => {
+    tree = createTreeWithEmptyWorkspace();
+    tree.write('.gitignore', '');
+    await applicationGenerator(tree, { name: 'test', skipFormat: true });
+  });
+
+  it('should update karma project', async () => {
+    const projectConfig = readProjectConfiguration(tree, 'test');
+    if (projectConfig.targets?.['test']) {
+      projectConfig.targets['test'].executor =
+        '@angular-devkit/build-angular:karma';
+      updateProjectConfiguration(tree, 'test', projectConfig);
+    }
+    await configureTestCiGenerator(tree, { skipFormat: true });
+    // Run twice to ensure that the generator is idempotent.
+    await configureTestCiGenerator(tree, { skipFormat: true });
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config.targets?.['test']).toBeDefined();
+    expect(config.targets?.['test']).toMatchInlineSnapshot(`
+      Object {
+        "configurations": Object {
+          "ci": Object {
+            "browsers": "ChromeHeadlessNoSandbox",
+            "codeCoverage": true,
+            "progress": false,
+            "sourceMap": true,
+            "watch": false,
+          },
+        },
+        "executor": "@angular-devkit/build-angular:karma",
+        "options": Object {
+          "jestConfig": "test/jest.config.ts",
+          "passWithNoTests": true,
+        },
+        "outputs": Array [
+          "{workspaceRoot}/coverage/{projectRoot}",
+        ],
+      }
+    `);
+  });
+
+  it('should update jest project', async () => {
+    await configureTestCiGenerator(tree, { skipFormat: true });
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config.targets?.['test']).toBeDefined();
+    expect(config.targets?.['test']).toMatchInlineSnapshot(`
+      Object {
+        "configurations": Object {
+          "ci": Object {
+            "ci": true,
+            "codeCoverage": true,
+            "runInBand": true,
+          },
+        },
+        "executor": "@nx/jest:jest",
+        "options": Object {
+          "jestConfig": "test/jest.config.ts",
+          "passWithNoTests": true,
+        },
+        "outputs": Array [
+          "{workspaceRoot}/coverage/{projectRoot}",
+        ],
+      }
+    `);
+  });
+
+  it('should ignore other project', async () => {
+    const projectConfig = readProjectConfiguration(tree, 'test');
+    if (projectConfig.targets?.['test']) {
+      delete projectConfig.targets['test'].configurations;
+      projectConfig.targets['test'].executor = 'nx:run-commands';
+      projectConfig.targets['test'].options = {
+        command: "echo 'Hello world.'",
+      };
+      updateProjectConfiguration(tree, 'test', projectConfig);
+    }
+    await configureTestCiGenerator(tree, { skipFormat: true });
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config.targets?.['test']).toBeDefined();
+    expect(config.targets?.['test']).toMatchInlineSnapshot(`
+      Object {
+        "executor": "nx:run-commands",
+        "options": Object {
+          "command": "echo 'Hello world.'",
+        },
+        "outputs": Array [
+          "{workspaceRoot}/coverage/{projectRoot}",
+        ],
+      }
+    `);
+  });
+});
