4edba22f593285ee887947afd7a29c0c8b65b9d0
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2023-11-08 09:20:58 -0500

    Apply suggestions from code review
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>

--- libs/components/popovers/src/lib/modules/dropdown/dropdown.component.ts
+++ libs/components/popovers/src/lib/modules/dropdown/dropdown.component.ts
@@ -104,7 +104,7 @@ export class SkyDropdownComponent implements OnInit, OnDestroy {
   /**
    * The ARIA label for the dropdown. This sets the dropdown's `aria-label` attribute to provide a text equivalent for screen readers
    * [to support accessibility](https://developer.blackbaud.com/skyux/learn/accessibility). If multiple dropdowns with no label or the same label appear on the same page,
-   * other, they must have unique aria labels that provide context. For example, "Context menu for Robert Hernandez".
+   * they must have unique ARIA labels that provide context, such as "Context menu for Robert Hernandez" or "Edit Robert Hernandez."
    * For more information about the `aria-label` attribute, see the [WAI-ARIA definition](https://www.w3.org/TR/wai-aria/#aria-label).
    */
   @Input()
