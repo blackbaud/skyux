614a8ab255bbaa15a7fcbdf9e8b8a6e77f5c5aa3
John White <750350+johnhwhite@users.noreply.github.com>
2023-12-14 14:40:39 -0500

    fix(components/ag-grid): clarify header sort buttons

--- libs/components/ag-grid/src/lib/modules/ag-grid/header/header.component.spec.ts
+++ libs/components/ag-grid/src/lib/modules/ag-grid/header/header.component.spec.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { expect, expectAsync } from '@skyux-sdk/testing';
+import { SkyI18nModule } from '@skyux/i18n';
 import { SkyIconModule } from '@skyux/indicators';
 import { SkyThemeModule } from '@skyux/theme';
 
@@ -35,7 +36,7 @@ describe('HeaderComponent', () => {
         TestHelpComponent,
         OtherTestHelpComponent,
       ],
-      imports: [SkyIconModule, SkyThemeModule],
+      imports: [SkyI18nModule, SkyIconModule, SkyThemeModule],
     });
     apiEvents = {};
     columnEvents = {};
