e63093cfa96d05e132925476e45358459f4a57d3
John White <750350+johnhwhite@users.noreply.github.com>
2024-01-09 15:27:43 -0500

    docs(code-examples): simplify data grid template column code example

--- apps/code-examples/src/app/code-examples/ag-grid/data-grid/template-ref-column/demo.component.ts
+++ apps/code-examples/src/app/code-examples/ag-grid/data-grid/template-ref-column/demo.component.ts
@@ -1,18 +1,15 @@
-import { AsyncPipe, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
   Component,
+  OnInit,
   TemplateRef,
   ViewChild,
   inject,
 } from '@angular/core';
 import { SkyAgGridModule, SkyAgGridService, SkyCellType } from '@skyux/ag-grid';
-import { SkyDataManagerService } from '@skyux/data-manager';
 
 import { AgGridModule } from 'ag-grid-angular';
-import { GridApi, GridOptions, GridReadyEvent } from 'ag-grid-community';
-import { BehaviorSubject } from 'rxjs';
+import { GridOptions } from 'ag-grid-community';
 
 import { AG_GRID_DEMO_DATA } from './data';
 
@@ -20,11 +17,9 @@ import { AG_GRID_DEMO_DATA } from './data';
   standalone: true,
   selector: 'app-demo',
   templateUrl: './demo.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [SkyDataManagerService],
-  imports: [AgGridModule, SkyAgGridModule, NgIf, AsyncPipe],
+  imports: [AgGridModule, SkyAgGridModule, NgIf],
 })
-export class DemoComponent implements AfterViewInit {
+export class DemoComponent implements OnInit {
   @ViewChild('boldColumn', { static: true })
   protected boldColumn: TemplateRef<unknown> | undefined;
 
@@ -33,54 +28,45 @@ export class DemoComponent implements AfterViewInit {
 
   protected gridData = AG_GRID_DEMO_DATA;
   protected gridOptions: GridOptions | undefined;
-  protected ready = new BehaviorSubject(false);
-
-  #gridApi: GridApi | undefined;
 
   readonly #agGridSvc = inject(SkyAgGridService);
 
-  public ngAfterViewInit(): void {
-    const gridOptions: GridOptions = {
-      columnDefs: [
-        {
-          field: 'name',
-          headerName: 'Name',
-        },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: SkyCellType.Number,
-          maxWidth: 60,
-        },
-        {
-          field: 'department',
-          headerName: 'Department',
-          type: SkyCellType.Template,
-          cellRendererParams: {
-            template: this.boldColumn,
-          },
-        },
-        {
-          field: 'jobTitle',
-          headerName: 'Title',
-          type: SkyCellType.Template,
-          cellRendererParams: {
-            template: this.emphasizedColumn,
-          },
-        },
-      ],
-      onGridReady: (gridReadyEvent): void => this.onGridReady(gridReadyEvent),
-    };
-
+  public ngOnInit(): void {
     this.gridOptions = this.#agGridSvc.getGridOptions({
-      gridOptions,
+      gridOptions: {
+        columnDefs: [
+          {
+            field: 'name',
+            headerName: 'Name',
+            initialWidth: 150,
+          },
+          {
+            field: 'age',
+            headerName: 'Age',
+            type: SkyCellType.Number,
+            maxWidth: 80,
+            resizable: false,
+          },
+          {
+            field: 'department',
+            headerName: 'Department',
+            type: SkyCellType.Template,
+            cellRendererParams: {
+              template: this.boldColumn,
+            },
+            initialWidth: 220,
+          },
+          {
+            field: 'jobTitle',
+            headerName: 'Title',
+            type: SkyCellType.Template,
+            cellRendererParams: {
+              template: this.emphasizedColumn,
+            },
+            initialWidth: 220,
+          },
+        ],
+      },
     });
-
-    this.ready.next(true);
-  }
-
-  public onGridReady(gridReadyEvent: GridReadyEvent): void {
-    this.#gridApi = gridReadyEvent.api;
-    this.#gridApi.sizeColumnsToFit();
   }
 }
