f85dd4e5d36e1409d7b5c9102a313c5e4a2cf025
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-01-17 14:25:47 -0500

    Visual test and playground changes

--- apps/playground/src/app/components/avatar/avatar/avatar.component.ts
+++ apps/playground/src/app/components/avatar/avatar/avatar.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
+  styleUrls: ['./avatar.component.scss'],
   templateUrl: './avatar.component.html',
 })
 export class AvatarComponent {
-  public name = 'Sandhya Rajasabeson';
+  public name = 'Robert C. Hernandez';
 }
