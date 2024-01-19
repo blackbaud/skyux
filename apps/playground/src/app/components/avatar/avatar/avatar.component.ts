62c69d60de2cd57172b53cf2340786bb71cf26f1
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-01-19 11:37:06 -0500

    fix(components/avatar): colors used within avatars are accessible (#1941)

--- apps/playground/src/app/components/avatar/avatar/avatar.component.ts
+++ apps/playground/src/app/components/avatar/avatar/avatar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
+  styleUrls: ['./avatar.component.scss'],
   templateUrl: './avatar.component.html',
 })
 export class AvatarComponent {
--- apps/playground/src/app/components/avatar/avatar/avatar.module.ts
+++ apps/playground/src/app/components/avatar/avatar/avatar.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SkyAvatarModule } from '@skyux/avatar';
+import { SkyPageModule } from '@skyux/pages';
 
 import { AvatarRoutingModule } from './avatar-routing.module';
 import { AvatarComponent } from './avatar.component';
 
 @NgModule({
   declarations: [AvatarComponent],
-  imports: [CommonModule, AvatarRoutingModule, SkyAvatarModule],
+  imports: [CommonModule, AvatarRoutingModule, SkyAvatarModule, SkyPageModule],
 })
 export class AvatarModule {
   public static routes = AvatarRoutingModule.routes;
