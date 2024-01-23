fdf3a7901d51d24070837e78a6391cb7559a5be5
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-01-23 16:43:44 -0500

    chore(apps/playground): add popovers playground page

---
+++ apps/playground/src/app/components/popovers/popovers/popovers.component.ts
@@ -0,0 +1,17 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { SkyPageModule } from '@skyux/pages';
+import { SkyPopoverModule } from '@skyux/popovers';
+
+@Component({
+  selector: 'app-popovers',
+  templateUrl: './popovers.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [SkyPopoverModule, SkyPageModule],
+})
+export class PopoversComponent {}
