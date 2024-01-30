464defcba28ce4f14673016e5456901a3c66d1e3
John White <750350+johnhwhite@users.noreply.github.com>
2024-01-30 11:25:13 -0500

    Update AG Grid interface

--- libs/components/ag-grid/src/lib/modules/ag-grid/types/currency-properties.ts
+++ libs/components/ag-grid/src/lib/modules/ag-grid/types/currency-properties.ts
@@ -1,5 +1,7 @@
+import AutoNumeric from 'autonumeric';
+
 export interface SkyAgGridCurrencyProperties {
   currencySymbol?: string;
-  decimalPlaces?: number;
-  negativeBracketsTypeOnBlur?: string;
+  decimalPlaces?: number | string;
+  negativeBracketsTypeOnBlur?: AutoNumeric.NegativeBracketsTypeOnBlurOption | null;
 }
