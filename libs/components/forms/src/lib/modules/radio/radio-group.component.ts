5f681495fd7ef819754ba449af80c0398f06d321
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-02-15 10:23:10 -0500

    update switch group styles

--- libs/components/forms/src/lib/modules/radio/radio-group.component.ts
+++ libs/components/forms/src/lib/modules/radio/radio-group.component.ts
@@ -35,13 +35,6 @@ let nextUniqueId = 0;
   selector: 'sky-radio-group',
   templateUrl: './radio-group.component.html',
   providers: [SkyRadioGroupIdService],
-  styles: [
-    `
-      :host {
-        display: block;
-      }
-    `,
-  ],
 })
 export class SkyRadioGroupComponent
   implements AfterContentInit, AfterViewInit, OnDestroy
