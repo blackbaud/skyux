b599bd14d2cf08130c3a635856ae8dee0b9cee7f
Paul Crowder <paul.crowder@blackbaud.com>
2024-03-06 13:16:58 -0500

    refactor(components/packages): use regular expression to rename tabIndex

--- libs/components/packages/src/schematics/migrations/update-10/rename-tabset-tabindex/rename-tabset-tabindex.schematic.ts
+++ libs/components/packages/src/schematics/migrations/update-10/rename-tabset-tabindex/rename-tabset-tabindex.schematic.ts
@@ -1,61 +1,30 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
 
-import * as cheerio from 'cheerio';
-
 import { visitProjectFiles } from '../../../utility/visit-project-files';
 import { getWorkspace } from '../../../utility/workspace';
 
-function replaceTabIndexAttr(
-  $elem: cheerio.Cheerio<cheerio.Element>,
-  oldName: string,
-  newName: string,
-): void {
-  const value = $elem.attr(oldName);
-
-  if (value) {
-    $elem.attr(newName, value);
-    $elem.removeAttr(oldName);
-  }
-}
+const TAB_INDEX_BRACKETS_REGEX =
+  /(?<beforeTabIndex><sky-tab[^>]+)\[tabIndex\]=/g;
+const TAB_INDEX_NO_BRACKETS_REGEX =
+  /(?<beforeTabIndex><sky-tab[^>]+)tabIndex=/g;
 
 function renameTabIndex(html: string): string {
-  const $ = cheerio.load(
-    html,
-    {
-      // Ensures attribute name casing is left intact.
-      xml: true,
-    },
-    false,
-  );
-
-  const tabs = $('sky-tab');
-
-  tabs.each((_, elem) => {
-    const $elem = $(elem);
-    replaceTabIndexAttr($elem, 'tabIndex', 'tabIndexValue');
-    replaceTabIndexAttr($elem, '[tabIndex]', '[tabIndexValue]');
-  });
-
-  return $.html({
-    lowerCaseAttributeNames: false,
-    lowerCaseTags: false,
-  }).toString();
+  return html
+    .replaceAll(
+      TAB_INDEX_BRACKETS_REGEX,
+      (_, beforeTabIndex) => `${beforeTabIndex}[tabIndexValue]=`,
+    )
+    .replaceAll(
+      TAB_INDEX_NO_BRACKETS_REGEX,
+      (_, beforeTabIndex) => `${beforeTabIndex}tabIndexValue=`,
+    );
 }
 
 async function updateSourceFiles(tree: Tree): Promise<void> {
   const { workspace } = await getWorkspace(tree);
   workspace.projects.forEach((project) => {
     visitProjectFiles(tree, project.sourceRoot || project.root, (filePath) => {
-      if (!filePath.endsWith('.html')) {
-        return;
-      }
-
       const content = tree.readText(filePath);
-
-      if (!content || !content.includes('sky-tab')) {
-        return;
-      }
-
       const updatedContent = renameTabIndex(content);
 
       if (updatedContent !== content) {
