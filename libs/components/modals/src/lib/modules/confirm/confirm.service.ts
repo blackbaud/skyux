4b792e370fc1b8a24c22b33137cf70b676312dff
Steve Brush <steve.brush@blackbaud.com>
2024-03-11 11:53:35 -0400

    feat(components/modals): add `close` method to `SkyConfirmInstance` (#2112)

--- libs/components/modals/src/lib/modules/confirm/confirm.service.ts
+++ libs/components/modals/src/lib/modules/confirm/confirm.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
 
-import { SkyModalCloseArgs } from '../modal/modal-close-args';
-import { SkyModalInstance } from '../modal/modal-instance';
 import { SkyModalService } from '../modal/modal.service';
 
-import { SkyConfirmCloseEventArgs } from './confirm-closed-event-args';
 import { SkyConfirmConfig } from './confirm-config';
 import { SKY_CONFIRM_CONFIG } from './confirm-config-token';
 import { SkyConfirmInstance } from './confirm-instance';
@@ -28,31 +25,19 @@ export class SkyConfirmService {
    * @param config Specifies configuration options for the dialog.
    */
   public open(config: SkyConfirmConfig): SkyConfirmInstance {
-    const modalInstance: SkyModalInstance = this.#modalService.open(
-      SkyConfirmComponent,
-      {
-        providers: [
-          {
-            provide: SKY_CONFIRM_CONFIG,
-            useValue: config,
-          },
-        ],
-      },
-    );
-
     const confirmInstance = new SkyConfirmInstance();
 
-    modalInstance.closed.subscribe((args: SkyModalCloseArgs) => {
-      let result: SkyConfirmCloseEventArgs = args.data;
-
-      // The modal was closed using the ESC key.
-      if (result === undefined) {
-        result = {
-          action: 'cancel',
-        };
-      }
-
-      confirmInstance.notifyClosed(result);
+    this.#modalService.open(SkyConfirmComponent, {
+      providers: [
+        {
+          provide: SKY_CONFIRM_CONFIG,
+          useValue: config,
+        },
+        {
+          provide: SkyConfirmInstance,
+          useValue: confirmInstance,
+        },
+      ],
     });
 
     return confirmInstance;
