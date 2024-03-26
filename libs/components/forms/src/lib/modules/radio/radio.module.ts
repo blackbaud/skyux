af92fdd92047bef06c243a576d01ad426c3ec4e7
Corey Archer <corey.archer@blackbaud.me>
2024-03-26 11:15:10 -0400

    remove unneeded component

--- libs/components/forms/src/lib/modules/radio/radio.module.ts
+++ libs/components/forms/src/lib/modules/radio/radio.module.ts
@@ -9,7 +9,6 @@ import { SkyFormErrorsModule } from '../form-error/form-errors.module';
 import { SkyFormsResourcesModule } from '../shared/sky-forms-resources.module';
 
 import { SkyRadioGroupComponent } from './radio-group.component';
-import { SkyRadioLabelTextLabelComponent } from './radio-label-text-label.component';
 import { SkyRadioLabelComponent } from './radio-label.component';
 import { SkyRadioComponent } from './radio.component';
 
@@ -18,7 +17,6 @@ import { SkyRadioComponent } from './radio.component';
     SkyRadioComponent,
     SkyRadioGroupComponent,
     SkyRadioLabelComponent,
-    SkyRadioLabelTextLabelComponent,
   ],
   imports: [
     CommonModule,
