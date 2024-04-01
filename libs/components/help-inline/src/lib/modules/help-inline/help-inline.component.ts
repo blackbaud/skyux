43f1c156d19a9bd97b260d32ef9dcb12c9981545
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.me>
2024-04-01 15:52:33 -0400

    resources

--- libs/components/help-inline/src/lib/modules/help-inline/help-inline.component.ts
+++ libs/components/help-inline/src/lib/modules/help-inline/help-inline.component.ts
@@ -7,12 +7,13 @@ import {
   TemplateRef,
 } from '@angular/core';
 import { SkyIdModule, SkyTrimModule } from '@skyux/core';
-import { SkyI18nModule } from '@skyux/i18n';
 import { SkyIconModule } from '@skyux/indicators';
 import { SkyPopoverModule } from '@skyux/popovers';
+import { SkyThemeModule } from '@skyux/theme';
+
+import { SkyHelpInlineResourcesModule } from '../shared/sky-help-inline-resources.module';
 
 import { SkyHelpInlineAriaExpandedPipe } from './help-inline-aria-expanded.pipe';
-import { SkyHelpInlineModule } from './help-inline.module';
 
 @Component({
   selector: 'sky-help-inline',
@@ -22,12 +23,12 @@ import { SkyHelpInlineModule } from './help-inline.module';
   imports: [
     CommonModule,
     SkyIdModule,
-    SkyHelpInlineModule,
     SkyTrimModule,
     SkyPopoverModule,
-    SkyI18nModule,
     SkyIconModule,
+    SkyThemeModule,
     SkyHelpInlineAriaExpandedPipe,
+    SkyHelpInlineResourcesModule,
   ],
 })
 export class SkyHelpInlineComponent {
@@ -53,16 +54,14 @@ export class SkyHelpInlineComponent {
   public labelText: string | undefined;
 
   @Input()
-  public helpPopoverTitle: string | undefined;
+  public popoverTitle: string | undefined;
 
   @Input()
-  public get helpPopoverContent(): string | TemplateRef<unknown> | undefined {
+  public get popoverContent(): string | TemplateRef<unknown> | undefined {
     return this.#_popoverContent;
   }
 
-  public set helpPopoverContent(
-    value: string | TemplateRef<unknown> | undefined,
-  ) {
+  public set popoverContent(value: string | TemplateRef<unknown> | undefined) {
     this.#_popoverContent = value;
     this.popoverTemplate = value instanceof TemplateRef ? value : undefined;
   }
