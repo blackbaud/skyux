9216ef9d0fe18b0b5194d8998962f59272162785
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.me>
2024-04-01 11:15:12 -0400

    help inline popover move

--- libs/components/help-inline/src/lib/modules/help-inline/help-inline.component.ts
+++ libs/components/help-inline/src/lib/modules/help-inline/help-inline.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+} from '@angular/core';
 import { SkyIdModule, SkyTrimModule } from '@skyux/core';
 import { SkyI18nModule } from '@skyux/i18n';
 import { SkyIconModule } from '@skyux/indicators';
@@ -40,4 +46,28 @@ export class SkyHelpInlineComponent {
   public onClick(): void {
     this.actionClick.emit();
   }
+
+  // popover
+
+  @Input()
+  public labelText: string | undefined;
+
+  @Input()
+  public helpPopoverTitle: string | undefined;
+
+  @Input()
+  public get helpPopoverContent(): string | TemplateRef<unknown> | undefined {
+    return this.#_popoverContent;
+  }
+
+  public set helpPopoverContent(
+    value: string | TemplateRef<unknown> | undefined,
+  ) {
+    this.#_popoverContent = value;
+    this.popoverTemplate = value instanceof TemplateRef ? value : undefined;
+  }
+
+  protected popoverTemplate: TemplateRef<unknown> | undefined;
+
+  #_popoverContent: string | TemplateRef<unknown> | undefined;
 }
