9af376ccef193a315ba04bf1095d3ad556d8f900
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.me>
2024-04-05 10:33:03 -0400

    visual test

---
+++ apps/e2e/help-inline-storybook/src/app/help-inline/help-inline.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-inline',
+  templateUrl: './help-inline.component.html',
+  styleUrls: ['./help-inline.component.scss'],
+})
+export class HelpInlineComponent {}
