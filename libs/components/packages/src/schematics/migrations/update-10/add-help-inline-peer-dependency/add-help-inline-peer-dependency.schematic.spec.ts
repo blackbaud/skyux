9f400d6519c5b88af1ea841b8cf8a29638187bf2
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.me>
2024-04-12 10:02:20 -0400

    peer dependency

---
+++ libs/components/packages/src/schematics/migrations/update-10/add-help-inline-peer-dependency/add-help-inline-peer-dependency.schematic.spec.ts
@@ -0,0 +1,74 @@
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+
+import { join } from 'path';
+
+import { createTestApp } from '../../../testing/scaffold';
+
+describe('Migrations > add help-inline peer dependency', () => {
+  const runner = new SchematicTestRunner(
+    'migrations',
+    join(__dirname, '../../migration-collection.json'),
+  );
+
+  async function setupTest() {
+    const tree = await createTestApp(runner, {
+      projectName: 'my-app',
+    });
+
+    return {
+      runSchematic: () =>
+        runner.runSchematic('add-help-inline-peer-dependency', {}, tree),
+      tree,
+    };
+  }
+
+  it('should add @skyux/help-inline if @skyux/forms is installed in dependencies', async () => {
+    const { runSchematic, tree } = await setupTest();
+
+    tree.overwrite(
+      '/package.json',
+      '{"dependencies": {"@skyux/forms": "10.2.0"}}',
+    );
+
+    await runSchematic();
+
+    expect(tree.readJson('/package.json')).toEqual({
+      dependencies: {
+        '@skyux/forms': '10.2.0',
+        '@skyux/help-inline': '10.2.0',
+      },
+    });
+  });
+
+  it('should add @skyux/help-inline if @skyux/forms is installed in devDependencies', async () => {
+    const { runSchematic, tree } = await setupTest();
+
+    tree.overwrite(
+      '/package.json',
+      '{"devDependencies": {"@skyux/forms": "10.2.0"}}',
+    );
+
+    await runSchematic();
+
+    expect(tree.readJson('/package.json')).toEqual({
+      dependencies: {
+        '@skyux/help-inline': '10.2.0',
+      },
+      devDependencies: {
+        '@skyux/forms': '10.2.0',
+      },
+    });
+  });
+
+  it('should not add @skyux/help-inline if @skyux/help-inline is not installed', async () => {
+    const { runSchematic, tree } = await setupTest();
+
+    tree.overwrite('/package.json', '{"dependencies": {}}');
+
+    await runSchematic();
+
+    expect(tree.readJson('/package.json')).toEqual({
+      dependencies: {},
+    });
+  });
+});
