ba8f6cd69dfb6bbf1e490dd0d210100adb20580a
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-04-12 13:51:50 -0400

    fix(components/forms): multi file attachment announces required state to screen readers

--- libs/components/forms/src/lib/modules/file-attachment/file-drop.component.ts
+++ libs/components/forms/src/lib/modules/file-attachment/file-drop.component.ts
@@ -164,6 +164,17 @@ export class SkyFileDropComponent implements OnDestroy {
   @Input()
   public hintText: string | undefined;
 
+  /**
+   * Whether the input is required for form validation.
+   * When you set this property to `true`, the component adds `aria-required` and `required`
+   * attributes to the input elements so that screen readers announce an invalid state until the input element
+   * is complete.
+   * For more information about the `aria-required` attribute, see the [WAI-ARIA definition](https://www.w3.org/TR/wai-aria/#aria-required).
+   * @preview
+   */
+  @Input()
+  public required: boolean | undefined = false;
+
   @ViewChild('fileInput')
   public inputEl: ElementRef | undefined;
 
