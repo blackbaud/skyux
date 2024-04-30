797881e525322d55cbaa2461e4e70be512f60462
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-04-30 09:34:01 -0400

    feat(components/forms): add help inline to toggle switch

--- libs/components/forms/src/lib/modules/toggle-switch/toggle-switch.module.ts
+++ libs/components/forms/src/lib/modules/toggle-switch/toggle-switch.module.ts
@@ -2,13 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SkyIdModule, SkyTrimModule } from '@skyux/core';
+import { SkyHelpInlineModule } from '@skyux/help-inline';
 
 import { SkyToggleSwitchLabelComponent } from './toggle-switch-label.component';
 import { SkyToggleSwitchComponent } from './toggle-switch.component';
 
 @NgModule({
   declarations: [SkyToggleSwitchLabelComponent, SkyToggleSwitchComponent],
-  imports: [CommonModule, FormsModule, SkyTrimModule, SkyIdModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SkyHelpInlineModule,
+    SkyIdModule,
+    SkyTrimModule,
+  ],
   exports: [SkyToggleSwitchLabelComponent, SkyToggleSwitchComponent],
 })
 export class SkyToggleSwitchModule {}
