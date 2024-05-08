fe122276f4e01ae6d609966affb8ff2ce8c7bc64
Corey Archer <Corey.Archer@blackbaud.me>
2024-05-08 10:28:47 -0400

    fix(components/datetime): date range picker only shows datepicker hint text when appropriate (#2278)

--- libs/components/datetime/src/lib/modules/datepicker/datepicker-hint-token.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-/**
- * @internal
- */
-export const SKY_DATEPICKER_HINT_TEXT_HIDDEN = new InjectionToken<boolean>(
-  'SkyDatepickerHintTextHidden',
-  { factory: () => false },
-);
