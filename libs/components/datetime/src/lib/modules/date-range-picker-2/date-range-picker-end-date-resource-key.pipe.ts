bc720637cef84b77a6417bfb557bfc19f2f2f02b
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-05-13 12:14:03 -0400

    add datepickers

---
+++ libs/components/datetime/src/lib/modules/date-range-picker-2/date-range-picker-end-date-resource-key.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { SkyDateRangeCalculatorType } from './types/date-range-calculator-type';
+
+@Pipe({
+  name: 'skyDateRangePickerEndDateResourceKey',
+  standalone: true,
+})
+export class SkyDateRangePickerEndDateResourceKeyPipe implements PipeTransform {
+  public transform(
+    calculatorType: SkyDateRangeCalculatorType | undefined,
+  ): string {
+    if (calculatorType === SkyDateRangeCalculatorType.Range) {
+      return 'skyux_date_range_picker_end_date_label';
+    }
+
+    return 'skyux_date_range_picker_before_date_label';
+  }
+}
--- libs/components/datetime/src/lib/modules/date-range-picker-2/date-range-picker.component.ts
+++ libs/components/datetime/src/lib/modules/date-range-picker-2/date-range-picker.component.ts
@@ -33,13 +33,17 @@ import {
   SkyInputBoxModule,
 } from '@skyux/forms';
 
+import { SkyDatepickerModule } from '../datepicker/datepicker.module';
 import { SkyDatetimeResourcesModule } from '../shared/sky-datetime-resources.module';
 
+import { SkyDateRangePickerEndDateResourceKeyPipe } from './date-range-picker-end-date-resource-key.pipe';
+import { SkyDateRangePickerStartDateResourceKeyPipe } from './date-range-picker-start-date-resource-key.pipe';
 import { SkyDateRangePickerService } from './date-range-picker.service';
 import { SkyDateRangeService } from './date-range.service';
 import { SkyDateRangeCalculation } from './types/date-range-calculation';
 import { SkyDateRangeCalculator } from './types/date-range-calculator';
 import { SkyDateRangeCalculatorId } from './types/date-range-calculator-id';
+import { SkyDateRangeCalculatorType } from './types/date-range-calculator-type';
 import { SKY_DEFAULT_CALCULATOR_CONFIGS } from './types/date-range-default-calculator-configs';
 
 const DEFAULT_CALCULATOR_IDS: SkyDateRangeCalculatorId[] = [
@@ -72,6 +76,9 @@ const DEFAULT_CALCULATOR_IDS: SkyDateRangeCalculatorId[] = [
   imports: [
     CommonModule,
     FormsModule,
+    SkyDatepickerModule,
+    SkyDateRangePickerEndDateResourceKeyPipe,
+    SkyDateRangePickerStartDateResourceKeyPipe,
     SkyDatetimeResourcesModule,
     SkyInputBoxModule,
   ],
@@ -89,13 +96,15 @@ const DEFAULT_CALCULATOR_IDS: SkyDateRangeCalculatorId[] = [
   ],
   selector: 'sky-date-range-picker',
   standalone: true,
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
+  styleUrl: './date-range-picker.component.scss',
   template: `<div class="sky-date-range-picker" (focusout)="onBlur()">
-    <div class="sky-date-range-picker-form-group">
+    <div
+      class="sky-date-range-picker-form-group"
+      [ngClass]="{
+        'sky-date-range-picker-last-input':
+          !showStartDatePicker && !showEndDatePicker
+      }"
+    >
       <sky-input-box
         [helpPopoverContent]="helpPopoverContent"
         [helpPopoverTitle]="helpPopoverTitle"
@@ -130,8 +139,79 @@ const DEFAULT_CALCULATOR_IDS: SkyDateRangeCalculatorId[] = [
         </select>
       </sky-input-box>
     </div>
-    <div class="sky-date-range-picker-form-group"></div>
-    <div class="sky-date-range-picker-form-group"></div>
+    <div
+      class="sky-date-range-picker-form-group"
+      [hidden]="!showStartDatePicker"
+      [ngClass]="{
+        'sky-date-range-picker-last-input':
+          showStartDatePicker && !showEndDatePicker
+      }"
+    >
+      <sky-input-box
+        [labelText]="
+          selectedCalculator.type
+            | skyDateRangePickerStartDateResourceKey
+            | skyLibResources
+        "
+      >
+        <sky-datepicker>
+          <input
+            skyDatepickerInput
+            type="text"
+            [attr.aria-label]="
+              label
+                ? ('skyux_date_range_picker_default_aria_label'
+                  | skyLibResources
+                    : (selectedCalculator.type
+                        | skyDateRangePickerStartDateResourceKey
+                        | skyLibResources)
+                    : label)
+                : (selectedCalculator.type
+                  | skyDateRangePickerStartDateResourceKey
+                  | skyLibResources)
+            "
+            [disabled]="disabled"
+            [required]="isRequired()"
+            [(ngModel)]="value.startDate"
+          />
+        </sky-datepicker>
+      </sky-input-box>
+    </div>
+    <div
+      class="sky-date-range-picker-form-group"
+      [hidden]="!showEndDatePicker"
+      [ngClass]="{ 'sky-date-range-picker-last-input': showEndDatePicker }"
+    >
+      <sky-input-box
+        [labelText]="
+          selectedCalculator.type
+            | skyDateRangePickerEndDateResourceKey
+            | skyLibResources
+        "
+      >
+        <sky-datepicker>
+          <input
+            skyDatepickerInput
+            type="text"
+            [attr.aria-label]="
+              label
+                ? ('skyux_date_range_picker_default_aria_label'
+                  | skyLibResources
+                    : (selectedCalculator.type
+                        | skyDateRangePickerEndDateResourceKey
+                        | skyLibResources)
+                    : label)
+                : (selectedCalculator.type
+                  | skyDateRangePickerEndDateResourceKey
+                  | skyLibResources)
+            "
+            [disabled]="disabled"
+            [required]="isRequired()"
+            [(ngModel)]="value.endDate"
+          />
+        </sky-datepicker>
+      </sky-input-box>
+    </div>
   </div>`,
 })
 export class SkyDateRangePickerComponent
@@ -139,6 +219,7 @@ export class SkyDateRangePickerComponent
 {
   @Input()
   public set calculatorIds(value: SkyDateRangeCalculatorId[] | undefined) {
+    console.log('set calculatorIds', value);
     this.#_calculatorIds = value ?? DEFAULT_CALCULATOR_IDS;
 
     this.calculators = this.#dateRangeSvc.filterCalculators(
@@ -147,6 +228,7 @@ export class SkyDateRangePickerComponent
 
     if (!this.#_calculatorIds.includes(this.value.calculatorId)) {
       this.value = this.#getDefaultValue();
+      this.#updatePickerVisibility();
       this.#notifyChange?.(this.value);
     }
   }
@@ -182,6 +264,7 @@ export class SkyDateRangePickerComponent
 
   protected set value(value: SkyDateRangeCalculation | null | undefined) {
     this.#_value = value ?? this.#getDefaultValue();
+    this.selectedCalculator = this.#getSelectedCalculator();
 
     if (!value) {
       this.#setDefaultValueOnControl();
@@ -193,6 +276,9 @@ export class SkyDateRangePickerComponent
   }
 
   protected calculators: SkyDateRangeCalculator[] = [];
+  protected selectedCalculator: SkyDateRangeCalculator;
+  protected showEndDatePicker = false;
+  protected showStartDatePicker = false;
 
   #_calculatorIds = DEFAULT_CALCULATOR_IDS;
   #_value: SkyDateRangeCalculation;
@@ -211,13 +297,17 @@ export class SkyDateRangePickerComponent
   );
 
   constructor() {
-    this.#_value = this.#getDefaultValue();
     this.calculators = this.#dateRangeSvc.calculators;
+    this.selectedCalculator = this.calculators[0];
+    this.#_value = this.#getDefaultValue();
   }
 
   public ngAfterViewInit(): void {
+    console.log('ngAfterViewInit()');
+
     if (this.#labelTextRequiredSvc) {
       this.#labelTextRequiredSvc.validateLabelText(this.label);
+
       if (!this.label) {
         this.display = 'none';
       }
@@ -228,6 +318,7 @@ export class SkyDateRangePickerComponent
     });
 
     this.#isInitialized = true;
+    this.#updatePickerVisibility();
   }
 
   // Implemented as part of ControlValueAccessor.
@@ -262,7 +353,15 @@ export class SkyDateRangePickerComponent
 
   // Implemented as part of ControlValueAccessor.
   public writeValue(value: SkyDateRangeCalculation | undefined): void {
+    console.log('writeValue()', value);
+
+    const oldValue = this.value;
+
     this.value = value;
+
+    if (oldValue !== value) {
+      this.#updatePickerVisibility();
+    }
   }
 
   protected isRequired(): boolean {
@@ -276,8 +375,13 @@ export class SkyDateRangePickerComponent
   }
 
   protected onCalculatorChange(): void {
+    console.log('onCalculatorChange()');
     const value = this.value;
     value.calculatorId = +value.calculatorId;
+
+    this.selectedCalculator = this.#getSelectedCalculator();
+    this.#updatePickerVisibility();
+
     this.#notifyChange?.(value);
   }
 
@@ -287,13 +391,53 @@ export class SkyDateRangePickerComponent
     };
   }
 
+  #getSelectedCalculator(): SkyDateRangeCalculator {
+    const found = this.calculators.find(
+      (c) => c.calculatorId === this.value.calculatorId,
+    );
+
+    /*safety check: should not happen*/
+    /*istanbul ignore if*/
+    if (!found) {
+      throw new Error(
+        'A selected date range calculator could not be determined.',
+      );
+    }
+
+    return found;
+  }
+
   #setDefaultValueOnControl(): void {
     this.#control?.setValue(this.value, { emitEvent: false, onlySelf: true });
   }
-}
 
-// TODO: how to set the default value of the control on init?
-// Should we be doing that?
-// What if we set the default only if it's undefined when the form is submitted?
-// How is Paul doing it for UIModel?
-// What about the date picker?
+  #updatePickerVisibility(): void {
+    if (this.#isInitialized) {
+      console.log('#updatePickerVisibility()', this.selectedCalculator);
+
+      let showEndDatePicker = false;
+      let showStartDatePicker = false;
+
+      switch (this.selectedCalculator.type) {
+        case SkyDateRangeCalculatorType.Before:
+          showEndDatePicker = true;
+          break;
+
+        case SkyDateRangeCalculatorType.After:
+          showStartDatePicker = true;
+          break;
+
+        case SkyDateRangeCalculatorType.Range:
+          showEndDatePicker = true;
+          showStartDatePicker = true;
+          break;
+
+        default:
+          break;
+      }
+
+      this.showEndDatePicker = showEndDatePicker;
+      this.showStartDatePicker = showStartDatePicker;
+    }
+  }
+}
