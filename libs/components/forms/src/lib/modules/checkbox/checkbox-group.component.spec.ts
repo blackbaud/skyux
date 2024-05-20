461db7ff760a1e801f755a4b0c69b892ea4de66c
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.me>
2024-05-20 15:42:48 -0400

    import remove

--- libs/components/forms/src/lib/modules/checkbox/checkbox-group.component.spec.ts
+++ libs/components/forms/src/lib/modules/checkbox/checkbox-group.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@skyux-sdk/testing';
 import {
   SkyHelpTestingController,
