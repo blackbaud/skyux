ffd3eaef5036ab80de6a718b2ae63af5789d0335
John White <750350+johnhwhite@users.noreply.github.com>
2024-05-30 15:23:47 -0400

    fix(components/popovers): allow taller dropdowns in small viewports

--- apps/playground/src/app/components/popovers/dropdown/dropdown.module.ts
+++ apps/playground/src/app/components/popovers/dropdown/dropdown.module.ts
@@ -1,21 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { SkyIconModule } from '@skyux/indicators';
-import { SkyDropdownModule } from '@skyux/popovers';
 
 import { DropdownRoutingModule } from './dropdown-routing.module';
 import { DropdownComponent } from './dropdown.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DropdownRoutingModule,
-    SkyDropdownModule,
-    SkyIconModule,
-    FormsModule,
-  ],
-  declarations: [DropdownComponent],
+  imports: [DropdownComponent, DropdownRoutingModule],
 })
 export class DropdownModule {
   public static routes = DropdownRoutingModule.routes;
