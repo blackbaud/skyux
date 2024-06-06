3208309599ddf605eb47d5127dc3ef39acbfa040
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-06-06 10:31:00 -0400

    update fuzzy code example

--- apps/code-examples/src/app/code-examples/datetime/datepicker/fuzzy/demo.component.ts
+++ apps/code-examples/src/app/code-examples/datetime/datepicker/fuzzy/demo.component.ts
@@ -5,6 +5,7 @@ import {
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { SkyDatepickerModule } from '@skyux/datetime';
 import { SkyInputBoxModule } from '@skyux/forms';
@@ -22,14 +23,20 @@ import { SkyInputBoxModule } from '@skyux/forms';
 })
 export class DemoComponent {
   protected formGroup: FormGroup;
+  protected helpPopoverContent =
+    'Your date of birth ensures that your benefits include the supplemental at-home services for your age group.';
+  protected hintText =
+    "Include a partial date if you don't have the exact date.";
 
   constructor() {
     this.formGroup = inject(FormBuilder).group({
-      myDate: new FormControl(new Date(1955, 10, 5)),
+      dob: new FormControl(new Date(1955, 10, 5), {
+        validators: Validators.required,
+      }),
     });
   }
 
   protected get getFuzzyDateForDisplay(): string {
-    return JSON.stringify(this.formGroup.get('myDate')?.value);
+    return JSON.stringify(this.formGroup.get('dob')?.value);
   }
 }
