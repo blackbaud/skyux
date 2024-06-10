69461571cbc1c5531338123e19b5818f24023e3a
John White <750350+johnhwhite@users.noreply.github.com>
2024-06-10 14:21:27 -0400

    fix(components/popovers): support popover in horizontal scrolling elements

---
+++ apps/playground/src/app/components/core/affix/affix.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SkyPopoverModule } from '@skyux/popovers';
+
+import { AffixRoutingModule } from './affix-routing.module';
+import { AffixComponent } from './affix.component';
+
+const routes: Routes = [{ path: '', component: AffixComponent }];
+
+@NgModule({
+  declarations: [AffixComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), SkyPopoverModule],
+})
+export class AffixModule {
+  public static routes = AffixRoutingModule.routes;
+}
