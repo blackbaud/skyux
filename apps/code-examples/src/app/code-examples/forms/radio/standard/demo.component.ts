532b6827c9dc770486d2169f622185d43657f09c
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-06-11 14:29:00 -0400

    standard code example update

--- apps/code-examples/src/app/code-examples/forms/radio/standard/demo.component.ts
+++ apps/code-examples/src/app/code-examples/forms/radio/standard/demo.component.ts
@@ -1,17 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
+  FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  ValidationErrors,
 } from '@angular/forms';
 import { SkyRadioModule } from '@skyux/forms';
 
 interface Item {
   name: string;
   value: string;
-  disabled: boolean;
+  disabled?: boolean;
+  hintText?: string;
+  helpContent?: string;
 }
 
 @Component({
@@ -21,17 +26,48 @@ interface Item {
   imports: [CommonModule, FormsModule, ReactiveFormsModule, SkyRadioModule],
 })
 export class DemoComponent {
-  protected formGroup: FormGroup;
+  protected formGroup: FormGroup<{ paymentMethod: FormControl<string | null> }>;
+  protected helpPopoverContent =
+    "We don't charge fees for any payment method. The only exception is when credit card payments are late, which incurs a 2% fee.";
+  protected helpPopoverTitle = 'Are there fees?';
+  protected hintText = 'Card methods require proof of identification.';
+  protected paymentMethod: FormControl<string | null>;
 
-  protected options: Item[] = [
-    { name: 'Option 1', value: '1', disabled: false },
-    { name: 'Option 2', value: '2', disabled: false },
-    { name: 'Option 3 is disabled', value: '3', disabled: true },
+  protected paymentOptions: Item[] = [
+    {
+      name: 'Cash',
+      value: 'cash',
+      helpContent:
+        'We accept cash at any of our locations and affiliated partners.',
+    },
+    { name: 'Check', value: 'check' },
+    { name: 'Apple pay', value: 'apple', disabled: true },
+    {
+      name: 'Credit',
+      value: 'credit',
+      hintText: 'A 2% late fee is applied to payments made after the due date.',
+    },
+    { name: 'Debit', value: 'debit' },
   ];
 
+  #formBuilder = inject(FormBuilder);
+
   constructor() {
+    this.paymentMethod = this.#formBuilder.control(
+      this.paymentOptions[0].name,
+      {
+        validators: this.#validatePaymentMethod,
+      },
+    );
     this.formGroup = inject(FormBuilder).group({
-      myOption: this.options[0].name,
+      paymentMethod: this.paymentMethod,
     });
+  }
+
+  #validatePaymentMethod(control: AbstractControl): ValidationErrors | null {
+    if (control.value === 'check') {
+      return { processingIssue: true };
+    }
+    return null;
   }
 }
