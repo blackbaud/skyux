69ece27e7ce2670e3d86743205721d5351b7a344
Steve Brush <steve.brush@blackbaud.com>
2024-06-25 12:46:04 -0400

    feat(components/tiles): add help key and inline help to the tile component (#2390)

---
+++ apps/playground/src/app/components/tiles/tile-dashboard/tile3.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SkyTilesModule } from '@skyux/tiles';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [SkyTilesModule],
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'div.tile2',
+  standalone: true,
+  template: `
+    <sky-tile [showHelp]="true" (helpClick)="onHelpClick()">
+      <sky-tile-title> Tile 3 w/ legacy help click </sky-tile-title>
+      <sky-tile-content>
+        <sky-tile-content-section> Content here. </sky-tile-content-section>
+      </sky-tile-content>
+    </sky-tile>
+  `,
+})
+export class Tile3Component {
+  public onHelpClick(): void {
+    console.log('help clicked');
+  }
+}
