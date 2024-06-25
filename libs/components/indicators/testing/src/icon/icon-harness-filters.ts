6bac16cffa5d90002db0a59a5117efcb89c8239c
John White <750350+johnhwhite@users.noreply.github.com>
2024-06-25 09:31:19 -0400

    Re-export test harness

--- libs/components/indicators/testing/src/icon/icon-harness-filters.ts
+++
@@ -1,7 +0,0 @@
-import { SkyHarnessFilters } from '@skyux/core/testing';
-
-/**
- * A set of criteria that can be used to filter a list of SkyIconHarness instances.
- */
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface SkyIconHarnessFilters extends SkyHarnessFilters {}
