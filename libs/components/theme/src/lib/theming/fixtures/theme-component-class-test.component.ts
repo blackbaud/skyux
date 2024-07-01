e048fefcae992b21b70bf6e9525a34303c77f25e
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-07-01 15:55:46 -0400

    remove unused test code

--- libs/components/theme/src/lib/theming/fixtures/theme-component-class-test.component.ts
+++ libs/components/theme/src/lib/theming/fixtures/theme-component-class-test.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
-import { SkyTheme } from '../theme';
 import { SkyThemeComponentClassDirective } from '../theme-component-class.directive';
-import { SkyThemeMode } from '../theme-mode';
-import { SkyThemeSettings } from '../theme-settings';
 
 @Component({
   selector: 'app-theme-component-class-test',
@@ -11,24 +8,4 @@ import { SkyThemeSettings } from '../theme-settings';
   standalone: true,
   hostDirectives: [SkyThemeComponentClassDirective],
 })
-export class SkyThemeComponentClassTestComponent {
-  public themeSettings: SkyThemeSettings | undefined;
-
-  constructor() {
-    this.useDefaultTheme();
-  }
-
-  public useDefaultTheme(): void {
-    this.themeSettings = new SkyThemeSettings(
-      SkyTheme.presets.default,
-      SkyThemeMode.presets.light,
-    );
-  }
-
-  public useModernTheme(): void {
-    this.themeSettings = new SkyThemeSettings(
-      SkyTheme.presets.modern,
-      SkyThemeMode.presets.light,
-    );
-  }
-}
+export class SkyThemeComponentClassTestComponent {}
