007961dbdd56343fb9738a6cbabcd9ab0eb4746c
John White <750350+johnhwhite@users.noreply.github.com>
2024-07-01 13:13:33 -0400

    feat(components/layout): add `helpKey`, `helpPopoverContent`, and `helpPopoverTitle` inputs to the description list component (#2413)
    * feat(components/layout): add `helpKey`, `helpPopoverContent`, and `helpPopoverTitle` inputs to the description list component
    
    * Updates based on feedback
    
    * Add peer dependency
    
    * Update wrapping behavior

--- libs/components/layout/src/lib/modules/description-list/description-list-content.component.ts
+++ libs/components/layout/src/lib/modules/description-list/description-list-content.component.ts
@@ -1,4 +1,10 @@
-import { Component, ContentChildren, QueryList } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  Input,
+  QueryList,
+  TemplateRef,
+} from '@angular/core';
 
 import { SkyDescriptionListDescriptionComponent } from './description-list-description.component';
 import { SkyDescriptionListTermComponent } from './description-list-term.component';
@@ -18,4 +24,29 @@ export class SkyDescriptionListContentComponent {
   public descriptionComponents:
     | QueryList<SkyDescriptionListDescriptionComponent>
     | undefined;
+
+  /**
+   * A help key that identifies the global help content to display. When specified, a [help inline](https://developer.blackbaud.com/skyux/components/help-inline) button is
+   * placed beside the description list content label. Clicking the button invokes global help as configured by the application.
+   * @preview
+   */
+  @Input()
+  public helpKey: string | undefined;
+
+  /**
+   * The content of the help popover. When specified, a [help inline](https://developer.blackbaud.com/skyux/components/help-inline)
+   * button is added to the description list content. The help inline button displays a [popover](https://developer.blackbaud.com/skyux/components/popover)
+   * when clicked using the specified content and optional title.
+   * @preview
+   */
+  @Input()
+  public helpPopoverContent: string | TemplateRef<unknown> | undefined;
+
+  /**
+   * The title of the help popover. This property only applies when `helpPopoverContent` is
+   * also specified.
+   * @preview
+   */
+  @Input()
+  public helpPopoverTitle: string | undefined;
 }
