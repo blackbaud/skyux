6d0457bfbe23248f677a7f551c4cddb171ab8188
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2024-07-03 15:55:11 -0400

    fix(code-examples): satisfy ESLint rules for colorpicker code examples (#2424) (#2429)

--- apps/code-examples/src/app/code-examples/colorpicker/colorpicker/programmatic/demo.component.ts
+++ apps/code-examples/src/app/code-examples/colorpicker/colorpicker/programmatic/demo.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import {
-  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -12,9 +11,18 @@ import {
   SkyColorpickerMessage,
   SkyColorpickerMessageType,
   SkyColorpickerModule,
+  SkyColorpickerOutput,
 } from '@skyux/colorpicker';
 
 import { Subject } from 'rxjs';
+
+interface DemoForm {
+  favoriteColor: FormControl<SkyColorpickerOutput | string>;
+}
+
+function isColorpickerOutput(value: unknown): value is SkyColorpickerOutput {
+  return !!(value && typeof value === 'object' && 'rgba' in value);
+}
 
 @Component({
   standalone: true,
@@ -24,22 +32,24 @@ import { Subject } from 'rxjs';
 })
 export class DemoComponent {
   protected colorpickerController = new Subject<SkyColorpickerMessage>();
-  protected favoriteColor: FormControl<string | null>;
-  protected formGroup: FormGroup;
+  protected favoriteColor: FormControl<SkyColorpickerOutput | string>;
+  protected formGroup: FormGroup<DemoForm>;
   protected showResetButton = false;
 
   constructor() {
-    this.favoriteColor = new FormControl('#f00', [
-      (control: AbstractControl): ValidationErrors | null => {
-        if (control.value?.rgba?.alpha < 0.8) {
-          return { opaque: true };
-        }
+    this.favoriteColor = new FormControl('#f00', {
+      nonNullable: true,
+      validators: [
+        (control): ValidationErrors | null => {
+          return isColorpickerOutput(control.value) &&
+            control.value.rgba.alpha < 0.8
+            ? { opaque: true }
+            : null;
+        },
+      ],
+    });
 
-        return null;
-      },
-    ]);
-
-    this.formGroup = inject(FormBuilder).group({
+    this.formGroup = inject(FormBuilder).group<DemoForm>({
       favoriteColor: this.favoriteColor,
     });
   }
@@ -57,7 +67,6 @@ export class DemoComponent {
   }
 
   #sendMessage(type: SkyColorpickerMessageType): void {
-    const message: SkyColorpickerMessage = { type };
-    this.colorpickerController.next(message);
+    this.colorpickerController.next({ type });
   }
 }
