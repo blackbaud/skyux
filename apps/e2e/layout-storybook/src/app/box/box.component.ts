ffd8cedc82c476df860d0509f7e22a48d68f77fa
Corey Archer <corey.archer@blackbaud.me>
2024-07-08 15:15:48 -0400

    update storybook

--- apps/e2e/layout-storybook/src/app/box/box.component.ts
+++ apps/e2e/layout-storybook/src/app/box/box.component.ts
@@ -16,6 +16,27 @@ export class BoxComponent {
       name: 'Box with help',
       showHelp: true,
     },
+    {
+      name: 'Box easy mode H2',
+      useHeader: true,
+      helpContent: 'Help',
+    },
+    {
+      name: 'Box easy mode H3',
+      useHeader: true,
+      heading: 3,
+    },
+    {
+      name: 'Box easy mode H4',
+      useHeader: true,
+      heading: 4,
+      helpContent: 'Help',
+    },
+    {
+      name: 'Box easy mode H5',
+      useHeader: true,
+      heading: 5,
+    },
   ];
 
   protected readonly ready$ = inject(FontLoadingService).ready();
