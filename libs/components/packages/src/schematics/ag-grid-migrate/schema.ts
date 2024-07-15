b4b1c14eaa0d4e8d33e343f412edb27ade1cab7f
John White <750350+johnhwhite@users.noreply.github.com>
2024-07-15 13:39:50 -0400

    fix(components/packages): switch to `@ag-grid-devtools/cli` for AG Grid codemods

--- libs/components/packages/src/schematics/ag-grid-migrate/schema.ts
+++ libs/components/packages/src/schematics/ag-grid-migrate/schema.ts
@@ -5,5 +5,9 @@ export interface Schema {
   /**
    * The name of the project to add polyfills to.
    */
-  sourceRoot: string;
+  sourceRoot?: string;
+  /**
+   * The version of AG Grid to migrate from. Defaults to the version found in the project's package-lock.json.
+   */
+  from?: string;
 }
