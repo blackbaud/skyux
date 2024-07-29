d8c3dff16db3d9e72f88b1f42272bd928d07a768
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-07-29 17:25:02 -0400

    Add utility function

--- libs/components/packages/src/schematics/migrations/update-11/help-inline-import-path/help-inline-import-path.schematic.ts
+++ libs/components/packages/src/schematics/migrations/update-11/help-inline-import-path/help-inline-import-path.schematic.ts
@@ -1,7 +1,7 @@
 import { Tree } from '@angular-devkit/schematics';
 import { isImported, parseSourceFile } from '@angular/cdk/schematics';
-import ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
-import { findNodes } from '@schematics/angular/utility/ast-utils';
+
+import { moveClassToLibrary } from '../../../utility/move-class-to-library';
 
 export default function () {
   return (tree: Tree) => {
@@ -26,72 +26,11 @@ export default function () {
         return;
       }
 
-      const recorder = tree.beginUpdate(path);
-
-      const helpInlineImportSpecifier = findNodes(
-        sourceFile,
-        ts.SyntaxKind.ImportSpecifier,
-      ).filter(
-        (node): node is ts.ImportSpecifier =>
-          ts.isImportSpecifier(node) &&
-          node.name.text === 'SkyHelpInlineModule',
-      )[0];
-
-      const indicatorsImports = findNodes(
-        sourceFile,
-        ts.SyntaxKind.ImportDeclaration,
-      ).filter(
-        (node): node is ts.ImportDeclaration =>
-          ts.isImportDeclaration(node) &&
-          !!node.importClause?.namedBindings &&
-          ts.isNamedImports(node.importClause?.namedBindings) &&
-          !!node.importClause.namedBindings.elements.find(
-            (specifier) => specifier.getText() === 'SkyHelpInlineModule',
-          ),
-      )[0];
-
-      if (
-        indicatorsImports.importClause?.namedBindings &&
-        ts.isNamedImports(indicatorsImports.importClause.namedBindings) &&
-        indicatorsImports.importClause.namedBindings.elements.length > 1
-      ) {
-        recorder.remove(
-          helpInlineImportSpecifier.getStart(sourceFile),
-          helpInlineImportSpecifier.getWidth(sourceFile),
-        );
-
-        if (
-          content.substring(
-            helpInlineImportSpecifier.getStart(sourceFile) +
-              helpInlineImportSpecifier.getWidth(sourceFile),
-            helpInlineImportSpecifier.getStart(sourceFile) +
-              helpInlineImportSpecifier.getWidth(sourceFile) +
-              2,
-          ) === ', '
-        ) {
-          recorder.remove(
-            helpInlineImportSpecifier.getStart(sourceFile) +
-              helpInlineImportSpecifier.getWidth(sourceFile),
-            2,
-          );
-        }
-      } else {
-        recorder.remove(
-          indicatorsImports.getStart(sourceFile),
-          indicatorsImports.getWidth(sourceFile),
-        );
-      }
-
-      if (
-        !isImported(sourceFile, 'SkyHelpInlineModule', '@skyux/help-inline')
-      ) {
-        recorder.insertLeft(
-          indicatorsImports.getStart(sourceFile),
-          "import { SkyHelpInlineModule } from '@skyux/help-inline';\n",
-        );
-      }
-
-      tree.commitUpdate(recorder);
+      moveClassToLibrary(tree, path, sourceFile, content, {
+        classNames: ['SkyHelpInlineModule'],
+        previousLibrary: '@skyux/indicators',
+        newLibrary: '@skyux/help-inline',
+      });
     });
   };
 }
