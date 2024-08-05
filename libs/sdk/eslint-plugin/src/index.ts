00e8310a6f709bd0659dad35234f0e31ae0cd2a8
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-08-05 14:39:19 -0400

    feat(sdk/eslint-plugin): add eslint plugin

---
+++ libs/sdk/eslint-plugin/src/index.ts
@@ -0,0 +1,3 @@
+import skyuxPlugin from './lib/skyux-plugin';
+
+export default skyuxPlugin;
---
+++ libs/sdk/eslint-plugin/src/lib/skyux-plugin.ts
@@ -0,0 +1,15 @@
+const skyuxPlugin = {
+  meta: {
+    name: '@skyux-sdk/eslint-config',
+    version: '0.0.0-PLACEHOLDER',
+  },
+  // configs: {},
+  rules: {},
+  // processors: {},
+};
+
+// for ESM
+export default skyuxPlugin;
+
+// OR for CommonJS
+module.exports = skyuxPlugin;
