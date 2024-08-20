3531ff8ab8cea0a26e24414f900a55c72c4c3cd0
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-08-20 14:34:32 -0400

    deprecation(components/lookup): lookup component's `data` input has been deprecated and consumers should use the `searchAsync` event instead (#2617)

--- libs/components/lookup/src/lib/modules/lookup/lookup.component.ts
+++ libs/components/lookup/src/lib/modules/lookup/lookup.component.ts
@@ -95,6 +95,7 @@ export class SkyLookupComponent
    * enter text. You can specify static data such as an array of objects, or
    * you can pull data from a database.
    * @default []
+   * @deprecated Use the `searchAsync` event emitter and callback instead to provide data to the lookup component.
    */
   @Input()
   public set data(value: any[] | undefined) {
