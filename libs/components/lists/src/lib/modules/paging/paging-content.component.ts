fe8f917c8dc0aa904315f013b3566365609df292
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-08-28 13:47:54 -0400

    refactor: implement Angular control flow for `layout` and `lists` libraries

--- libs/components/lists/src/lib/modules/paging/paging-content.component.ts
+++ libs/components/lists/src/lib/modules/paging/paging-content.component.ts
@@ -1,9 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
   selector: 'sky-paging-content',
   template: `<ng-content />`,
 })
