76be215f6c72472d9941ae7d6e659049bb9b087b
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-09-05 14:09:57 -0400

    more

--- libs/components/core/src/lib/modules/id/id.service.ts
+++ libs/components/core/src/lib/modules/id/id.service.ts
@@ -12,9 +12,6 @@ export class SkyIdService {
   public generateId(): string {
     idIndex++;
 
-    // Include timestamp and an incrementing index to guarantee unique IDs both during the application
-    // lifecycle as well as across sessions, since browsers will try to apply autocomplete options to
-    // elements with the same ID across sessions.
     return `sky-id-gen__${new Date().getTime()}__${idIndex}`;
   }
 }
