f5820126cc4f4a48009cc7af9ad62349cb19f6ab
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-09-12 15:46:29 -0400

    refactor: ran `nx lint --fix`

--- libs/components/ag-grid/src/lib/modules/ag-grid/ag-grid-row-delete.component.ts
+++ libs/components/ag-grid/src/lib/modules/ag-grid/ag-grid-row-delete.component.ts
@@ -26,6 +26,7 @@ import { SkyInlineDeleteModule } from '@skyux/layout';
       #inlineDeleteTemplateRef
     >
       <div
+        #inlineDeleteRef
         class="sky-ag-grid-row-delete"
         [id]="'row-delete-ref-' + row.id"
         [ngStyle]="{
@@ -33,13 +34,12 @@ import { SkyInlineDeleteModule } from '@skyux/layout';
           position: 'fixed',
           width: tableWidth + 'px',
         }"
-        #inlineDeleteRef
       >
         <sky-inline-delete
           [pending]="getRowDeleteItem(row).pending"
           (cancelTriggered)="cancelRowDelete(row)"
           (deleteTriggered)="confirmRowDelete(row)"
-        ></sky-inline-delete>
+        />
       </div>
     </ng-template>
   `,
