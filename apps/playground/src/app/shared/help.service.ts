52e3124ceb4e658c7bbc0cb4f1e31ed927ad69df
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-09-12 16:34:04 -0400

    Fix apps

--- apps/playground/src/app/shared/help.service.ts
+++ apps/playground/src/app/shared/help.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
-import { SkyHelpOpenArgs, SkyHelpService } from '@skyux/core';
+import {
+  SkyHelpOpenArgs,
+  SkyHelpService,
+  SkyHelpUpdateArgs,
+} from '@skyux/core';
 
 @Injectable()
 export class PlaygroundHelpService extends SkyHelpService {
+  public override updateHelp(args: SkyHelpUpdateArgs): void {
+    if ('helpKey' in args) {
+      alert('help key update: ' + args.helpKey);
+    }
+
+    if ('pageDefaultHelpKey' in args) {
+      alert('page default help key update: ' + args.pageDefaultHelpKey);
+    }
+  }
+
   public override openHelp(args: SkyHelpOpenArgs): void {
     alert('help key: ' + args.helpKey);
   }
