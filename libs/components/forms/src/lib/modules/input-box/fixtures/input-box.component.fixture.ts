4a3a280ac0377bd80711f2f2d645e5b42b20da94
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-09-16 12:15:30 -0400

    input box test

--- libs/components/forms/src/lib/modules/input-box/fixtures/input-box.component.fixture.ts
+++ libs/components/forms/src/lib/modules/input-box/fixtures/input-box.component.fixture.ts
@@ -49,6 +49,9 @@ export class InputBoxFixtureComponent {
   @Input()
   public insetIconDisabled: boolean | undefined;
 
+  @Input()
+  public labelText: string | undefined = 'Easy mode';
+
   public errorField: UntypedFormControl;
 
   public errorForm: UntypedFormGroup;
