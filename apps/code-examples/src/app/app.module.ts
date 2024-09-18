8d7893527d533ee408182d26607f57bec86a55fb
Trevor Burch <Trevor.Burch@blackbaud.com>
2024-09-18 15:26:20 -0400

    fix(components/ag-grid): non-resizeable columns show a column divider but do not show a resize cursor or hover indicator

--- apps/code-examples/src/app/app.module.ts
+++ apps/code-examples/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { SkyThemeService } from '@skyux/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkyThemeSelectorComponent } from './shared/theme-selector/theme-selector.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    SkyThemeSelectorComponent,
+  ],
   providers: [SkyThemeService],
   bootstrap: [AppComponent],
 })
