5c82827cfff26d4df6db53ff8dea2168eb6ee004
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-09-24 15:40:56 -0400

    add reasons

--- libs/sdk/eslint-plugin/src/rules/template/no-deprecated-directives.ts
+++ libs/sdk/eslint-plugin/src/rules/template/no-deprecated-directives.ts
@@ -1,25 +1,57 @@
 import { type TmplAstElement } from '@angular-eslint/bundled-angular-compiler';
 import { getTemplateParserServices } from '@angular-eslint/utils';
 
+import deprecationsJson from '../../__deprecations.json';
 import { createESLintRule } from '../../utils/create-eslint-rule';
+
+const DEPRECATIONS = deprecationsJson as {
+  directives: {
+    selectors: DeprecatedDirective[];
+    properties: DeprecatedProperties[];
+  };
+};
+
+interface DeprecatedDirective {
+  selector: string;
+  reason: string;
+}
+
+interface DeprecatedProperty {
+  name: string;
+  reason: string;
+}
+
+interface DeprecatedProperties {
+  selector: string;
+  inputs?: DeprecatedProperty[];
+  outputs?: DeprecatedProperty[];
+}
 
 export const RULE_NAME = 'no-deprecated-directives';
 
-const DEPRECATED_SELECTORS = ['sky-card', 'sky-checkbox-label'].join('|');
+// const DEPRECATED_SELECTORS = ['sky-card', 'sky-checkbox-label'].join('|');
 
-const DEPRECATED_PROPERTIES: Record<string, Record<string, string[]>> = {
-  'sky-checkbox': {
-    inputs: ['label'],
-  },
-  'sky-file-attachment': {
-    inputs: ['validatorFn'],
-    outputs: ['fileChange'],
-  },
-};
+// const DEPRECATED_PROPERTIES: Record<string, Record<string, string[]>> = {
+//   'sky-checkbox': {
+//     inputs: ['label'],
+//   },
+//   'sky-file-attachment': {
+//     inputs: ['validatorFn'],
+//     outputs: ['fileChange'],
+//   },
+// };
 
-const SELECTORS_WITH_DEPRECATED_PROPERTIES = Object.keys(
-  DEPRECATED_PROPERTIES,
-).join('|');
+// const SELECTORS_WITH_DEPRECATED_PROPERTIES = Object.keys(
+//   DEPRECATED_PROPERTIES,
+// ).join('|');
+
+const DEPRECATED_SELECTORS = DEPRECATIONS.directives.selectors
+  .map((s) => s.selector)
+  .join('|');
+
+const SELECTORS_WITH_DEPRECATED_PROPERTIES = DEPRECATIONS.directives.properties
+  .map((p) => p.selector)
+  .join('|');
 
 export const rule = createESLintRule({
   create(context) {
@@ -29,29 +61,42 @@ export const rule = createESLintRule({
       [`Element$1[name=/^(${SELECTORS_WITH_DEPRECATED_PROPERTIES})$/]`](
         el: TmplAstElement,
       ) {
-        for (const input of DEPRECATED_PROPERTIES[el.name]['inputs']) {
-          const property = el.inputs.find((i) => i.name === input);
+        for (const directive of DEPRECATIONS.directives.properties) {
+          if (directive.selector === el.name) {
+            let property: DeprecatedProperty | undefined;
 
-          if (property) {
-            context.report({
-              loc: parserServices.convertNodeSourceSpanToLoc(
-                property.sourceSpan,
-              ),
-              messageId: 'noDeprecatedDirectiveProperties',
-              data: {
-                element: el.name,
-                property: property.name,
-              },
+            const input = el.inputs.find((input) => {
+              property = directive.inputs?.find((i) => i.name === input.name);
+              return !!property;
             });
+
+            if (input) {
+              context.report({
+                loc: parserServices.convertNodeSourceSpanToLoc(
+                  input.sourceSpan,
+                ),
+                messageId: 'noDeprecatedDirectiveProperties',
+                data: {
+                  element: el.name,
+                  property: input.name,
+                  reason: property?.reason,
+                },
+              });
+            }
           }
         }
       },
       [`Element$1[name=/^(${DEPRECATED_SELECTORS})$/]`](el: TmplAstElement) {
+        const found = DEPRECATIONS.directives.selectors.find(
+          (s) => s.selector === el.name,
+        );
+
         context.report({
           loc: parserServices.convertNodeSourceSpanToLoc(el.sourceSpan),
           messageId: 'noDeprecatedDirectives',
           data: {
             element: el.name,
+            reason: found?.reason,
           },
         });
       },
@@ -63,10 +108,9 @@ export const rule = createESLintRule({
       description: '',
     },
     messages: {
-      noDeprecatedDirectives:
-        '<{{element}} /> element is deprecated and should not be used.',
+      noDeprecatedDirectives: '<{{element}} /> is deprecated. {{reason}}',
       noDeprecatedDirectiveProperties:
-        "The property {{property}} on the <{{element}} /> element is deprecated. Refer to the component's documentation for next steps.",
+        'The property {{property}} on the <{{element}} /> element is deprecated. {{reason}}',
     },
     schema: [],
     type: 'problem',
