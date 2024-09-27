138c561dcdcfe395c2486bdf82ca7cd40046f7e4
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-09-27 16:29:42 -0400

    try bumping up the window size

--- apps/e2e/popovers-storybook-e2e/src/e2e/popover.component.cy.ts
+++ apps/e2e/popovers-storybook-e2e/src/e2e/popover.component.cy.ts
@@ -5,7 +5,7 @@ describe(`popovers-storybook`, () => {
     describe(`in ${theme} theme`, () => {
       beforeEach(() =>
         cy
-          .viewport(1280, 1200)
+          .viewport(1280, 1500)
           .visit(
             `/iframe.html?globals=theme:${theme}&id=popovercomponent-popover--popover`,
           ),
