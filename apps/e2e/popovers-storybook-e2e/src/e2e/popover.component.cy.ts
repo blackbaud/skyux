f54ebb05e929cfc638254a77a85016695104fd7d
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-09-30 15:28:35 -0400

    try setting screenshot minheight

--- apps/e2e/popovers-storybook-e2e/src/e2e/popover.component.cy.ts
+++ apps/e2e/popovers-storybook-e2e/src/e2e/popover.component.cy.ts
@@ -5,7 +5,7 @@ describe(`popovers-storybook`, () => {
     describe(`in ${theme} theme`, () => {
       beforeEach(() =>
         cy
-          .viewport(1280, 2000)
+          .viewport(1280, 1600)
           .visit(
             `/iframe.html?globals=theme:${theme}&id=popovercomponent-popover--popover`,
           ),
@@ -51,6 +51,7 @@ describe(`popovers-storybook`, () => {
         cy.window().percySnapshot(
           `popovercomponent-popover--popover-${theme}`,
           {
+            minHeight: 1600,
             widths: [1280],
           },
         );
