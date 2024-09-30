2779754f4ea70aaed3e702af120d6cdc9322b779
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-09-30 16:04:08 -0400

    remove button troubleshooting code

--- apps/playground/src/app/components/theme/buttons/buttons.module.ts
+++ apps/playground/src/app/components/theme/buttons/buttons.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { SkyIconModule } from '@skyux/icon';
 
 import { ButtonsRoutingModule } from './buttons-routing.module';
 import { ButtonsComponent } from './buttons.component';
 
 @NgModule({
   declarations: [ButtonsComponent],
-  imports: [ButtonsRoutingModule, SkyIconModule],
+  imports: [ButtonsRoutingModule],
 })
 export class ButtonsModule {
   public static routes = ButtonsRoutingModule.routes;
