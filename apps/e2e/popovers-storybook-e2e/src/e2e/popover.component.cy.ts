a2ad57a47622672bfa4433a6d2744a9a8c184367
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-09-30 13:50:14 -0400

    also update the screenshot size

--- apps/e2e/popovers-storybook-e2e/src/e2e/popover.component.cy.ts
+++ apps/e2e/popovers-storybook-e2e/src/e2e/popover.component.cy.ts
@@ -51,7 +51,7 @@ describe(`popovers-storybook`, () => {
         cy.window().percySnapshot(
           `popovercomponent-popover--popover-${theme}`,
           {
-            widths: [1280],
+            widths: [2200],
           },
         );
         cy.window().screenshot(`popovercomponent-popover--popover-${theme}`);
