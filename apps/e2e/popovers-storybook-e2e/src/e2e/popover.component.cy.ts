4f9df761f5952cf81006dc44618505bc108c74f1
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2024-09-30 14:14:51 -0400

    try wider width and bump up min width style

--- apps/e2e/popovers-storybook-e2e/src/e2e/popover.component.cy.ts
+++ apps/e2e/popovers-storybook-e2e/src/e2e/popover.component.cy.ts
@@ -5,7 +5,7 @@ describe(`popovers-storybook`, () => {
     describe(`in ${theme} theme`, () => {
       beforeEach(() =>
         cy
-          .viewport(1280, 1200)
+          .viewport(2200, 1200)
           .visit(
             `/iframe.html?globals=theme:${theme}&id=popovercomponent-popover--popover`,
           ),
@@ -51,7 +51,7 @@ describe(`popovers-storybook`, () => {
         cy.window().percySnapshot(
           `popovercomponent-popover--popover-${theme}`,
           {
-            widths: [1280],
+            widths: [2200],
           },
         );
         cy.window().screenshot(`popovercomponent-popover--popover-${theme}`);
