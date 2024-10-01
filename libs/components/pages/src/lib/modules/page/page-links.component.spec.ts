fd726372fcf981300e1bdc03faf729a24922a27e
John White <750350+johnhwhite@users.noreply.github.com>
2024-10-01 17:22:52 -0400

    Simplify selector, add test assertions.

--- libs/components/pages/src/lib/modules/page/page-links.component.spec.ts
+++ libs/components/pages/src/lib/modules/page/page-links.component.spec.ts
@@ -1,24 +1,34 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { SkyLayoutHostService } from '@skyux/core';
 
 import { SkyPageLinksComponent } from './page-links.component';
 
+@Component({
+  standalone: true,
+  selector: 'sky-page-links-test',
+  imports: [SkyPageLinksComponent],
+  template: `<sky-page-links>Links.</sky-page-links>`,
+})
+class SkyPageLinksTestComponent {}
+
 describe('PageLinksComponent', () => {
-  let component: SkyPageLinksComponent;
-  let fixture: ComponentFixture<SkyPageLinksComponent>;
+  let component: SkyPageLinksTestComponent;
+  let fixture: ComponentFixture<SkyPageLinksTestComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [SkyPageLinksComponent],
-      providers: [SkyLayoutHostService],
+      imports: [SkyPageLinksTestComponent],
     });
 
-    fixture = TestBed.createComponent(SkyPageLinksComponent);
+    fixture = TestBed.createComponent(SkyPageLinksTestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should sky-page-links as block element', () => {
     expect(component).toBeTruthy();
+    const pageLinksEl = fixture.nativeElement.querySelector('sky-page-links');
+    expect(getComputedStyle(pageLinksEl).display).toBe('block');
+    expect(pageLinksEl.textContent).toContain('Links.');
   });
 });
