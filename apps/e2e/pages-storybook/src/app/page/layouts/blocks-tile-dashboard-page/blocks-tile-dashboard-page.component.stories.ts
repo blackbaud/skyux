d12547e457dd648bf9b979a8101a3e012328e9c0
Paul Crowder <paul.crowder@blackbaud.com>
2024-10-02 09:58:15 -0400

    fix(components/tiles): remove horizontal spacing from tile dashboard in blocks page layout

---
+++ apps/e2e/pages-storybook/src/app/page/layouts/blocks-tile-dashboard-page/blocks-tile-dashboard-page.component.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import BlocksTileDashboardPageComponent from './blocks-tile-dashboard-page.component';
+
+export default {
+  id: 'blocks-tile-dashboard-pagecomponent',
+  title: 'Components/Page/Layouts/Blocks Tile Dashboard',
+  component: BlocksTileDashboardPageComponent,
+} as Meta<BlocksTileDashboardPageComponent>;
+type Story = StoryObj<BlocksTileDashboardPageComponent>;
+export const BlocksTileDashboardPage: Story = {};
+BlocksTileDashboardPage.args = {};
