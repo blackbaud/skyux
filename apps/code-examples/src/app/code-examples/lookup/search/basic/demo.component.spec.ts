dc0d5f81b71c63cbfae9b587af77d081aefd010a
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.me>
2024-10-03 09:20:20 -0400

    code example

---
+++ apps/code-examples/src/app/code-examples/lookup/search/basic/demo.component.spec.ts
@@ -0,0 +1,95 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SkyMediaBreakpoints, SkyMediaQueryService } from '@skyux/core';
+import { MockSkyMediaQueryService } from '@skyux/core/testing';
+import { SkySearchComponent } from '@skyux/lookup';
+import { SkySearchHarness } from '@skyux/lookup/testing';
+
+import { DemoComponent } from './demo.component';
+
+describe('Basic search demo', () => {
+  async function setupTest(options: { dataSkyId: string }): Promise<{
+    harness: SkySearchHarness;
+    fixture: ComponentFixture<DemoComponent>;
+    mockMediaQuery: MockSkyMediaQueryService;
+  }> {
+    const mockMediaQuery = new MockSkyMediaQueryService();
+
+    await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: SkyMediaQueryService,
+          useValue: mockMediaQuery,
+        },
+      ],
+    }).compileComponents();
+    const fixture = TestBed.overrideComponent(SkySearchComponent, {
+      add: {
+        providers: [
+          {
+            provide: SkyMediaQueryService,
+            useValue: mockMediaQuery,
+          },
+        ],
+      },
+    }).createComponent(DemoComponent);
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+
+    const harness = await loader.getHarness(
+      SkySearchHarness.with({ dataSkyId: options.dataSkyId }),
+    );
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    return { harness, fixture, mockMediaQuery };
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, DemoComponent],
+    });
+  });
+
+  it('should setup search component', async () => {
+    const { harness } = await setupTest({
+      dataSkyId: 'demo-search',
+    });
+
+    await expectAsync(harness.getAriaLabel()).toBeResolvedTo(
+      'Search reminders',
+    );
+    await expectAsync(harness.getPlaceholderText()).toBeResolvedTo(
+      'Search through reminders.',
+    );
+  });
+
+  it('should interact with search function', async () => {
+    const { harness } = await setupTest({
+      dataSkyId: 'demo-search',
+    });
+
+    await harness.enterText('Send');
+    await expectAsync(harness.getValue()).toBeResolvedTo('Send');
+
+    await harness.clickClearButton();
+    await expectAsync(harness.getValue()).toBeResolvedTo('');
+  });
+
+  it('should interact with search on mobile', async () => {
+    const { harness, fixture, mockMediaQuery } = await setupTest({
+      dataSkyId: 'demo-search',
+    });
+
+    mockMediaQuery.fire(SkyMediaBreakpoints.xs);
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    await harness.clickSearchOpenButton();
+    await expectAsync(harness.isCollapsed()).toBeResolvedTo(false);
+
+    await harness.enterText('Send');
+    await expectAsync(harness.getValue()).toBeResolvedTo('Send');
+  });
+});
