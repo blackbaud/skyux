ae6e0460be3ca53cf780864a2a783263c1a75da2
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-10-04 15:00:23 -0400

    JSDocs

--- libs/components/core/testing/src/media-query/media-query-testing.controller.ts
+++ libs/components/core/testing/src/media-query/media-query-testing.controller.ts
@@ -1,6 +1,12 @@
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { SkyMediaBreakpoints } from '@skyux/core';
 
+/**
+ * Provides methods to interact with media queries in tests.
+ */
 export abstract class SkyMediaQueryTestingController {
+  /**
+   * Emits the provided breakpoint to all subscribers.
+   */
   public abstract setBreakpoint(breakpoint: SkyMediaBreakpoints): void;
 }
--- libs/components/core/testing/src/media-query/provide-media-query-testing.ts
+++ libs/components/core/testing/src/media-query/provide-media-query-testing.ts
@@ -5,6 +5,9 @@ import { SkyMediaQueryService } from '@skyux/core';
 import { SkyMediaQueryTestingController } from './media-query-testing.controller';
 import { SkyMediaQueryTestingService } from './media-query-testing.service';
 
+/**
+ * Mocks the media query service for unit tests.
+ */
 export function provideSkyMediaQueryTesting(): EnvironmentProviders {
   return makeEnvironmentProviders([
     SkyMediaQueryTestingService,
