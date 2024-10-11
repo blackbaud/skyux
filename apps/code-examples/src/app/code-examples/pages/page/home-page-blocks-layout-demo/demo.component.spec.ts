535b036ad7b631d687b2a14f27a39cd0d877c105
John White <750350+johnhwhite@users.noreply.github.com>
2024-10-11 18:12:36 -0400

    Fix test, adjust layout

--- apps/code-examples/src/app/code-examples/pages/page/home-page-blocks-layout-demo/demo.component.spec.ts
+++ apps/code-examples/src/app/code-examples/pages/page/home-page-blocks-layout-demo/demo.component.spec.ts
@@ -1,11 +1,8 @@
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterTestingModule } from '@angular/router/testing';
-import {
-  SkyHelpTestingController,
-  SkyHelpTestingModule,
-} from '@skyux/core/testing';
+import { provideRouter } from '@angular/router';
+import { SkyHelpTestingModule } from '@skyux/core/testing';
 import { SkyPageHarness } from '@skyux/pages/testing';
 
 import { DemoComponent } from './demo.component';
@@ -14,25 +11,19 @@ describe('Record page blocks layout demo', () => {
   async function setupTest(): Promise<{
     pageHarness: SkyPageHarness;
     fixture: ComponentFixture<DemoComponent>;
-    helpController: SkyHelpTestingController;
   }> {
     const fixture = TestBed.createComponent(DemoComponent);
 
     const loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
     const pageHarness = await loader.getHarness(SkyPageHarness);
-    const helpController = TestBed.inject(SkyHelpTestingController);
 
-    return { pageHarness, fixture, helpController };
+    return { pageHarness, fixture };
   }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        DemoComponent,
-        SkyHelpTestingModule,
-        NoopAnimationsModule,
-        RouterTestingModule,
-      ],
+      imports: [DemoComponent, SkyHelpTestingModule, NoopAnimationsModule],
+      providers: [provideRouter([])],
     });
   });
 
@@ -49,18 +40,10 @@ describe('Record page blocks layout demo', () => {
 
     const pageHeaderHarness = await pageHarness.getPageHeader();
 
-    await expectAsync(pageHeaderHarness.getPageTitle()).toBeResolvedTo(
-      '$500 pledge',
-    );
+    await expectAsync(pageHeaderHarness.getPageTitle()).toBeResolvedTo('Home');
 
-    await expectAsync(pageHeaderHarness.getParentLinkText()).toBeResolvedTo(
-      'Pledges',
-    );
-  });
-
-  it('should have the correct help key', async () => {
-    const { helpController } = await setupTest();
-
-    helpController.expectCurrentHelpKey('demo-help');
+    await expectAsync(
+      pageHeaderHarness.getParentLinkText(),
+    ).toBeRejectedWithError(/No parent link was found in the page header/);
   });
 });
