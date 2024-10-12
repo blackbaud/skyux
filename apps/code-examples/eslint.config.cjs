840adf7d1635bae75fdf1e5ce1ba544478ab983e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-10-12 09:38:34 -0400

    fix code examples

--- apps/code-examples/eslint.config.cjs
+++ apps/code-examples/eslint.config.cjs
@@ -1,7 +1,15 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
 const nx = require('@nx/eslint-plugin');
 const prettier = require('eslint-config-prettier');
+const path = require('node:path');
 const baseConfig = require('../../eslint-base.config.cjs');
 const overrides = require('../../eslint-overrides-angular.config.cjs');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
 
 module.exports = [
   ...baseConfig,
@@ -40,5 +48,36 @@ module.exports = [
       ],
     },
   },
+  ...compat
+    .config({
+      extends: ['../../libs/sdk/eslint-config/recommended'],
+    })
+    .map((config) => ({
+      files: ['**/code-examples/**/*.ts'],
+      languageOptions: {
+        ...config.languageOptions,
+        parserOptions: {
+          project: [path.join(__dirname, 'tsconfig.editor.json')],
+          tsconfigRootDir: '.',
+        },
+      },
+      linterOptions: { reportUnusedDisableDirectives: true },
+      rules: {
+        ...config.rules,
+        'no-alert': 'warn',
+        'no-console': 'warn',
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: [
+              {
+                group: ['../*'],
+                message: 'Make sure to import from local files only.',
+              },
+            ],
+          },
+        ],
+      },
+    })),
   prettier,
 ];
