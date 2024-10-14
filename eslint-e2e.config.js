dd5b75fa0aa54cdfb522c7da6d38d5843f99c962
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-10-14 18:22:16 -0400

    simplify config; use .js

--- eslint-e2e.config.js
+++ eslint-e2e.config.js
@@ -1,7 +1,7 @@
 const prettier = require('eslint-config-prettier');
 const cypress = require('eslint-plugin-cypress/flat');
-const baseConfig = require('../../eslint.config');
-const overrides = require('../../eslint-overrides.config');
+const baseConfig = require('./eslint.config');
+const overrides = require('./eslint-overrides.config');
 
 module.exports = [
   cypress.configs['recommended'],
--- eslint.config.cjs
+++
@@ -1,5 +0,0 @@
-const prettier = require('eslint-config-prettier');
-const baseConfig = require('./eslint-base.config.cjs');
-const overrides = require('./eslint-overrides.config.cjs');
-
-module.exports = [...baseConfig, ...overrides, prettier];
