da8dc362b1e9e6109c632bc9c3b0d643b5476a65
Sandhya Raja Sabeson <sandhya.rajasabeson@blackbaud.me>
2024-11-07 11:29:26 -0500

    ugh

--- apps/code-examples/src/app/code-examples/datetime/date-range-picker/basic/demo.component.ts
+++ apps/code-examples/src/app/code-examples/datetime/date-range-picker/basic/demo.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import {
   AbstractControl,
@@ -6,14 +7,42 @@ import {
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  ValidationErrors,
 } from '@angular/forms';
-import { SkyDateRangePickerModule } from '@skyux/datetime';
+import {
+  SkyDateRangeCalculatorId,
+  SkyDateRangePickerModule,
+} from '@skyux/datetime';
+
+function dateRangeExcludesWeekend(
+  control: AbstractControl,
+): ValidationErrors | null {
+  const startDate = control.value.startDate;
+  const endDate = control.value.endDate;
+
+  const isWeekend = (value: unknown): boolean => {
+    return (
+      value instanceof Date && (value.getDay() === 6 || value.getDay() === 0)
+    );
+  };
+
+  if (isWeekend(startDate) || isWeekend(endDate)) {
+    return { dateWeekend: true };
+  }
+
+  return null;
+}
 
 @Component({
   standalone: true,
   selector: 'app-demo',
   templateUrl: './demo.component.html',
-  imports: [FormsModule, ReactiveFormsModule, SkyDateRangePickerModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    SkyDateRangePickerModule,
+    CommonModule,
+  ],
 })
 export class DemoComponent {
   protected get reactiveRange(): AbstractControl | null {
@@ -26,11 +55,22 @@ export class DemoComponent {
   protected hintText =
     'Donations received today are updated at the top of each hour.';
   protected labelText = 'Last donation';
+  protected lastDonation: FormControl;
   protected required = true;
 
   constructor() {
-    this.formGroup = inject(FormBuilder).group({
-      lastDonation: new FormControl({ value: '', disabled: this.disabled }),
+    this.lastDonation = new FormControl(
+      { value: '', disabled: this.disabled },
+      [dateRangeExcludesWeekend],
+    );
+    this.lastDonation.setValue({
+      calculatorId: SkyDateRangeCalculatorId.SpecificRange,
+      startDate: new Date('11/09/2024'),
+      endDate: new Date('11/09/2024'),
     });
+    this.formGroup = inject(FormBuilder).group({
+      lastDonation: this.lastDonation,
+    });
+    this.formGroup.markAllAsTouched();
   }
 }
