66614515f3549de9b4c54acd4365870fca6a23ff
John White <750350+johnhwhite@users.noreply.github.com>
2024-11-14 10:45:55 -0500

    Revert lint fix, add override

--- libs/components/datetime/eslint.config.js
+++ libs/components/datetime/eslint.config.js
@@ -1,3 +1,12 @@
 const config = require('../../../eslint-libs.config');
 
-module.exports = config;
+module.exports = [
+  ...config,
+  {
+    files: ['**/*.html'],
+    rules: {
+      // todo: find an alternative to using autofocus
+      '@angular-eslint/template/no-autofocus': 'warn',
+    },
+  },
+];
