a46d661f793bffa0b27157a9b7f7c39c97773ef1
John White <750350+johnhwhite@users.noreply.github.com>
2024-11-14 10:51:41 -0500

    Disable lint rule for one file

--- libs/components/datetime/eslint.config.js
+++ libs/components/datetime/eslint.config.js
@@ -3,10 +3,10 @@ const config = require('../../../eslint-libs.config');
 module.exports = [
   ...config,
   {
-    files: ['**/*.html'],
+    files: ['**/datepicker-calendar.component.html'],
     rules: {
       // todo: find an alternative to using autofocus
-      '@angular-eslint/template/no-autofocus': 'warn',
+      '@angular-eslint/template/no-autofocus': 'off',
     },
   },
 ];
