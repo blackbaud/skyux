f2b224d5689ee2da3b6de528ef3d219b5046ed6e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-11-20 09:30:20 -0500

    more dirs

--- libs/components/list-builder-view-grids/testing/src/public-api.ts
+++ libs/components/list-builder-view-grids/testing/src/public-api.ts
@@ -1,4 +1,4 @@
-export { SkyListViewGridFixture } from './list-view-grid-fixture';
-export { SkyListViewGridFixtureCell } from './list-view-grid-fixture-cell';
-export { SkyListViewGridFixtureHeader } from './list-view-grid-fixture-header';
-export { SkyListViewGridFixtureRow } from './list-view-grid-fixture-row';
+export { SkyListViewGridFixture } from './legacy/list-view-grid-fixture';
+export { SkyListViewGridFixtureCell } from './legacy/list-view-grid-fixture-cell';
+export { SkyListViewGridFixtureHeader } from './legacy/list-view-grid-fixture-header';
+export { SkyListViewGridFixtureRow } from './legacy/list-view-grid-fixture-row';
