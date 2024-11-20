e7e50a17eb52fff713afed85177ddc43825890c2
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2024-11-20 11:05:06 -0500

    remove sections; replace with arrays

---
+++ scripts/manifest/fixtures/example-packages/foo/testing/src/public-api.ts
@@ -0,0 +1,1 @@
+export { SkyFooHarness } from './modules/foo/foo-harness';
--- scripts/manifest/get-public-api.ts
+++ scripts/manifest/get-public-api.ts
@@ -1,8 +1,10 @@
+import fsPromises from 'fs/promises';
+import path from 'node:path';
 import { DeclarationReflection, ReflectionKind } from 'typedoc';
 
 import { getEntryPointsReflections } from './get-entry-points-reflections';
 import { getProjects } from './get-projects';
-import { SkyManifestPackageSection } from './types/manifest-types';
+import { SkyManifestTopLevelDefinition } from './types/manifest-types';
 import { getClass } from './utility/get-class';
 import { getDecorator } from './utility/get-decorator';
 import { getDirective } from './utility/get-directive';
@@ -12,83 +14,67 @@ import { getInterface } from './utility/get-interface';
 import { getPipe } from './utility/get-pipe';
 import { getTypeAlias } from './utility/get-type-alias';
 import { getVariable } from './utility/get-variable';
-import { sortMapByKey } from './utility/maps';
 
-export type PackagesMap = Map<string, PackageSectionsMap>;
-type PackageSectionsMap = Map<string, SkyManifestPackageSection>;
+export type PackagesMap = Map<string, SkyManifestTopLevelDefinition[]>;
 
 function handleClassKind(
   child: DeclarationReflection,
-  section: SkyManifestPackageSection,
-): void {
+): SkyManifestTopLevelDefinition {
   const decoratorName = getDecorator(child);
 
   switch (decoratorName) {
     case 'Injectable': {
-      section.services.push(getClass(child, 'service'));
-      break;
+      return getClass(child, 'service');
     }
 
     case 'Component': {
-      section.directives.push(getDirective(child, 'component'));
-      break;
+      return getDirective(child, 'component');
     }
 
     case 'Directive': {
-      section.directives.push(getDirective(child, 'directive'));
-      break;
+      return getDirective(child, 'directive');
     }
 
     case 'NgModule': {
-      section.modules.push(getClass(child, 'module'));
-      break;
+      return getClass(child, 'module');
     }
 
     case 'Pipe': {
-      section.pipes.push(getPipe(child));
-      break;
+      return getPipe(child);
     }
 
     default: {
-      section.classes.push(getClass(child, 'class'));
-      break;
+      return getClass(child, 'class');
     }
   }
 }
 
-function assignChildToSection(
+function getManifestItem(
   child: DeclarationReflection,
-  section: SkyManifestPackageSection,
-): void {
+): SkyManifestTopLevelDefinition {
   switch (child.kind) {
     case ReflectionKind.Class: {
-      handleClassKind(child, section);
-      break;
+      return handleClassKind(child);
     }
 
     case ReflectionKind.TypeAlias: {
-      section.typeAliases.push(getTypeAlias(child));
-      break;
+      return getTypeAlias(child);
     }
 
     case ReflectionKind.Enum: {
-      section.enumerations.push(getEnum(child));
-      break;
+      return getEnum(child);
     }
 
     case ReflectionKind.Function: {
-      section.functions.push(getFunction(child));
-      break;
+      return getFunction(child);
     }
 
     case ReflectionKind.Interface: {
-      section.interfaces.push(getInterface(child));
-      break;
+      return getInterface(child);
     }
 
     case ReflectionKind.Variable: {
-      section.variables.push(getVariable(child));
-      break;
+      return getVariable(child);
     }
 
     default: {
@@ -99,14 +85,52 @@ function assignChildToSection(
   }
 }
 
-function getSectionName(filePath: string): string {
-  return filePath.split('src/lib/modules/')?.[1]?.split('/')[0] ?? 'root';
+function getSectionName(filePath: string): string | undefined {
+  return filePath.split('src/lib/modules/')?.[1]?.split('/')[0];
+}
+
+function maybeSetDocumentationSection(
+  filePath: string,
+  projectName: string,
+  docsSections: any,
+  hasTestingEntryPoint: boolean,
+): void {
+  const sectionName = getSectionName(filePath);
+
+  if (!sectionName) {
+    console.warn(`A section name could not be determined for ${filePath}.`);
+    return;
+  }
+
+  if (docsSections[sectionName] === undefined) {
+    docsSections[sectionName] = {
+      api: [`libs/components/${projectName}/src/lib/modules/${sectionName}/*`],
+    };
+
+    if (hasTestingEntryPoint) {
+      docsSections[sectionName].testing = [
+        `libs/components/${projectName}/testing/src/modules/${sectionName}/*`,
+      ];
+    }
+  }
 }
 
 export async function getPublicApi(): Promise<PackagesMap> {
   const nxProjects = await getProjects();
 
-  const packages: PackagesMap = new Map<string, PackageSectionsMap>();
+  const skyuxDevConfig = JSON.parse(
+    await fsPromises.readFile('.skyuxdev.json', 'utf-8'),
+  );
+  skyuxDevConfig.documentation = {
+    sections: {},
+  };
+
+  const docsSections = skyuxDevConfig.documentation.sections;
+
+  const packages: PackagesMap = new Map<
+    string,
+    SkyManifestTopLevelDefinition[]
+  >();
 
   for (const {
     entryPoints,
@@ -122,11 +146,12 @@ export async function getPublicApi(): Promise<PackagesMap> {
       projectRoot,
     });
 
+    const hasTestingEntryPoint = entryPointReflections.length === 2;
+
     for (const refl of entryPointReflections) {
       if (refl.children) {
-        const sections: PackageSectionsMap =
-          packages.get(refl.entryName) ??
-          new Map<string, SkyManifestPackageSection>();
+        const items: SkyManifestTopLevelDefinition[] =
+          packages.get(refl.entryName) ?? [];
 
         for (const child of refl.children) {
           const filePath = child.sources?.[0].fullFileName;
@@ -135,33 +160,27 @@ export async function getPublicApi(): Promise<PackagesMap> {
             continue;
           }
 
-          const sectionName = getSectionName(filePath);
+          maybeSetDocumentationSection(
+            filePath,
+            projectName,
+            docsSections,
+            hasTestingEntryPoint,
+          );
 
-          const section: SkyManifestPackageSection = sections.get(
-            sectionName,
-          ) ?? {
-            classes: [],
-            components: [],
-            directives: [],
-            enumerations: [],
-            functions: [],
-            interfaces: [],
-            modules: [],
-            pipes: [],
-            services: [],
-            typeAliases: [],
-            variables: [],
-          };
-
-          assignChildToSection(child, section);
-
-          sections.set(sectionName, section);
+          items.push(getManifestItem(child));
         }
 
-        packages.set(refl.entryName, sortMapByKey(sections));
+        packages.set(refl.entryName, items);
       }
     }
   }
+
+  console.log('DOCS', docsSections);
+
+  await fsPromises.writeFile(
+    '.skyuxdev.json',
+    JSON.stringify(skyuxDevConfig, undefined, 2),
+  );
 
   return packages;
 }
