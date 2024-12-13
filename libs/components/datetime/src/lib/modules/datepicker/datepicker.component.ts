3b840776952ed7416c9d1eda679faf6bdad24307
Steve Brush <steve.brush@blackbaud.com>
2024-12-13 10:29:42 -0500

    fix(components/datetime): date range picker emits touched status when focus leaves composite control (#2947)

--- libs/components/datetime/src/lib/modules/datepicker/datepicker.component.ts
+++ libs/components/datetime/src/lib/modules/datepicker/datepicker.component.ts
@@ -232,6 +232,7 @@ export class SkyDatepickerComponent
   readonly #zIndex: Observable<number> | undefined;
 
   readonly #datepickerHostSvc = inject(SkyDatepickerHostService);
+  readonly #elementRef = inject(ElementRef);
 
   constructor(
     affixService: SkyAffixService,
@@ -350,6 +351,17 @@ export class SkyDatepickerComponent
         }
       }
     }
+  }
+
+  /**
+   * Whether the datepicker component contains the provided focus event target.
+   * @internal
+   */
+  public containsTarget(target: EventTarget): boolean {
+    return (
+      this.#elementRef.nativeElement.contains(target) ||
+      this.getPickerRef()?.nativeElement.contains(target)
+    );
   }
 
   /**
