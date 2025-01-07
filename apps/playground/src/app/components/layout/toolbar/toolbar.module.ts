3c04ba0e105c6c50f917b85746efee7fab9e5e21
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2025-01-07 17:12:13 -0500

    feat(components/layout): tokenize toolbar component (#2980)

--- apps/playground/src/app/components/layout/toolbar/toolbar.module.ts
+++ apps/playground/src/app/components/layout/toolbar/toolbar.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SkyViewkeeperModule } from '@skyux/core';
+import { SkyRadioModule } from '@skyux/forms';
 import { SkyIconModule } from '@skyux/icon';
 import { SkyToolbarModule } from '@skyux/layout';
+import { SkySearchModule } from '@skyux/lookup';
 
 import { ToolbarRoutingModule } from './toolbar-routing.module';
 import { ToolbarComponent } from './toolbar.component';
@@ -11,8 +14,11 @@ import { ToolbarComponent } from './toolbar.component';
   imports: [
     ToolbarRoutingModule,
     SkyIconModule,
+    SkyRadioModule,
+    SkySearchModule,
     SkyToolbarModule,
     SkyViewkeeperModule,
+    FormsModule,
   ],
 })
 export class ToolbarModule {
