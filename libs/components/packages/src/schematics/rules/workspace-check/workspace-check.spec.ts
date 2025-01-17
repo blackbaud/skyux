e933fce3723e393046add8dad413f1870d028bd6
John White <750350+johnhwhite@users.noreply.github.com>
2025-01-17 08:50:44 -0500

    fix(components/ag-grid): header should have readable text (#3037) (#3041)
    [AB#3204448](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3204448)
    🍒 from #3037

--- libs/components/packages/src/schematics/rules/workspace-check/workspace-check.spec.ts
+++ libs/components/packages/src/schematics/rules/workspace-check/workspace-check.spec.ts
@@ -41,4 +41,20 @@ describe('Workspace check', () => {
     await workspaceCheck()(tree, context as SchematicContext);
     expect(warn).not.toHaveBeenCalled();
   });
+
+  it('should not warn when there is no build step', async () => {
+    const tree = await createTestApp(runner, {
+      projectName: 'test-project',
+      options: { ssr: false },
+    });
+    const workspace: any = tree.readJson('angular.json');
+    delete workspace.projects['test-project'].architect.build;
+    tree.overwrite('angular.json', JSON.stringify(workspace, null, 2));
+    const context: Pick<SchematicContext, 'logger'> = {
+      logger: new logging.NullLogger(),
+    };
+    const warn = jest.spyOn(context.logger, 'warn');
+    await workspaceCheck()(tree, context as SchematicContext);
+    expect(warn).not.toHaveBeenCalled();
+  });
 });
