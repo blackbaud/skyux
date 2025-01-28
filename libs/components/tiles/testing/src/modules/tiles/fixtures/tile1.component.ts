dd47e232ad053eec1dd7a3375e9213aef40de9ca
Corey Archer <Corey.Archer@blackbaud.me>
2025-01-28 11:09:07 -0500

    feat(components/tiles): add test harnesses for tile and tile dashboard (#3052)

--- libs/components/tiles/testing/src/modules/tiles/fixtures/tile1.component.ts
+++ libs/components/tiles/testing/src/modules/tiles/fixtures/tile1.component.ts
@@ -3,12 +3,17 @@ import { SkyTilesModule } from '@skyux/tiles';
 
 @Component({
   standalone: true,
-  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'div.tile1',
   templateUrl: './tile1.component.html',
   imports: [SkyTilesModule],
 })
 export class Tile1Component {
+  public helpKey: string | undefined;
+  public helpContent: string | undefined;
+  public helpTitle: string | undefined;
+  public showSections = true;
+  public showSettings = true;
+
   public tileSettingsClick(): void {
     alert('tile settings clicked');
   }
