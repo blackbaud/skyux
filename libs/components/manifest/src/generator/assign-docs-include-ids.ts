4684d2163dec474486abbe1d1cc8b763173b03a6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-01-29 11:17:01 -0500

    revert

--- libs/components/manifest/src/generator/assign-docs-include-ids.ts
+++ libs/components/manifest/src/generator/assign-docs-include-ids.ts
@@ -1,11 +1,7 @@
 import fsPromises from 'node:fs/promises';
 import * as ts from 'typescript';
 
-import { SkyManifestParentDefinition } from '../types/base-def';
-
 import { type PackagesMap } from './get-public-api';
-
-const stuff: Record<string, SkyManifestParentDefinition[]> = {};
 
 /**
  * Assign `docsIncludeIds` for each entry-point module, based on the types
@@ -16,31 +12,6 @@ export async function assignDocsIncludeIds(
 ): Promise<void> {
   for (const [, definitions] of packages) {
     for (const definition of definitions) {
-      // const packageName = definition.filePath.split('libs/components/')[1].split('/')[0];
-      let section = definition.filePath
-        .split('/src/lib/modules/')[1]
-        ?.split('/')[0];
-
-      if (!section) {
-        section = definition.filePath
-          .split('/testing/')[1]
-          ?.split('src/modules/')[1]
-          ?.split('/')[0];
-
-        if (section) {
-          section += '-testing';
-        }
-      }
-
-      if (section) {
-        stuff[section] ??= [];
-
-        if (!definition.isInternal) {
-          stuff[section].push(definition);
-          (definition as any).section = section;
-        }
-      }
-
       if (
         definition.kind !== 'module' ||
         definition.docsIncludeIds ||
@@ -109,49 +80,7 @@ export async function assignDocsIncludeIds(
         return definition.docsId;
       });
 
-      if (!section) {
-        await fsPromises.writeFile(
-          definition.filePath,
-          contents.replace(
-            '@NgModule({',
-            `throw new Error('Missing section for ${definition.filePath}');
-      /**
-       * @docsIncludeIds ${moduleExports.join(', ')}
-       */
-      @NgModule({`,
-          ),
-        );
-      }
-
       definition.docsIncludeIds = moduleExports;
-    }
-  }
-
-  // console.log('STUFF', stuff);
-
-  for (const [section, definitions] of Object.entries(stuff)) {
-    const module = definitions.find((d) => d.kind === 'module');
-
-    if (module) {
-      const types = stuff[section]
-        .map((d) => d.docsId)
-        .filter((n) => n !== module.docsId)
-        .concat(stuff[`${section}-testing`]?.map((d) => d.docsId))
-        .filter((n) => n);
-
-      const contents = await fsPromises.readFile(module.filePath, 'utf-8');
-
-      await fsPromises.writeFile(
-        module.filePath,
-        contents.replace(
-          '@NgModule({',
-          `
-/**
- * @docsIncludeIds ${types.join(', ')}
- */
-@NgModule({`,
-        ),
-      );
     }
   }
 }
