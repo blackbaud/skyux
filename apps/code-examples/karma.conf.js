a8efb596ffdcf1bb150240c7553003b653673b5c
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-02-01 10:47:19 -0500

    try fix build

--- apps/code-examples/karma.conf.js
+++ apps/code-examples/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
     ...baseConfig,
     coverageReporter: {
       ...baseConfig.coverageReporter,
-      dir: join(__dirname, '../../coverage/apps/code-examples-playground'),
+      dir: join(__dirname, '../../coverage/apps/code-examples'),
       // TODO: remove these threshold overrides to meet 100% coverage!
       check: {
         global: {
