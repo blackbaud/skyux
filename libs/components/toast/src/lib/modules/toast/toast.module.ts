55d99ffb9ed41d7a1530045aabbe36cdd22543ef
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-02-05 08:46:53 -0500

    revert toast change

--- libs/components/toast/src/lib/modules/toast/toast.module.ts
+++ libs/components/toast/src/lib/modules/toast/toast.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { SkyToastComponent } from './toast.component';
+
 /**
  * @deprecated The `SkyToastModule` is no longer needed and can be removed from your application.
  * @internal
  */
-@NgModule({})
+@NgModule({
+  imports: [SkyToastComponent],
+  exports: [SkyToastComponent],
+})
 export class SkyToastModule {}
