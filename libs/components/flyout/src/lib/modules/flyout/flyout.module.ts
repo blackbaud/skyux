5efe5885e8cf8fec775e0db0d95ada469e2f7969
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-02-05 08:53:10 -0500

    update

--- libs/components/flyout/src/lib/modules/flyout/flyout.module.ts
+++ libs/components/flyout/src/lib/modules/flyout/flyout.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { SkyFlyoutComponent } from './flyout.component';
+
 /**
  * @deprecated The `SkyFlyoutModule` is no longer needed and can be removed from your application.
  * @internal
  */
-@NgModule({})
+@NgModule({
+  imports: [SkyFlyoutComponent],
+  exports: [SkyFlyoutComponent],
+})
 export class SkyFlyoutModule {}
