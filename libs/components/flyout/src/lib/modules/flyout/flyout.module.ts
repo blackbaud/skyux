7bc9c95238dedc60f2e8e78201cee26217b71910
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-02-05 11:01:25 -0500

    revert deprecations

--- libs/components/flyout/src/lib/modules/flyout/flyout.module.ts
+++ libs/components/flyout/src/lib/modules/flyout/flyout.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { SkyFlyoutComponent } from './flyout.component';
 
-/**
- * @deprecated The `SkyFlyoutModule` is no longer needed and can be removed from your application.
- * @internal
- */
 @NgModule({
   imports: [SkyFlyoutComponent],
   exports: [SkyFlyoutComponent],
--- libs/components/toast/src/lib/modules/toast/toast.module.ts
+++ libs/components/toast/src/lib/modules/toast/toast.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { SkyToastComponent } from './toast.component';
 
-/**
- * @deprecated The `SkyToastModule` is no longer needed and can be removed from your application.
- * @internal
- */
 @NgModule({
   imports: [SkyToastComponent],
   exports: [SkyToastComponent],
