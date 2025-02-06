dae319123a2e1146f70641a15065712c0f3d373e
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2025-02-06 15:33:09 -0500

    feat(components/layout): add harness for description list (#3124) (#3125)

---
+++ libs/components/layout/testing/src/modules/description-list/fixtures/description-list-test.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { SkyHelpInlineModule } from '@skyux/help-inline';
+import {
+  SkyDescriptionListModeType,
+  SkyDescriptionListModule,
+} from '@skyux/layout';
+
+@Component({
+  standalone: true,
+  selector: 'sky-description-list-fixture',
+  templateUrl: './description-list-test.component.html',
+  imports: [SkyDescriptionListModule, SkyHelpInlineModule],
+})
+export class DescriptionListHarnessTestComponent {
+  public mode: SkyDescriptionListModeType = 'vertical';
+
+  protected items: {
+    term?: string;
+    description?: string;
+    hideDescription?: boolean;
+    helpContent?: string;
+    helpTitle?: string;
+    helpKey?: string;
+    dataSkyId?: string;
+  }[] = [
+    {
+      term: 'College',
+      description: 'Humanities and Social Sciences',
+    },
+    {
+      term: 'Department',
+    },
+    {
+      description: 'Anthropology',
+      hideDescription: true,
+    },
+    {
+      term: 'Advisor',
+      description: 'Cathy Green',
+      helpContent: 'The faculty member who advises the student.',
+      helpTitle: 'Help inline title',
+    },
+    {
+      term: 'Class year',
+      description: '2024',
+      helpKey: 'helpKey.html',
+    },
+  ];
+
+  public onActionClick(): void {
+    alert('Help inline button clicked!');
+  }
+}
