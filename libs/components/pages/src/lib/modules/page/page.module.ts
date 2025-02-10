306f929dce5f92dbdcf8ecbd250c4c6fd431ee81
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2025-02-10 15:41:27 -0500

    feat(components/pages): add `sky-modal-link-list` component (#3131) (#3132)
    :cherries: Cherry picked from #3131 [feat(components/pages): add
    `sky-modal-link-list`
    component](https://github.com/blackbaud/skyux/pull/3131)
    
    
    [AB#3252968](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3252968)
    
    Co-authored-by: John White <750350+johnhwhite@users.noreply.github.com>

--- libs/components/pages/src/lib/modules/page/page.module.ts
+++ libs/components/pages/src/lib/modules/page/page.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { SkyLinkListRecentlyAccessedComponent } from '../link-list-recently-accessed/link-list-recently-accessed.component';
 import { SkyLinkListItemComponent } from '../link-list/link-list-item.component';
 import { SkyLinkListComponent } from '../link-list/link-list.component';
+import { SkyLinkListModule } from '../link-list/link-list.module';
+import { SkyModalLinkListModule } from '../modal-link-list/modal-link-list.module';
 import { SkyPageHeaderModule } from '../page-header/page-header.module';
 
 import { SkyPageContentComponent } from './page-content.component';
@@ -12,15 +14,19 @@ import { SkyPageComponent } from './page.component';
 @NgModule({
   declarations: [SkyPageComponent, SkyPageContentComponent],
   imports: [
+    SkyLinkListModule,
     SkyPageLinksComponent,
     SkyLinkListComponent,
     SkyLinkListItemComponent,
     SkyLinkListRecentlyAccessedComponent,
+    SkyModalLinkListModule,
   ],
   exports: [
     SkyLinkListComponent,
     SkyLinkListItemComponent,
     SkyLinkListRecentlyAccessedComponent,
+    SkyLinkListModule,
+    SkyModalLinkListModule,
     SkyPageComponent,
     SkyPageHeaderModule,
     SkyPageContentComponent,
