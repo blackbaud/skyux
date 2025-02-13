969dad3be19717a95650ca034156886bf5d5a4ed
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-02-13 09:42:44 -0500

    tests

---
+++ libs/components/docs-utils/src/lib/modules/clipboard/clipboard.service.spec.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SkyAppWindowRef } from '@skyux/core';
+
+import { SkyClipboardService } from './clipboard.service';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    @if (true) {
+      <div class="foo">&lt;h1>Hello, world!&lt;/h1></div>
+    }
+  `,
+})
+class TestComponent {}
+
+describe('clipboard.service', () => {
+  function setupTest(): {
+    clipboardSpy: jasmine.Spy<jasmine.Func>;
+    clipboardSvc: SkyClipboardService;
+    fixture: ComponentFixture<TestComponent>;
+  } {
+    TestBed.configureTestingModule({
+      imports: [TestComponent],
+    });
+
+    const clipboardSpy = spyOn(
+      TestBed.inject(SkyAppWindowRef).nativeWindow.navigator.clipboard,
+      'writeText',
+    );
+
+    const clipboardSvc = TestBed.inject(SkyClipboardService);
+    const fixture = TestBed.createComponent(TestComponent);
+
+    return { clipboardSpy, clipboardSvc, fixture };
+  }
+
+  it("should save an element's text content to the system clipboard", () => {
+    const { clipboardSpy, clipboardSvc, fixture } = setupTest();
+
+    fixture.detectChanges();
+
+    clipboardSvc.copyTextContent(fixture.elementRef, 'Copied');
+
+    expect(clipboardSpy).toHaveBeenCalledWith('<h1>Hello, world!</h1>');
+  });
+});
