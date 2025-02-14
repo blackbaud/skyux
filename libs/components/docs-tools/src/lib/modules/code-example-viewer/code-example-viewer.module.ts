494e8da6d8594654c773f77c35453769bdd3a874
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-02-14 14:09:58 -0500

    feat: add code example viewer component (internal use only)

---
+++ libs/components/docs-tools/src/lib/modules/code-example-viewer/code-example-viewer.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { SkyCodeExampleViewerComponent } from './code-example-viewer.component';
+
+/**
+ * @internal
+ */
+@NgModule({
+  imports: [SkyCodeExampleViewerComponent],
+  exports: [SkyCodeExampleViewerComponent],
+})
+export class SkyCodeExampleViewerModule {}
