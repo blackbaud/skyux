2dad6c73ffcb5c16a5fde0d4d489cff0010e94bb
John White <750350+johnhwhite@users.noreply.github.com>
2025-02-25 08:43:21 -0500

    Fix checkbox border color

--- libs/components/ag-grid/src/lib/styles/ag-grid-theme.ts
+++ libs/components/ag-grid/src/lib/styles/ag-grid-theme.ts
@@ -22,7 +22,7 @@ const defaultsForAllThemes = {
   checkboxIndeterminateBackgroundColor:
     'var(--sky-override-switch-checked-color, var(--sky-color-icon-inverse, transparent))',
   checkboxUncheckedBackgroundColor: 'transparent',
-  checkboxUncheckedBorderColor: 'var(--sky-border-color-neutral-medium)',
+  checkboxUncheckedBorderColor: 'var(--sky-border-color-neutral-medium-dark)',
   checkboxCheckedShapeImage: {
     svg: `<svg width="18" height="18" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m8.5 16.586-3.793-3.793a1 1 0 0 0-1.414 1.414l4.5 4.5a1 1 0 0 0 1.414 0l11-11a1 1 0 0 0-1.414-1.414L8.5 16.586Z" fill="#212121"/></svg>`,
   },
