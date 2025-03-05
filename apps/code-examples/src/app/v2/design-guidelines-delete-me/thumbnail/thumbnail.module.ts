9d7fa01c70e248f52d140fb4a5728280264b0b67
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-03-05 11:47:34 -0500

    remove anatomy components

--- apps/code-examples/src/app/v2/design-guidelines-delete-me/thumbnail/thumbnail.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { SkyDocsThumbnailComponent } from './thumbnail.component';
-
-@NgModule({
-  imports: [SkyDocsThumbnailComponent],
-  exports: [SkyDocsThumbnailComponent],
-})
-export class SkyDocsThumbnailModule {}
