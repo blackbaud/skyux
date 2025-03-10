8a3744a6473f9800b783cd7778d019038578ec07
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2025-03-10 11:53:04 -0400

    feat(components/lists): add paging test harnesses (#3185) (#3218)

---
+++ libs/components/code-examples/src/lib/modules/lists/paging/with-content/example.component.spec.ts
@@ -0,0 +1,82 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SkyPagingHarness, SkyRepeaterHarness } from '@skyux/lists/testing';
+
+import { ListsPagingWithContentExampleComponent } from './example.component';
+
+describe('Paging example', () => {
+  async function setupTest(
+    options: {
+      dataSkyId?: string;
+    } = {},
+  ): Promise<{
+    pagingHarness: SkyPagingHarness;
+    fixture: ComponentFixture<ListsPagingWithContentExampleComponent>;
+  }> {
+    await TestBed.configureTestingModule({
+      imports: [ListsPagingWithContentExampleComponent, NoopAnimationsModule],
+    }).compileComponents();
+
+    const fixture = TestBed.createComponent(
+      ListsPagingWithContentExampleComponent,
+    );
+    const loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
+
+    const pagingHarness: SkyPagingHarness = options.dataSkyId
+      ? await loader.getHarness(
+          SkyPagingHarness.with({
+            dataSkyId: options.dataSkyId,
+          }),
+        )
+      : await loader.getHarness(SkyPagingHarness);
+
+    return { pagingHarness, fixture };
+  }
+
+  it('should set up the paging content', async () => {
+    const { pagingHarness, fixture } = await setupTest({
+      dataSkyId: 'my-paging-content',
+    });
+
+    await expectAsync(pagingHarness.getCurrentPage()).toBeResolvedTo(1);
+
+    const contentHarness = await (
+      await pagingHarness.getPagingContent()
+    ).queryHarness(SkyRepeaterHarness);
+
+    let items = await contentHarness.getRepeaterItems();
+
+    await expectAsync(items[0].getTitleText()).toBeResolvedTo('Abed');
+
+    await pagingHarness.clickPageButton(3);
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    await expectAsync(pagingHarness.getCurrentPage()).toBeResolvedTo(3);
+
+    items = await contentHarness.getRepeaterItems();
+
+    await expectAsync(items[0].getTitleText()).toBeResolvedTo('Leonard');
+
+    await pagingHarness.clickNextButton();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    await expectAsync(pagingHarness.getCurrentPage()).toBeResolvedTo(4);
+
+    items = await contentHarness.getRepeaterItems();
+
+    await expectAsync(items[0].getTitleText()).toBeResolvedTo('Shirley');
+
+    await expectAsync(pagingHarness.clickNextButton()).toBeRejectedWithError(
+      'Could not click the next button because it is disabled.',
+    );
+
+    await expectAsync(pagingHarness.clickPageButton(1)).toBeRejectedWithError(
+      'Could not find page button 1.',
+    );
+  });
+});
