blackbaud/skyux

commit d33574a630db71ef0dbfd6f2e2a74201fcb5e492

fix(components/ag-grid): show validator changes for editable cell (#3194)
[AB#3265129](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3265129)

libs/components/ag-grid/src/lib/modules/ag-grid/cell-validator/ag-grid-cell-validator-tooltip.component.html
@@ -11,6 +11,10 @@
     class="sky-pull-right"
   />
 </div>
-<sky-popover #validatorPopover popoverType="danger">
+<sky-popover
+  #validatorPopover
+  data-sky-id="validatorPopover"
+  popoverType="danger"
+>
   {{ validatorMessage }}
 </sky-popover>

libs/components/ag-grid/src/lib/modules/ag-grid/cell-validator/ag-grid-cell-validator-tooltip.component.ts
@@ -1,9 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  Input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { SkyPopoverMessage, SkyPopoverMessageType } from '@skyux/popovers';
 
 import { AgColumn, CellFocusedEvent } from 'ag-grid-community';
@@ -25,95 +20,70 @@ export class SkyAgGridCellValidatorTooltipComponent {
   public set params(value: SkyCellRendererValidatorParams | undefined) {
     this.cellRendererParams = value;
 
-    /*istanbul ignore next*/
-    this.cellRendererParams?.api?.addEventListener(
+    value?.api?.addEventListener(
       'cellFocused',
       (eventParams: CellFocusedEvent) => {
         // We want to close any popovers that are opened when other cells are focused, but open a popover if the current cell is focused.
         if (
-          (eventParams.column as AgColumn).getColId() !==
-            this.cellRendererParams?.column?.getColId() ||
-          eventParams.rowIndex !== this.cellRendererParams?.node?.rowIndex
+          (eventParams.column as AgColumn).getColId() ===
+            value?.column?.getColId() &&
+          eventParams.rowIndex === value?.node?.rowIndex
         ) {
+          this.showPopover();
+        } else {
           this.hidePopover();
         }
       },
     );
 
-    /*istanbul ignore next*/
-    this.cellRendererParams?.eGridCell?.addEventListener('keyup', (event) => {
-      if (
-        ['ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp'].includes(event.key)
-      ) {
-        this.showPopover();
-      }
-    });
-
-    this.cellRendererParams?.eGridCell?.addEventListener('mouseenter', () => {
-      this.#scheduleDelayedPopover();
-    });
-
-    this.cellRendererParams?.eGridCell?.addEventListener('mouseleave', () => {
+    value?.api?.addEventListener('cellEditingStarted', () => {
       this.hidePopover();
     });
 
-    /*istanbul ignore next*/
-    this.cellRendererParams?.api?.addEventListener('cellEditingStarted', () => {
-      this.hidePopover();
-    });
-
-    if (
-      typeof this.cellRendererParams?.skyComponentProperties
-        ?.validatorMessage === 'function'
-    ) {
-      this.validatorMessage =
-        this.cellRendererParams.skyComponentProperties.validatorMessage(
-          this.cellRendererParams.value,
-          this.cellRendererParams.data,
-          this.cellRendererParams?.node?.rowIndex ?? undefined,
-        );
+    if (typeof value?.skyComponentProperties?.validatorMessage === 'function') {
+      this.validatorMessage = value.skyComponentProperties.validatorMessage(
+        value.value,
+        value.data,
+        value?.node?.rowIndex,
+      );
     } else {
-      this.validatorMessage =
-        this.cellRendererParams?.skyComponentProperties?.validatorMessage;
+      this.validatorMessage = value?.skyComponentProperties?.validatorMessage;
     }
-
-    this.#changeDetector.markForCheck();
   }
 
   public popoverMessageStream = new Subject<SkyPopoverMessage>();
   public validatorMessage: string | undefined;
   public cellRendererParams: SkyCellRendererValidatorParams | undefined;
 
-  #hoverTimeout: number | undefined;
-  #changeDetector: ChangeDetectorRef;
-
-  constructor(changeDetector: ChangeDetectorRef) {
-    this.#changeDetector = changeDetector;
-  }
-
   public hidePopover(): void {
-    this.#cancelDelayedPopover();
     this.popoverMessageStream.next({ type: SkyPopoverMessageType.Close });
   }
 
   public showPopover(): void {
-    this.#cancelDelayedPopover();
-    this.popoverMessageStream.next({ type: SkyPopoverMessageType.Open });
-  }
-
-  #scheduleDelayedPopover(): void {
-    /* istanbul ignore else */
-    if (!this.#hoverTimeout) {
-      this.#hoverTimeout = window.setTimeout(() => {
-        this.showPopover();
-      }, 300);
+    if (this.#shouldShowPopover()) {
+      this.popoverMessageStream.next({ type: SkyPopoverMessageType.Open });
     }
   }
 
-  #cancelDelayedPopover(): void {
-    if (this.#hoverTimeout) {
-      window.clearTimeout(this.#hoverTimeout);
-      this.#hoverTimeout = undefined;
+  #shouldShowPopover(): boolean {
+    if (!this.validatorMessage) {
+      return false;
+    }
+    const editingCells = this.cellRendererParams?.api.getEditingCells() ?? [];
+    if (editingCells.length > 0) {
+      return false;
+    }
+    if (
+      typeof this.cellRendererParams?.skyComponentProperties?.validator ===
+      'function'
+    ) {
+      return !this.cellRendererParams.skyComponentProperties.validator(
+        this.cellRendererParams.value,
+        this.cellRendererParams.data,
+        this.cellRendererParams.node?.rowIndex,
+      );
+    } else {
+      return true;
     }
   }
 }

libs/components/ag-grid/src/lib/styles/_base.scss
@@ -192,6 +192,17 @@ $modernDarkThemeCompact: map.merge($modernDarkThemeBase, $modernThemeCompact);
   .ag-ltr .ag-header-cell-resize {
     right: -4px;
   }
+
+  // Stretch the cell wrapper to the full height of the row so validator popovers are triggered on the entire cell.
+  .ag-row > .ag-cell.sky-ag-grid-cell-validator > .ag-cell-wrapper {
+    align-items: stretch;
+    height: 100%;
+  }
+
+  .ag-cell.sky-ag-grid-cell-invalid {
+    border: 1px solid var(--sky-highlight-color-danger);
+    --ag-range-selection-border-color: var(--sky-highlight-color-danger);
+  }
 }
 
 .ag-theme-sky-data-grid-default,

libs/components/ag-grid/src/lib/modules/ag-grid/cell-validator/ag-grid-cell-validator-tooltip.component.spec.ts
@@ -1,135 +1,211 @@
-import { TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { expect } from '@skyux-sdk/testing';
+import { SkyPopoverContentHarness } from '@skyux/popovers/testing';
+
+import { CellFocusedEvent } from 'ag-grid-community';
 
 import { SkyAgGridCellValidatorTooltipFixtureComponent } from '../fixtures/ag-grid-cell-validator-tooltip.component.fixture';
 import { SkyAgGridFixtureModule } from '../fixtures/ag-grid.module.fixture';
 import { SkyCellRendererValidatorParams } from '../types/cell-renderer-validator-params';
 
-import { SkyAgGridCellValidatorTooltipComponent } from './ag-grid-cell-validator-tooltip.component';
-
-const NOOP = (): void => {
-  return;
-};
-
 describe('SkyAgGridCellValidatorTooltipComponent', () => {
+  let fixture: ComponentFixture<SkyAgGridCellValidatorTooltipFixtureComponent>;
+  let component: SkyAgGridCellValidatorTooltipFixtureComponent;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [SkyAgGridFixtureModule],
     });
+    fixture = TestBed.createComponent(
+      SkyAgGridCellValidatorTooltipFixtureComponent,
+    );
+    component = fixture.componentInstance;
+    component.parameters = {
+      addRenderedRowListener: jasmine.createSpy('addRenderedRowListener'),
+      api: {
+        addEventListener: jasmine.createSpy('addEventListener'),
+        getEditingCells: jasmine.createSpy('getEditingCells'),
+      },
+      column: {
+        getColId: () => 'test',
+      },
+      formatValue: jasmine.createSpy('formatValue'),
+      getValue: jasmine.createSpy('getValue'),
+      refreshCell: jasmine.createSpy('refreshCell'),
+      node: {
+        rowIndex: 0,
+      },
+      setValue: jasmine.createSpy('setValue'),
+    } as unknown as SkyCellRendererValidatorParams;
   });
 
   it('should create an instance', () => {
-    const fixture = TestBed.createComponent(
-      SkyAgGridCellValidatorTooltipFixtureComponent,
-    );
-    fixture.componentInstance.parameters = {
-      addRenderedRowListener: NOOP,
-      column: {
-        getActualWidth(): number {
-          return -1;
-        },
-      },
-      formatValue: NOOP,
-      getValue: NOOP,
-      refreshCell: NOOP,
-      rowIndex: 0,
-      setValue: NOOP,
-    } as unknown as SkyCellRendererValidatorParams;
     fixture.detectChanges();
     expect(fixture.componentInstance).toBeTruthy();
   });
 
-  it('should toggle popover', fakeAsync(() => {
-    const fixture = TestBed.createComponent(
-      SkyAgGridCellValidatorTooltipComponent,
-    );
-
-    const parameters = {
-      addRenderedRowListener: NOOP,
-      column: {
-        getActualWidth(): number {
-          return -1;
-        },
-      },
-      formatValue: NOOP,
-      getValue: NOOP,
-      refreshCell: NOOP,
-      rowIndex: 0,
-      setValue: NOOP,
-    } as unknown as SkyCellRendererValidatorParams;
-
-    fixture.componentInstance.params = {
-      ...parameters,
+  it('should toggle popover', async () => {
+    fixture.componentInstance.parameters = {
+      ...fixture.componentInstance.parameters,
       skyComponentProperties: {
         validatorMessage: 'Test message ABC',
       },
-    };
+    } as SkyCellRendererValidatorParams;
     fixture.detectChanges();
     expect(fixture.componentInstance).toBeTruthy();
 
-    fixture.componentInstance.showPopover();
-    tick();
+    component.tooltip()?.showPopover();
+    fixture.detectChanges();
+    await fixture.whenStable();
 
-    fixture.componentInstance.params = {
-      ...parameters,
+    const loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
+    const popoverHarness = await loader.getHarness(
+      SkyPopoverContentHarness.with({
+        dataSkyId: 'validatorPopover',
+      }),
+    );
+    expect(await popoverHarness.getBodyText()).toEqual('Test message ABC');
+
+    fixture.componentInstance.parameters = {
+      ...fixture.componentInstance.parameters,
       skyComponentProperties: {
         validatorMessage: (): string => 'Test message XYZ',
       },
-    };
-    expect(fixture.componentInstance.validatorMessage).toBe('Test message XYZ');
-  }));
+    } as SkyCellRendererValidatorParams;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(await popoverHarness.getBodyText()).toEqual('Test message XYZ');
+  });
 
-  it('should show popover on delayed hover', fakeAsync(async () => {
-    const fixture = TestBed.createComponent(
-      SkyAgGridCellValidatorTooltipComponent,
-    );
-    const eventHandlers: Record<string, (event: Event) => void> = {};
-    fixture.componentInstance.params = {
-      addRenderedRowListener: NOOP,
-      column: {
-        getActualWidth(): number {
-          return -1;
-        },
+  it('should hide empty messages', async () => {
+    fixture.componentInstance.parameters = {
+      ...fixture.componentInstance.parameters,
+      skyComponentProperties: {
+        validatorMessage: '',
       },
-      eGridCell: {
-        addEventListener: (eventName: string, handler: () => void) => {
-          eventHandlers[eventName] = handler;
-        },
-      } as HTMLElement,
-      formatValue: NOOP,
-      getValue: NOOP,
-      refreshCell: NOOP,
-      rowIndex: 0,
-      setValue: NOOP,
+    } as SkyCellRendererValidatorParams;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(fixture.componentInstance).toBeTruthy();
+
+    component.tooltip()?.showPopover();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
+    await expectAsync(
+      loader.getHarness(
+        SkyPopoverContentHarness.with({
+          dataSkyId: 'validatorPopover',
+        }),
+      ),
+    ).toBeRejectedWithError(
+      /^Failed to find element matching one of the following queries/,
+    );
+  });
+
+  it('should hide messages when editing', async () => {
+    fixture.componentInstance.parameters = {
+      ...fixture.componentInstance.parameters,
       skyComponentProperties: {
         validatorMessage: 'Test message ABC',
       },
-    } as unknown as SkyCellRendererValidatorParams;
-    const popover = (): HTMLElement | null =>
-      (fixture.nativeElement as HTMLElement).ownerDocument.querySelector(
-        'sky-popover-content',
-      );
-    fixture.detectChanges();
-    expect(eventHandlers['keyup']).toBeTruthy();
-    expect(eventHandlers['mouseenter']).toBeTruthy();
-    expect(eventHandlers['mouseleave']).toBeTruthy();
-    eventHandlers['keyup']({ key: 'ArrowRight' } as KeyboardEvent);
-    tick();
+    } as SkyCellRendererValidatorParams;
+    (
+      fixture.componentInstance.parameters?.api.getEditingCells as jasmine.Spy
+    ).and.returnValue(['test']);
     fixture.detectChanges();
     await fixture.whenStable();
-    expect(popover()).toBeTruthy();
-    eventHandlers['mouseleave']({} as MouseEvent);
-    tick();
-    fixture.detectChanges();
-    tick();
+    expect(fixture.componentInstance).toBeTruthy();
+
+    component.tooltip()?.showPopover();
     fixture.detectChanges();
     await fixture.whenStable();
-    expect(popover()).toBeFalsy();
-    eventHandlers['mouseenter']({} as MouseEvent);
-    tick();
-    flush();
+
+    const loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
+    await expectAsync(
+      loader.getHarness(
+        SkyPopoverContentHarness.with({
+          dataSkyId: 'validatorPopover',
+        }),
+      ),
+    ).toBeRejectedWithError(
+      /^Failed to find element matching one of the following queries/,
+    );
+  });
+
+  it('should show messages when not valid', async () => {
+    fixture.componentInstance.parameters = {
+      ...fixture.componentInstance.parameters,
+      skyComponentProperties: {
+        validator: () => false,
+        validatorMessage: 'Test message ABC',
+      },
+    } as SkyCellRendererValidatorParams;
+    (
+      fixture.componentInstance.parameters?.api.getEditingCells as jasmine.Spy
+    ).and.returnValue([]);
     fixture.detectChanges();
     await fixture.whenStable();
-    expect(popover()).toBeTruthy();
-  }));
+    expect(fixture.componentInstance).toBeTruthy();
+
+    component.tooltip()?.showPopover();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
+    const popoverHarness = await loader.getHarness(
+      SkyPopoverContentHarness.with({
+        dataSkyId: 'validatorPopover',
+      }),
+    );
+    expect(await popoverHarness.getBodyText()).toEqual('Test message ABC');
+  });
+
+  it('should show messages when a cell is focused', async () => {
+    fixture.componentInstance.parameters = {
+      ...fixture.componentInstance.parameters,
+      skyComponentProperties: {
+        validator: () => false,
+        validatorMessage: 'Test message ABC',
+      },
+    } as SkyCellRendererValidatorParams;
+    (
+      fixture.componentInstance.parameters?.api.getEditingCells as jasmine.Spy
+    ).and.returnValue([]);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(fixture.componentInstance).toBeTruthy();
+
+    expect(
+      fixture.componentInstance.parameters.api.addEventListener,
+    ).toHaveBeenCalledTimes(2);
+    expect(
+      fixture.componentInstance.parameters.api.addEventListener,
+    ).toHaveBeenCalledWith('cellFocused', jasmine.any(Function));
+    expect(
+      fixture.componentInstance.parameters.api.addEventListener,
+    ).toHaveBeenCalledWith('cellEditingStarted', jasmine.any(Function));
+    const eventListener = (
+      fixture.componentInstance.parameters.api.addEventListener as jasmine.Spy
+    ).calls.argsFor(0)[1] as (eventParams: CellFocusedEvent) => void;
+    expect(typeof eventListener).toEqual('function');
+    eventListener({
+      column: {
+        getColId: () => 'test',
+      },
+      rowIndex: 0,
+    } as unknown as CellFocusedEvent);
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
+    const popoverHarness = await loader.getHarness(
+      SkyPopoverContentHarness.with({
+        dataSkyId: 'validatorPopover',
+      }),
+    );
+    expect(await popoverHarness.getBodyText()).toEqual('Test message ABC');
+  });
 });

apps/e2e/ag-grid-storybook/src/app/ag-grid/ag-grid-stories.component.ts
@@ -18,7 +18,12 @@ import { SkyDockLocation, SkyDockService } from '@skyux/core';
 import { FontLoadingService } from '@skyux/storybook/font-loading';
 import { SkyThemeService, SkyThemeSettings } from '@skyux/theme';
 
-import { ColDef, GridOptions, RowSelectedEvent } from 'ag-grid-community';
+import {
+  ColDef,
+  GridApi,
+  GridOptions,
+  RowSelectedEvent,
+} from 'ag-grid-community';
 import { BehaviorSubject, Observable, Subscription, combineLatest } from 'rxjs';
 import { delay, filter, map } from 'rxjs/operators';
 
@@ -64,6 +69,7 @@ export class AgGridStoriesComponent
   public skyTheme: SkyThemeSettings | undefined;
 
   readonly #gridsReady = new Map<string, Observable<boolean>>();
+  readonly #gridsApi = new Map<string, GridApi>();
   readonly #agGridService: SkyAgGridService;
   readonly #themeSvc: SkyThemeService;
   readonly #changeDetectorRef: ChangeDetectorRef;
@@ -177,6 +183,7 @@ export class AgGridStoriesComponent
           suppressHorizontalScroll: true,
           suppressRowVirtualisation: true,
           onGridReady: (params) => {
+            this.#gridsApi.set(dataSet.id, params.api);
             if (dataSet.id === 'row-delete') {
               params.api.addEventListener(
                 'rowSelected',
@@ -223,26 +230,23 @@ export class AgGridStoriesComponent
 
           setTimeout(() => {
             // Select a row to show the row delete button.
-            this.#doc
-              .querySelector(
-                /* spell-checker: disable-next-line */
-                '#row-delete .sky-ag-grid-row-killeha01 [col-id="select"] label',
-              )
-              ?.dispatchEvent(new MouseEvent('click'));
+            this.#gridsApi.get('row-delete')?.setNodesSelected({
+              nodes: [
+                this.#gridsApi.get('row-delete')!.getRowNode('killeha01')!,
+              ],
+              newValue: true,
+            });
 
             setTimeout(() => {
               // Trigger validation popover to show up.
-              this.#doc
-                .querySelector(
-                  /* spell-checker: disable-next-line */
-                  '#validation .sky-ag-grid-row-martipe02 [col-id="seasons_played"]',
-                )
-                ?.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowUp' }));
+              this.#gridsApi
+                .get('validation')
+                ?.setFocusedCell(1, 'seasons_played');
 
               // Tell Cypress we're ready.
               setTimeout(() => this.ready.next(true), 100);
-            });
-          });
+            }, 300);
+          }, 300);
         }),
     );
     if (!this.skyTheme) {

libs/components/ag-grid/src/lib/modules/ag-grid/ag-grid.service.ts
@@ -103,6 +103,7 @@ function getValidatorCellRendererSelector(component: string, fallback?: any) {
         ?.validator === 'function'
     ) {
       if (
+        params.column?.isCellEditable(params.node) ||
         !params.colDef.cellRendererParams.skyComponentProperties.validator(
           params.value,
           params.data,
@@ -345,6 +346,7 @@ export class SkyAgGridService implements OnDestroy {
 
     const validatorCellClassRules = {
       [SkyCellClass.Invalid]: getValidatorFn(),
+      [SkyCellClass.Validator]: cellClassRuleTrueExpression,
     };
 
     function getHeaderClass(

libs/components/ag-grid/src/lib/modules/ag-grid/cell-renderers/cell-renderer-currency/cell-renderer-currency-validator.component.ts
@@ -14,6 +14,11 @@ import { SkyCellRendererCurrencyParams } from '../../types/cell-renderer-currenc
  */
 @Component({
   selector: 'sky-ag-grid-cell-renderer-currency-validator',
+  styles: `
+    :host {
+      display: contents;
+    }
+  `,
   templateUrl: 'cell-renderer-currency-validator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })

libs/components/ag-grid/src/lib/modules/ag-grid/cell-renderers/cell-renderer-currency/cell-renderer-currency.component.html
@@ -1,10 +1,3 @@
-<span
-  [style.width.px]="columnWidth"
-  [style.height.px]="rowHeightWithoutBorders"
->
-  {{
-    value === undefined
-      ? undefined
-      : (value | skyNumeric: skyComponentProperties)
-  }}
-</span>
+{{
+  value === undefined ? undefined : (value | skyNumeric: skyComponentProperties)
+}}

commit c062faa1cb225266266c494346a0beffa00cee70

fix(components/ag-grid): validator cell not updating status (#3222)
[AB#3265129](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3265129)

Fixes a regression from #3194

libs/components/ag-grid/src/lib/modules/ag-grid/cell-validator/ag-grid-cell-validator-tooltip.component.html
@@ -1,20 +1,28 @@
-<div
-  skyPopoverTrigger="mouseenter"
-  class="sky-validator-cell"
-  [skyPopover]="validatorPopover"
-  [skyPopoverMessageStream]="popoverMessageStream"
->
+<ng-template #displayValue>
   <ng-content />
-  <sky-status-indicator
-    descriptionType="none"
-    indicatorType="danger"
-    class="sky-pull-right"
-  />
-</div>
-<sky-popover
-  #validatorPopover
-  data-sky-id="validatorPopover"
-  popoverType="danger"
->
-  {{ validatorMessage }}
-</sky-popover>
+</ng-template>
+
+@if (showValidatorMessage()) {
+  <div
+    skyPopoverTrigger="mouseenter"
+    class="sky-validator-cell"
+    [skyPopover]="validatorPopover"
+    [skyPopoverMessageStream]="popoverMessageStream"
+  >
+    <ng-container *ngTemplateOutlet="displayValue" />
+    <sky-status-indicator
+      descriptionType="none"
+      indicatorType="danger"
+      class="sky-pull-right"
+    />
+  </div>
+  <sky-popover
+    #validatorPopover
+    data-sky-id="validatorPopover"
+    popoverType="danger"
+  >
+    {{ validatorMessage }}
+  </sky-popover>
+} @else {
+  <ng-container *ngTemplateOutlet="displayValue" />
+}

libs/components/ag-grid/src/lib/modules/ag-grid/cell-validator/ag-grid-cell-validator-tooltip.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  signal,
+} from '@angular/core';
 import { SkyPopoverMessage, SkyPopoverMessageType } from '@skyux/popovers';
 
 import { AgColumn, CellFocusedEvent } from 'ag-grid-community';
@@ -20,25 +25,29 @@ export class SkyAgGridCellValidatorTooltipComponent {
   public set params(value: SkyCellRendererValidatorParams | undefined) {
     this.cellRendererParams = value;
 
-    value?.api?.addEventListener(
-      'cellFocused',
-      (eventParams: CellFocusedEvent) => {
-        // We want to close any popovers that are opened when other cells are focused, but open a popover if the current cell is focused.
-        if (
-          (eventParams.column as AgColumn).getColId() ===
-            value?.column?.getColId() &&
-          eventParams.rowIndex === value?.node?.rowIndex
-        ) {
-          this.showPopover();
-        } else {
-          this.hidePopover();
-        }
-      },
-    );
+    if (value?.api && !this.#listenersAdded) {
+      this.#listenersAdded = true;
 
-    value?.api?.addEventListener('cellEditingStarted', () => {
-      this.hidePopover();
-    });
+      value.api.addEventListener(
+        'cellFocused',
+        (eventParams: CellFocusedEvent) => {
+          // We want to close any popovers that are opened when other cells are focused, but open a popover if the current cell is focused.
+          if (
+            (eventParams.column as AgColumn).getColId() ===
+              value.column?.getColId() &&
+            eventParams.rowIndex === value.node?.rowIndex
+          ) {
+            this.showPopover();
+          } else {
+            this.hidePopover();
+          }
+        },
+      );
+
+      value.api.addEventListener('cellEditingStarted', () => {
+        this.hidePopover();
+      });
+    }
 
     if (typeof value?.skyComponentProperties?.validatorMessage === 'function') {
       this.validatorMessage = value.skyComponentProperties.validatorMessage(
@@ -49,11 +58,17 @@ export class SkyAgGridCellValidatorTooltipComponent {
     } else {
       this.validatorMessage = value?.skyComponentProperties?.validatorMessage;
     }
+
+    this.showValidatorMessage.set(this.#showValidatorMessage());
   }
 
   public popoverMessageStream = new Subject<SkyPopoverMessage>();
   public validatorMessage: string | undefined;
   public cellRendererParams: SkyCellRendererValidatorParams | undefined;
+
+  protected readonly showValidatorMessage = signal(false);
+
+  #listenersAdded = false;
 
   public hidePopover(): void {
     this.popoverMessageStream.next({ type: SkyPopoverMessageType.Close });
@@ -65,12 +80,8 @@ export class SkyAgGridCellValidatorTooltipComponent {
     }
   }
 
-  #shouldShowPopover(): boolean {
+  #showValidatorMessage(): boolean {
     if (!this.validatorMessage) {
-      return false;
-    }
-    const editingCells = this.cellRendererParams?.api.getEditingCells() ?? [];
-    if (editingCells.length > 0) {
       return false;
     }
     if (
@@ -85,5 +96,13 @@ export class SkyAgGridCellValidatorTooltipComponent {
     } else {
       return true;
     }
+  }
+
+  #shouldShowPopover(): boolean {
+    if (!this.showValidatorMessage()) {
+      return false;
+    }
+    const editingCells = this.cellRendererParams?.api.getEditingCells() ?? [];
+    return editingCells.length === 0;
   }
 }

libs/components/ag-grid/src/lib/modules/ag-grid/cell-validator/ag-grid-cell-validator.module.ts
@@ -1,3 +1,4 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SkyStatusIndicatorModule } from '@skyux/indicators';
 import { SkyPopoverModule } from '@skyux/popovers';
@@ -7,6 +8,6 @@ import { SkyAgGridCellValidatorTooltipComponent } from './ag-grid-cell-validator
 @NgModule({
   declarations: [SkyAgGridCellValidatorTooltipComponent],
   exports: [SkyAgGridCellValidatorTooltipComponent],
-  imports: [SkyPopoverModule, SkyStatusIndicatorModule],
+  imports: [SkyPopoverModule, SkyStatusIndicatorModule, NgTemplateOutlet],
 })
 export class SkyAgGridCellValidatorModule {}

libs/components/ag-grid/src/lib/styles/_base.scss
@@ -199,7 +199,14 @@ $modernDarkThemeCompact: map.merge($modernDarkThemeBase, $modernThemeCompact);
     height: 100%;
   }
 
-  .ag-cell.sky-ag-grid-cell-invalid {
+  .ag-cell.sky-ag-grid-cell-invalid:not(
+      .ag-cell-inline-editing,
+      .ag-cell-popup-editing
+    ),
+  .ag-cell.sky-ag-grid-cell-validator:not(.sky-ag-grid-cell-invalid):not(
+      .ag-cell-inline-editing,
+      .ag-cell-popup-editing
+    ):has(.sky-validator-cell) {
     border: 1px solid var(--sky-highlight-color-danger);
     --ag-range-selection-border-color: var(--sky-highlight-color-danger);
   }

libs/components/ag-grid/src/lib/modules/ag-grid/cell-validator/ag-grid-cell-validator-tooltip.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { expect } from '@skyux-sdk/testing';
 import { SkyPopoverContentHarness } from '@skyux/popovers/testing';
 
@@ -109,6 +110,7 @@ describe('SkyAgGridCellValidatorTooltipComponent', () => {
     fixture.componentInstance.parameters = {
       ...fixture.componentInstance.parameters,
       skyComponentProperties: {
+        validator: () => false,
         validatorMessage: 'Test message ABC',
       },
     } as SkyCellRendererValidatorParams;
@@ -118,6 +120,9 @@ describe('SkyAgGridCellValidatorTooltipComponent', () => {
     fixture.detectChanges();
     await fixture.whenStable();
     expect(fixture.componentInstance).toBeTruthy();
+    expect(
+      fixture.debugElement.query(By.css('.sky-validator-cell')),
+    ).toBeTruthy();
 
     component.tooltip()?.showPopover();
     fixture.detectChanges();
@@ -133,6 +138,22 @@ describe('SkyAgGridCellValidatorTooltipComponent', () => {
     ).toBeRejectedWithError(
       /^Failed to find element matching one of the following queries/,
     );
+  });
+
+  it('should hide messages when valid', async () => {
+    fixture.componentInstance.parameters = {
+      ...fixture.componentInstance.parameters,
+      skyComponentProperties: {
+        validator: () => true,
+        validatorMessage: 'Test message ABC',
+      },
+    } as SkyCellRendererValidatorParams;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(fixture.componentInstance).toBeTruthy();
+    expect(
+      fixture.debugElement.query(By.css('.sky-validator-cell')),
+    ).toBeFalsy();
   });
 
   it('should show messages when not valid', async () => {