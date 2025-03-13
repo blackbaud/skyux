7112c078a599cc2e8548d9df588781b78bfb6218
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-03-13 09:48:17 -0400

    basic code example

---
+++ apps/code-examples/src/app/code-examples/tabs/vertical-tabs/basic/demo.component.spec.ts
@@ -0,0 +1,43 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SkyVerticalTabsetHarness } from '@skyux/tabs/testing';
+
+import { DemoComponent } from './demo.component';
+
+fdescribe('Basic vertical tabs demo', () => {
+  async function setupTest(options: { dataSkyId?: string }): Promise<{
+    harness: SkyVerticalTabsetHarness;
+    fixture: ComponentFixture<DemoComponent>;
+  }> {
+    const fixture = TestBed.createComponent(DemoComponent);
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+
+    const harness = await loader.getHarness(
+      SkyVerticalTabsetHarness.with({ dataSkyId: options.dataSkyId }),
+    );
+
+    return { harness, fixture };
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [DemoComponent, NoopAnimationsModule],
+    });
+  });
+
+  it('should set up vertical tabs', async () => {
+    const { harness } = await setupTest({ dataSkyId: 'vertical-tabs-basic' });
+
+    const allTabs = await harness.getTabs();
+    expect(allTabs.length).toBe(3);
+
+    const activeTab = await harness.getActiveTab();
+    expect(await activeTab?.getTabHeading()).toBe('Tab 2');
+    const activeTabContent = await activeTab?.getTabContent();
+    expect(await activeTabContent?.isVisible()).toBeTrue();
+
+    const disabledTab = await harness.getTabByHeading('Tab 3');
+    expect(await disabledTab?.isDisabled()).toBeTrue();
+  });
+});
