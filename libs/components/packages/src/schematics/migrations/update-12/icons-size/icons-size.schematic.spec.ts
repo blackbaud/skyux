c3e0203994a822294cf410c0aca38255f65466c6
Trevor Burch <Trevor.Burch@blackbaud.com>
2025-03-19 12:38:03 -0400

    fix(components/packages): icon size schematic does not add extra property if size already exists and is the first property on the icon (#3264)
    [AB#3295970](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3295970)

--- libs/components/packages/src/schematics/migrations/update-12/icons-size/icons-size.schematic.spec.ts
+++ libs/components/packages/src/schematics/migrations/update-12/icons-size/icons-size.schematic.spec.ts
@@ -39,11 +39,16 @@ export class Icon3Component {}`,
 
     tree.create(
       '/icon4.component.html',
-      `<sky-icon icon="plus-circle" size="sm"></sky-icon>`,
+      `<sky-icon size="sm" icon="plus-circle"></sky-icon>`,
     );
 
     tree.create(
       '/icon5.component.html',
+      `<sky-icon variant="line" size="sm" icon="plus-circle"></sky-icon>`,
+    );
+
+    tree.create(
+      '/icon6.component.html',
       `<sky-icon iconName="eye"></sky-icon>`,
     );
 
@@ -93,18 +98,26 @@ export class Icon3Component {}`,
     );
   });
 
-  it('should handle icon which already sets the size', async () => {
+  it('should handle icon which already sets the size and is the first property', async () => {
     const { tree } = await setupTest();
 
     expect(tree.readText('/icon4.component.html')).toBe(
-      `<sky-icon icon="plus-circle" size="sm"></sky-icon>`,
+      `<sky-icon size="sm" icon="plus-circle"></sky-icon>`,
+    );
+  });
+
+  it('should handle icon which already sets the size and is not the first property', async () => {
+    const { tree } = await setupTest();
+
+    expect(tree.readText('/icon5.component.html')).toBe(
+      `<sky-icon variant="line" size="sm" icon="plus-circle"></sky-icon>`,
     );
   });
 
   it('should handle iconName missing the size', async () => {
     const { tree } = await setupTest();
 
-    expect(tree.readText('/icon5.component.html')).toBe(
+    expect(tree.readText('/icon6.component.html')).toBe(
       `<sky-icon size="md" iconName="eye"></sky-icon>`,
     );
   });
