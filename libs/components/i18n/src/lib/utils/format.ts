5f2ab8ee814a0096540557adc40de87594225127
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-03-26 12:29:40 -0400

    refactor: address warnings from TypeDoc regarding JSDoc comments

--- libs/components/i18n/src/lib/utils/format.ts
+++ libs/components/i18n/src/lib/utils/format.ts
@@ -1,9 +1,6 @@
 // Using this class instead of the one found in `@skyux/core`
 // to avoid a circular dependency.
 
-// Need to add the following to classes which contain static methods.
-// See: https://github.com/ng-packagr/ng-packagr/issues/641
-// @dynamic
 export class Format {
   public static formatText(format: string, ...args: any[]): string {
     return String(format).replace(
