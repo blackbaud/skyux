318e913cfd06cf3fa2589871e149bde036bb9340
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-03-26 17:05:52 -0400

    revert newline

--- libs/components/i18n/src/lib/utils/format.ts
+++ libs/components/i18n/src/lib/utils/format.ts
@@ -1,5 +1,6 @@
 // Using this class instead of the one found in `@skyux/core`
 // to avoid a circular dependency.
+
 export class Format {
   public static formatText(format: string, ...args: any[]): string {
     return String(format).replace(
