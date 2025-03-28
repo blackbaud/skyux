02a268825a61e2e42db9c566b7e17c2d2992726e
Trevor Burch <Trevor.Burch@blackbaud.com>
2025-03-28 14:20:24 -0400

    Add modern token check to FontLoadingService

--- libs/components/storybook/src/lib/font-loading/font-loading.service.ts
+++ libs/components/storybook/src/lib/font-loading/font-loading.service.ts
@@ -7,17 +7,28 @@ import { map } from 'rxjs/operators';
 
 const SPRITE_ID = 'sky-icon-svg-sprite';
 
-async function waitForSvgSprite(): Promise<void> {
+async function waitForSvgSpriteAndTokens(): Promise<void> {
+  let svgAdded = false;
   return await new Promise<void>((resolve) => {
     if (document.getElementById(SPRITE_ID)) {
       resolve();
     } else {
       const observer = new MutationObserver((mutations) => {
         if (
+          !svgAdded &&
           mutations.some((mutation) =>
             Array.from(mutation.addedNodes).some(
               (node) => (node as Element).id === SPRITE_ID,
             ),
+          )
+        ) {
+          svgAdded = true;
+        }
+
+        if (
+          svgAdded &&
+          getComputedStyle(document.body).getPropertyValue(
+            'modern-font-size-100',
           )
         ) {
           observer.disconnect();
@@ -36,7 +47,7 @@ async function waitForSvgSprite(): Promise<void> {
   providedIn: 'root',
 })
 export class FontLoadingService {
-  public ready(waitForSvgIcons?: boolean): Observable<boolean> {
+  public ready(waitForSvgIconsAndTokens?: boolean): Observable<boolean> {
     const fonts: FontFaceObserver[] = [
       design.text.weight.light,
       design.text.weight.regular,
@@ -65,8 +76,8 @@ export class FontLoadingService {
 
     const fontPromises = fonts.map((font) => font.load());
 
-    if (waitForSvgIcons) {
-      fontPromises.push(waitForSvgSprite());
+    if (waitForSvgIconsAndTokens) {
+      fontPromises.push(waitForSvgSpriteAndTokens());
     }
 
     return from(Promise.all(fontPromises)).pipe(map(() => true));
