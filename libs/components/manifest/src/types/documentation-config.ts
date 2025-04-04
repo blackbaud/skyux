ed66f8f97aa055e9df10e41d5f3da0a45e03142a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-04-04 12:55:13 -0400

    update documentation.json files

--- libs/components/manifest/src/types/documentation-config.ts
+++ libs/components/manifest/src/types/documentation-config.ts
@@ -1,11 +1,20 @@
 /**
  * @internal
  */
+export interface SkyManifestDocumentationGroupConfig {
+  development: { docsIds: string[]; primaryDocsId: string };
+  testing: { docsIds: string[] };
+  codeExamples: { docsIds: string[] };
+}
+
+/**
+ * @internal
+ */
 export interface SkyManifestDocumentationConfig {
   packages: Record<
     string,
     {
-      groups: Record<string, { docsIds: string[]; primaryDocsId: string }>;
+      groups: Record<string, SkyManifestDocumentationGroupConfig>;
     }
   >;
 }
