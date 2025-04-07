2c18def090608dc68e78ee6ca66196e81c756c13
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-04-07 16:29:29 -0400

    cleanup

--- libs/components/layout/testing/src/modules/text-expand/text-expand-harness-filters.ts
+++ libs/components/layout/testing/src/modules/text-expand/text-expand-harness-filters.ts
@@ -2,7 +2,6 @@ import { SkyHarnessFilters } from '@skyux/core/testing';
 
 /**
  * A set of criteria that can be used to filter a list of `SkyTextExpandHarness` instances.
- * @internal
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-empty-object-type
 export interface SkyTextExpandHarnessFilters extends SkyHarnessFilters {}
