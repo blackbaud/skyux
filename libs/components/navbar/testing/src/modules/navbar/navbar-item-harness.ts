f58cbf0c9829d203c828047ec63dc595d2a53bc8
Corey Archer <corey.archer@blackbaud.me>
2025-04-21 11:04:24 -0400

    feat(components/navbar): add navbar test harness

---
+++ libs/components/navbar/testing/src/modules/navbar/navbar-item-harness.ts
@@ -0,0 +1,30 @@
+import { HarnessPredicate } from '@angular/cdk/testing';
+import { SkyQueryableComponentHarness } from '@skyux/core/testing';
+
+import { SkyNavbarItemHarnessFilters } from './navbar-item-harness-filters';
+
+/**
+ * Harness to interact with a navbar item component in tests.
+ */
+export class SkyNavbarItemHarness extends SkyQueryableComponentHarness {
+  /**
+   * @internal
+   */
+  public static hostSelector = 'sky-navbar-item';
+
+  #getItem = this.locatorFor('.sky-navbar-item');
+
+  /**
+   * Gets a `HarnessPredicate` that can be used to search for a
+   * `SkyNavbarItemHarness` that meets certain criteria.
+   */
+  public static with(
+    filters: SkyNavbarItemHarnessFilters,
+  ): HarnessPredicate<SkyNavbarItemHarness> {
+    return SkyNavbarItemHarness.getDataSkyIdPredicate(filters);
+  }
+
+  public async isActive(): Promise<boolean> {
+    return await (await this.#getItem()).hasClass('sky-navbar-item-active');
+  }
+}
