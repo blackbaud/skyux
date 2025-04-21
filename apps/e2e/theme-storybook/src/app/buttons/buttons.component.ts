54dc286b8adac430fa30cb660aa1a260150ff766
Paul Crowder <paul.crowder@blackbaud.com>
2025-04-21 15:29:02 -0400

    feat(components/theme): add block button styles

--- apps/e2e/theme-storybook/src/app/buttons/buttons.component.ts
+++ apps/e2e/theme-storybook/src/app/buttons/buttons.component.ts
@@ -1,9 +1,12 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
+import { SkyIconModule } from '@skyux/icon';
+import { SkyFluidGridModule } from '@skyux/layout';
 
 @Component({
   selector: 'app-buttons',
+  imports: [NgTemplateOutlet, SkyFluidGridModule, SkyIconModule],
   templateUrl: './buttons.component.html',
   styleUrls: ['./buttons.component.scss'],
-  standalone: false,
 })
 export class ButtonsComponent {}
