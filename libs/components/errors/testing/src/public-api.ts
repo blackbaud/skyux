b6d9a424b90a0616076d3b6cd130a7e7369d2cba
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-04-22 13:00:54 -0400

    cleanup

--- libs/components/errors/testing/src/public-api.ts
+++ libs/components/errors/testing/src/public-api.ts
@@ -5,5 +5,3 @@ export { SkyErrorHarnessFilters } from './modules/error/error-harness-filters';
 export { SkyErrorActionHarness } from './modules/error/error-action-harness';
 export { SkyErrorImageHarness } from './modules/error/error-image-harness';
 export { SkyErrorModalHarness } from './modules/error/error-modal-harness';
-export { SkyErrorActionHarness } from './modules/error/error-action-harness';
-export { SkyErrorImageHarness } from './modules/error/error-image-harness';
