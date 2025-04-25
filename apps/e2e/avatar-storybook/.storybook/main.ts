8672f2944894bed130cc78c3050dc311f1462223
Trevor Burch <Trevor.Burch@blackbaud.com>
2025-04-25 16:08:11 -0400

    Add back eslint disable

--- apps/e2e/avatar-storybook/.storybook/main.ts
+++ apps/e2e/avatar-storybook/.storybook/main.ts
@@ -1,5 +1,6 @@
 import type { StorybookConfig } from '@storybook/types';
 
+// eslint-disable-next-line @nx/enforce-module-boundaries
 import { rootMain } from '../../../../.storybook/main';
 
 const config: StorybookConfig = {
