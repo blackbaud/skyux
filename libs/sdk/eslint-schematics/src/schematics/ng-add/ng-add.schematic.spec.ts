574c70708c8f1ac2d384e6e8addfd206712f32ad
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-04-25 08:26:53 -0400

    fix(sdk/eslint-schematics): make `ng-add` schematic idempotent

--- libs/sdk/eslint-schematics/src/schematics/ng-add/ng-add.schematic.spec.ts
+++ libs/sdk/eslint-schematics/src/schematics/ng-add/ng-add.schematic.spec.ts
@@ -349,4 +349,67 @@ module.exports = tseslint.config(
       'A compatible ESLint config file could not be found.',
     );
   });
+
+  it('should abort if config already added', async () => {
+    const { runSchematic, tree } = await setup({
+      angularEslintInstalled: true,
+    });
+
+    const expectedContents = `// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const skyux = require("skyux-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      ...skyux.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+      ...skyux.configs.templateRecommended,
+    ],
+    rules: {},
+  }
+);
+`;
+
+    await runSchematic();
+
+    expect(tree.readText('/eslint.config.js')).toEqual(expectedContents);
+
+    // Run the schematic again, to confirm change wasn't made twice.
+    await runSchematic();
+
+    expect(tree.readText('/eslint.config.js')).toEqual(expectedContents);
+  });
 });
