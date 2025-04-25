442a25b5472907eabc2e381425e53f198156882f
Trevor Burch <Trevor.Burch@blackbaud.com>
2025-04-25 15:51:41 -0400

    chore: update avatar visual tests to use static asset image instead of pulling from network

--- apps/e2e/avatar-storybook/.storybook/main.ts
+++ apps/e2e/avatar-storybook/.storybook/main.ts
@@ -1,12 +1,12 @@
 import type { StorybookConfig } from '@storybook/types';
 
-// eslint-disable-next-line @nx/enforce-module-boundaries
 import { rootMain } from '../../../../.storybook/main';
 
 const config: StorybookConfig = {
   ...rootMain,
 
   stories: ['../src/app/**/*.stories.@(js|ts)'],
+  staticDirs: ['../assets'],
 };
 
 export default config;
