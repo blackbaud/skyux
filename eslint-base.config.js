6dda92de5824dd48cfa0f72121201a4f4524136e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-04-30 13:32:01 -0400

    fix(components/docs-tools): show `?` optional indicator only for optional interface properties

--- eslint-base.config.js
+++ eslint-base.config.js
@@ -42,15 +42,5 @@ module.exports = [
       ],
     },
   },
-  {
-    files: ['**/*.json'],
-    rules: {
-      '@nx/dependency-checks': [
-        'error',
-        { ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'] },
-      ],
-    },
-    languageOptions: { parser: require('jsonc-eslint-parser') },
-  },
   cspell,
 ];
