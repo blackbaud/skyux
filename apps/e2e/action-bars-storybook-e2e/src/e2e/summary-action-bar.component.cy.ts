69279b3b4bd399b4c3899d9cd269880ca301bfc5
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-05-20 17:53:46 -0400

    e2e addition

--- apps/e2e/action-bars-storybook-e2e/src/e2e/summary-action-bar.component.cy.ts
+++ apps/e2e/action-bars-storybook-e2e/src/e2e/summary-action-bar.component.cy.ts
@@ -12,35 +12,85 @@ Cypress.on(
 describe('action-bars-storybook - summary action bar', () => {
   E2eVariations.forEachTheme((theme) => {
     describe(`in ${theme} theme`, () => {
-      ['tab', 'page', 'split-view', 'modal', 'modal-full-page'].forEach(
-        (style) => {
-          const viewportSizes = [
-            ...E2eVariations.DISPLAY_WIDTHS.map((width) => [width, 960]),
-            ...E2eVariations.MOBILE_WIDTHS.map((width) => [width, 590]),
-          ];
-          // TODO: make new array with Lg and Xs widths
-          viewportSizes.forEach(([width, height]) => {
-            describe(`at ${width}px (${style})`, () => {
-              it(`should render the component at width ${width} (${style})`, () => {
+      [
+        'tab',
+        'page',
+        'split-view',
+        'modal',
+        'modal-full-page',
+        'single-error',
+        'overflow-errors',
+        'modal-errors',
+      ].forEach((style) => {
+        const viewportSizes = [
+          ...E2eVariations.DISPLAY_WIDTHS.map((width) => [width, 960]),
+          ...E2eVariations.MOBILE_WIDTHS.map((width) => [width, 590]),
+        ];
+        // TODO: make new array with Lg and Xs widths
+        viewportSizes.forEach(([width, height]) => {
+          describe(`at ${width}px (${style})`, () => {
+            it(`should render the component at width ${width} (${style})`, () => {
+              cy.viewport(width, height).visit(
+                `/iframe.html?globals=theme:${theme}&id=summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}`,
+              );
+
+              cy.skyReady('app-summary-action-bar');
+
+              cy.get('.sky-summary-action-bar')
+                .should('exist')
+                .should('be.visible');
+
+              if (style === 'modal') {
+                cy.get('sky-modal-content')
+                  .should('exist')
+                  .should('be.visible')
+                  .click();
+              }
+
+              cy.skyVisualTest(
+                `summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}-${width}-${theme}`,
+                {
+                  width: width,
+                  capture: 'viewport',
+                },
+              );
+            });
+
+            // Mobile width only/modal specific tests
+            if (
+              E2eVariations.MOBILE_WIDTHS.includes(width) ||
+              style === 'modal'
+            ) {
+              it(`should render the component at width ${width} and with a collapsed summary (${style})`, () => {
                 cy.viewport(width, height).visit(
                   `/iframe.html?globals=theme:${theme}&id=summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}`,
                 );
 
                 cy.skyReady('app-summary-action-bar');
 
-                cy.get('.sky-summary-action-bar')
+                cy.get('.sky-summary-action-bar-details-collapse button')
                   .should('exist')
-                  .should('be.visible');
+                  .should('be.visible')
+                  .click();
 
-                if (style === 'modal') {
+                if (style.startsWith('modal')) {
                   cy.get('sky-modal-content')
+                    .should('exist')
+                    .should('be.visible')
+                    .click();
+                } else {
+                  cy.get('app-summary-action-bar')
                     .should('exist')
                     .should('be.visible')
                     .click();
                 }
 
+                cy.get('.sky-summary-action-bar')
+                  .should('exist')
+                  .should('be.visible');
+
                 cy.skyVisualTest(
-                  `summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}-${width}-${theme}`,
+                  `summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}-${width}-${theme}-collapsed-summary`,
                   {
                     width: width,
                     capture: 'viewport',
@@ -48,83 +98,40 @@ describe('action-bars-storybook - summary action bar', () => {
                 );
               });
 
-              // Mobile width only/modal specific tests
-              if (
-                E2eVariations.MOBILE_WIDTHS.includes(width) ||
-                style === 'modal'
-              ) {
-                it(`should render the component at width ${width} and with a collapsed summary (${style})`, () => {
-                  cy.viewport(width, height).visit(
-                    `/iframe.html?globals=theme:${theme}&id=summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}`,
-                  );
+              it(`should render the component at width ${width} and with and open secondary actions menu (${style})`, () => {
+                cy.viewport(width, height).visit(
+                  `/iframe.html?globals=theme:${theme}&id=summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}`,
+                );
 
-                  cy.skyReady('app-summary-action-bar');
+                cy.skyReady('app-summary-action-bar')
 
-                  cy.get('.sky-summary-action-bar-details-collapse button')
-                    .should('exist')
-                    .should('be.visible')
-                    .click();
+                  .get('.sky-summary-action-bar')
+                  .should('exist')
+                  .should('be.visible')
+                  .end()
 
-                  if (style.startsWith('modal')) {
-                    cy.get('sky-modal-content')
-                      .should('exist')
-                      .should('be.visible')
-                      .click();
-                  } else {
-                    cy.get('app-summary-action-bar')
-                      .should('exist')
-                      .should('be.visible')
-                      .click();
-                  }
+                  .get('sky-summary-action-bar-secondary-actions button')
+                  .should('exist')
+                  .should('be.visible')
+                  .click();
 
-                  cy.get('.sky-summary-action-bar')
-                    .should('exist')
-                    .should('be.visible');
-
-                  cy.skyVisualTest(
-                    `summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}-${width}-${theme}-collapsed-summary`,
-                    {
-                      width: width,
-                      capture: 'viewport',
-                    },
-                  );
-                });
-
-                it(`should render the component at width ${width} and with and open secondary actions menu (${style})`, () => {
-                  cy.viewport(width, height).visit(
-                    `/iframe.html?globals=theme:${theme}&id=summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}`,
-                  );
-
-                  cy.skyReady('app-summary-action-bar')
-
-                    .get('.sky-summary-action-bar')
-                    .should('exist')
-                    .should('be.visible')
-                    .end()
-
-                    .get('sky-summary-action-bar-secondary-actions button')
-                    .should('exist')
-                    .should('be.visible')
-                    .click();
-
-                  cy.get(
-                    'sky-dropdown-menu sky-summary-action-bar-secondary-action',
-                  )
-                    .should('exist')
-                    .should('be.visible');
-                  cy.skyVisualTest(
-                    `summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}-${width}-${theme}-open-secondary-actions`,
-                    {
-                      width: width,
-                      capture: 'viewport',
-                    },
-                  );
-                });
-              }
-            });
+                cy.get(
+                  'sky-dropdown-menu sky-summary-action-bar-secondary-action',
+                )
+                  .should('exist')
+                  .should('be.visible');
+                cy.skyVisualTest(
+                  `summaryactionbarcomponent-summaryactionbar--summary-action-bar-${style}-${width}-${theme}-open-secondary-actions`,
+                  {
+                    width: width,
+                    capture: 'viewport',
+                  },
+                );
+              });
+            }
           });
-        },
-      );
+        });
+      });
     });
   }, true);
 });
