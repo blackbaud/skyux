8aa7bc513e1142ba8aa9046a45ca7cd6d373a940
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-05-20 18:01:52 -0400

    documentation

--- libs/components/action-bars/src/lib/modules/summary-action-bar/errors/summary-action-bar-error.ts
+++ libs/components/action-bars/src/lib/modules/summary-action-bar/errors/summary-action-bar-error.ts
@@ -1,3 +1,9 @@
+/**
+ * Interface to use to display errors in SummaryActionBarComponents.
+ */
 export interface SkySummaryActionBarError {
+  /**
+   * The error message to display.
+   */
   message: string;
 }
