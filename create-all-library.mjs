1fcd985aeafa33b4a29a4336070ad8961d90065e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-05-23 16:43:23 -0400

    feat: create one package to rule them all

---
+++ create-all-library.mjs
@@ -0,0 +1,56 @@
+// import glob from 'fast-glob';
+import path from 'node:path';
+
+// const packageJsonFiles = await glob('libs/components/*/package.json');
+
+// console.log('packageJsonFiles:', packageJsonFiles);
+
+const projects = [
+  'a11y',
+  'action-bars',
+  'angular-tree-component',
+  'ag-grid',
+  'animations',
+  'avatar',
+  'assets',
+  'autonumeric',
+  'colorpicker',
+  'core',
+  'config',
+  'data-manager',
+  'datetime',
+  'errors',
+  'flyout',
+  'forms',
+  'grids',
+  'help-inline',
+  'i18n',
+  'indicators',
+  'icon',
+  'inline-form',
+  'layout',
+  'list-builder',
+  'list-builder-common',
+  'list-builder-view-checklist',
+  'lists',
+  'lookup',
+  'list-builder-view-grids',
+  'modals',
+  'navbar',
+  'pages',
+  'phone-field',
+  'popovers',
+  'progress-indicator',
+  'router',
+  'select-field',
+  'split-view',
+  'tabs',
+  'text-editor',
+  'theme',
+  'tiles',
+  'toast',
+  'validation',
+];
+
+for (const project of projects) {
+}
---
+++ libs/components/skyux/indicators/src/public_api.ts
@@ -0,0 +1,2 @@
+/* eslint-disable no-restricted-syntax */
+export * from '@skyux/indicators';
