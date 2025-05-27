1632e977b63b804a9f616159f40449b2a32fa447
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-05-27 10:45:11 -0400

    cleanup

--- libs/sdk/skyux-eslint/eslint.config.js
+++ libs/sdk/skyux-eslint/eslint.config.js
@@ -2,14 +2,4 @@ const prettier = require('eslint-config-prettier');
 const baseConfig = require('../../../eslint-base.config');
 const overrides = require('../../../eslint-overrides.config');
 
-module.exports = [
-  ...baseConfig,
-  ...overrides,
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@typescript-eslint/consistent-type-imports': 'error',
-    },
-  },
-  prettier,
-];
+module.exports = [...baseConfig, ...overrides, prettier];
