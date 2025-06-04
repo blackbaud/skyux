9ecfaa87805ea54fbd2e5d0a27f6750f72b9eb26
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-06-04 12:43:22 -0400

    remove stuff

--- libs/components/action-bars/src/lib/modules/summary-action-bar/errors/summary-action-bar-error.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Contains an object with properties for displaying form-level errors in a summary action bar.
- */
-export interface SkySummaryActionBarError {
-  /**
-   * The error message to display.
-   */
-  message: string;
-}
