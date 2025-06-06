5c7d46473f17b6236c2e1743598cb5c11adf6ffa
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-06-06 15:07:10 -0400

    pr comments

--- libs/components/code-examples/src/lib/modules/action-bars/summary-action-bar/error/example.component.ts
+++ libs/components/code-examples/src/lib/modules/action-bars/summary-action-bar/error/example.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import {
@@ -6,6 +6,8 @@ import {
   SkySummaryActionBarModule,
 } from '@skyux/action-bars';
 import { SkyKeyInfoModule } from '@skyux/indicators';
+
+import { of, switchMap } from 'rxjs';
 
 interface donationSummary {
   value: number;
@@ -22,7 +24,7 @@ interface donationSummary {
   imports: [SkyKeyInfoModule, SkySummaryActionBarModule, ReactiveFormsModule],
 })
 export class ActionBarsSummaryActionBarErrorExampleComponent {
-  public donationSummary = new FormControl<donationSummary[] | null>([
+  protected donationSummary = new FormControl<donationSummary[] | null>([
     {
       value: 250,
       label: 'Given this month',
@@ -36,70 +38,36 @@ export class ActionBarsSummaryActionBarErrorExampleComponent {
       label: 'Given all time',
     },
   ]);
-  public exampleForm = inject(FormBuilder).group({
+  protected exampleForm = inject(FormBuilder).group({
     donationSummary: this.donationSummary,
   });
 
-  public valueChanged = toSignal(
-    this.donationSummary.valueChanges.pipe(takeUntilDestroyed()),
+  protected formErrors = toSignal<SkySummaryActionBarError[] | undefined>(
+    this.donationSummary.statusChanges.pipe(
+      switchMap(() => {
+        const errors = this.donationSummary.errors;
+
+        return of(
+          errors
+            ? Object.values(errors).map((message) => ({ message }))
+            : undefined,
+        );
+      }),
+      takeUntilDestroyed(),
+    ),
   );
 
-  public formErrors = computed(() => {
-    console.log('Value changed:', this.valueChanged());
-    const errorMessages: SkySummaryActionBarError[] = [];
-    if (this.donationSummary.errors) {
-      const errors = this.donationSummary.errors;
-      Object.keys(errors).forEach((error) => {
-        errorMessages.push({
-          message: errors[error] as string,
-        });
-      });
-    }
-    return errorMessages;
-  });
-
   public onPrimaryActionClick(): void {
-    this.donationSummary.setValue([
-      {
-        value: 250,
-        label: 'Given this month',
-      },
-      {
-        value: 1000,
-        label: 'Given this year',
-      },
-      {
-        value: 1300,
-        label: 'Given all time',
-      },
-    ]);
-
-    this.donationSummary.setErrors([]);
+    alert('Primary action button clicked.');
   }
 
   protected singleError(): void {
-    this.donationSummary.setValue([
-      {
-        value: 1000,
-        label: 'Given this year',
-      },
-      {
-        value: 1300,
-        label: 'Given all time',
-      },
-    ]);
     this.donationSummary.setErrors({
       singleError: 'Monthly donation is missing.',
     });
   }
 
   protected multipleErrors(): void {
-    this.donationSummary.setValue([
-      {
-        value: 1300,
-        label: 'Given all time',
-      },
-    ]);
     this.donationSummary.setErrors({
       firstError: 'Monthly donation is missing.',
       secondError: 'Yearly donation is missing.',
