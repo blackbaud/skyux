c2c449c336bf0e65a6ebd11f45330786bc3a4465
John White <750350+johnhwhite@users.noreply.github.com>
2025-06-12 16:38:38 -0400

    ci: adjust e2e skipped job handling (#3515)

--- libs/sdk/e2e-schematics/src/workflow/verify-e2e/verify-e2e.spec.ts
+++ libs/sdk/e2e-schematics/src/workflow/verify-e2e/verify-e2e.spec.ts
@@ -115,12 +115,12 @@ describe('verify-e2e', () => {
       fetch,
     );
     expect(core.setFailed).not.toHaveBeenCalled();
-    expect(core.info).toHaveBeenCalledWith('⏭️ project1');
     expect(core.info).toHaveBeenCalledWith('E2E Visual Review passed!');
   });
 
   it('should handle missing job', async () => {
-    const { verifyE2e, core, listJobsForWorkflowRun, exit } = await setupTest();
+    const { verifyE2e, core, fs, listJobsForWorkflowRun, exit } =
+      await setupTest();
     listJobsForWorkflowRun.mockResolvedValue([
       {
         name: 'End to end tests (project1)',
@@ -132,6 +132,9 @@ describe('verify-e2e', () => {
         ],
       },
     ]);
+    fs.existsSync.mockImplementation((path: string) => {
+      return path === '/tmp/path/percy-build-project1.txt';
+    });
     await verifyE2e(
       ['project1', 'project3', 'project2'],
       '/tmp/path',
@@ -143,8 +146,14 @@ describe('verify-e2e', () => {
       fetch,
       exit,
     );
+    expect(core.warning).toHaveBeenCalledWith(
+      `🚫 project2 (missing percy build ID file)`,
+    );
+    expect(core.warning).toHaveBeenCalledWith(
+      `🚫 project3 (missing percy build ID file)`,
+    );
     expect(core.setFailed).toHaveBeenCalledWith(
-      `E2E Visual Review not complete. Missing results for:\n - project2\n - project3`,
+      `E2E Visual Review not complete.`,
     );
     expect(exit).toHaveBeenCalledWith(1);
   });
@@ -177,8 +186,8 @@ describe('verify-e2e', () => {
         project,
         state: undefined,
         approved: false,
-        removedSnapshots: [],
-      }),
+        removedSnapshots: undefined,
+      } as unknown as BuildSummary),
     );
     await verifyE2e(
       ['project1', 'project2'],
--- libs/sdk/e2e-schematics/src/workflow/verify-e2e/verify-e2e.ts
+++ libs/sdk/e2e-schematics/src/workflow/verify-e2e/verify-e2e.ts
@@ -1,7 +1,7 @@
 import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
 
-import { Fetch, checkPercyBuild } from '../percy-api/percy-api';
+import { BuildSummary, Fetch, checkPercyBuild } from '../percy-api/percy-api';
 
 interface WorkflowJob {
   name: string;
@@ -77,40 +77,46 @@ export async function verifyE2e(
       return exit(1);
     }
     const e2eSteps = listPercyWorkflowSteps(jobs);
-    const e2eStepsThatWereRun = e2eSteps.filter((step) => !step.skipped);
-    const percyProjects = e2eStepsThatWereRun.map((step) => step.project);
     const skippedPercyProjects = e2eSteps
       .filter((step) => step.skipped)
       .map((step) => step.project);
 
+    // Some e2e projects may not have re-run in this workflow run, but we still want to verify them.
+    const e2eProjectsThatWereNotSkipped = e2eProjects.filter(
+      (project) => !skippedPercyProjects.includes(project),
+    );
+
     // Log the status of each E2E Visual Review.
     core.info('E2E Visual Review results:');
     let reviewComplete = true;
+    let alertWorthy = false;
     const missingScreenshots: {
       project: string;
       removedSnapshots: string[];
     }[] = [];
-    for (const e2eStep of e2eStepsThatWereRun) {
+    for (const e2eProject of e2eProjectsThatWereNotSkipped) {
       let icon: string;
       let summary: string;
       const buildIdFile = join(
         buildIdFilesPath,
-        `percy-build-${e2eStep.project}.txt`,
+        `percy-build-${e2eProject}.txt`,
       );
       if (!existsSync(buildIdFile)) {
         reviewComplete = false;
-        core.warning(`🚫 ${e2eStep.project} (missing percy build ID file)`);
+        alertWorthy = true;
+        core.warning(`🚫 ${e2eProject} (missing percy build ID file)`);
         continue;
       }
       const buildId = readFileSync(buildIdFile, 'utf-8').trim();
       if (!buildId) {
         reviewComplete = false;
-        core.warning(`🚫 ${e2eStep.project} (empty percy build ID file)`);
+        alertWorthy = true;
+        core.warning(`🚫 ${e2eProject} (empty percy build ID file)`);
         continue;
       }
 
-      const projectStatus = await checkPercyBuild(
-        `skyux-${e2eStep.project}`,
+      const projectStatus: Partial<BuildSummary> = await checkPercyBuild(
+        `skyux-${e2eProject}`,
         buildId,
         core,
         fetchClient,
@@ -118,22 +124,17 @@ export async function verifyE2e(
       if (projectStatus?.state !== 'finished' || !projectStatus?.approved) {
         reviewComplete = false;
       }
-      if (
-        !allowMissingScreenshots &&
-        projectStatus?.removedSnapshots?.length > 0
-      ) {
+      const removedSnapshots = projectStatus?.removedSnapshots ?? [];
+      if (!allowMissingScreenshots && removedSnapshots.length > 0) {
         missingScreenshots.push({
-          project: e2eStep.project,
-          removedSnapshots: projectStatus.removedSnapshots,
+          project: e2eProject,
+          removedSnapshots,
         });
       }
       switch (true) {
-        case !allowMissingScreenshots &&
-          projectStatus?.removedSnapshots?.length > 0:
+        case !allowMissingScreenshots && removedSnapshots.length > 0:
           icon = '❌';
-          summary = `missing screenshots: ${projectStatus.removedSnapshots.join(
-            ', ',
-          )}`;
+          summary = `missing screenshots: ${removedSnapshots.join(', ')}`;
           break;
         case projectStatus?.state === 'finished' && projectStatus?.approved:
           icon = '✅';
@@ -142,6 +143,7 @@ export async function verifyE2e(
         case projectStatus?.state === 'finished' && !projectStatus?.approved:
           icon = '⚠️';
           summary = 'needs approval';
+          alertWorthy = true;
           break;
         case ['waiting', 'pending', 'processing'].includes(
           projectStatus?.state ?? '',
@@ -152,60 +154,46 @@ export async function verifyE2e(
         case projectStatus?.state === 'failed':
           icon = '❌';
           summary = 'failed';
+          alertWorthy = true;
           break;
         case typeof projectStatus?.state === 'undefined':
           icon = '🚫';
           summary = 'no Percy build found';
+          alertWorthy = true;
           break;
         default:
           icon = '❓';
           summary = `Percy state: "${projectStatus.state}"`;
+          alertWorthy = true;
       }
-      core.info(`${icon} ${e2eStep.project} (${summary})`);
+      core.info(`${icon} ${e2eProject} (${summary})`);
     }
 
-    skippedPercyProjects.forEach((project) => core.info(`⏭️ ${project}`));
-    const missingProjects = e2eProjects.filter(
-      (project) =>
-        !percyProjects.concat(skippedPercyProjects).includes(project),
-    );
-
-    if (missingProjects.length === 0) {
-      // We have a check from Percy for all E2E Visual Reviews.
-      if (missingScreenshots.length > 0) {
-        const instructions = `If this is intentional, add the 'screenshot removed' label to this PR and re-run the workflow.`;
-        const missingScreenshotMessage = `Projects with missing screenshots:\n${missingScreenshots
-          .map(
-            (result) =>
-              ` * ${result.project}\n${result.removedSnapshots
-                .map((s) => `   - ${s}\n`)
-                .join('')}`,
-          )
-          .join(`\n`)}\n${instructions}`;
-        const missingScreenshotMessageForSlack = `Projects with missing screenshots: ${missingScreenshots
-          .map(
-            (result) =>
-              ` 👉 ${result.project} (${result.removedSnapshots.join(', ')})`,
-          )
-          .join(`; `)}. ${instructions}`;
-        core.setOutput('missingScreenshots', missingScreenshotMessageForSlack);
-        core.setFailed(missingScreenshotMessage);
-        return exit(1);
-      } else if (!reviewComplete) {
-        core.setFailed(`E2E Visual Review not complete.`);
-        return exit(1);
-      } else {
-        core.info('E2E Visual Review passed!');
-      }
-    } else {
-      // We don't have a check from Percy for all E2E projects.
-      missingProjects.sort((a, b) => a.localeCompare(b));
-      core.setFailed(
-        `E2E Visual Review not complete. Missing results for:\n - ${missingProjects.join(
-          `\n - `,
-        )}`,
-      );
+    if (missingScreenshots.length > 0) {
+      const instructions = `If this is intentional, add the 'screenshot removed' label to this PR and re-run the workflow.`;
+      const missingScreenshotMessage = `Projects with missing screenshots:\n${missingScreenshots
+        .map(
+          (result) =>
+            ` * ${result.project}\n${result.removedSnapshots
+              .map((s) => `   - ${s}\n`)
+              .join('')}`,
+        )
+        .join(`\n`)}\n${instructions}`;
+      const missingScreenshotMessageForSlack = `Projects with missing screenshots: ${missingScreenshots
+        .map(
+          (result) =>
+            ` 👉 ${result.project} (${result.removedSnapshots.join(', ')})`,
+        )
+        .join(`; `)}. ${instructions}`;
+      core.setOutput('missingScreenshots', missingScreenshotMessageForSlack);
+      core.setFailed(missingScreenshotMessage);
       return exit(1);
+    } else if (!reviewComplete) {
+      core.setOutput('shouldAlertSlack', `${alertWorthy}`);
+      core.setFailed(`E2E Visual Review not complete.`);
+      return exit(1);
+    } else {
+      core.info('E2E Visual Review passed!');
     }
   } else {
     core.info('No E2E Visual Review to verify.');
