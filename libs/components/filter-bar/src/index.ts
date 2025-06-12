a3801a59a9e36c3a8c6236f66cac4d801476a676
Corey Archer <corey.archer@blackbaud.me>
2025-06-12 16:56:20 -0400

    fix index exports

--- libs/components/filter-bar/src/index.ts
+++ libs/components/filter-bar/src/index.ts
@@ -1,5 +1,5 @@
-/*
- * Public API Surface of filter-bar
- */
+export { SkyToolbarFilterModule } from './lib/modules/filter-bar/filter-bar.module';
 
-export * from './lib/modules/filter-bar/filter-bar.module';
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of public API.
+export { SkyToolbarFilterComponent as λ1 } from './lib/modules/filter-bar/toolbar-filter.component';
