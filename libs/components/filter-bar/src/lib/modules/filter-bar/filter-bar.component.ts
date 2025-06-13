008f6bfca413863c956c760d969ab29f0fec14f1
Corey Archer <corey.archer@blackbaud.me>
2025-06-13 11:39:28 -0400

    basic component structure

--- libs/components/filter-bar/src/lib/modules/filter-bar/filter-bar.component.ts
+++ libs/components/filter-bar/src/lib/modules/filter-bar/filter-bar.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, input } from '@angular/core';
 import { SkyLibResourcesService } from '@skyux/i18n';
+import { SkyIconModule } from '@skyux/icon';
 import { SkyToolbarModule } from '@skyux/layout';
 import {
   SkySelectionModalOpenArgs,
+  SkySelectionModalSearchArgs,
   SkySelectionModalSearchResult,
   SkySelectionModalService,
 } from '@skyux/lookup';
@@ -12,16 +14,24 @@ import { Observable, take } from 'rxjs';
 
 import { SkyFilterBarResourcesModule } from '../shared/sky-filter-bar-resources.module';
 
+type SelectionModalSearchAsyncFn = (
+  args: SkySelectionModalSearchArgs,
+) => Observable<SkySelectionModalSearchResult> | undefined;
+
 @Component({
   selector: 'sky-filter-bar',
-  imports: [CommonModule, SkyFilterBarResourcesModule, SkyToolbarModule],
+  imports: [
+    CommonModule,
+    SkyFilterBarResourcesModule,
+    SkyIconModule,
+    SkyToolbarModule,
+  ],
   templateUrl: './filter-bar.component.html',
   styleUrl: './filter-bar.component.scss',
 })
 export class SkyFilterBarComponent {
   public filters = input<any[] | undefined>();
-  public filterAsyncSearchFn =
-    input<Observable<SkySelectionModalSearchResult>>();
+  public filterAsyncSearchFn = input<SelectionModalSearchAsyncFn>();
 
   readonly #modalSvc = inject(SkySelectionModalService);
   readonly #resourceSvc = inject(SkyLibResourcesService);
@@ -38,19 +48,23 @@ export class SkyFilterBarComponent {
   }
 
   protected openFilters(): void {
-    const filterArgs: SkySelectionModalOpenArgs = {
-      selectionDescriptor: this.#filterPickerDescriptor,
-      descriptorProperty: 'name',
-      idProperty: 'id',
-      selectMode: 'multiple',
-      value: this.filters(),
-      searchAsync: () => this.filterAsyncSearchFn(),
-    };
-    const modalInstance = this.#modalSvc.open(filterArgs);
+    const searchFn = this.filterAsyncSearchFn();
 
-    modalInstance.closed.subscribe((closeArgs) => {
-      if (closeArgs.reason === 'save') {
-      }
-    });
+    if (searchFn) {
+      const filterArgs: SkySelectionModalOpenArgs = {
+        selectionDescriptor: this.#filterPickerDescriptor,
+        descriptorProperty: 'name',
+        idProperty: 'id',
+        selectMode: 'multiple',
+        value: this.filters(),
+        searchAsync: searchFn,
+      };
+      const modalInstance = this.#modalSvc.open(filterArgs);
+
+      // modalInstance.closed.subscribe((closeArgs) => {
+      //   if (closeArgs.reason === 'save') {
+      //   }
+      // });
+    }
   }
 }
