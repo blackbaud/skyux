ba271a7b86c406457a7b40702a6f5d431028ec86
John White <750350+johnhwhite@users.noreply.github.com>
2025-06-17 17:42:03 -0400

    Fix linting issues

--- apps/e2e/filter-bar-storybook/eslint.config.cjs
+++ apps/e2e/filter-bar-storybook/eslint.config.cjs
@@ -1,34 +1,3 @@
-const nx = require('@nx/eslint-plugin');
-const baseConfig = require('../../../eslint.config.js');
+const config = require('../../../eslint-storybook.config');
 
-module.exports = [
-  ...baseConfig,
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
-    },
-  },
-  {
-    files: ['**/*.html'],
-    // Override or add rules here
-    rules: {},
-  },
-];
+module.exports = config;
