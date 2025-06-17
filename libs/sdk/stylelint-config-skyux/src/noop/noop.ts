130c583480c970ee84bfa4885a1816816aeda7e4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-06-17 12:27:19 -0400

    feat: add `stylelint-config-skyux` package

---
+++ libs/sdk/stylelint-config-skyux/src/noop/noop.ts
@@ -0,0 +1,4 @@
+import skyux from 'skyux-stylelint';
+import scss from 'stylelint-config-recommended-scss';
+
+export { skyux, scss };
---
+++ libs/sdk/stylelint-config-skyux/vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  cacheDir: '../../../node_modules/.vite',
+  test: {
+    reporters: ['default'],
+    coverage: {
+      all: true,
+      enabled: true,
+      exclude: ['src/index.ts', 'src/**/*.test.ts', 'src/testing/**/*'],
+      include: ['src/**/*.ts'],
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: '../../../coverage/libs/sdk/stylelint-config-skyux',
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
+    },
+  },
+});
