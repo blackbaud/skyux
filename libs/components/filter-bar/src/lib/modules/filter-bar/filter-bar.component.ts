eaf1450351281552ab52efed84ad5487bfad6295
Corey Archer <corey.archer@blackbaud.me>
2025-06-30 15:13:43 -0400

    chore(components/filter-bar): add clear all values button

--- libs/components/filter-bar/src/lib/modules/filter-bar/filter-bar.component.ts
+++ libs/components/filter-bar/src/lib/modules/filter-bar/filter-bar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, input, model } from '@angular/core';
+import { Component, computed, inject, input, model } from '@angular/core';
 import { SkyLibResourcesService } from '@skyux/i18n';
 import { SkyIconModule } from '@skyux/icon';
 import { SkyToolbarModule } from '@skyux/layout';
@@ -16,6 +16,7 @@ import { SkyFilterBarResourcesModule } from '../shared/sky-filter-bar-resources.
 
 import { SkyFilterBarItemComponent } from './filter-bar-item.component';
 import { SkyFilterBarFilterItem } from './models/filter-bar-filter-item';
+import { SkyFilterBarFilterValue } from './models/filter-bar-filter-value';
 
 type SelectionModalSearchAsyncFn = (
   args: SkySelectionModalSearchArgs,
@@ -39,6 +40,12 @@ type SelectionModalSearchAsyncFn = (
 export class SkyFilterBarComponent {
   public filters = model<SkyFilterBarFilterItem[] | undefined>();
   public filterAsyncSearchFn = input<SelectionModalSearchAsyncFn>();
+
+  protected hasFilters = computed(() => {
+    const filters = this.filters();
+
+    return filters?.some((filter) => !!filter.filterValue);
+  });
 
   readonly #modalSvc = inject(SkySelectionModalService);
   readonly #resourceSvc = inject(SkyLibResourcesService);
@@ -75,6 +82,31 @@ export class SkyFilterBarComponent {
           );
         }
       });
+    }
+  }
+
+  protected updateFilters(
+    value: SkyFilterBarFilterValue | undefined,
+    filter: SkyFilterBarFilterItem,
+  ): void {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const filters = [...this.filters()!];
+    if (filters.length) {
+      const index = filters?.indexOf(filter);
+      filters[index].filterValue = value;
+
+      this.filters.set(filters);
+    }
+  }
+
+  protected clearFilters(): void {
+    const filters = this.filters()?.map((filter) => {
+      filter.filterValue = undefined;
+      return filter;
+    });
+
+    if (filters) {
+      this.filters.set(filters);
     }
   }
 }
