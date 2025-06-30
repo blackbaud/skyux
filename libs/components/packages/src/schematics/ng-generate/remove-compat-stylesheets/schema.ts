cc31a043a01aa88a1b4e8b58730b7db65e48cec9
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-06-30 14:58:24 -0400

    update packages schematics

--- libs/components/packages/src/schematics/ng-generate/remove-compat-stylesheets/schema.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Represents options passed via Angular CLI.
- */
-export interface Schema {
-  /**
-   * Remove the compat stylesheets for SKY UX versions less than this number.
-   */
-  belowVersion: number;
-}
