39e80870fe36567580d98b2a456d5f8496e40893
John White <750350+johnhwhite@users.noreply.github.com>
2025-07-07 11:44:24 -0400

    feat(components/packages): create schematic to convert definition list to description list (#3570)
    [AB#3408280](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3408280)

---
+++ libs/components/packages/src/schematics/utility/template.spec.ts
@@ -0,0 +1,50 @@
+import {
+  getElementsByTagName,
+  isParentNode,
+  parseTemplate,
+  swapTags,
+} from './template';
+
+describe('template', () => {
+  it('should parse a template string into a ParentNode', () => {
+    const template = `<div>Test.</div>`;
+    const parsed = parseTemplate(template);
+    expect(parsed).toBeDefined();
+    expect(parsed.childNodes.length).toBe(1);
+    expect(parsed.childNodes[0].nodeName).toBe('div');
+    expect(isParentNode(parsed)).toBe(true);
+    expect(getElementsByTagName('div', parsed)).toHaveLength(1);
+  });
+
+  it('should swap tags', () => {
+    const template = `<h1>Test.</h1><span>Example</span>`;
+    const parsed = parseTemplate(template);
+    expect(parsed).toBeDefined();
+    const recorder = {
+      insertLeft: jest.fn(),
+      insertRight: jest.fn(),
+      remove: jest.fn(),
+    };
+    const offset = 0;
+    swapTags(
+      template,
+      recorder,
+      offset,
+      ['h1', 'span'],
+      (position, oldTag) => {
+        const char = position === 'open' ? '<' : '</';
+        if (oldTag === 'h1') {
+          return `${char}h2>`;
+        } else if (oldTag === 'span') {
+          return `${char}p>`;
+        }
+        throw new Error(`Unknown tag: ${oldTag}`);
+      },
+      parsed,
+    );
+    expect(recorder.remove).toHaveBeenCalledWith(0, 4);
+    expect(recorder.remove).toHaveBeenCalledWith(9, 5);
+    expect(recorder.insertLeft).toHaveBeenCalledWith(0, '<h2>');
+    expect(recorder.insertLeft).toHaveBeenCalledWith(27, '</p>');
+  });
+});
