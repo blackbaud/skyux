ca2adcf602576efe32b15f6749a79cf17986bfae
Corey Archer <Corey.Archer@blackbaud.me>
2025-07-08 12:18:59 -0400

    chore(components/filter-bar): add filter bar summary component (#3592)

---
+++ libs/components/filter-bar/src/lib/modules/filter-bar/filter-bar-summary.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-filter-bar-summary',
+  imports: [CommonModule],
+  templateUrl: './filter-bar-summary.component.html',
+  styleUrl: './filter-bar-summary.component.scss',
+})
+export class SkyFilterBarSummaryComponent {}
