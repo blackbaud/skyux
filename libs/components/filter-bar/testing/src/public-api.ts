d5a3c30eb0046ea5a26a15bb3f554956cc2c3dc5
Corey Archer <Corey.Archer@blackbaud.me>
2025-07-17 13:29:38 -0400

    test(components/filter-bar): add filter bar test harnesses (#3655)

---
+++ libs/components/filter-bar/testing/src/public-api.ts
@@ -0,0 +1,4 @@
+export { SkyFilterBarHarness } from './modules/filter-bar/filter-bar-harness';
+export { SkyFilterBarHarnessFilters } from './modules/filter-bar/filter-bar-harness-filters';
+export { SkyFilterBarItemHarness } from './modules/filter-bar/filter-bar-item-harness';
+export { SkyFilterBarItemHarnessFilters } from './modules/filter-bar/filter-bar-item-harness-filters';
