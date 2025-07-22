ae9eeee346ac79ba5c472c2ce34bd07cff9f4a01
Trevor Burch <Trevor.Burch@blackbaud.com>
2025-07-22 10:46:17 -0400

    fix(components/datetime): datepicker callendar header uses standard button styles in v2 modern (#3707)
    [AB#3485780](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3485780)

--- libs/components/datetime/src/lib/modules/datepicker/calendar/datepicker-calendar-inner.component.ts
+++ libs/components/datetime/src/lib/modules/datepicker/calendar/datepicker-calendar-inner.component.ts
@@ -14,6 +14,7 @@ import {
 import { SkyLiveAnnouncerService } from '@skyux/core';
 import { SkyLibResourcesService } from '@skyux/i18n';
 import { SkyIconModule } from '@skyux/icon';
+import { SkyThemeModule } from '@skyux/theme';
 
 import { Subject, takeUntil } from 'rxjs';
 
@@ -32,7 +33,7 @@ let nextDatepickerId = 0;
  */
 @Component({
   encapsulation: ViewEncapsulation.None,
-  imports: [CommonModule, SkyIconModule],
+  imports: [CommonModule, SkyIconModule, SkyThemeModule],
   selector: 'sky-datepicker-inner',
   templateUrl: './datepicker-calendar-inner.component.html',
   styleUrl: './datepicker-calendar-inner.component.scss',
