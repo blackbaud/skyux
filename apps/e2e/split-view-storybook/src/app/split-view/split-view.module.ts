12aebba42ca5e88f2733eaa4fd46b89dd8e0fa6a
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-07-29 12:54:48 -0400

    add repeater module

--- apps/e2e/split-view-storybook/src/app/split-view/split-view.module.ts
+++ apps/e2e/split-view-storybook/src/app/split-view/split-view.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkyRepeaterModule } from '@skyux/lists';
 import { SkySplitViewModule } from '@skyux/split-view';
 
 import { SplitViewComponent } from './split-view.component';
@@ -8,7 +9,12 @@ import { SplitViewComponent } from './split-view.component';
 const routes: Routes = [{ path: '', component: SplitViewComponent }];
 @NgModule({
   declarations: [SplitViewComponent],
-  imports: [RouterModule.forChild(routes), SkySplitViewModule, CommonModule],
+  imports: [
+    RouterModule.forChild(routes),
+    SkySplitViewModule,
+    CommonModule,
+    SkyRepeaterModule,
+  ],
   exports: [SplitViewComponent],
 })
 export class SplitViewModule {}
