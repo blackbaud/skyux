2012510ec36de0689880dae076b55ae9953b481d
Corey Archer <corey.archer@blackbaud.me>
2025-07-30 11:47:47 -0400

    fix module

--- libs/components/filter-bar/src/lib/modules/filter-bar/filter-bar.module.ts
+++ libs/components/filter-bar/src/lib/modules/filter-bar/filter-bar.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import { SkyFilterBarSummaryItemComponent } from './filter-bar-summary-item.component';
-import { SkyFilterBarSummaryComponent } from './filter-bar-summary.component';
 import { SkyFilterBarComponent } from './filter-bar.component';
 
 @NgModule({
-  imports: [
-    SkyFilterBarComponent,
-    SkyFilterBarSummaryComponent,
-    SkyFilterBarSummaryItemComponent,
-  ],
-  exports: [
-    SkyFilterBarComponent,
-    SkyFilterBarSummaryComponent,
-    SkyFilterBarSummaryItemComponent,
-  ],
+  imports: [SkyFilterBarComponent],
+  exports: [SkyFilterBarComponent],
 })
 export class SkyFilterBarModule {}
