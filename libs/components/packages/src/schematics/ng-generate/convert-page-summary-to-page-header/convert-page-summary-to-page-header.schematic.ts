7d0b8bb2c0af3a85a5045ada76b3966920d4da0b
John White <750350+johnhwhite@users.noreply.github.com>
2025-08-04 15:27:18 -0400

    Remove dependence on workspace utility functions

--- libs/components/packages/src/schematics/ng-generate/convert-page-summary-to-page-header/convert-page-summary-to-page-header.schematic.ts
+++ libs/components/packages/src/schematics/ng-generate/convert-page-summary-to-page-header/convert-page-summary-to-page-header.schematic.ts
@@ -1,18 +1,20 @@
 import { Rule } from '@angular-devkit/schematics';
+import { getProjectFromWorkspace } from '@angular/cdk/schematics';
+import { getWorkspace } from '@schematics/angular/utility/workspace';
 
 import { convertPageSummaryToPageHeader } from '../../rules/convert-page-summary-to-page-header/convert-page-summary-to-page-header';
-import { getRequiredProject } from '../../utility/workspace';
 
 import { Schema } from './schema';
 
 /**
- * Converts `<sky-definition-list>` to `<sky-description-list>`.
+ * Converts `<sky-page-summary>` components to `<sky-page-header>` components.
  */
 export default function convertPageSummaryToPageHeaderSchematic(
   options: Schema,
 ): Rule {
   return async (tree) => {
-    const { project } = await getRequiredProject(tree, options.project);
+    const workspace = await getWorkspace(tree);
+    const project = getProjectFromWorkspace(workspace, options.project);
 
     return convertPageSummaryToPageHeader(project.root);
   };
