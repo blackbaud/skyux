3bbfd6a237185a1a0a0229ddb3d56e84c9de8ea2
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2025-08-13 12:44:57 -0400

    add fix for colorpicker

--- libs/components/core/src/lib/modules/affix/affixer.ts
+++ libs/components/core/src/lib/modules/affix/affixer.ts
@@ -396,30 +396,22 @@ export class SkyAffixer {
 
     const parent = this.#getAutoFitContextParent();
     let parentOffset: Required<SkyAffixOffset>;
-    if (this.#config.autoFitContext === SkyAffixAutoFitContext.OverflowParent) {
-      if (this.#config.autoFitOverflowOffset) {
-        // When the config contains a specific offset.
-        parentOffset = getElementOffset(
-          parent,
-          this.#config.autoFitOverflowOffset,
-        );
-      } else if (
-        isOffsetFullyVisibleWithinParent(this.#viewportRuler, parent, baseRect)
-      ) {
-        // When the base element is fully visible within the parent, aim for the visible portion of the parent element.
-        parentOffset = getVisibleRectForElement(this.#viewportRuler, parent);
-      } else {
-        // Anywhere in the parent element.
-        parentOffset = getOuterRect(parent);
-      }
+
+    // Use the same logic for both OverflowParent and Viewport contexts
+    if (this.#config.autoFitOverflowOffset) {
+      // When the config contains a specific offset.
+      parentOffset = getElementOffset(
+        parent,
+        this.#config.autoFitOverflowOffset,
+      );
+    } else if (
+      isOffsetFullyVisibleWithinParent(this.#viewportRuler, parent, baseRect)
+    ) {
+      // When the base element is fully visible within the parent, aim for the visible portion of the parent element.
+      parentOffset = getVisibleRectForElement(this.#viewportRuler, parent);
     } else {
-      const viewportRect = this.#viewportRuler.getViewportRect();
-      parentOffset = {
-        top: -viewportRect.top,
-        left: -viewportRect.left,
-        bottom: viewportRect.height + viewportRect.top,
-        right: viewportRect.width + viewportRect.left,
-      };
+      // Anywhere in the parent element.
+      parentOffset = getOuterRect(parent);
     }
 
     // A pixel value representing the leeway between the edge of the overflow parent and the edge
