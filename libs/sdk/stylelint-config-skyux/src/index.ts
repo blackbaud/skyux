1aea3eef40ef5e369a49626827fc4f4fc4ab0538
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2025-08-21 10:47:25 -0400

    feat(sdk/stylelint-config-skyux): add `no-ng-deep` rule to prevent usage of `::ng-deep` in stylesheets (#3853) (#3857)

--- libs/sdk/stylelint-config-skyux/src/index.ts
+++ libs/sdk/stylelint-config-skyux/src/index.ts
@@ -2,6 +2,7 @@ export default {
   extends: ['stylelint-config-recommended-scss'],
   plugins: ['skyux-stylelint'],
   rules: {
+    'skyux-stylelint/no-ng-deep': true,
     'skyux-stylelint/no-sky-selectors': true,
     'skyux-stylelint/no-static-color-values': true,
   },
