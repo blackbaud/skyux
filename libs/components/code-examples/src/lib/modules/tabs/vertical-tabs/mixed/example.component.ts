18174848cbc8e2eab6ec9d32bd5726adc71503e2
Trevor Burch <Trevor.Burch@blackbaud.com>
2025-08-22 11:18:48 -0400

    Update code example to include a disabled group

--- libs/components/code-examples/src/lib/modules/tabs/vertical-tabs/mixed/example.component.ts
+++ libs/components/code-examples/src/lib/modules/tabs/vertical-tabs/mixed/example.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SkyVerticalTabsetModule } from '@skyux/tabs';
 
+import { TabGroup } from './group';
+
 /**
  * @title Vertical tabs with grouped and ungrouped tabs
  */
@@ -10,7 +12,7 @@ import { SkyVerticalTabsetModule } from '@skyux/tabs';
   imports: [SkyVerticalTabsetModule],
 })
 export class TabsVerticalTabsMixedExampleComponent {
-  protected groupedTabs = [
+  protected groupedTabs: TabGroup[] = [
     {
       heading: 'Group 1',
       isOpen: true,
@@ -37,6 +39,21 @@ export class TabsVerticalTabsMixedExampleComponent {
         {
           tabHeading: 'Group 2 — Tab 2',
           content: 'Group 2 — Tab 2 Content',
+        },
+      ],
+    },
+    {
+      heading: 'Group 3 - disabled',
+      isOpen: false,
+      isDisabled: true,
+      subTabs: [
+        {
+          tabHeading: 'Group 3 — Tab 1',
+          content: 'Group 3 — Tab 1 Content',
+        },
+        {
+          tabHeading: 'Group 3 — Tab 2',
+          content: 'Group 3 — Tab 2 Content',
         },
       ],
     },
