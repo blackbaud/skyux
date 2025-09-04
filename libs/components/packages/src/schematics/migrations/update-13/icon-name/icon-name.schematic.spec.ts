20f4eeabefe1725652f67fbe622a0cf46a44054f
John White <750350+johnhwhite@users.noreply.github.com>
2025-09-04 11:24:43 -0400

    Update description, add one more test

--- libs/components/packages/src/schematics/migrations/update-13/icon-name/icon-name.schematic.spec.ts
+++ libs/components/packages/src/schematics/migrations/update-13/icon-name/icon-name.schematic.spec.ts
@@ -126,13 +126,13 @@ describe('icons-name.schematic', () => {
         invalidTemplate: `
       <sky-icon iconName="add" fixedWidth="true"/>
       <sky-icon icon="bogus" fixedWidth="true"/>
-      <sky-icon icon="clock-o" [fixedWidth]="true"/>
+      <sky-icon icon="clock-o" iconType="fa" [fixedWidth]="true"/>
       <sky-icon icon="code-fork" fixedWidth/>
       `,
         convertedTemplate: `
       <sky-icon iconName="add" />
       <sky-icon icon="bogus" />
-      <sky-icon iconName="clock" />
+      <sky-icon iconName="clock"  />
       <sky-icon iconName="branch-fork" />
       `,
       },
