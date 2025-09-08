fdf3cff52e16db3a56ec9e2a88e6f7de8c26bf89
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-09-08 17:11:20 -0400

    data manager

--- apps/playground/src/app/components/split-view/split-view/split-view.module.ts
+++ apps/playground/src/app/components/split-view/split-view/split-view.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SkySummaryActionBarModule } from '@skyux/action-bars';
+import {
+  SkyDataManagerModule,
+  SkyDataManagerService,
+} from '@skyux/data-manager';
 import { SkyInputBoxModule } from '@skyux/forms';
 import {
   SkyDefinitionListModule,
@@ -37,7 +41,9 @@ import { SplitViewRoutingModule } from './split-view-routing.module';
     SplitViewRoutingModule,
     LipsumModule,
     DataManagerModule,
+    SkyDataManagerModule,
   ],
+  providers: [SkyDataManagerService],
 })
 export class SplitViewModule {
   public static routes = SplitViewRoutingModule.routes;
