60e1f4f15774fe035387aee8e1eb2d58ee0978ae
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2025-09-15 11:34:36 -0400

    number

--- libs/components/ag-grid/src/lib/modules/ag-grid/types/number-properties.ts
+++ libs/components/ag-grid/src/lib/modules/ag-grid/types/number-properties.ts
@@ -1,4 +1,10 @@
 export interface SkyAgGridNumberProperties {
+  /**
+   * The maximum number the user can enter in the cell.
+   */
   max?: number;
+  /**
+   * The minimum number the user can enter in the cell.
+   */
   min?: number;
 }
