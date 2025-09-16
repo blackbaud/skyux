6d026c29a37cd5b03a7daf4786b5ee3a9bf0d04a
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-09-16 10:52:10 -0400

    viewport and barebones

--- apps/playground/src/app/components/split-view/split-view/split-view.module.ts
+++ apps/playground/src/app/components/split-view/split-view/split-view.module.ts
@@ -41,6 +41,7 @@ import { SplitViewRoutingModule } from './split-view-routing.module';
     SplitViewRoutingModule,
     LipsumModule,
     DataManagerModule,
+    SkyDataManagerModule,
   ],
   providers: [SkyDataManagerService],
 })
