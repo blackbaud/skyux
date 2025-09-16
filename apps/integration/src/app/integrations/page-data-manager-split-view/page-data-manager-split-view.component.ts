70f5e787db03c24ef3cfd513191bc4ebf383be9e
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-09-16 09:28:49 -0400

    barebones

---
+++ apps/integration/src/app/integrations/page-data-manager-split-view/page-data-manager-split-view.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-data-manager-split-view',
+  templateUrl: './page-data-manager-split-view.component.html',
+  styleUrls: ['./page-data-manager-split-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PageDataManagerSplitViewComponent {}
