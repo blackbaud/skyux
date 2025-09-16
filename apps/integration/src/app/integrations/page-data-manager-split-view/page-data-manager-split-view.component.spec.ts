9e7f20c02718de137086611c7cef5324beab651b
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-09-16 09:28:49 -0400

    barebones

---
+++ apps/integration/src/app/integrations/page-data-manager-split-view/page-data-manager-split-view.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageDataManagerSplitViewComponent } from './page-data-manager-split-view.component';
+
+describe('PageDataManagerSplitViewComponent', () => {
+  let component: PageDataManagerSplitViewComponent;
+  let fixture: ComponentFixture<PageDataManagerSplitViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PageDataManagerSplitViewComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PageDataManagerSplitViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
