de6a45e80698b5b173324f3293f62448524c2641
Sandhya Adhirvuh <sandhya.rajasabeson@blackbaud.me>
2025-09-17 10:04:19 -0400

    lets try this

--- apps/integration-e2e/src/e2e/page-data-manager-split-view.cy.ts
+++ apps/integration-e2e/src/e2e/page-data-manager-split-view.cy.ts
@@ -2,19 +2,16 @@ import { E2eVariations } from '@skyux-sdk/e2e-schematics';
 
 describe('Page Data Manager Split View', () => {
   E2eVariations.forEachTheme((theme) => {
-    beforeEach(() => {
-      cy.visit('/')
-        .skyChooseTheme(theme)
-        .get('a[href="#/integrations/page-data-manager-split-view"]')
-        .click();
-      cy.skyReady('app-page-data-manager-split-view').end();
-    });
-
     E2eVariations.DISPLAY_WIDTHS.concat(E2eVariations.MOBILE_WIDTHS).forEach(
       (width) => {
-        it(`should render the page with split view inside data manager at ${width}px`, () => {
-          cy.viewport(width, 960);
-          cy.get('h1')
+        it(`should render the page with split view inside data manager in ${theme} at ${width}px`, () => {
+          cy.viewport(width, 960)
+            .visit('/')
+            .skyChooseTheme(theme)
+            .get('a[href="#/integrations/page-data-manager-split-view"]')
+            .click();
+          cy.skyReady('app-page-data-manager-split-view')
+            .get('h1')
             .should('be.visible')
             .should('contain.text', 'Data manager with split view in page');
 
