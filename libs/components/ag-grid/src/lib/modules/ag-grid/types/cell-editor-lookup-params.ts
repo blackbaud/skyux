8699ede919d6ba9f06df4a601e75bb6055914532
Erika McVey <50454925+Blackbaud-ErikaMcVey@users.noreply.github.com>
2025-09-18 15:12:50 -0400

    fix(components/ag-grid): add JSDocs for cell editor params interfaces (#3939)
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>

--- libs/components/ag-grid/src/lib/modules/ag-grid/types/cell-editor-lookup-params.ts
+++ libs/components/ag-grid/src/lib/modules/ag-grid/types/cell-editor-lookup-params.ts
@@ -3,5 +3,8 @@ import { ICellEditorParams } from 'ag-grid-community';
 import { SkyAgGridLookupProperties } from './lookup-properties';
 
 export interface SkyCellEditorLookupParams extends ICellEditorParams {
+  /**
+   * The parameters provided to the lookup component.
+   */
   skyComponentProperties?: SkyAgGridLookupProperties;
 }
