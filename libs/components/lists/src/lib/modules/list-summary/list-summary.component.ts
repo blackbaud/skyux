65f6d17e4d67ae3a808bebca193ee039b496bacb
Corey Archer <corey.archer@blackbaud.me>
2025-10-02 14:15:08 -0400

    feat(components/lists): add list summary component

---
+++ libs/components/lists/src/lib/modules/list-summary/list-summary.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-list-summary',
+  imports: [CommonModule],
+  templateUrl: './list-summary.component.html',
+  styleUrl: './list-summary.component.scss',
+})
+export class SkyListSummaryComponent {}
