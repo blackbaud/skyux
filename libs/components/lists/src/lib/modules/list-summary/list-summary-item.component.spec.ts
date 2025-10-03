1dce47a15596320201a9a889394758a30c405438
Corey Archer <corey.archer@blackbaud.me>
2025-10-03 10:11:35 -0400

    .

--- libs/components/lists/src/lib/modules/list-summary/list-summary-item.component.spec.ts
+++ libs/components/lists/src/lib/modules/list-summary/list-summary-item.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-import { SkyListSummaryItemComponent } from './lis./list-summary-item.component
+import { SkyListSummaryItemComponent } from './list-summary-item.component';
 
 describe('Filter bar summary item component', () => {
   let component: SkyListSummaryItemComponent;
