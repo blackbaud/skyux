66a6c0c0ebc0650cbd4c80817b2491c8bbbd09e2
Corey Archer <corey.archer@blackbaud.me>
2025-10-03 10:48:17 -0400

    remove div

--- libs/components/lists/src/lib/modules/list-summary/list-summary.component.ts
+++ libs/components/lists/src/lib/modules/list-summary/list-summary.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SkyThemeComponentClassDirective } from '@skyux/theme';
 
 @Component({
   selector: 'sky-list-summary',
   templateUrl: './list-summary.component.html',
   styleUrl: './list-summary.component.scss',
+  hostDirectives: [SkyThemeComponentClassDirective],
 })
 export class SkyListSummaryComponent {}
