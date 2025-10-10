c061dd51fe474786e4ede8fd003bfbc2278eca68
John White <750350+johnhwhite@users.noreply.github.com>
2025-10-10 07:27:59 -0400

    fix(components/lookup): clicking selection modal label text selects item

--- libs/components/lookup/src/lib/modules/lookup/types/lookup-show-more-native-picker-config.ts
+++ libs/components/lookup/src/lib/modules/lookup/types/lookup-show-more-native-picker-config.ts
@@ -8,8 +8,9 @@ export interface SkyLookupShowMoreNativePickerConfig {
   /**
    * The template that formats each option in the picker. The lookup component
    * injects values into the template as `item` variables that reference all the object
-   * properties of the options. If you do not specify a template, the picker uses
-   * the same template as the dropdown list.
+   * properties of the options, and `checkboxId` when using multi-select, suitable for
+   * using with `<label [for]="checkboxId">...` to toggle selection. If you do not
+   * specify a template, the picker uses the same template as the dropdown list.
    */
   itemTemplate?: TemplateRef<unknown>;
 
