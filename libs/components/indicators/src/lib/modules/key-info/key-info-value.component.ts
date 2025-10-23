a25b524e3abc8067dfb1d3710b9043a45ecc81b3
Steve Brush <steve.brush@blackbaud.com>
2025-10-23 12:29:07 -0400

    fix(components/indicators): align key info value line-height with font class (#4046)
    [AB#3591446](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3591446)

--- libs/components/indicators/src/lib/modules/key-info/key-info-value.component.ts
+++ libs/components/indicators/src/lib/modules/key-info/key-info-value.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 
 /**
  * Specifies a value to display in larger, bold text.
  * @required
  */
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'sky-key-info-value',
+  styleUrl: './key-info-value.component.scss',
   template: '<ng-content />',
-  standalone: false,
+  // Disable view encapsulation to reduce selector specificity, making it
+  // possible for users to override styles with font classes.
+  encapsulation: ViewEncapsulation.None,
 })
 export class SkyKeyInfoValueComponent {}
