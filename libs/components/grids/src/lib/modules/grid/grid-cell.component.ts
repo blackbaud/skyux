83246762af92c31b884afdf1723701eb78c5f4db
John White <750350+johnhwhite@users.noreply.github.com>
2025-11-04 13:17:46 -0500

    Make the grids selectors distinguished from the list view grid selectors

--- libs/components/grids/src/lib/modules/grid/grid-cell.component.ts
+++ libs/components/grids/src/lib/modules/grid/grid-cell.component.ts
@@ -14,7 +14,7 @@ import { ListItemModel, getData } from '@skyux/list-builder-common';
  * @deprecated `SkyGridComponent` and its features are deprecated. We recommend using the data grid instead. For more information, see https://developer.blackbaud.com/skyux/components/data-grid
  */
 @Component({
-  selector: 'sky-grid-cell',
+  selector: 'sky-grid-cell:not(.legacy)',
   template: '<ng-template #cell />',
   styleUrls: ['./grid-cell.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
