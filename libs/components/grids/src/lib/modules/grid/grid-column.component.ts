d8472c8d41c698505839c5c15cf59929037fd34a
John White <750350+johnhwhite@users.noreply.github.com>
2025-11-04 13:17:46 -0500

    Make the grids selectors distinguished from the list view grid selectors

--- libs/components/grids/src/lib/modules/grid/grid-column.component.ts
+++ libs/components/grids/src/lib/modules/grid/grid-column.component.ts
@@ -20,7 +20,7 @@ import { SkyGridColumnInlineHelpPopoverModelChange } from './types/grid-column-i
  * @deprecated `SkyGridComponent` and its features are deprecated. We recommend using the data grid instead. For more information, see https://developer.blackbaud.com/skyux/components/data-grid
  */
 @Component({
-  selector: 'sky-grid-column',
+  selector: 'sky-grid-column:not(.legacy)',
   template: '<ng-content />',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
--- libs/components/grids/src/lib/modules/grid/grid.component.ts
+++ libs/components/grids/src/lib/modules/grid/grid.component.ts
@@ -74,7 +74,7 @@ let nextId = 0;
  * @deprecated `SkyGridComponent` and its features are deprecated. We recommend using the data grid instead. For more information, see https://developer.blackbaud.com/skyux/components/data-grid
  */
 @Component({
-  selector: 'sky-grid',
+  selector: 'sky-grid:not(.legacy)',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss'],
   viewProviders: [DragulaService],
