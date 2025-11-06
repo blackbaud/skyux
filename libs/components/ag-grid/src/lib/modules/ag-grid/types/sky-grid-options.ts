bcabffc731160f08949fcdca0b9a9739b2db7051
John White <750350+johnhwhite@users.noreply.github.com>
2025-11-06 10:40:53 -0500

    fix(components/ag-grid): maintain column types when using service grid options (#4078)

--- libs/components/ag-grid/src/lib/modules/ag-grid/types/sky-grid-options.ts
+++ libs/components/ag-grid/src/lib/modules/ag-grid/types/sky-grid-options.ts
@@ -1,10 +1,10 @@
 import { GridOptions } from 'ag-grid-community';
 
-export interface SkyGetGridOptionsArgs {
+export interface SkyGetGridOptionsArgs<T = any> {
   /**
    * The [AG Grid `gridOptions`](https://www.ag-grid.com/javascript-grid-properties/) that override default SKY UX `gridOptions`. SKY UX column types for components and column `cellClassRules` enforce required cell styling and cannot be overridden.
    */
-  gridOptions: GridOptions;
+  gridOptions: GridOptions<T>;
   /**
    * The locale for location-specific formatting, such as date values for the `SkyCellType.Date` column.
    */
