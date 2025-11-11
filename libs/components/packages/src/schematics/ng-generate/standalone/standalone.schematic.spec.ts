877fc96035e6cf7d8d151b51a0636fbc278308df
John White <750350+johnhwhite@users.noreply.github.com>
2025-11-11 11:20:14 -0500

    feat(components/packages): create schematic to convert SKY UX projects to use standalone components

---
+++ libs/components/packages/src/schematics/ng-generate/standalone/standalone.schematic.spec.ts
@@ -0,0 +1,314 @@
+import { externalSchematic } from '@angular-devkit/schematics';
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
+import {
+  SkyManifestClassDefinition,
+  SkyManifestPublicApi,
+} from '@skyux/manifest-local';
+
+import path from 'node:path';
+
+import { createTestApp } from '../../testing/scaffold';
+
+jest.mock('@angular-devkit/schematics', () => {
+  const schematics = jest.requireActual('@angular-devkit/schematics');
+  return {
+    ...schematics,
+    externalSchematic: jest.fn().mockReturnValue(jest.fn()),
+  };
+});
+jest.mock('@skyux/manifest', () => ({
+  getPublicApi: (): SkyManifestPublicApi => ({
+    packages: {
+      '@skyux/datetime': [
+        {
+          name: 'SkyDatePipeModule',
+          kind: 'module',
+        } as SkyManifestClassDefinition,
+      ],
+      '@skyux/modals': [
+        {
+          name: 'SkyModalModule',
+          kind: 'module',
+        } as SkyManifestClassDefinition,
+      ],
+      '@skyux/missing': [],
+    },
+  }),
+}));
+
+const dateTime = `import * as i0 from '@angular/core';
+
+declare class SkyDatePipe {}
+
+declare class SkyDatePipeModule {
+  static ɵfac: i0.ɵɵFactoryDeclaration<SkyDatePipeModule, never>;
+  static ɵmod: i0.ɵɵNgModuleDeclaration<SkyDatePipeModule, never, [typeof SkyDatePipe], [typeof SkyDatePipe]>;
+  static ɵinj: i0.ɵɵInjectorDeclaration<SkyDatePipeModule>;
+}
+
+export { SkyDatePipeModule, SkyDatePipe };
+`;
+
+const modals = `import * as i0 from '@angular/core';
+
+declare class SkyModalComponent {}
+declare class SkyModalContentComponent {}
+declare class SkyModalFooterComponent {}
+declare class SkyModalHeaderComponent {}
+declare class SkyModalIsDirtyDirective {}
+
+declare class SkyModalModule {
+  static ɵfac: i0.ɵɵFactoryDeclaration<SkyModalModule, never>;
+  static ɵmod: i0.ɵɵNgModuleDeclaration<SkyModalModule, never, [
+    typeof SkyModalComponent,
+    typeof SkyModalContentComponent,
+    typeof SkyModalFooterComponent,
+    typeof SkyModalHeaderComponent,
+    typeof SkyModalIsDirtyDirective
+  ], [typeof SkyModalComponent, typeof SkyModalContentComponent, typeof SkyModalFooterComponent, typeof SkyModalHeaderComponent, typeof SkyModalIsDirtyDirective]>;
+  static ɵinj: i0.ɵɵInjectorDeclaration<SkyModalModule>;
+}
+
+export { SkyModalModule, SkyModalComponent as λ5 };
+`;
+
+describe('standalone', () => {
+  const runner = new SchematicTestRunner(
+    'migrations',
+    path.join(__dirname, '../../../../collection.json'),
+  );
+
+  async function setup(): Promise<{
+    tree: UnitTestTree;
+    runSchematic: () => Promise<UnitTestTree>;
+  }> {
+    const tree = await createTestApp(runner, {
+      projectName: 'my-test-app',
+    });
+    tree.create('node_modules/@skyux/datetime/index.d.ts', dateTime);
+    tree.create('node_modules/@skyux/modals/index.d.ts', modals);
+
+    return {
+      tree,
+      runSchematic: async () =>
+        await runner.runSchematic('standalone-migration', {}, tree),
+    };
+  }
+
+  it('should migrate a standalone component to use SKY UX modules', async () => {
+    const { tree } = await setup();
+    tree.create(
+      'src/app/test.component.ts',
+      `
+    import { Component } from '@angular/core';
+    import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+    import { SkyModalComponent } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div>{{ test | async }}</div>',
+      imports: [AsyncPipe, NgTemplateOutlet, SkyModalComponent],
+    })
+    export class TestComponent {}
+    `,
+    );
+    await runner.runSchematic('standalone-migration', {}, tree);
+    expect(tree.readText('src/app/test.component.ts')).toEqual(`
+    import { Component } from '@angular/core';
+    import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+    import {  SkyModalModule } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div>{{ test | async }}</div>',
+      imports: [AsyncPipe, NgTemplateOutlet, SkyModalModule],
+    })
+    export class TestComponent {}
+    `);
+  });
+
+  it('should migrate a standalone component with a single import to use SKY UX modules', async () => {
+    const { tree } = await setup();
+    tree.create(
+      'src/app/test.component.ts',
+      `
+    import { Component } from '@angular/core';
+    import { SkyModalComponent } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div></div>',
+      imports: [SkyModalComponent],
+    })
+    export class TestComponent {}
+    `,
+    );
+    await runner.runSchematic('standalone-migration', {}, tree);
+    expect(tree.readText('src/app/test.component.ts')).toEqual(`
+    import { Component } from '@angular/core';
+    import {  SkyModalModule } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div></div>',
+      imports: [SkyModalModule],
+    })
+    export class TestComponent {}
+    `);
+  });
+
+  it('should migrate a standalone component with λ imports to use SKY UX modules', async () => {
+    const { tree } = await setup();
+    tree.create(
+      'src/app/test.component.ts',
+      `
+    import { Component } from '@angular/core';
+    import { λ5 } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div></div>',
+      imports: [λ5],
+    })
+    export class TestComponent {}
+    `,
+    );
+    await runner.runSchematic('standalone-migration', {}, tree);
+    expect(tree.readText('src/app/test.component.ts')).toEqual(`
+    import { Component } from '@angular/core';
+    import {  SkyModalModule } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div></div>',
+      imports: [SkyModalModule],
+    })
+    export class TestComponent {}
+    `);
+  });
+
+  it('should migrate a standalone component with λ and other imports to use SKY UX modules', async () => {
+    const { tree } = await setup();
+    tree.create(
+      'src/app/test.component.ts',
+      `
+    import { Component } from '@angular/core';
+    import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+    import { λ5 } from '@skyux/modals';
+
+    import { BehaviorSubject } from 'rxjs';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div>{{ test | async }}</div>',
+      imports: [λ5, AsyncPipe, NgTemplateOutlet],
+    })
+    export class TestComponent {
+      protected readonly test = new BehaviorSubject<number>(1);
+    }
+    `,
+    );
+    await runner.runSchematic('standalone-migration', {}, tree);
+    expect(tree.readText('src/app/test.component.ts')).toEqual(`
+    import { Component } from '@angular/core';
+    import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+    import {  SkyModalModule } from '@skyux/modals';
+
+    import { BehaviorSubject } from 'rxjs';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div>{{ test | async }}</div>',
+      imports: [AsyncPipe, NgTemplateOutlet, SkyModalModule],
+    })
+    export class TestComponent {
+      protected readonly test = new BehaviorSubject<number>(1);
+    }
+    `);
+  });
+
+  it('should do nothing', async () => {
+    const { tree } = await setup();
+    tree.create(
+      'src/app/test.component.ts',
+      `
+    import { Component } from '@angular/core';
+    import { SkyModalModule } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div></div>',
+      imports: [SkyModalModule],
+    })
+    export class TestComponent {}
+    `,
+    );
+    await runner.runSchematic('standalone-migration', {}, tree);
+    expect(tree.readText('src/app/test.component.ts')).toEqual(`
+    import { Component } from '@angular/core';
+    import { SkyModalModule } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div></div>',
+      imports: [SkyModalModule],
+    })
+    export class TestComponent {}
+    `);
+  });
+
+  it('should call angular standalone first', async () => {
+    const { tree } = await setup();
+    tree.create(
+      'src/app/test.component.ts',
+      `
+    import { Component } from '@angular/core';
+    import { SkyModalModule } from '@skyux/modals';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div></div>',
+      imports: [SkyModalModule],
+    })
+    export class TestComponent {}
+    `,
+    );
+    await runner.runSchematic('standalone-migration', {}, tree);
+    expect(externalSchematic).toHaveBeenCalledWith(
+      '@angular/core',
+      'standalone-migration',
+      {
+        interactive: false,
+        mode: 'convert-to-standalone',
+        path: '',
+      },
+    );
+  });
+
+  it('should handle missing install', async () => {
+    const { tree } = await setup();
+    tree.create(
+      'src/app/test.component.ts',
+      `
+    import { Component } from '@angular/core';
+    import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+    import { λ5 } from '@skyux/missing';
+
+    @Component({
+      selector: 'app-test',
+      template: '<div>{{ test | async }}</div>',
+      imports: [AsyncPipe, NgTemplateOutlet, λ5],
+    })
+    export class TestComponent {}
+    `,
+    );
+    await expect(
+      runner.runSchematic('standalone-migration', {}, tree),
+    ).rejects.toThrowError(
+      `Could not find package @skyux/missing -- please run 'npm install'.`,
+    );
+  });
+});
---
+++ libs/components/packages/src/schematics/ng-generate/standalone/standalone.schematic.ts
@@ -0,0 +1,415 @@
+import { join, normalize } from '@angular-devkit/core';
+import {
+  Rule,
+  Tree,
+  UpdateRecorder,
+  chain,
+  externalSchematic,
+} from '@angular-devkit/schematics';
+import ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
+import {
+  findNodes,
+  getDecoratorMetadata,
+  getMetadataField,
+  insertImport,
+  isImported,
+} from '@schematics/angular/utility/ast-utils';
+import { applyToUpdateRecorder } from '@schematics/angular/utility/change';
+import { SkyManifestPublicApi, getPublicApi } from '@skyux/manifest';
+
+import {
+  isImportedFromPackage,
+  parseSourceFile,
+} from '../../utility/typescript/ng-ast';
+import {
+  RemoveImportOptions,
+  removeImport,
+} from '../../utility/typescript/remove-import';
+import { visitProjectFiles } from '../../utility/visit-project-files';
+
+type NgModuleDeclaration = ts.PropertyDeclaration & {
+  type: ts.TypeReferenceNode;
+} & {
+  type: { typeArguments: ts.NodeArray<ts.TypeNode> };
+};
+
+type PackageMetadata = {
+  lambdaMap: Record<string, string>;
+  moduleData: Record<string, { exports: string[] }>;
+};
+
+interface SkyUxSymbol {
+  packageName: string;
+  sourceName: string;
+  localName: string;
+}
+
+interface SkyUxSymbolToNgModule extends SkyUxSymbol {
+  ngModule: string;
+  maintainImport: boolean;
+}
+
+function getDecoratedClassMetadata(
+  sourceFile: ts.SourceFile,
+): ts.ObjectLiteralExpression[] {
+  const metadata: ts.ObjectLiteralExpression[] = [];
+  ['NgModule', 'Component', 'Directive'].forEach((identifierName) => {
+    if (isImportedFromPackage(sourceFile, identifierName, '@angular/core')) {
+      metadata.push(
+        ...getDecoratorMetadata(
+          sourceFile,
+          identifierName,
+          '@angular/core',
+        ).filter((node) => ts.isObjectLiteralExpression(node)),
+      );
+    }
+  });
+  return metadata;
+}
+
+function getPackageTypeSourceFile(
+  tree: Tree,
+  packageName: string,
+): ts.SourceFile {
+  const filePath = join(normalize('node_modules'), packageName, 'index.d.ts');
+  if (!packageName || !tree.exists(filePath)) {
+    throw new Error(
+      `Could not find package ${packageName} -- please run 'npm install'.`,
+    );
+  }
+  return parseSourceFile(tree, filePath);
+}
+
+function getLambdaMap(sourceFile: ts.SourceFile): Record<string, string> {
+  const lambdaExports = findNodes(
+    sourceFile,
+    (node): node is ts.ExportSpecifier & { propertyName: ts.StringLiteral } =>
+      ts.isExportSpecifier(node) &&
+      ts.isIdentifier(node.name) &&
+      node.name.text.startsWith('λ') &&
+      !!node.propertyName &&
+      ts.isIdentifier(node.propertyName),
+  );
+  return Object.fromEntries(
+    lambdaExports.map((exp) => [exp.name.text, exp.propertyName.text]),
+  );
+}
+
+function getPackageNgModuleMetadata(
+  packageName: string,
+  packageSourceFile: ts.SourceFile,
+  publicApi: SkyManifestPublicApi,
+): Record<string, { exports: string[] }> {
+  const expectedModules = publicApi.packages[packageName]
+    .filter((item) => item.kind === 'module')
+    .map(({ name }) => name);
+  const findNgModuleDeclaration = (
+    member: ts.Node,
+  ): member is NgModuleDeclaration =>
+    ts.isPropertyDeclaration(member) &&
+    !!member.type &&
+    ts.isTypeReferenceNode(member.type) &&
+    member.type.getText().includes('NgModuleDeclaration') &&
+    !!member.type.typeArguments?.[0] &&
+    !!member.type.typeArguments?.[3] &&
+    expectedModules.includes(member.type.typeArguments[0].getText()) &&
+    ts.isTupleTypeNode(member.type.typeArguments[3]);
+  const packageNgModuleMetadata = findNodes(
+    packageSourceFile,
+    (
+      node,
+    ): node is ts.ClassDeclaration & {
+      members: ts.NodeArray<ts.ClassElement>;
+    } =>
+      ts.isClassDeclaration(node) && node.members.some(findNgModuleDeclaration),
+  )
+    .map((node) => {
+      const member = node.members.find(
+        findNgModuleDeclaration,
+      ) as NgModuleDeclaration;
+      const moduleName = member.type.typeArguments[0].getText();
+      const moduleExports = (
+        member.type.typeArguments[3] as ts.TupleTypeNode
+      ).elements
+        .filter(
+          (
+            element,
+          ): element is ts.TypeQueryNode & { exprName: ts.Identifier } =>
+            ts.isTypeQueryNode(element) && ts.isIdentifier(element.exprName),
+        )
+        .map((element) => element.exprName.text);
+      return [moduleName, { exports: moduleExports }];
+    })
+    .filter(Boolean) as [string, { exports: string[] }][];
+  return Object.fromEntries(packageNgModuleMetadata);
+}
+
+function getSkyuxImports(
+  sourceFile: ts.SourceFile,
+  publicApi: SkyManifestPublicApi,
+): SkyUxSymbol[] {
+  return findNodes(
+    sourceFile,
+    (
+      node,
+    ): node is ts.ImportDeclaration & {
+      importClause: { namedBindings: ts.NamedImports };
+      moduleSpecifier: ts.StringLiteral;
+    } =>
+      ts.isImportDeclaration(node) &&
+      ts.isStringLiteral(node.moduleSpecifier) &&
+      node.moduleSpecifier.text in publicApi.packages &&
+      !!node.importClause?.namedBindings &&
+      ts.isNamedImports(node.importClause.namedBindings),
+  ).flatMap((node) =>
+    node.importClause.namedBindings.elements.map((sym) => ({
+      packageName: node.moduleSpecifier.text,
+      localName: sym.name.text,
+      sourceName: (sym.propertyName ?? sym.name).text,
+    })),
+  );
+}
+
+function updateNgModuleBindings(
+  recorder: UpdateRecorder,
+  metadata: ts.ObjectLiteralExpression,
+  field: string,
+  symbolsToUpdate: SkyUxSymbolToNgModule[],
+): void {
+  const matchingProperties = getMetadataField(metadata, field);
+  const assignment = matchingProperties[0];
+  if (
+    !assignment ||
+    !ts.isPropertyAssignment(assignment) ||
+    !ts.isArrayLiteralExpression(assignment.initializer) ||
+    !assignment.initializer.elements.filter(ts.isIdentifier).length
+  ) {
+    return;
+  }
+  const identifiers = assignment.initializer.elements.filter(ts.isIdentifier);
+  const symbolsToAdd = symbolsToUpdate
+    .filter((sym) => identifiers.some(({ text }) => sym.localName === text))
+    .filter(
+      ({ ngModule }) => !identifiers.some(({ text }) => ngModule === text),
+    )
+    .filter(
+      (sym, index, arr) =>
+        arr.findIndex(
+          (m) =>
+            m.packageName === sym.packageName && m.ngModule === sym.ngModule,
+        ) === index,
+    );
+  const symbolsToRemove = identifiers.filter(({ text }) =>
+    symbolsToUpdate.some((sym) => sym.localName === text),
+  );
+  const array = Array.from(assignment.initializer.elements.values());
+  const firstIndexThatStays = array.findIndex(
+    (identifier) =>
+      !ts.isIdentifier(identifier) ||
+      !symbolsToRemove.some(({ text }) => text === identifier.text),
+  );
+  if (firstIndexThatStays === -1) {
+    recorder.remove(
+      assignment.initializer.getStart(),
+      assignment.initializer.getWidth(),
+    );
+    recorder.insertLeft(
+      assignment.initializer.getStart(),
+      `[${symbolsToAdd.map(({ ngModule }) => ngModule).join(', ')}]`,
+    );
+  } else {
+    const spaceBetween = assignment.initializer
+      .getText()
+      .slice(
+        array[0].getEnd() - assignment.initializer.getStart(),
+        array[1].getStart() - assignment.initializer.getStart(),
+      );
+    symbolsToRemove.forEach((identifier) => {
+      const index = array.indexOf(identifier);
+      const start =
+        index > firstIndexThatStays
+          ? array[index - 1].getEnd()
+          : identifier.getStart();
+      const width =
+        (index > firstIndexThatStays
+          ? identifier.getEnd()
+          : array[index + 1].getStart()) - start;
+      recorder.remove(start, width);
+    });
+    symbolsToAdd.forEach((imp) => {
+      recorder.insertRight(
+        array[array.length - 1].getEnd(),
+        `${spaceBetween}${imp.ngModule}`,
+      );
+    });
+  }
+}
+
+function getSymbolsToUpdate(
+  skyuxImports: SkyUxSymbol[],
+  packageMetadata: Record<string, PackageMetadata>,
+  sourceFile: ts.SourceFile,
+  decoratedClasses: ts.ObjectLiteralExpression[],
+): SkyUxSymbolToNgModule[] {
+  const lastImport = findNodes(
+    sourceFile,
+    ts.isImportDeclaration,
+  ).pop() as ts.ImportDeclaration;
+  return skyuxImports
+    .filter(
+      (imp) =>
+        imp.sourceName in packageMetadata[imp.packageName].lambdaMap ||
+        (!(imp.sourceName in packageMetadata[imp.packageName].moduleData) &&
+          Object.values(packageMetadata[imp.packageName].moduleData).some(
+            (mod) => mod.exports.includes(imp.sourceName),
+          )),
+    )
+    .map((imp): SkyUxSymbolToNgModule => {
+      const isLambda =
+        imp.sourceName in packageMetadata[imp.packageName].lambdaMap;
+      const sourceName = isLambda
+        ? packageMetadata[imp.packageName].lambdaMap[imp.sourceName]
+        : imp.sourceName;
+      const ngModule = Object.entries(
+        packageMetadata[imp.packageName].moduleData,
+      ).find(([, mod]) => mod.exports.includes(sourceName))?.[0] as string;
+      return {
+        ...imp,
+        ngModule,
+        maintainImport:
+          !isLambda &&
+          findNodes(
+            sourceFile,
+            (node): node is ts.Identifier =>
+              ts.isIdentifier(node) &&
+              node.text === sourceName &&
+              node.getStart() > lastImport.getEnd() &&
+              !decoratedClasses.some(
+                (metadata) =>
+                  node.getStart() > metadata.getStart() &&
+                  node.getEnd() < metadata.getEnd(),
+              ),
+          ).length > 0,
+      };
+    });
+}
+
+function useSkyUxModules(tree: Tree): void {
+  const publicApi = getPublicApi();
+  const packageMetadata: Record<string, PackageMetadata> = {};
+  visitProjectFiles(tree, '', (file) => {
+    if (file.endsWith('.ts')) {
+      let sourceFile = parseSourceFile(tree, file);
+      const skyuxImports = getSkyuxImports(sourceFile, publicApi);
+      if (skyuxImports.length) {
+        // Preload package data that was not already loaded.
+        skyuxImports
+          .map((imp) => imp.packageName)
+          .filter((packageName) => !(packageName in packageMetadata))
+          .forEach((packageName) => {
+            const sourceFile = getPackageTypeSourceFile(tree, packageName);
+            packageMetadata[packageName] = {
+              moduleData: getPackageNgModuleMetadata(
+                packageName,
+                sourceFile,
+                publicApi,
+              ),
+              lambdaMap: getLambdaMap(sourceFile),
+            };
+          });
+
+        const decoratedClasses = getDecoratedClassMetadata(sourceFile);
+
+        // Find the lambda imports and components that should be loaded via modules.
+        const symbolsToUpdate = getSymbolsToUpdate(
+          skyuxImports,
+          packageMetadata,
+          sourceFile,
+          decoratedClasses,
+        );
+
+        if (symbolsToUpdate.length === 0) {
+          return;
+        }
+
+        // Add new imports.
+        let recorder = tree.beginUpdate(file);
+        symbolsToUpdate
+          .filter(
+            ({ ngModule, packageName }, index, modulesNeeded) =>
+              modulesNeeded.findIndex(
+                (m) => m.ngModule === ngModule && m.packageName === packageName,
+              ) === index && !isImported(sourceFile, ngModule, packageName),
+          )
+          .forEach((imp) => {
+            applyToUpdateRecorder(recorder, [
+              insertImport(sourceFile, file, imp.ngModule, imp.packageName),
+            ]);
+          });
+
+        decoratedClasses.forEach((metadata) => {
+          updateNgModuleBindings(
+            recorder,
+            metadata,
+            'imports',
+            symbolsToUpdate,
+          );
+          updateNgModuleBindings(
+            recorder,
+            metadata,
+            'exports',
+            symbolsToUpdate,
+          );
+        });
+        tree.commitUpdate(recorder);
+        sourceFile = parseSourceFile(tree, file);
+        recorder = tree.beginUpdate(file);
+
+        const removeImports = [
+          ...new Set(
+            symbolsToUpdate
+              .filter((sym) => !sym.maintainImport)
+              .map(({ packageName }) => packageName),
+          ),
+        ].map(
+          (moduleName): RemoveImportOptions => ({
+            classNames: symbolsToUpdate
+              .filter(
+                (sym) => sym.packageName === moduleName && !sym.maintainImport,
+              )
+              .map((sym) => sym.localName),
+            moduleName,
+          }),
+        );
+        removeImports.forEach((removeImportOptions) => {
+          removeImport(recorder, sourceFile, removeImportOptions);
+        });
+
+        tree.commitUpdate(recorder);
+      }
+    }
+  });
+}
+
+export default function standaloneSchematic(): Rule {
+  return chain([
+    externalSchematic('@angular/core', 'standalone-migration', {
+      interactive: false,
+      mode: 'convert-to-standalone',
+      path: '',
+    }),
+    useSkyUxModules,
+    externalSchematic('@angular/core', 'standalone-migration', {
+      interactive: false,
+      mode: 'prune-ng-modules',
+      path: '',
+    }),
+    externalSchematic('@angular/core', 'cleanup-unused-imports', {
+      interactive: false,
+    }),
+    externalSchematic('@angular/core', 'route-lazy-loading-migration', {
+      interactive: false,
+      path: '',
+    }),
+  ]);
+}
