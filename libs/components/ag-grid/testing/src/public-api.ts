e5ea2e9dd506bf711e3fc1b70c04fdbf81162427
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2026-01-13 14:09:16 -0500

    fix ag grid

--- libs/components/ag-grid/testing/src/public-api.ts
+++ libs/components/ag-grid/testing/src/public-api.ts
@@ -1,2 +1,2 @@
 export { SkyAgGridWrapperHarness } from './modules/ag-grid-wrapper/ag-grid-wrapper-harness';
-export { SkyAgGridWrapperHarnessFilters } from './modules/ag-grid-wrapper/ag-grid-wrapper-harness.filters';
+export type { SkyAgGridWrapperHarnessFilters } from './modules/ag-grid-wrapper/ag-grid-wrapper-harness.filters';
