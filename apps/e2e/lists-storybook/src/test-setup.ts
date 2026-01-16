c901349c160f12fa68cdd49c520713f955c21007
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2026-01-16 09:12:25 -0500

    remove jest configs

--- apps/e2e/lists-storybook/src/test-setup.ts
+++
@@ -1,6 +0,0 @@
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
-import 'resize-observer-polyfill';
-
-setupZoneTestEnv();
-
-// jsdom (used by Jest) doesn't natively support ResizeObserver, so use a polyfill.
