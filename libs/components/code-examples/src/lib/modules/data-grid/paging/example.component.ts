88393efb363f209c2aeace279261cb633293b7c4
John White <750350+johnhwhite@users.noreply.github.com>
2026-01-28 13:11:42 -0500

    Conditionally use router in the paging example

--- libs/components/code-examples/src/lib/modules/data-grid/paging/example.component.ts
+++ libs/components/code-examples/src/lib/modules/data-grid/paging/example.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+  numberAttribute,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { SkyDataGridModule } from '@skyux/data-grid';
 
 import { DATA_GRID_DEMO_DATA } from './data';
@@ -14,4 +21,13 @@ import { DATA_GRID_DEMO_DATA } from './data';
 })
 export class DataGridPagingComponent {
   protected readonly data = DATA_GRID_DEMO_DATA;
+
+  // When running this example as its own SPA, this parameter is set by calling `provideRouter` with `withComponentInputBinding`.
+  public readonly page = input<number, unknown>(1, {
+    transform: numberAttribute,
+  });
+
+  // For demo purposes, only use the query string if we're running the demo in its own SPA as a route and not on the documentation site.
+  protected readonly pageQueryParam =
+    inject(ActivatedRoute).component === DataGridPagingComponent ? 'page' : '';
 }
