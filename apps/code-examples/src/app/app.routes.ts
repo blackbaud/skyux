1da5bcf3e12331dbd18921b86a6ea741afa1ae18
John White <750350+johnhwhite@users.noreply.github.com>
2026-01-29 17:09:14 -0500

    build: create routes for code examples (#4173)
    Create a route for every code example, including ones that don't run in
    the docs site like
    [PagesPageDataManagerSplitViewFitLayoutExampleComponent](https://blackbaud.github.io/skyux-pr-preview/4173/code-examples-playground/#/examples/PagesPageDataManagerSplitViewFitLayoutExampleComponent).

--- apps/code-examples/src/app/app.routes.ts
+++ apps/code-examples/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { routes as CodeExampleRoutes } from '@skyux/code-examples/routes';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./home/home.component'),
     pathMatch: 'full',
+  },
+  {
+    path: 'examples',
+    children: CodeExampleRoutes,
   },
 ];
