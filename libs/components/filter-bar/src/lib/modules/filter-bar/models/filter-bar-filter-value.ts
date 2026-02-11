88280748e5b701c1fcf525cb693d971046bb588d
Corey Archer <corey.archer@blackbaud.me>
2026-02-11 09:55:31 -0500

    format

--- libs/components/filter-bar/src/lib/modules/filter-bar/models/filter-bar-filter-value.ts
+++ libs/components/filter-bar/src/lib/modules/filter-bar/models/filter-bar-filter-value.ts
@@ -5,5 +5,6 @@ import { SkyFilterStateFilterValue } from '@skyux/lists';
  * @typeParam TValue - The type of the filter value. Defaults to `unknown` for backward compatibility.
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-empty-interface
-export interface SkyFilterBarFilterValue<TValue = unknown>
-  extends SkyFilterStateFilterValue<TValue> {}
+export interface SkyFilterBarFilterValue<
+  TValue = unknown,
+> extends SkyFilterStateFilterValue<TValue> {}
