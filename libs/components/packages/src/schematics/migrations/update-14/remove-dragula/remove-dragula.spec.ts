38efd4d1a6e70be2ee2c4a8efe43cb62b1b08da2
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2026-02-24 12:53:27 -0500

    more tests

--- libs/components/packages/src/schematics/migrations/update-14/remove-dragula/remove-dragula.spec.ts
+++ libs/components/packages/src/schematics/migrations/update-14/remove-dragula/remove-dragula.spec.ts
@@ -119,4 +119,117 @@ export class AutoscrollerComponent {
 
     await expect(runSchematic()).resolves.toBeInstanceOf(UnitTestTree);
   });
+
+  describe('ng2-dragula overrides', () => {
+    it('should remove ng2-dragula override when package is removed', async () => {
+      const { runSchematic, tree } = await setup();
+
+      const packageJson = JSON.parse(tree.readText('/package.json'));
+      packageJson.dependencies['ng2-dragula'] = '2.0.0';
+      packageJson.overrides = {
+        'ng2-dragula@2.0.0': {
+          '@angular/animations': '>=21.0.0',
+          '@angular/core': '>=21.0.0',
+          '@angular/common': '>=21.0.0',
+        },
+      };
+      tree.overwrite('/package.json', JSON.stringify(packageJson, null, 2));
+
+      const updatedTree = await runSchematic();
+
+      const updatedPackageJson = JSON.parse(
+        updatedTree.readText('/package.json'),
+      );
+
+      expect(updatedPackageJson.dependencies['ng2-dragula']).toBeUndefined();
+      expect(updatedPackageJson.overrides).toBeUndefined();
+    });
+
+    it('should remove ng2-dragula override without version', async () => {
+      const { runSchematic, tree } = await setup();
+
+      const packageJson = JSON.parse(tree.readText('/package.json'));
+      packageJson.dependencies['ng2-dragula'] = '2.0.0';
+      packageJson.overrides = {
+        'ng2-dragula': {
+          '@angular/core': '>=21.0.0',
+        },
+      };
+      tree.overwrite('/package.json', JSON.stringify(packageJson, null, 2));
+
+      const updatedTree = await runSchematic();
+
+      const updatedPackageJson = JSON.parse(
+        updatedTree.readText('/package.json'),
+      );
+
+      expect(updatedPackageJson.dependencies['ng2-dragula']).toBeUndefined();
+      expect(updatedPackageJson.overrides).toBeUndefined();
+    });
+
+    it('should keep overrides section when other packages have overrides', async () => {
+      const { runSchematic, tree } = await setup();
+
+      const packageJson = JSON.parse(tree.readText('/package.json'));
+      packageJson.dependencies['ng2-dragula'] = '2.0.0';
+      packageJson.overrides = {
+        'ng2-dragula@2.0.0': {
+          '@angular/core': '>=21.0.0',
+        },
+        'other-package@1.0.0': {
+          '@angular/common': '>=21.0.0',
+        },
+      };
+      tree.overwrite('/package.json', JSON.stringify(packageJson, null, 2));
+
+      const updatedTree = await runSchematic();
+
+      const updatedPackageJson = JSON.parse(
+        updatedTree.readText('/package.json'),
+      );
+
+      expect(updatedPackageJson.dependencies['ng2-dragula']).toBeUndefined();
+      expect(updatedPackageJson.overrides).toBeDefined();
+      expect(updatedPackageJson.overrides['ng2-dragula@2.0.0']).toBeUndefined();
+      expect(updatedPackageJson.overrides['other-package@1.0.0']).toBeDefined();
+    });
+
+    it('should keep ng2-dragula override when package is still in use', async () => {
+      const { runSchematic, tree } = await setup();
+
+      const packageJson = JSON.parse(tree.readText('/package.json'));
+      packageJson.dependencies['ng2-dragula'] = '2.0.0';
+      packageJson.overrides = {
+        'ng2-dragula@2.0.0': {
+          '@angular/core': '>=21.0.0',
+        },
+      };
+      tree.overwrite('/package.json', JSON.stringify(packageJson, null, 2));
+
+      // Create a file that imports ng2-dragula
+      tree.create(
+        '/src/app/dragula.component.ts',
+        `import { DragulaService } from 'ng2-dragula';`,
+      );
+
+      const updatedTree = await runSchematic();
+
+      const updatedPackageJson = JSON.parse(
+        updatedTree.readText('/package.json'),
+      );
+
+      expect(updatedPackageJson.dependencies['ng2-dragula']).toBe('2.0.0');
+      expect(updatedPackageJson.overrides['ng2-dragula@2.0.0']).toBeDefined();
+    });
+
+    it('should not fail when package.json has no overrides', async () => {
+      const { runSchematic, tree } = await setup();
+
+      const packageJson = JSON.parse(tree.readText('/package.json'));
+      packageJson.dependencies['ng2-dragula'] = '2.0.0';
+      tree.overwrite('/package.json', JSON.stringify(packageJson, null, 2));
+
+      await expect(runSchematic()).resolves.toBeInstanceOf(UnitTestTree);
+    });
+  });
 });
--- libs/components/packages/src/schematics/migrations/update-14/remove-dragula/remove-dragula.ts
+++ libs/components/packages/src/schematics/migrations/update-14/remove-dragula/remove-dragula.ts
@@ -1,14 +1,81 @@
-import { Rule } from '@angular-devkit/schematics';
+import { Rule, Tree, chain } from '@angular-devkit/schematics';
+import { getDependency } from '@schematics/angular/utility/dependency';
 
 import { removeUnusedDependencies } from '../../../rules/remove-unused-dependencies';
+import { JsonFile } from '../../../utility/json-file';
+
+interface PackageJson {
+  overrides?: Record<string, unknown>;
+  [key: string]: unknown;
+}
+
+const NG2_DRAGULA = 'ng2-dragula';
 
 /**
  * Remove dragula packages if they are not being used.
  */
 export default function (): Rule {
-  return removeUnusedDependencies([
-    'dragula',
-    'ng2-dragula',
-    'dom-autoscroller',
+  return chain([
+    removeUnusedDependencies(['dragula', NG2_DRAGULA, 'dom-autoscroller']),
+    removeNg2DragulaOverrides(),
   ]);
+}
+
+/**
+ * Checks if an override key is for ng2-dragula (handles "ng2-dragula" or "ng2-dragula@version").
+ */
+function isNg2DragulaOverride(overrideKey: string): boolean {
+  return overrideKey.startsWith(NG2_DRAGULA);
+}
+
+/**
+ * Removes ng2-dragula package overrides from package.json if ng2-dragula
+ * is not present in dependencies or devDependencies.
+ * This handles override keys like "ng2-dragula@version".
+ */
+function removeNg2DragulaOverrides(): Rule {
+  return (tree: Tree) => {
+    const packageJsonPath = '/package.json';
+
+    /* v8 ignore next 3 -- @preserve */
+    if (!tree.exists(packageJsonPath)) {
+      return tree;
+    }
+
+    // Don't remove overrides if the package is still in use
+    if (getDependency(tree, NG2_DRAGULA)) {
+      return tree;
+    }
+
+    const jsonFile = new JsonFile(tree, packageJsonPath);
+    const packageJson = jsonFile.get([]) as PackageJson;
+
+    if (!packageJson.overrides || typeof packageJson.overrides !== 'object') {
+      return tree;
+    }
+
+    let hasChanges = false;
+    const overrideKeys = Object.keys(packageJson.overrides);
+
+    for (const overrideKey of overrideKeys) {
+      if (isNg2DragulaOverride(overrideKey)) {
+        delete packageJson.overrides[overrideKey];
+        hasChanges = true;
+      }
+    }
+
+    // If overrides is now empty, remove the entire overrides section
+    if (Object.keys(packageJson.overrides).length === 0) {
+      delete packageJson.overrides;
+    }
+
+    if (hasChanges) {
+      tree.overwrite(
+        packageJsonPath,
+        JSON.stringify(packageJson, null, '\t') + '\n',
+      );
+    }
+
+    return tree;
+  };
 }
