3d7f24c0c111735dea427e5ed255c2f5803eff07
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2026-02-28 12:22:48 -0500

    cleanup

--- libs/components/theme/src/lib/animations/animations-enabled.ts
+++ libs/components/theme/src/lib/animations/animations-enabled.ts
@@ -1,13 +1,13 @@
 /**
- * Returns `true` if CSS transitions are enabled on the given element based on
- * the computed value of `--sky-theme-animations-transition-duration`. The
- * value is set to `none` when animations are globally disabled (e.g. via the
- * `sky-theme-animations-disabled` class on the document body).
+ * Returns `true` if CSS animations and transitions are enabled on the given
+ * element based on the computed value of `--sky-theme-animations-duration`.
+ * The value is set to `0s` when animations are globally disabled (e.g. via
+ * the `sky-theme-animations-disabled` class on the document body).
  */
 export function skyAnimationsEnabled(element: Element): boolean {
   return (
     getComputedStyle(element)
-      .getPropertyValue('--sky-theme-animations-transition-duration')
-      .trim() !== 'none'
+      .getPropertyValue('--sky-theme-animations-duration')
+      .trim() !== '0s'
   );
 }
