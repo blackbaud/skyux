b9228ce0f30cd8cc782c52e383ad9ddb8cb630c2
John White <750350+johnhwhite@users.noreply.github.com>
2026-03-11 18:16:15 -0400

    fix(components/lookup): update search background when using expand mode fit

--- apps/e2e/lookup-storybook/src/app/search/search.module.ts
+++ apps/e2e/lookup-storybook/src/app/search/search.module.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkyToolbarModule } from '@skyux/layout';
 import { SkySearchModule } from '@skyux/lookup';
+import { SkyPageModule } from '@skyux/pages';
 
 import { SearchComponent } from './search.component';
 
 const routes: Routes = [{ path: '', component: SearchComponent }];
 @NgModule({
   declarations: [SearchComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), SkySearchModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SkyPageModule,
+    SkySearchModule,
+    SkyToolbarModule,
+  ],
   exports: [SearchComponent],
 })
 export class SearchModule {}
