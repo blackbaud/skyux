a4e96e38e39152c4fdd81a4605468427eb9791b2
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2026-03-18 10:23:36 -0400

    fix(components/layout)!: replace `@angular/animations` in text expand repeater component with CSS transitions

--- libs/components/layout/src/lib/modules/text-expand-repeater/text-expand-repeater.module.ts
+++ libs/components/layout/src/lib/modules/text-expand-repeater/text-expand-repeater.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { _SkyTransitionEndHandlerDirective } from '@skyux/core';
 
 import { SkyLayoutResourcesModule } from '../shared/sky-layout-resources.module';
 
@@ -7,7 +8,11 @@ import { SkyTextExpandRepeaterComponent } from './text-expand-repeater.component
 
 @NgModule({
   declarations: [SkyTextExpandRepeaterComponent],
-  imports: [SkyLayoutResourcesModule, CommonModule],
+  imports: [
+    SkyLayoutResourcesModule,
+    CommonModule,
+    _SkyTransitionEndHandlerDirective,
+  ],
   exports: [SkyTextExpandRepeaterComponent],
 })
 export class SkyTextExpandRepeaterModule {}
