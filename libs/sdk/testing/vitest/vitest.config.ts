56dedfe611846fc24f611725c9ee14062131781c
Steve Brush <steve.brush@blackbaud.com>
2026-04-17 11:27:52 -0400

    feat(sdk/testing): add `toBeAccessible` matcher for vitest assertions (#4219)

---
+++ libs/sdk/testing/vitest/vitest.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  cacheDir: '../../../../node_modules/.vite',
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['libs/sdk/testing/vitest/src/**/*.integration.spec.ts'],
+    setupFiles: [
+      resolve(
+        __dirname,
+        '../../../..',
+        'dist/libs/sdk/testing/fesm2022/skyux-sdk-testing-vitest.mjs',
+      ),
+    ],
+  },
+});
