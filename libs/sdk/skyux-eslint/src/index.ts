ed3351eecc58be98eead208a37329e27d4057db4
Erika McVey <erika.mcvey@blackbaud.com>
2026-04-22 10:20:31 -0400

    remove experimental ruleset

--- libs/sdk/skyux-eslint/src/index.ts
+++ libs/sdk/skyux-eslint/src/index.ts
@@ -1,18 +1,14 @@
 import templateAll from './configs/template-all';
-import templateExperimental from './configs/template-experimental';
 import templateRecommended from './configs/template-recommended';
 import tsAll from './configs/ts-all';
-import tsExperimental from './configs/ts-experimental';
 import tsRecommended from './configs/ts-recommended';
 import templatePlugin from './plugins/template-plugin';
 import tsPlugin from './plugins/ts-plugin';
 
 const configs = {
   templateAll,
-  templateExperimental,
   templateRecommended,
   tsAll,
-  tsExperimental,
   tsRecommended,
 };
 
