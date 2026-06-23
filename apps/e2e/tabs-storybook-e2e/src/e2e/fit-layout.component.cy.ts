1c6c7b78f659db1504fc54fa041056e36a181422
John White <750350+johnhwhite@users.noreply.github.com>
2026-06-23 15:43:06 -0400

    fix(components/tabs): fit layout tabs should not shift with hidden tabs

---
+++ apps/e2e/tabs-storybook-e2e/src/e2e/fit-layout.component.cy.ts
@@ -0,0 +1,25 @@
+import { E2eVariations } from '@skyux-sdk/e2e-schematics';
+
+describe('tabs fit layout', () => {
+  E2eVariations.forEachTheme((theme) => {
+    describe(`in ${theme} theme`, () => {
+      beforeEach(() =>
+        cy.visit(
+          `/iframe.html?globals=theme:${theme}&id=fit-layoutcomponent--fit-layout`,
+        ),
+      );
+
+      it('should render the tabs using fit layout', () => {
+        cy.skyReady('app-fit-layout').should('exist').should('be.visible');
+        cy.get('sky-tab-button:nth-child(2) [role="tab"]').click();
+        cy.get('sky-tab:nth-of-type(2) [role="tabpanel"] .placeholder')
+          .should('exist')
+          .should('be.visible');
+        cy.window().screenshot(`fit-layout-${theme}`);
+        cy.percySnapshot(`fit-layout-${theme}`, {
+          widths: E2eVariations.DISPLAY_WIDTHS,
+        });
+      });
+    });
+  });
+});
