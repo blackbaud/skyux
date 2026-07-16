966f418b41914148e05029e29e74380bfa4a5693
Steve Brush <steve.brush@blackbaud.com>
2026-07-16 11:03:58 -0400

    pr feedback

--- libs/components/charts/src/lib/shared/measure-scale-type.ts
+++
@@ -1,7 +0,0 @@
-/**
- * The scale type for a chart measure axis. Use `logarithmic` when values span
- * several orders of magnitude; otherwise use the default `linear` scale.
- *
- * @preview
- */
-export type SkyChartValueScaleType = 'linear' | 'logarithmic';
