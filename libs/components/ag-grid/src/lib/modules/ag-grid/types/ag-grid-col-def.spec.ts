87b0d4405a1737ccb5da0a080b5b6aaad9ba33e0
Steve Brush <steve.brush@blackbaud.com>
2026-08-04 12:00:20 -0400

    pr feedback

--- libs/components/ag-grid/src/lib/modules/ag-grid/types/ag-grid-col-def.spec.ts
+++ libs/components/ag-grid/src/lib/modules/ag-grid/types/ag-grid-col-def.spec.ts
@@ -11,4 +11,56 @@ describe('defineSkyAgGridColDef', () => {
 
     expect(defineSkyAgGridColDef(colDef)).toBe(colDef);
   });
+
+  it('should validate params against the cell type', () => {
+    const validatorColDef = defineSkyAgGridColDef({
+      field: 'endDate',
+      type: [SkyCellType.Date, SkyCellType.Validator],
+      cellRendererParams: {
+        skyComponentProperties: {
+          validator: (value: Date): boolean => !!value,
+          validatorMessage: 'Enter a valid date',
+        },
+      },
+    });
+
+    const autocompleteColDef = defineSkyAgGridColDef({
+      field: 'department',
+      type: SkyCellType.Autocomplete,
+      cellEditorParams: {
+        skyComponentProperties: { data: [] },
+      },
+    });
+
+    expect(validatorColDef.type).toEqual([
+      SkyCellType.Date,
+      SkyCellType.Validator,
+    ]);
+    expect(autocompleteColDef.type).toBe(SkyCellType.Autocomplete);
+
+    defineSkyAgGridColDef({
+      field: 'name',
+      type: SkyCellType.Text,
+      // @ts-expect-error - misspelled properties are rejected
+      cellRenderrerParams: {},
+    });
+
+    defineSkyAgGridColDef({
+      field: 'endDate',
+      type: [SkyCellType.Date, SkyCellType.Validator],
+      cellRendererParams: {
+        skyComponentProperties: {
+          // @ts-expect-error - validatorMessage must be a string
+          validatorMessage: 123,
+        },
+      },
+    });
+
+    defineSkyAgGridColDef({
+      field: 'template',
+      type: SkyCellType.Template,
+      // @ts-expect-error - Template cells require a template param
+      cellRendererParams: {},
+    });
+  });
 });
