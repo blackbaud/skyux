956f55c8d91604c71df103b210eebce095379010
Steve Brush <steve.brush@blackbaud.com>
2026-08-20 09:53:22 -0400

    fix(components/router): add documentation for exported types

--- libs/components/router/src/lib/modules/href/href-resolver.ts
+++ libs/components/router/src/lib/modules/href/href-resolver.ts
@@ -1,5 +1,12 @@
 import { SkyHref } from './types/href';
 
+/**
+ * A contract for resolving the links used by the `skyHref` directive.
+ */
 export interface SkyHrefResolver {
+  /**
+   * Resolves a link's final URL and whether the current user may access it.
+   * @param param The link to resolve.
+   */
   resolveHref(param: { url: string }): Promise<SkyHref>;
 }
