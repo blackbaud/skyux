f99a8dc30ba1772d6091abfd1a48ac2170f60d0b
John White <750350+johnhwhite@users.noreply.github.com>
2026-08-22 17:19:31 -0400

    Assert event types

--- libs/components/core/testing/src/modules/test-utility/test-utility.spec.ts
+++ libs/components/core/testing/src/modules/test-utility/test-utility.spec.ts
@@ -146,11 +146,19 @@ describe('Test utility', () => {
   });
 
   it('should set the value of an input', () => {
-    spyOn(inputEl, 'dispatchEvent').and.callThrough();
+    const dispatchEventSpy = spyOn(
+      inputEl,
+      'dispatchEvent',
+    ).and.callThrough();
     expect(inputEl.value).toEqual('');
     SkyAppTestUtility.setInputValue(inputEl, 'foobar');
     expect(inputEl.value).toEqual('foobar');
-    expect(inputEl.dispatchEvent).toHaveBeenCalledTimes(2);
+
+    const dispatchedEventTypes = dispatchEventSpy.calls
+      .allArgs()
+      .map(([event]) => event.type);
+
+    expect(dispatchedEventTypes).toEqual(['input', 'change']);
   });
 
   it('should throw and error if `fireDomEvent` is called with an null element', () => {
