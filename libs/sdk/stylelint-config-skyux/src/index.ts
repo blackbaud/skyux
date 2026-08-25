ac2c696d765efc67011be746db52281d54ff73fc
John White <750350+johnhwhite@users.noreply.github.com>
2026-08-25 12:05:49 -0400

    feat!: add style public API rules to recommended ESLint and Stylelint rulesets (#4684)
    [AB#3958896](https://dev.azure.com/blackbaud/f565481a-7bc9-4083-95d5-4f953da6d499/_workitems/edit/3958896)
    
    BREAKING CHANGE: the recommended ESLint and Stylelint configs now enable
    `no-invalid-sky-classnames` (TS and template),
    `no-invalid-sky-custom-properties`,
    and `no-deprecated-sky-scss-variables`.

--- libs/sdk/stylelint-config-skyux/src/index.ts
+++ libs/sdk/stylelint-config-skyux/src/index.ts
@@ -2,6 +2,8 @@ export default {
   extends: ['stylelint-config-recommended-scss'],
   plugins: ['skyux-stylelint'],
   rules: {
+    'skyux-stylelint/no-deprecated-sky-scss-variables': true,
+    'skyux-stylelint/no-invalid-sky-custom-properties': true,
     'skyux-stylelint/no-ng-deep': true,
     'skyux-stylelint/no-sky-selectors': true,
     'skyux-stylelint/no-static-color-values': true,
