b3547d705a010e255cd9f702efa9a9b069de1e3d
Trevor Burch <trevor.burch@blackbaud.me>
2026-08-25 13:16:06 -0400

    feat(sdk/e2e-schematics): add modern-v2-dark to E2eVariations.forEachTheme

--- libs/sdk/e2e-schematics/src/lib/e2e-variations/e2e-variations.spec.ts
+++ libs/sdk/e2e-schematics/src/lib/e2e-variations/e2e-variations.spec.ts
@@ -4,9 +4,10 @@ describe('e2e variations', function () {
   it('should run all variations', function () {
     const callback = jest.fn();
     E2eVariations.forEachTheme(callback);
-    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenCalledTimes(3);
     expect(callback).toHaveBeenCalledWith('default');
     expect(callback).toHaveBeenCalledWith('modern-v2-light');
+    expect(callback).toHaveBeenCalledWith('modern-v2-dark');
     expect(callback).not.toHaveBeenCalledWith('modern-light');
     expect(callback).not.toHaveBeenCalledWith('modern-dark');
     callback.mockReset();
