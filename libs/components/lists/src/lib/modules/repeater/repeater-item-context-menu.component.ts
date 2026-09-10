abb012313bbeb2a180e71d7946cf20c709bce9a0
John Lyons <John.Lyons@blackbaud.com>
2026-09-10 09:53:50 -0400

    Apply suggestion from @blackbaud-johnly

--- libs/components/lists/src/lib/modules/repeater/repeater-item-context-menu.component.ts
+++ libs/components/lists/src/lib/modules/repeater/repeater-item-context-menu.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 /**
  * Wraps a dropdown menu or a single button to display actions that users can
  * perform on repeater items. For multiple actions, display the actions in a
- * [`sky-dropdown` component](https://developer.blackbaud.com/skyux/components/dropdown).
+ * [dropdown component](https://developer.blackbaud.com/skyux/components/dropdown).
  * For a single action, such as edit or delete, use a
  * [button](https://developer.blackbaud.com/skyux/components/button)
  * with the `sky-btn-icon-borderless` class.
