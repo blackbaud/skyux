18ea3465975571592b67725b7d9826042f8c79fc
Paul Crowder <paul.crowder@blackbaud.com>
2026-09-22 11:04:51 -0400

    feat(components/popovers): document dropdown `messageStream` input (#4802)

--- libs/components/popovers/src/lib/modules/dropdown/fixtures/dropdown.component.fixture.ts
+++ libs/components/popovers/src/lib/modules/dropdown/fixtures/dropdown.component.fixture.ts
@@ -40,6 +40,8 @@ export class DropdownFixtureComponent {
 
   public messageStream = new Subject<SkyDropdownMessage>();
 
+  public undefinedMessageStream: undefined;
+
   public menuAriaLabelledBy: string | undefined;
 
   public menuAriaRole: string | undefined;
@@ -51,6 +53,8 @@ export class DropdownFixtureComponent {
   public useNativeFocus: boolean | undefined;
 
   public useCustomTrigger = false;
+
+  public useUndefinedMessageStream = false;
 
   //#endregion directive properties
 
@@ -67,7 +71,7 @@ export class DropdownFixtureComponent {
   @ViewChildren(SkyDropdownItemComponent)
   public dropdownItemRefs: QueryList<SkyDropdownItemComponent> | undefined;
 
-  public items: any[] = [
+  public items: { name: string; disabled: boolean }[] = [
     { name: 'Option 1', disabled: false },
     { name: 'Option 2', disabled: true },
     { name: 'Option 3', disabled: false },
@@ -78,17 +82,21 @@ export class DropdownFixtureComponent {
 
   readonly #changeDetector = inject(ChangeDetectorRef);
 
-  public onMenuChanges(): void {}
+  public onMenuChanges(): void {
+    //
+  }
 
-  public onItemClick(): void {}
+  public onItemClick(): void {
+    //
+  }
 
-  public changeItems() {
+  public changeItems(): void {
     this.items.pop();
     this.#changeDetector.detectChanges();
   }
 
   public setManyItems(): void {
-    const items: any[] = [];
+    const items: { name: string; disabled: boolean }[] = [];
 
     for (let i = 0; i < 50; i++) {
       items.push({
@@ -101,7 +109,7 @@ export class DropdownFixtureComponent {
     this.#changeDetector.markForCheck();
   }
 
-  public sendMessage(type: SkyDropdownMessageType) {
+  public sendMessage(type: SkyDropdownMessageType): void {
     this.messageStream.next({ type });
   }
 }
