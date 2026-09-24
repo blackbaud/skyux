d1cbb45743ea63e770e7421cf4bb66a6d4a8df76
Steve Brush <steve.brush@blackbaud.com>
2026-09-24 10:32:29 -0400

    feat(components/core): add instrumentation to capture user events (#4811)

--- libs/components/help-inline/src/lib/modules/help-inline/help-inline.component.spec.ts
+++ libs/components/help-inline/src/lib/modules/help-inline/help-inline.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { Provider } from '@angular/core';
+import { EnvironmentProviders, Provider } from '@angular/core';
 import {
   ComponentFixture,
   TestBed,
@@ -14,6 +14,10 @@ import {
   SkyHelpService,
   SkyIdService,
 } from '@skyux/core';
+import {
+  SkyInstrumentationTestingController,
+  provideSkyInstrumentationTesting,
+} from '@skyux/core/testing';
 import { SkyPopoverHarness } from '@skyux/popovers/testing';
 import {
   SkyTheme,
@@ -117,8 +121,9 @@ describe('Help inline component', () => {
         ?.get as unknown as jasmine.Spy
     ).and.returnValue(readyStateChange);
 
-    const providers: Provider[] = [
+    const providers: (Provider | EnvironmentProviders)[] = [
       { provide: SkyThemeService, useValue: mockThemeSvc },
+      provideSkyInstrumentationTesting(),
     ];
 
     if (provideHelpSvc) {
@@ -156,6 +161,17 @@ describe('Help inline component', () => {
       fixture.detectChanges();
 
       expect(component.showHelpText).toBe(true);
+    });
+
+    it('should emit a user event on button click', () => {
+      getHelpButton(fixture).click();
+
+      fixture.detectChanges();
+
+      TestBed.inject(SkyInstrumentationTestingController).expectEventCount(
+        { eventName: 'sky.help-inline.help-requested' },
+        1,
+      );
     });
 
     it('should pass accessibility with default inputs', async () => {
@@ -313,6 +329,11 @@ describe('Help inline component', () => {
       expect(
         await (await popoverHarness.getPopoverContent()).getBodyText(),
       ).toBe('content');
+
+      TestBed.inject(SkyInstrumentationTestingController).expectEventCount(
+        { eventName: 'sky.help-inline.help-requested' },
+        1,
+      );
     });
 
     it('should render help popover title', async () => {
@@ -417,6 +438,20 @@ describe('Help inline component', () => {
 
       expect(mockHelpSvc.openHelp).toHaveBeenCalledWith({
         helpKey: 'test.html',
+      });
+    });
+
+    it('should include the help key with the user event', () => {
+      setupTest(true);
+
+      component.helpKey = 'test.html';
+      fixture.detectChanges();
+
+      getHelpButton(fixture).click();
+
+      TestBed.inject(SkyInstrumentationTestingController).expectEvent({
+        eventName: 'sky.help-inline.help-requested',
+        eventDetail: { helpKey: 'test.html' },
       });
     });
 
